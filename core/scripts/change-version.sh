bf9934b20a83cbe018a2f194381accfffbc66d32
dariadomagala <40663133+dariadomagala@users.noreply.github.com>
2018-08-07 14:00:32 +0200

    Prevent 3rd part libraries from spoofing (#26)

--- core/scripts/change-version.sh
+++
@@ -1,8 +0,0 @@
-OLDVERSION=$(cat package.json | jq ".version")
-echo old version: $OLDVERSION
-read -p "Provide a new version: " NEWVERSION
-npm version --no-git-tag-version -- $NEWVERSION
-echo new version: $NEWVERSION
-CURRENTBRANCH=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
-git commit -a -m "Console version updated to: '$NEWVERSION'"
-git push origin $CURRENTBRANCH
--- core/scripts/tag-version.sh
+++
@@ -1,6 +0,0 @@
-VERSION=$(cat package.json | jq ".version")
-VERSION="${VERSION%\"}"
-VERSION="${VERSION#\"}"
-echo Tagging version: $VERSION
-git tag -a $VERSION -m "$VERSION"
-git push origin $VERSION
