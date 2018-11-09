3e0f03fe6fed97daee682bc7e5eb0d45c6a63a17
Krzysztof <6783567+kwiatekus@users.noreply.github.com>
2018-11-09 14:42:32 +0100

    Move `deploy new resource` link (#311)
    * move update resource link

--- core/src/app/content/environments/configuration/resources/resources.component.ts
+++ core/src/app/content/environments/configuration/resources/resources.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ResourceUploaderModalComponent } from '../../../../shared/components/resource-uploader/resource-uploader-modal/resource-uploader-modal.component';
 
 @Component({
   selector: 'app-resources',
@@ -9,10 +10,16 @@ import { Component, OnInit } from '@angular/core';
 export class ResourcesComponent implements OnInit {
   limitsTabExpanded = true;
   quotasTabExpanded = false;
+  @ViewChild('uploaderModal')
+  private uploaderModal: ResourceUploaderModalComponent;
 
   constructor() {}
 
   ngOnInit() {}
+
+  openUploadResourceModal() {
+    this.uploaderModal.show();
+  }
 
   changeTab = tab => {
     this.limitsTabExpanded = tab === 'limits';
