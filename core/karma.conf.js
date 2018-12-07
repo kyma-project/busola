cfdab0cd87524fb7605d7d68b74b3b79735a25d9
Kamil K <kamil.kupczynski@sap.com>
2018-12-07 13:52:21 +0100

    Replace phantomjs with chrome (#374)
    * change phantomjs to chrome
    
    * add puppeteer to console dependencies
    
    * add flags to chrome launch
    
    * add flags to chrome launch
    
    * change puppeteer dependency to puppeteer-core
    
    * change puppeteer dependency to puppeteer-core
    
    * change puppeteer-core dependency to puppeteer
    
    * set exact puppeteer version
    
    * change buildpack
    
    * change buildpack

--- core/karma.conf.js
+++ core/karma.conf.js
@@ -1,13 +1,15 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
 module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-phantomjs-launcher'),
+      require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -27,7 +29,13 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['PhantomJS'],
+    browsers: ['ChromeHeadlessWithFlag'],
+    customLaunchers: {
+      ChromeHeadlessWithFlag: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false,
     browserNoActivityTimeout: 20000
   });
