ed838d3fcc87dce4195fa102d708fa916e57fbb2
Daria Domagała <40663133+dariadomagala@users.noreply.github.com>
2019-03-26 10:34:19 +0100

    Fix json editor size and ignore logging config (#717)
    * Fix json editor size
    
    * Update package.json to ignore logging config

--- logging/src/assets/config/config.js
+++ logging/src/assets/config/config.js
@@ -1,26 +1,3 @@
-var domain = 'kyma.local';
-var localDomain = 'console-dev.kyma.local';
-
-window.clusterConfig = {
-  graphqlApiUrl: 'https://ui-api.' + domain + '/graphql',
-  graphqlApiUrlLocal: 'http://' + localDomain + ':3000/graphql',
-
-  subscriptionsApiUrl: 'wss://ui-api.' + domain + '/graphql',
-  subscriptionsApiUrlLocal: 'ws://' + localDomain + ':3000/graphql',
-
-  authRedirectUri: 'http://' + localDomain + ':4200',
-  domain: domain,
-  consoleClientId: 'console',
-  orgId: 'my-org-123',
-  orgName: 'My Organization',
-  scope:
-    'audience:server:client_id:kyma-client audience:server:client_id:console openid profile email groups',
-  gateway_kyma_project_io_version: 'v1alpha2',
-
-  serviceCatalogModuleUrl: 'http://' + localDomain + ':8000',
-  serviceInstancesModuleUrl: 'http://' + localDomain + ':8001',
-  serviceBrokersModuleUrl: 'http://' + localDomain + ':8002',
-  docsModuleUrl: 'http://' + localDomain + ':8003',
-  lambdasModuleUrl: 'http://' + localDomain + ':4201',
-  logsModuleUrl: 'http://' + localDomain + ':4400',
-};
+// deliberately empty
+// this file should be replaced by a configMap injection inside a kubernetes cluster
+console.log('Local dev instance');
