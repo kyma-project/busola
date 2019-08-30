1dbcfeb64c7f76709305af3709611c1abf7c7420
Krzysztof <6783567+kwiatekus@users.noreply.github.com>
2019-08-30 14:14:13 +0200

    Configure logoutUrl to break infinite redirection loop (#1194)
    Configure logoutUrl to break infinite redirection loop

--- core/src/luigi-config/luigi-config.js
+++ core/src/luigi-config/luigi-config.js
@@ -593,7 +593,7 @@ if (token) {
 Promise.all(initPromises)
   .then(
     res => {
-      if (token) {
+      if (token && res[1]) {
         const modules = res[1].backendModules;
         const subjectRules = res[1].selfSubjectRules;
         const cmfs = res[1].clusterMicroFrontends;
@@ -760,7 +760,8 @@ Promise.all(initPromises)
             scope:
               'audience:server:client_id:kyma-client audience:server:client_id:console openid profile email groups',
             automaticSilentRenew: true,
-            loadUserInfo: false
+            loadUserInfo: false,
+            logoutUrl: 'logout.html'
           },
 
           events: {
