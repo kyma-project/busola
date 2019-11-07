b6fbb8444f4ebc3d52753b36ccd0c2c392ed3fcb
Krzysztof <6783567+kwiatekus@users.noreply.github.com>
2019-11-07 09:19:34 +0100

    Adding  kubernetes-dashboard serverless-system to system namespaces (#1374)

--- core/src/luigi-config/luigi-config.js
+++ core/src/luigi-config/luigi-config.js
@@ -27,7 +27,7 @@ var config = {
   graphqlApiUrl: 'https://console-backend.' + k8sDomain + '/graphql',
   disabledNavigationNodes: '',
   systemNamespaces:
-    'compass-system istio-system knative-eventing knative-serving kube-public kube-system kyma-backup kyma-installer kyma-integration kyma-system natss kube-node-lease'
+    'compass-system istio-system knative-eventing knative-serving kube-public kube-system kyma-backup kyma-installer kyma-integration kyma-system natss kube-node-lease kubernetes-dashboard serverless-system'
 };
 
 if (clusterConfig) {
@@ -526,7 +526,7 @@ window.addEventListener('message', e => {
   }
   else if (e.data && e.data.msg === SHOW_SYSTEM_NAMESPACES_CHANGE_EVENT) {
     Luigi.customMessages().sendToAll({
-      id: SHOW_SYSTEM_NAMESPACES_CHANGE_EVENT, 
+      id: SHOW_SYSTEM_NAMESPACES_CHANGE_EVENT,
       showSystemNamespaces: e.data.showSystemNamespaces,
     });
   }
