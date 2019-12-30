53d35d8b540a90d660e0fd15e1679fb00023813d
y-kkamil <kamilgithubsap@gmail.com>
2019-12-30 13:54:12 +0100

    Change Apollo fetch policy to avoid caching issues (#1477)

--- service-catalog-ui/instances/src/store/index.js
+++ service-catalog-ui/instances/src/store/index.js
@@ -45,7 +45,7 @@ export function createApolloClient() {
         fetchPolicy: 'network-only',
       },
       query: {
-        fetchPolicy: 'no-cache',
+        fetchPolicy: 'network-only',
       },
     },
   });
