38937759eff6e61b529091568628da1872932414
Piotr Wawrzyńczyk <Wawrzyn321@gmail.com>
2020-03-12 22:10:29 +0100

    Don't refresh view on every context change (#1607)
    * Rewrite namespace tests
    
    * Modify ApplicationContextProvider

--- shared/contexts/ApplicationContext.js
+++ shared/contexts/ApplicationContext.js
@@ -1,23 +1,41 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, Component } from 'react';
 import LuigiClient from '@kyma-project/luigi-client';
+import { isEqual } from 'lodash';
+
+const watchedProperties = ['tenantId', 'idToken'];
 
 export const ApplicationContext = createContext({});
 
-export const ApplicationContextProvider = ({ children }) => {
-  const [context, setContext] = useState(LuigiClient.getContext());
+export class ApplicationContextProvider extends Component {
+  constructor(props) {
+    super(props);
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  React.useEffect(() => {
-    LuigiClient.addInitListener(setContext);
-    LuigiClient.addContextUpdateListener(setContext);
-  });
+    this.state = { context: LuigiClient.getContext() };
 
-  return (
-    <ApplicationContext.Provider value={context}>
-      {children}
-    </ApplicationContext.Provider>
-  );
-};
+    LuigiClient.addInitListener(this.updateContext);
+    LuigiClient.addContextUpdateListener(this.updateContext);
+  }
+
+  hasPropertyChanged = ctx => {
+    return watchedProperties.some(
+      property => !isEqual(ctx[property], this.state.context[property]),
+    );
+  };
+
+  updateContext = ctx => {
+    if (this.hasPropertyChanged(ctx)) {
+      this.setState({ context: ctx });
+    }
+  };
+
+  render() {
+    return (
+      <ApplicationContext.Provider value={this.state.context}>
+        {this.props.children}
+      </ApplicationContext.Provider>
+    );
+  }
+}
 
 export function useApplicationContext() {
   return useContext(ApplicationContext);
