cd347a615323a5342afc4f8a4c4eb888dfde26a7
Krzysztof <6783567+kwiatekus@users.noreply.github.com>
2020-03-13 08:49:15 +0100

    do not store profile data in session storage (#1613)
    Co-authored-by: Kwiatosz, Krzysztof <kwiatekus@users.noreply.github.com>

--- compass/src/config/luigi-config/auth.js
+++ compass/src/config/luigi-config/auth.js
@@ -2,21 +2,33 @@ const domain =
   (window.clusterConfig && window.clusterConfig['domain']) || 'kyma.local';
 
 const authClusterConfig = window.clusterConfig && window.clusterConfig.auth;
-
+const clientId = authClusterConfig
+  ? authClusterConfig['client_id']
+  : 'compass-ui';
 const auth = {
   use: 'openIdConnect',
   openIdConnect: {
     authority: authClusterConfig
       ? authClusterConfig['authority']
       : `https://dex.${domain}`,
-    client_id: authClusterConfig
-      ? authClusterConfig['client_id']
-      : 'compass-ui',
+    client_id: clientId,
     scope: authClusterConfig
       ? authClusterConfig['scope']
       : 'audience:server:client_id:compass-ui openid profile email groups',
     loadUserInfo: false,
     logoutUrl: '/logout.html',
+    profileStorageInterceptorFn: () => {
+      try {
+        const oidcUserStoreKey = `oidc.user:https://dex.${domain}:${clientId}`;
+        const oidsUserStore = JSON.parse(
+          sessionStorage.getItem(oidcUserStoreKey),
+        );
+        oidsUserStore.profile = undefined;
+        sessionStorage.setItem(oidcUserStoreKey, JSON.stringify(oidsUserStore));
+      } catch (e) {
+        console.error('Error parsing oidc user data', e);
+      }
+    },
   },
   storage: 'sessionStorage',
 };
