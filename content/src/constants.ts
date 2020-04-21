df43ef46a5ece278c42f9beab88c065fac23169f
Jan Sudczak <jan.sudczak@sap.com>
2020-04-21 09:29:48 +0200

    Change back button text (#1717)
    * change back button text
    
    * Update content/src/constants.ts
    
    Co-Authored-By: Maja Kurcius <maja.kurcius@sap.com>
    
    Co-authored-by: Maja Kurcius <maja.kurcius@sap.com>

--- content/src/constants.ts
+++ content/src/constants.ts
@@ -2,7 +2,7 @@ export const FILTER_EXTENSIONS = ['md', 'markdown'];
 export const VIEW_CONTEXT = 'docs-ui';
 export const ROOT_GROUP = 'root';
 export const KYMA_TOPIC = 'kyma';
-export const BACK_TO_NAMESPACES = 'Back to Environment';
+export const BACK_TO_NAMESPACES = 'Back to the Namespace list';
 
 export const ERRORS = {
   SERVER: 'Server error. Please contact the admin of the cluster.',
