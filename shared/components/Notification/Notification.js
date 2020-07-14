d5447546c96e3d10f9b0095a929f7b02f2726807
Agata Kucharska <akucharska@users.noreply.github.com>
2020-07-14 13:08:36 +0200

    Show successful notifications as message toast (#1886)
    * Show successfull notifications as message toast
    
    * Dark styles

--- shared/components/Notification/Notification.js
+++ shared/components/Notification/Notification.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
 import { Icon } from 'fundamental-react';
 import './Notification.scss';
@@ -13,27 +13,41 @@ export const Notification = ({
   visible,
   orientation = 'top',
 }) => {
-  const classes = classnames('notification', {
+  const deprecatedClasses = classnames('notification', {
     [`type-${type}`]: type,
     [`orientation-${orientation}`]: orientation,
     visible,
   });
+
+  if (type !== 'success') {
+    // deprecated notification
+    return (
+      <div className={deprecatedClasses} onClick={onClick}>
+        <div className="notification-header">
+          <span className="notification-title">{title}</span>
+          <div className="notification-icon">
+            <span className="notification-icon">
+              <Icon glyph={icon} />
+            </span>
+          </div>
+        </div>
+        {content && (
+          <>
+            <div className="notification-separator" />
+            <div className="notification-body">{content}</div>
+          </>
+        )}
+      </div>
+    );
+  }
+
+  const classes = classnames('message-toast--wrapper', {
+    visible,
+  });
+  // message toast
   return (
     <div className={classes} onClick={onClick}>
-      <div className="notification-header">
-        <span className="notification-title">{title}</span>
-        <div className="notification-icon">
-          <span className="notification-icon">
-            <Icon glyph={icon} />
-          </span>
-        </div>
-      </div>
-      {content && (
-        <>
-          <div className="notification-separator" />
-          <div className="notification-body">{content}</div>
-        </>
-      )}
+      <div className="fd-message-toast">{content || title}</div>
     </div>
   );
 };
