a351741209b0032a5cd577346dad70d48bb11b9a
Piotr Wawrzyńczyk <Wawrzyn321@gmail.com>
2020-09-17 12:56:04 +0200

    Fix system namespaces switch (#1988)

--- shared/hooks/useShowSystemNamespaces.js
+++ shared/hooks/useShowSystemNamespaces.js
@@ -1,12 +1,7 @@
 import LuigiClient from '@luigi-project/client';
-import { useMemo } from 'react';
 
 export function useShowSystemNamespaces() {
-  return useMemo(
-    () =>
-      (LuigiClient.getActiveFeatureToggles() || []).includes(
-        'showSystemNamespaces',
-      ),
-    [],
+  return (LuigiClient.getActiveFeatureToggles() || []).includes(
+    'showSystemNamespaces',
   );
 }
