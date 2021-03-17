f0a79543aef0640742f1fc0b3abbff40bec90d2c
Piotr Wawrzyńczyk <Wawrzyn321@gmail.com>
2021-03-17 14:40:29 +0100

    Move npx scripts to root (#8)
    * Move npx scripts to root
    
    * Restore deleted file

--- backend/webpack-npx.config.js
+++ backend/webpack-npx.config.js
@@ -3,7 +3,7 @@ const defaultConfig = require('./webpack.config');
 const { merge } = require('webpack-merge');
 
 module.exports = merge(defaultConfig, {
-  output: { filename: 'backend-npx.js' },
+  output: { filename: 'index-npx.js' },
   plugins: [
     new webpack.BannerPlugin({ banner: '#!/usr/bin/env node', raw: true }),
     new webpack.DefinePlugin({ 'process.env.NODE_ENV': "'npx'" }),
