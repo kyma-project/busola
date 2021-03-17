d63c74b48af6253a06cc854e4a84632bcc177563
Wawrzyn321 <Wawrzyn321@gmail.com>
2021-03-17 10:22:24 +0100

    Fetch kyma-gateway only if needed

--- core-ui/src/components/ApiRules/useGetGatewayDomain/useGetGatewayDomain.js
+++ core-ui/src/components/ApiRules/useGetGatewayDomain/useGetGatewayDomain.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import { useGet } from 'react-shared';
+import { useGet, useMicrofrontendContext } from 'react-shared';
 
 const apiGatewayUri =
   '/apis/networking.istio.io/v1beta1/namespaces/kyma-system/gateways/kyma-gateway';
 
 export function useGetGatewayDomain() {
-  const [domain, setDomain] = React.useState('');
-  const [loading, setLoading] = React.useState(true);
+  const k8sApiUrl = useMicrofrontendContext().k8sApiUrl;
+
+  const [domain, setDomain] = React.useState(
+    (k8sApiUrl || '').replace('api.', ''),
+  );
+  const [loading, setLoading] = React.useState(!domain);
   const [error, setError] = React.useState(null);
 
-  const { error: getError, loading: getLoading, data } = useGet(
-    apiGatewayUri,
-    {},
-  );
+  const { error: getError, loading: getLoading, data } = useGet(apiGatewayUri, {
+    skip: !!domain,
+  });
   React.useEffect(() => {
+    if (!data) return;
     setError(getError);
     setLoading(getLoading);
     setDomain(data?.spec.servers[0].hosts[0].replace('*.', '') || '');
