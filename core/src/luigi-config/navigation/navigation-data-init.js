1b0ad58e0ee9b2f1215288f7a032568fa81b04b2
parostatkiem <jan.sudczak@sap.com>
2021-03-22 10:58:26 +0100

    prettify navigation-data-init

--- core/src/luigi-config/navigation/navigation-data-init.js
+++ core/src/luigi-config/navigation/navigation-data-init.js
@@ -4,18 +4,18 @@ import {
   coreUIViewGroupName,
   catalogViewGroupName,
   getStaticChildrenNodesForNamespace,
-  getStaticRootNodes,
+  getStaticRootNodes
 } from './static-navigation-model';
 import navigationPermissionChecker, {
   setInitValues,
-  backendModules,
+  backendModules
 } from './permissions';
 
 import {
   hideDisabledNodes,
   createNamespacesList,
   clearToken,
-  getToken,
+  getToken
 } from './navigation-helpers';
 import { groups } from '../auth';
 import { getInitParams } from '../init-params';
@@ -24,50 +24,50 @@ export let resolveNavigationNodes;
 export let navigation = {
   viewGroupSettings: {
     [coreUIViewGroupName]: {
-      preloadUrl: config.coreUIModuleUrl + '/preload',
+      preloadUrl: config.coreUIModuleUrl + '/preload'
     },
     [catalogViewGroupName]: {
-      preloadUrl: config.serviceCatalogModuleUrl + '/preload',
-    },
+      preloadUrl: config.serviceCatalogModuleUrl + '/preload'
+    }
   },
   nodeAccessibilityResolver: navigationPermissionChecker,
   contextSwitcher: {
     defaultLabel: 'Select Namespace ...',
     parentNodePath: '/home/namespaces', // absolute path
     lazyloadOptions: true, // load options on click instead on page load
-    options: getNamespaces,
+    options: getNamespaces
   },
   profile: {
     logout: {
-      label: 'Logout',
+      label: 'Logout'
     },
     items: [
       {
         icon: 'settings',
         label: 'Preferences',
-        link: '/home/preferences',
+        link: '/home/preferences'
       },
       {
         icon: 'download',
         label: 'Get Kubeconfig',
-        link: '/home/download-kubeconfig',
-      },
-    ],
+        link: '/home/download-kubeconfig'
+      }
+    ]
   },
-  nodes: new Promise((res) => {
+  nodes: new Promise(res => {
     resolveNavigationNodes = res;
-  }),
+  })
 };
 
 export function getNavigationData(token) {
-  return new Promise(function (resolve, reject) {
+  return new Promise(function(resolve, reject) {
     fetchBusolaInitData(token)
       .then(
-        (res) => {
+        res => {
           setInitValues(res.backendModules, res.selfSubjectRules || []);
           return res;
         },
-        (err) => {
+        err => {
           if (err === 'access denied') {
             clearToken();
             window.location.pathname = '/nopermissions.html';
@@ -78,7 +78,7 @@ export function getNavigationData(token) {
         }
       )
       // 'Finally' not supported by IE and FIREFOX (if 'finally' is needed, update your .babelrc)
-      .then((res) => {
+      .then(res => {
         const params = getInitParams();
         const { disabledNavigationNodes, systemNamespaces } = params.config;
         const { bebEnabled } = params.features;
@@ -94,22 +94,22 @@ export function getNavigationData(token) {
               systemNamespaces,
               showSystemNamespaces:
                 localStorage.getItem('busola.showSystemNamespaces') === 'true',
-              cluster: params.cluster,
+              cluster: params.cluster
             },
-            children: function () {
+            children: function() {
               const staticNodes = getStaticRootNodes(
                 getChildrenNodesForNamespace,
                 res.apiGroups
               );
               hideDisabledNodes(disabledNavigationNodes, staticNodes, false);
               return staticNodes;
-            },
-          },
+            }
+          }
         ];
 
         resolve(nodes);
       })
-      .catch((err) => {
+      .catch(err => {
         console.error('Config Init Error', err);
         reject(err);
       });
@@ -126,7 +126,7 @@ async function getNamespaces() {
     return [];
   }
   if (localStorage.getItem('busola.showSystemNamespaces') !== 'true') {
-    namespaces = namespaces.filter((ns) => !systemNamespaces.includes(ns.name));
+    namespaces = namespaces.filter(ns => !systemNamespaces.includes(ns.name));
   }
   return createNamespacesList(namespaces);
 }
