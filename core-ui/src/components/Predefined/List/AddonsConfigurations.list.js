72842c79c314c7c28cc20a8a3f79d405dd8ea6aa
parostatkiem <jan.sudczak@sap.com>
2021-03-22 14:46:15 +0100

    remove obsolete code

--- core-ui/src/components/Predefined/List/AddonsConfigurations.list.js
+++ core-ui/src/components/Predefined/List/AddonsConfigurations.list.js
@@ -1,87 +1,48 @@
 import React from 'react';
 import { StatusBadge } from 'react-shared';
 import './AddonsConfigurations.list.scss';
-import { Badge } from 'fundamental-react';
-
-const calculatePodState = pod => {
-  const containerStatuses = pod?.status?.containerStatuses;
-  if (containerStatuses?.length > 0) {
-    const waitingStatus = containerStatuses
-      .reverse()
-      .find(element => element.state.waiting);
-    if (waitingStatus) {
-      return {
-        status: waitingStatus.state.waiting.reason || 'Waiting',
-        message: waitingStatus.state.waiting.message,
-      };
-    } else {
-      const terminatedStatus = containerStatuses
-        .reverse()
-        .find(element => element.state.terminated);
-      if (terminatedStatus) {
-        return {
-          status: terminatedStatus.state.terminated.reason || 'Terminated',
-          message: terminatedStatus.state.terminated.message,
-        };
-      }
-    }
-  }
-  return { status: 'Running' };
-};
-
-const badgeType = status => {
-  switch (status) {
-    case 'Running':
-    case 'Completed':
-      return 'success';
-    case 'Terminated':
-    case 'Terminating':
-    case 'PodInitializing':
-      return 'info';
-    default:
-      return 'error';
-  }
-};
 
 const URL_SUMMARY_DIGIT = 23;
 
-const RepositoryUrlList = ({ addonStatus }) => {
-  console.log(addonStatus);
-  return (
-    <div className="addons__repository">
-      {addonStatus.repositories.map(r => (
-        <>
-          <span
-            className="addons__repository-url"
-            key={r.url + 'summary'}
-            title={r.url}
-          >
-            {r.url.length > URL_SUMMARY_DIGIT
-              ? '…' + r.url.slice(-URL_SUMMARY_DIGIT)
-              : r.url}
-          </span>
-          <StatusBadge
-            key={r.url + 'status'}
-            tooltipContent={r.message}
-            autoResolveType={true}
-          >
-            {r.status}
-          </StatusBadge>
-        </>
-      ))}
-    </div>
-  );
+const RepositoryUrlList = ({ addonStatus }) => (
+  <div className="addons__repository">
+    {addonStatus.repositories.map(r => (
+      <>
+        <span
+          className="addons__repository-url"
+          key={r.url + 'summary'}
+          title={r.url}
+        >
+          {r.url.length > URL_SUMMARY_DIGIT
+            ? '…' + r.url.slice(-URL_SUMMARY_DIGIT)
+            : r.url}
+        </span>
+        <StatusBadge
+          key={r.url + 'status'}
+          tooltipContent={r.message}
+          autoResolveType={true}
+        >
+          {r.status}
+        </StatusBadge>
+      </>
+    ))}
+  </div>
+);
+const repositoryUrlsColumn = {
+  header: 'Repository URLs',
+  value: addon => <RepositoryUrlList addonStatus={addon.status} />,
+};
+
+export const AddonsConfigurationsList = DefaultRenderer => ({
+  ...otherParams
+}) => {
+  const customColumns = [repositoryUrlsColumn];
+  return <DefaultRenderer customColumns={customColumns} {...otherParams} />;
 };
 
 export const ClusterAddonsConfigurationsList = DefaultRenderer => ({
   ...otherParams
 }) => {
-  const customColumns = [
-    {
-      header: 'Repository URLs',
-      value: addon => <RepositoryUrlList addonStatus={addon.status} />,
-    },
-  ];
-
+  const customColumns = [repositoryUrlsColumn];
   return <DefaultRenderer customColumns={customColumns} {...otherParams} />;
 };
