1035cb10ec4baabd7d83c8eb5d66c1dd9184a9bf
parostatkiem <jan.sudczak@sap.com>
2021-03-22 10:43:50 +0100

    invalidate form if no url is added

--- core-ui/src/components/Predefined/Create/ClusterAddonsConfigurations.create.js
+++ core-ui/src/components/Predefined/Create/ClusterAddonsConfigurations.create.js
@@ -2,19 +2,6 @@ import React from 'react';
 import { AddonsConfigurations } from './AddonsConfigurations/AddonsConfigurations.js';
 
 export const ClusterAddonsConfigurationsCreate = ({
-  formElementRef,
-  onChange,
   resourceType,
-  resourceUrl,
-  namespace,
-  refetchList,
-}) => (
-  <AddonsConfigurations
-    formElementRef={formElementRef}
-    onChange={onChange}
-    resourceType={resourceType}
-    resourceUrl={resourceUrl}
-    namespace={namespace}
-    refetchList={refetchList}
-  />
-);
+  ...props
+}) => <AddonsConfigurations resourceType={resourceType} {...props} />;
