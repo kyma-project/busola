d7e11f1b4edbc84b43ff9ce1eb1a2660ebc8e6a3
dariadomagala <daria.domagala@sap.com>
2021-03-23 08:25:47 +0100

    Add new columns for functions lists

--- core-ui/src/components/Predefined/List/Function/Functions.list.js
+++ core-ui/src/components/Predefined/List/Function/Functions.list.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import CreateNewFunction from './CreateNewFunction';
 import { Button } from 'fundamental-react';
 import LuigiClient from '@luigi-project/client';
+
+import { prettySourceType } from 'components/Lambdas/helpers/lambdas';
+import { prettyRuntime } from 'components/Lambdas/helpers/runtime';
+import { LambdaStatusBadge } from 'components/Lambdas/LambdaStatusBadge/LambdaStatusBadge';
+import CreateNewFunction from './CreateNewFunction';
 
 export const FunctionsList = DefaultRenderer => ({ ...otherParams }) => {
   function goToGitRepositories() {
@@ -9,6 +13,7 @@ export const FunctionsList = DefaultRenderer => ({ ...otherParams }) => {
       .fromContext('namespaces')
       .navigate(`gitrepositories`);
   }
+
   const headerActions = (
     <Button option="light" onClick={goToGitRepositories}>
       Connected repositories
@@ -19,10 +24,30 @@ export const FunctionsList = DefaultRenderer => ({ ...otherParams }) => {
     <CreateNewFunction namespaceName={otherParams.namespace} />
   );
 
+  const customColumns = [
+    {
+      header: 'Runtime',
+      value: func => {
+        return prettyRuntime(func.spec.runtime);
+      },
+    },
+    {
+      header: 'Source',
+      value: func => {
+        return prettySourceType(func.spec.type);
+      },
+    },
+    {
+      header: 'Status',
+      value: resource => <LambdaStatusBadge status={resource.status} />,
+    },
+  ];
+
   return (
     <DefaultRenderer
       customHeaderActions={headerActions}
       listHeaderActions={listActions}
+      customColumns={customColumns}
       {...otherParams}
     />
   );
