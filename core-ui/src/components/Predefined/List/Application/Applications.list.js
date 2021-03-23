a34b13cd125e2db74d40fe1d3716b2d84c666e14
Agata Kucharska <agata.kucharska@sap.com>
2021-03-23 13:13:06 +0100

    Add simplified status

--- core-ui/src/components/Predefined/List/Application/Applications.list.js
+++ core-ui/src/components/Predefined/List/Application/Applications.list.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import CreateApplicationModal from './CreateApplicationModal';
+import { StatusBadge } from 'react-shared';
+
+const isStatusOk = application => {
+  return application.status?.installationStatus?.status === 'deployed';
+};
+const isStatusEmpty = application => !application.status;
+
+const getStatus = application => {
+  if (isStatusEmpty(application)) return 'provisioning';
+  return isStatusOk(application) ? 'deployed' : 'error';
+};
+
+const getStatusType = application => {
+  if (isStatusEmpty(application)) return 'warning';
+  return isStatusOk(application) ? 'success' : 'error';
+};
 
 export const ApplicationsList = DefaultRenderer => ({ ...otherParams }) => {
+  const customColumns = [
+    {
+      header: 'Status',
+      value: application => {
+        const status = getStatus(application);
+        const statusType = getStatusType(application);
+        return <StatusBadge type={statusType}>{status}</StatusBadge>;
+      },
+    },
+  ];
+
   return (
     <DefaultRenderer
+      customColumns={customColumns}
       customHeaderActions={<CreateApplicationModal />}
       {...otherParams}
     />
