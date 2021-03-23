f0a78addaead127fea57768b802f7cf272cd7d99
parostatkiem <jan.sudczak@sap.com>
2021-03-23 15:19:48 +0100

    use CreateApiRule modal

--- core-ui/src/components/Predefined/Details/Service.details.js
+++ core-ui/src/components/Predefined/Details/Service.details.js
@@ -1,10 +1,46 @@
 import React from 'react';
+import LuigiClient from '@luigi-project/client';
 import { getComponentForList } from 'shared/getComponents';
+import { Button } from 'fundamental-react';
+import CreateApiRule from 'components/ApiRules/CreateApiRule/CreateApiRule.js';
 
 export const ServicesDetails = DefaultRenderer => ({ ...otherParams }) => {
+  function createApiRuleModal(
+    service,
+
+    redirectPath,
+    redirectCtx,
+    portForCreate,
+  ) {
+    LuigiClient.linkManager()
+      .fromContext('namespaces')
+      .withParams({
+        serviceName: 'service.metadata.name',
+        port: 'portForCreate',
+        openedInModal: true,
+        redirectCtx: null,
+        redirectPath: encodeURIComponent('redirectPath'),
+      })
+      .openAsModal(`apirules/create`, {
+        title: 'PANEL.CREATE_MODAL.TITLE',
+      });
+  }
+
+  const exposeServiceButton = (
+    <Button
+      glyph="add"
+      option="light"
+      onClick={() => createApiRuleModal()}
+      // disabled={disableExposeButton || !!(serverDataLoading || serverDataError)}
+    >
+      Expose
+    </Button>
+  );
+
   const ApiRuleList = getComponentForList({
     name: 'apiruleList',
     params: {
+      listHeaderActions: exposeServiceButton,
       hasDetailsView: true,
       fixedPath: true,
       resourceUrl: `/apis/gateway.kyma-project.io/v1alpha1/namespaces/${otherParams.namespace}/apirules`,
