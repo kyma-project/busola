8a383d7c5851f66828f9e10ecee71ab8ded12bde
parostatkiem <jan.sudczak@sap.com>
2021-03-26 14:08:58 +0100

    introduce EMPTY_TEXT_PLACEHOLDER

--- core-ui/src/components/Predefined/List/LimitRanges.list.js
+++ core-ui/src/components/Predefined/List/LimitRanges.list.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import { EMPTY_TEXT_PLACEHOLDER } from 'react-shared';
 
 export const LimitRangesList = DefaultRenderer => ({ ...otherParams }) => {
   const customColumns = [
     {
       header: 'Max',
-      value: limit => limit.spec.limits[0]?.max?.memory,
+      value: limit =>
+        limit.spec.limits[0]?.max?.memory || EMPTY_TEXT_PLACEHOLDER,
     },
     {
       header: 'Default',
-      value: limit => limit.spec.limits[0]?.default?.memory,
+      value: limit =>
+        limit.spec.limits[0]?.default?.memory || EMPTY_TEXT_PLACEHOLDER,
     },
     {
       header: 'Default Request',
-      value: limit => limit.spec.limits[0]?.defaultRequest?.memory,
+      value: limit =>
+        limit.spec.limits[0]?.defaultRequest?.memory || EMPTY_TEXT_PLACEHOLDER,
     },
   ];
 
