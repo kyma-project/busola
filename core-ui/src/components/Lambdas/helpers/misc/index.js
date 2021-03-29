17c6f8633380e939c102ec35a90c9c6a46a84e7b
dariadomagala <daria.domagala@sap.com>
2021-03-29 08:11:27 +0200

    Remove unused file

--- core-ui/src/components/Lambdas/helpers/misc/index.js
+++ core-ui/src/components/Lambdas/helpers/misc/index.js
@@ -2,7 +2,6 @@ export * from './backendModulesExist';
 export * from './constants';
 export * from './formatMessage';
 export * from './handleSubscriptionEvent';
-export * from './omitTypenames';
 export * from './randomNameGenerator';
 export * from './renderMarkdown';
 export * from './useConfigData';
