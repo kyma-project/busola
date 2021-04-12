a5691ea1731fe12fb56e521f4fd2d1ec771026ff
parostatkiem <jan.sudczak@sap.com>
2021-04-12 11:31:07 +0200

    fix MicrofrontendContext re-updating with the same value

--- shared/contexts/MicrofrontendContext.js
+++ shared/contexts/MicrofrontendContext.js
@@ -1,20 +1,35 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  useRef,
+} from 'react';
 import LuigiClient from '@luigi-project/client';
 
 export const MicrofrontendContext = createContext({});
 
 export function MicrofrontendContextProvider({ children }) {
   const [context, setContext] = useState({});
+  const lastContext = useRef({});
 
   useEffect(() => {
-    const initHandle = LuigiClient.addInitListener(setContext);
-    const updateHandle = LuigiClient.addContextUpdateListener(setContext);
-
+    const initHandle = LuigiClient.addInitListener(handleContextChanged);
+    const updateHandle = LuigiClient.addContextUpdateListener(
+      handleContextChanged,
+    );
     return () => {
       LuigiClient.removeContextUpdateListener(updateHandle);
       LuigiClient.removeInitListener(initHandle);
     };
-  }, []);
+  }, [context]);
+
+  function handleContextChanged(newContext) {
+    if (JSON.stringify(lastContext.current) !== JSON.stringify(newContext)) {
+      lastContext.current = newContext;
+      setContext(newContext);
+    }
+  }
 
   return (
     <MicrofrontendContext.Provider value={context}>
@@ -22,7 +37,6 @@ export function MicrofrontendContextProvider({ children }) {
     </MicrofrontendContext.Provider>
   );
 }
-
 export function useModuleEnabled(module) {
   const { backendModules } = useMicrofrontendContext();
   return backendModules && backendModules.includes(module);
