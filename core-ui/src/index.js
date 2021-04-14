48d1ede540446bb848edb8b3d2473225afd1fb3f
parostatkiem <jan.sudczak@sap.com>
2021-04-14 08:38:28 +0200

    remove preloadingStrategy wrapper from core-ui

--- core-ui/src/index.js
+++ core-ui/src/index.js
@@ -2,20 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { BrowserRouter } from 'react-router-dom';
-import { preloadingStrategy } from '@kyma-project/common';
 import './index.scss';
 import App from './components/App/App';
 import { Microfrontend } from 'react-shared';
 
-preloadingStrategy(async () => {
-  const isNpx = window.location.origin === 'http://localhost:3001';
+const isNpx = window.location.origin === 'http://localhost:3001';
 
-  ReactDOM.render(
-    <Microfrontend env={process.env}>
-      <BrowserRouter basename={isNpx ? '/core-ui' : '/'}>
-        <App />
-      </BrowserRouter>
-    </Microfrontend>,
-    document.getElementById('root'),
-  );
-});
+ReactDOM.render(
+  <Microfrontend env={process.env}>
+    <BrowserRouter basename={isNpx ? '/core-ui' : '/'}>
+      <App />
+    </BrowserRouter>
+  </Microfrontend>,
+  document.getElementById('root'),
+);
