f293f7984c6684cc96f7c4917c2825a666d2beb3
Agata Kucharska <agata.kucharska@sap.com>
2021-04-14 11:30:08 +0200

    Show error on text in the center

--- shared/components/CircleProgress/CircleProgress.js
+++ shared/components/CircleProgress/CircleProgress.js
@@ -3,33 +3,12 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import './CircleProgress.scss';
 
-const colorTresholds = [
-  { percent: 0, color: { r: 0xff, g: 0x00, b: 0 } },
-  { percent: 50, color: { r: 0xff, g: 0xff, b: 0 } },
-  { percent: 100, color: { r: 0x00, g: 0xff, b: 0 } },
-];
-
-const getColorForPercentage = (percent, reversed) => {
+const isInErrorState = (percent, reversed) => {
   if (reversed) {
     percent = 100 - percent;
   }
-  for (var i = 1; i < colorTresholds.length - 1; i++) {
-    if (percent < colorTresholds[i].percent) {
-      break;
-    }
-  }
-  var lower = colorTresholds[i - 1];
-  var upper = colorTresholds[i];
-  var range = upper.percent - lower.percent;
-  var rangepercent = (percent - lower.percent) / range;
-  var percentLower = 1 - rangepercent;
-  var percentUpper = rangepercent;
-  var color = {
-    r: Math.floor(lower.color.r * percentLower + upper.color.r * percentUpper),
-    g: Math.floor(lower.color.g * percentLower + upper.color.g * percentUpper),
-    b: Math.floor(lower.color.b * percentLower + upper.color.b * percentUpper),
-  };
-  return 'rgb(' + [color.r, color.g, color.b].join(',') + ')';
+
+  return percent < 33;
 };
 
 export const CircleProgress = ({
@@ -51,31 +30,31 @@ export const CircleProgress = ({
   const circleProgressClasses = classNames(`circle-progress`, {
     'cursor-pointer': !!onClick,
   });
+
+  const circleInnerClasses = classNames(`percentage`, {
+    'is-error': max > 0 && isInErrorState(percent, reversed),
+  });
   const containerStyle = {
     width: size + 'px',
     height: size + 'px',
   };
   const valueIndicatorStyle = {
-    backgroundImage: `conic-gradient(transparent ${100 -
-      percent}%, ${getColorForPercentage(percent, reversed)} 0)`, // we have to prepare it here to avoid using styledComponents
-  };
-  const backgroundStyle = {
-    // backgroundColor: `${getColorForPercentage(percent, reversed)}`,
-
     backgroundColor: `#f0f0f0`,
-    opacity: `0.5`,
-    // backgroundImage: `radial-gradient(#adadad 0.5px, #f0f0f0 0.5px)`,
-    // backgroundSize: `5px 5px`,
+    backgroundImage: `conic-gradient(transparent ${100 -
+      percent}%, ${color} 0)`, // we have to prepare it here to avoid using styledComponents
+  };
+
+  const innerStyle = {
+    fontSize: textSize,
   };
 
   return (
     <div className={circleProgressClasses} onClick={onClick}>
       <span className="title">{title}</span>
       <div className="circle__container" style={containerStyle}>
-        <div className="background" style={backgroundStyle}></div>
         <div className="value-indicator" style={valueIndicatorStyle}></div>
         <div className="inner-area">
-          <div className="percentage" style={{ fontSize: textSize }}>
+          <div className={circleInnerClasses} style={innerStyle}>
             {text}
           </div>
         </div>
