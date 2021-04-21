dc8e3f1c1dfa90ce4809049827a636af754bed62
Agata Kucharska <agata.kucharska@sap.com>
2021-04-21 12:24:25 +0200

    Remove unused packages vol 3

--- core-ui/src/components/Lambdas/helpers/misc/index.js
+++ core-ui/src/components/Lambdas/helpers/misc/index.js
@@ -1,7 +1,6 @@
 export * from './backendModulesExist';
 export * from './constants';
 export * from './formatMessage';
-export * from './renderMarkdown';
 export * from './useConfigData';
 export * from './useLogsView';
 export * from './validateResourceName';
