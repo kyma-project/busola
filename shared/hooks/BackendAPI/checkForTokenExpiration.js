c1f6d862a513c2b8537f1a9942be6e3c45316630
Wawrzyn321 <Wawrzyn321@gmail.com>
2021-04-21 12:34:50 +0200

    Refresh page when token is about to expire

--- shared/hooks/BackendAPI/checkForTokenExpiration.js
+++ shared/hooks/BackendAPI/checkForTokenExpiration.js
@@ -1,19 +1,16 @@
 import parseJWT from 'jwt-decode';
 import LuigiClient from '@luigi-project/client';
 
-const warningTime = 2 * 60; // 2 mins
-
-let hasShowExpirationWarning = false;
+const timeout = 30; // s
 
 export function checkForTokenExpiration(idToken) {
   try {
     const expirationTimestamp = parseJWT(idToken).exp;
     const secondsLeft = new Date(expirationTimestamp) - Date.now() / 1000;
 
-    if (secondsLeft < warningTime && !hasShowExpirationWarning) {
-      hasShowExpirationWarning = true;
+    if (secondsLeft < timeout) {
       LuigiClient.sendCustomMessage({
-        id: 'busola.showTokenExpirationWarning',
+        id: 'busola.reload',
       });
     }
   } catch (_) {} // ignore errors from non-JWT tokens
