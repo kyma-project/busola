16489227d8086e5606542e3c32fb16b11558c134
parostatkiem <jan.sudczak@sap.com>
2021-04-23 10:37:47 +0200

    add fiori-helpers.scss

--- core-ui/src/index.js
+++ core-ui/src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 
 import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
+import './fiori-helpers.scss';
 import App from './components/App/App';
 import { Microfrontend } from 'react-shared';
+
 // import 'fiori-fundamentals/dist/fiori-fundamentals.min.css'; // half of our helper classes is in here
 
 const isNpx = window.location.origin === 'http://localhost:3001';
