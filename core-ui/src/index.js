d635a2a845fbe5fe140b643cee93226f321d18c7
parostatkiem <jan.sudczak@sap.com>
2021-04-23 15:04:42 +0200

    actually remove fiori-fundamentals

--- core-ui/src/index.js
+++ core-ui/src/index.js
@@ -7,8 +7,6 @@ import './fiori-helpers.scss';
 import App from './components/App/App';
 import { Microfrontend } from 'react-shared';
 
-// import 'fiori-fundamentals/dist/fiori-fundamentals.min.css'; // half of our helper classes is in here
-
 const isNpx = window.location.origin === 'http://localhost:3001';
 
 ReactDOM.render(
