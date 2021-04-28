96b22dd2623b4b6a7ce2850e3b1853d652423d12
parostatkiem <jan.sudczak@sap.com>
2021-04-28 09:10:13 +0200

    adjust "connect application modal" textarea

--- core-ui/src/components/Predefined/Details/Application/ConnectApplicationModal.js
+++ core-ui/src/components/Predefined/Details/Application/ConnectApplicationModal.js
@@ -89,7 +89,11 @@ export default function ConnectApplicationModal({ applicationName }) {
         Copy the following URL and use it at the external system that you would
         like to connect to:
       </p>
-      <textarea readOnly value={url} style={{ minHeight: '100px' }} />
+      <textarea
+        readOnly
+        value={url}
+        style={{ minHeight: '100px', width: '100%', marginTop: '0.5rem' }}
+      />
     </Modal>
   );
 }
