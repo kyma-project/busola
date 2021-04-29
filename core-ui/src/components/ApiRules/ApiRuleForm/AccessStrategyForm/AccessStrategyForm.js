36211b6081df12da2efe4d5d94762808d74bb160
Jan Sudczak <jan.sudczak@sap.com>
2021-04-29 10:21:29 +0200

    Improve api rule form responsiveness (#122)
    * improve api rule form responsiveness
    
    * bump images

--- core-ui/src/components/ApiRules/ApiRuleForm/AccessStrategyForm/AccessStrategyForm.js
+++ core-ui/src/components/ApiRules/ApiRuleForm/AccessStrategyForm/AccessStrategyForm.js
@@ -45,66 +45,55 @@ export default function AccessStrategyForm({
       <div className="access-strategy access-strategy--form">
         <div className="content">
           <FormGroup>
-            <div
-              style={{
-                display: 'grid',
-                gridTemplateColumns: '1fr 1fr 1fr',
-                gridGap: '0.5rem',
-              }}
-            >
-              <FormItem>
-                <FormInput
-                  placeholder="Enter the path"
-                  type="text"
-                  value={strategy.path}
-                  required
-                  aria-label="Access strategy path"
-                  pattern="^[a-z0-9\/\(\)\?.!*\-]+"
-                  title="Path must consist of alphanumeric and the following characters: /.*?!-()"
-                  onChange={e =>
-                    setStrategy({ ...strategy, path: e.target.value })
+            <FormItem style={{ marginRight: '0.5rem', flexShrink: 1 }}>
+              <FormInput
+                placeholder="Enter the path"
+                type="text"
+                value={strategy.path}
+                required
+                aria-label="Access strategy path"
+                pattern="^[a-z0-9\/\(\)\?.!*\-]+"
+                title="Path must consist of alphanumeric and the following characters: /.*?!-()"
+                onChange={e =>
+                  setStrategy({ ...strategy, path: e.target.value })
+                }
+              />
+            </FormItem>
+            <FormItem style={{ flexShrink: 0 }}>
+              <FormSelect
+                defaultValue={selectedType}
+                aria-label="Access strategy type"
+                id="select-1"
+                onChange={e => {
+                  const newStrategy = {
+                    ...strategy,
+                    accessStrategies: [
+                      {
+                        ...strategy.accessStrategies[0],
+                        handler: e.target.value,
+                      },
+                    ],
+                  };
+                  // strategy type changed, reset current values
+                  if (e.target.value !== strategy.accessStrategies[0].handler) {
+                    newStrategy.accessStrategies[0].config = {};
                   }
-                />
-              </FormItem>
-              <FormItem>
-                <FormSelect
-                  style={{ marginTop: '0.25rem' }}
-                  defaultValue={selectedType}
-                  aria-label="Access strategy type"
-                  id="select-1"
-                  onChange={e => {
-                    const newStrategy = {
-                      ...strategy,
-                      accessStrategies: [
-                        {
-                          ...strategy.accessStrategies[0],
-                          handler: e.target.value,
-                        },
-                      ],
-                    };
-                    // strategy type changed, reset current values
-                    if (
-                      e.target.value !== strategy.accessStrategies[0].handler
-                    ) {
-                      newStrategy.accessStrategies[0].config = {};
-                    }
-                    setStrategy(newStrategy);
-                    handleFormChanged();
-                  }}
-                >
-                  {Object.values(accessStrategyTypes).map(ac => (
-                    <option key={ac.value} value={ac.value}>
-                      {ac.displayName}
-                    </option>
-                  ))}
-                </FormSelect>
-              </FormItem>
-              <MethodsForm
-                methods={strategy.methods}
-                setMethods={methods => setStrategy({ ...strategy, methods })}
-                isRelevant={usesMethods(selectedType)}
-              ></MethodsForm>
-            </div>
+                  setStrategy(newStrategy);
+                  handleFormChanged();
+                }}
+              >
+                {Object.values(accessStrategyTypes).map(ac => (
+                  <option key={ac.value} value={ac.value}>
+                    {ac.displayName}
+                  </option>
+                ))}
+              </FormSelect>
+            </FormItem>
+            <MethodsForm
+              methods={strategy.methods}
+              setMethods={methods => setStrategy({ ...strategy, methods })}
+              isRelevant={usesMethods(selectedType)}
+            ></MethodsForm>
           </FormGroup>
 
           <Details
