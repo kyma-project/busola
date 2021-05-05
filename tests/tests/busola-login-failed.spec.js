14faa6539a904543fe7afd21944514b55efc902f
Wojciech Sołtys <wojciech.soltys@sap.com>
2021-05-05 15:12:48 +0200

    Fix cypress config

--- tests/tests/busola-login-failed.spec.js
+++ tests/tests/busola-login-failed.spec.js
@@ -5,12 +5,6 @@ const ADDRESS = config.localDev
   ? `http://localhost:4200`
   : `https://busola.${config.domain}`;
 
-Cypress.on('uncaught:exception', (err, runnable) => {
-  // returning false here prevents Cypress from failing the test
-  debugger;
-  return false;
-});
-
 context('Busola Login Failed', () => {
   it('Use wrong kubeconfig', () => {
     cy.visit(ADDRESS)
