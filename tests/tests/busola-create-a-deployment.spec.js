052edbbf3d854527d18a876fb70fb8c5be55952d
Agata Kucharska <agata.kucharska@sap.com>
2021-05-07 09:58:00 +0200

    Test deployments

---
+++ tests/tests/busola-create-a-deployment.spec.js
@@ -0,0 +1,143 @@
+/// <reference types="cypress" />
+import config from '../config';
+import 'cypress-file-upload';
+
+const ADDRESS = config.localDev
+  ? `http://localhost:4200`
+  : `https://busola.${config.domain}`;
+
+const random = Math.floor(Math.random() * 1000);
+const NAMESPACE_NAME = `b-busola-test-${random}`;
+
+const DOCKER_IMAGE = 'eu.gcr.io/kyma-project/pr/orders-service:PR-162';
+
+context('Busola - Create a Deployment', () => {
+  const getLeftNav = () => cy.get('nav[data-testid=semiCollapsibleLeftNav]');
+
+  before(() => {
+    cy.visit(ADDRESS)
+      .contains('Drag file here')
+      .attachFile('kubeconfig.yaml', { subjectType: 'drag-n-drop' });
+
+    cy.get('#error').should('not.exist');
+    cy.url().should('eq', ADDRESS + '/home/workspace');
+    cy.getIframeBody()
+      .find('thead')
+      .should('be.visible'); //wait for the namespaces XHR request to finish to continue running the tests. There's no <thead> while the request is pending.
+  });
+
+  after(() => {
+    getLeftNav()
+      .contains('Namespaces') //it finds Namespaces (expected) or Back to Namespaces (if tests fail in the middle)
+      .click({ force: true }) //we need to use force when others elements make menu not visible
+      .wait(0);
+
+    cy.getIframeBody()
+      .find('[aria-label="open-search"]')
+      .click();
+
+    cy.getIframeBody()
+      .find('[placeholder="Search"]')
+      .type(NAMESPACE_NAME);
+
+    cy.getIframeBody()
+      .find('[aria-label="Delete"]')
+      .click();
+
+    cy.getIframeBody()
+      .find('[role="status"]')
+      .should('have.text', 'TERMINATING');
+  });
+
+  it('Create a new namespace', () => {
+    getLeftNav()
+      .contains('Namespaces')
+      .click();
+
+    cy.getIframeBody()
+      .contains('Create Namespace')
+      .click();
+
+    cy.getIframeBody()
+      .find('[role=dialog]')
+      .find("input[placeholder='Namespace name']")
+      .should('be.visible')
+      .type(NAMESPACE_NAME);
+
+    cy.getIframeBody()
+      .find('[role=dialog]')
+      .contains('button', 'Create')
+      .click();
+  });
+
+  it('Go to the details of namespace', () => {
+    cy.getIframeBody()
+      .contains('a', NAMESPACE_NAME)
+      .click();
+  });
+
+  it('Create a Deployment', () => {
+    cy.getIframeBody()
+      .contains('Deploy new workload')
+      .click();
+
+    cy.getIframeBody()
+      .find('[role="menuitem"]')
+      .contains('Create Deployment')
+      .click();
+
+    cy.getIframeBody()
+      .find('[placeholder="Deployment name"]')
+      .clear()
+      .type('orders-service');
+
+    cy.getIframeBody()
+      .find('[placeholder="Enter Labels key=value"]')
+      .type('app=orders-service');
+
+    cy.getIframeBody()
+      .contains('label', 'Labels')
+      .click();
+
+    cy.getIframeBody()
+      .find('[placeholder="Enter Labels key=value"]')
+      .type('example=orders-service');
+
+    cy.getIframeBody()
+      .contains('label', 'Labels')
+      .click();
+
+    cy.getIframeBody()
+      .find('[placeholder="Enter Docker image"]')
+      .type(DOCKER_IMAGE);
+
+    cy.getIframeBody()
+      .contains('label', 'Memory requests')
+      .next('input')
+      .clear()
+      .type('16Mi');
+
+    cy.getIframeBody()
+      .contains('label', 'Memory limits')
+      .next('input')
+      .clear()
+      .type('32Mi');
+
+    cy.getIframeBody()
+      .contains('label', 'CPU requests')
+      .next('input')
+      .clear()
+      .type('10m');
+
+    cy.getIframeBody()
+      .contains('label', 'CPU limits')
+      .next('input')
+      .clear()
+      .type('20m');
+
+    cy.getIframeBody()
+      .contains('button', 'Create')
+      .click()
+      .wait(2000);
+  });
+});
