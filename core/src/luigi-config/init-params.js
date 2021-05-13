25bf3882f917f82126e57389e005de9708484c51
Piotr Wawrzyńczyk <piotwaw927@student.polsl.pl>
2021-05-13 14:16:42 +0200

    Restore init-params.js

--- core/src/luigi-config/init-params.js
+++ core/src/luigi-config/init-params.js
@@ -1,5 +1,4 @@
 import createEncoder from 'json-url';
-import { DEFAULT_MODULES } from './default-modules';
 import {
   saveClusterParams,
   saveActiveClusterName,
@@ -24,6 +23,17 @@ function createSystemNamespacesList(namespaces) {
 }
 
 export async function saveInitParamsIfPresent() {
+  const DEFAULT_MODULES = {
+    SERVICE_CATALOG: 'servicecatalog.k8s.io',
+    SERVICE_CATALOG_ADDONS: 'servicecatalog.kyma-project.io',
+    EVENTING: 'eventing.kyma-project.io',
+    API_GATEWAY: 'gateway.kyma-project.io',
+    APPLICATIONS: 'applicationconnector.kyma-project.io',
+    ADDONS: 'addons.kyma-project.io',
+    SERVERLESS: 'serverless.kyma-project.io',
+    SERVERLESS_REPOS: 'gitrepositories.serverless.kyma-project.io',
+  };
+
   const initParams = new URL(location).searchParams.get('init');
   if (initParams) {
     const decoded = await encoder.decompress(initParams);
