d86f72b8cb00b5d985b0601705f5b2959f12e861
parostatkiem <jan.sudczak@sap.com>
2021-05-14 14:32:18 +0200

    create API rule for Lambda

--- tests/tests/busola-create-a-function.spec.js
+++ tests/tests/busola-create-a-function.spec.js
@@ -6,10 +6,11 @@ const ADDRESS = config.localDev
   ? `http://localhost:4200`
   : `https://busola.${config.domain}`;
 
-const random = Math.floor(Math.random() * 1000);
+const random = Math.floor(Math.random() * 9999) + 1000;
 const NAMESPACE_NAME = `a-busola-test-${random}`;
 const FUNCTION_CODE_URL =
   'https://raw.githubusercontent.com/kyma-project/examples/main/orders-service/function/handler.js';
+const API_RULE_HOST_PREFIX = `https://orders-function-host.`;
 
 context('Busola - Create a Function', () => {
   const getLeftNav = () => cy.get('nav[data-testid=semiCollapsibleLeftNav]');
@@ -45,24 +46,27 @@ context('Busola - Create a Function', () => {
       .contains('Namespaces') //it finds Namespaces (expected) or Back to Namespaces (if tests fail in the middle)
       .click({ force: true }); //we need to use force when others elements make menu not visible
 
-    cy.getIframeBody()
-      .find('[aria-label="open-search"]')
-      .click();
+    cy.wait(3000);
+    // cy.get();
 
     cy.getIframeBody()
-      .find('[aria-expanded="true"]')
+      .find('[aria-label="open-search"]')
+      .click({ force: true });
+    cy.wait(1000);
+
+    cy.getIframeBody()
+      .find('[role=search][aria-expanded="true"]')
       .find('input[placeholder = "Search"]')
       .type(NAMESPACE_NAME);
 
-    // cy.wait(1000);
+    cy.wait(1000);
     cy.getIframeBody()
       .find('[aria-label="Delete"]')
-      .click();
+      .click({ force: true });
 
-    // cy.wait(5000);
     cy.getIframeBody()
       .find('[role="status"]')
-      .should('have.text', 'TERMINATING');
+      .contains('TERMINATING');
   });
 
   beforeEach(() => {
@@ -165,10 +169,63 @@ context('Busola - Create a Function', () => {
         });
     });
 
-    cy.wait(1000);
+    // cy.wait(1000);
     cy.getIframeBody()
       .find('.lambda-details')
       .contains('button', 'Save')
+      .should('not.be.disabled')
       .click();
+
+    //TODO use one namespace per all tests. Then we'll be able create the lambda at the beginning and create API Rule for it at the end
+    cy.getIframeBody()
+      .find('[role="status"]', { timeout: 60000 })
+      .should('have.text', 'RUNNING');
+  });
+
+  it('Create API Rule for the Function', () => {
+    getLeftNav()
+      .contains('Discovery and Network')
+      .click();
+
+    getLeftNav()
+      .contains('API Rules')
+      .click();
+
+    cy.getIframeBody()
+      .contains('Create apirules')
+      .click();
+
+    // cy.wait(2000);
+
+    cy.getModalBody().within($modal => {
+      cy.get('[placeholder="API Rule name"]').type('orders-function');
+      cy.get('[placeholder="Enter the hostname"]').type('orders-function-host'); //the host is ocupied by another virtualservice
+      cy.get('[role="select"]#service').select('orders-function:80');
+
+      cy.get('[aria-label="Access strategy type"]').select('noop');
+
+      // cy.wait(10000);
+      // inputs are invisible because the Fundamental uses label::before to display the check area
+      cy.get('input[type="checkbox"]').check(['GET', 'POST'], { force: true });
+      cy.get('input[type="checkbox"]').uncheck(
+        ['PUT', 'PATCH', 'DELETE', 'HEAD'],
+        { force: true },
+      );
+      cy.get('[aria-label="submit-form"]')
+        // .contains('button', 'Create')
+        .should('not.be.disabled')
+        .click();
+    });
+  });
+
+  it('Check Host value for the API Rule', () => {
+    getLeftNav()
+      .contains('API Rules')
+      .click();
+
+    cy.getIframeBody()
+      .find('tbody>tr')
+      .find(`a[href^="${API_RULE_HOST_PREFIX}"]`)
+      .should('exist');
   });
 });
