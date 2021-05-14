e463121f46b329172092ec9a9c3f45334bac5468
parostatkiem <jan.sudczak@sap.com>
2021-05-14 14:32:02 +0200

    add getModalIframe command

--- tests/support/iframe-commands.js
+++ tests/support/iframe-commands.js
@@ -15,3 +15,21 @@ Cypress.Commands.add('getIframeBody', () => {
       .then(body => cy.wrap(body, { log: false }))
   );
 });
+
+Cypress.Commands.add('getModalBody', () => {
+  // get the iframe > document > body
+  // and retry until the body element is not empty
+  cy.log('getModalBody');
+
+  return (
+    cy
+      .get('.iframeModalCtn iframe', { log: false })
+      .filter(':visible')
+      .its('0.contentDocument.body', { log: false })
+      .should('not.be.empty')
+      // wraps "body" DOM element to allow
+      // chaining more Cypress commands, like ".find(...)"
+      // https://on.cypress.io/wrap
+      .then(body => cy.wrap(body, { log: false }))
+  );
+});
