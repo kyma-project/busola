4c3c387c23a60e6c5d8dce96476efba8a13274e5
Agata Kucharska <agata.kucharska@sap.com>
2021-05-18 14:44:02 +0200

    Clear test

--- tests/tests/busola-create-a-deployment.spec.js
+++ tests/tests/busola-create-a-deployment.spec.js
@@ -1,71 +1,13 @@
 /// <reference types="cypress" />
 import config from '../config';
 import 'cypress-file-upload';
-import config from '../config';
-
-const ADDRESS = config.localDev
-  ? `http://localhost:4200`
-  : `https://busola.${config.domain}`;
 
 const DOCKER_IMAGE = 'eu.gcr.io/kyma-project/pr/orders-service:PR-162';
 const DEPLOYMENT_NAME = 'orders-service';
-console.log(Cypress.env());
 const NAMESPACE_NAME = config.namespace;
+
 context('Busola - Create a Deployment', () => {
   const getLeftNav = () => cy.get('nav[data-testid=semiCollapsibleLeftNav]');
-
-  // before(() => {
-  //   cy.visit(ADDRESS)
-  //     .getIframeBody()
-  //     .contains('Add Cluster')
-  //     .click();
-
-  //   cy.getIframeBody()
-  //     .contains('Drag file here')
-  //     .attachFile('kubeconfig.yaml', { subjectType: 'drag-n-drop' });
-
-  //   cy.getIframeBody()
-  //     .find('[role=alert]')
-  //     .should('not.exist');
-  //   cy.url().should('match', /namespaces$/);
-  //   cy.getIframeBody()
-  //     .find('thead')
-  //     .should('be.visible'); //wait for the namespaces XHR request to finish to continue running the tests. There's no <thead> while the request is pending.
-  // });
-
-  // after(() => {
-  //   getLeftNav()
-  //     .contains('Namespaces') //it finds Namespaces (expected) or Back to Namespaces (if tests fail in the middle)
-  //     .click({ force: true }); //we need to use force when others elements make menu not visible
-
-  //   cy.wait(1000);
-  //   cy.getIframeBody()
-  //     .find('[aria-label="open-search"]')
-  //     .click({ force: true });
-
-  //   cy.wait(1000);
-  //   cy.getIframeBody()
-  //     .find('input[placeholder="Search"]')
-  //     .type(NAMESPACE_NAME);
-
-  //   cy.wait(1000);
-  //   cy.getIframeBody()
-  //     .find('[aria-label="Delete"]')
-  //     .click({ force: true });
-
-  //   cy.wait(5000);
-  //   cy.getIframeBody()
-  //     .find('[role="status"]')
-  //     .should('have.text', 'TERMINATING');
-  // });
-
-  // beforeEach(() => {
-  //   cy.restoreLocalStorageCache();
-  // });
-
-  // afterEach(() => {
-  //   cy.saveLocalStorageCache();
-  // });
 
   it('Create a new namespace', () => {
     cy.wait(3000);
--- tests/tests/busola-smoke-tests.spec.js
+++ tests/tests/busola-smoke-tests.spec.js
@@ -2,68 +2,10 @@
 import config from '../config';
 import 'cypress-file-upload';
 
-const ADDRESS = config.localDev
-  ? `http://localhost:4200`
-  : `https://busola.${config.domain}`;
-
-const random = Math.floor(Math.random() * 1000);
-// const NAMESPACE_NAME = `a-busola-test-${random}`;
 const NAMESPACE_NAME = config.namespace;
 
 context('Busola - Smoke Tests', () => {
   const getLeftNav = () => cy.get('nav[data-testid=semiCollapsibleLeftNav]');
-
-  // before(() => {
-  //   cy.visit(ADDRESS)
-  //     .getIframeBody()
-  //     .contains('Add Cluster')
-  //     .click()
-  //     .getIframeBody()
-  //     .contains('Drag file here')
-  //     .attachFile('kubeconfig.yaml', { subjectType: 'drag-n-drop' });
-
-  //   cy.getIframeBody()
-  //     .find('[role=alert]')
-  //     .should('not.exist');
-  //   cy.url().should('match', /namespaces$/);
-  //   cy.getIframeBody()
-  //     .find('thead')
-  //     .should('be.visible'); //wait for the namespaces XHR request to finish to continue running the tests. There's no <thead> while the request is pending.
-  // });
-
-  // after(() => {
-  //   getLeftNav()
-  //     .contains('Namespaces') //it finds Namespaces (expected) or Back to Namespaces (if tests fail in the middle)
-  //     .click({ force: true }); //we need to use force when others elements make menu not visible
-  //   cy.wait(1000);
-
-  //   cy.getIframeBody()
-  //     .find('[aria-label="open-search"]')
-  //     .click({ force: true });
-  //   cy.wait(1000);
-
-  //   cy.getIframeBody()
-  //     .find('[placeholder="Search"]')
-  //     .type(NAMESPACE_NAME);
-  //   cy.wait(1000);
-
-  //   cy.getIframeBody()
-  //     .find('[aria-label="Delete"]')
-  //     .click();
-  //   cy.wait(5000);
-
-  //   cy.getIframeBody()
-  //     .find('[role="status"]')
-  //     .should('have.text', 'TERMINATING');
-  // });
-
-  // beforeEach(() => {
-  //   cy.restoreLocalStorageCache();
-  // });
-
-  // afterEach(() => {
-  //   cy.saveLocalStorageCache();
-  // });
 
   it('Renders navigation nodes', () => {
     ['Namespaces', 'Administration', 'Diagnostics'].forEach(node => {
