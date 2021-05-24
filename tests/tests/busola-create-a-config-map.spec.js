f862bc9f184546100ea100a6ddda38852ac388b2
Agata Kucharska <agata.kucharska@sap.com>
2021-05-24 13:47:44 +0200

    Test (Cluster) Roles, (Cluster) Roles Bindings

--- tests/tests/busola-create-a-config-map.spec.js
+++ tests/tests/busola-create-a-config-map.spec.js
@@ -4,6 +4,8 @@ import 'cypress-file-upload';
 const DOCKER_IMAGE = 'eu.gcr.io/kyma-project/pr/orders-service:PR-162';
 const CONFIG_MAP_NAME = 'test-configmap';
 const SECRET_NAME = 'test-secret';
+const USER_NAME = 'user@kyma.eu';
+const ROLE_NAME = 'view (CR)';
 
 context('Busola - Testing Configuration', () => {
   const getLeftNav = () => cy.get('nav[data-testid=semiCollapsibleLeftNav]');
@@ -43,6 +45,10 @@ context('Busola - Testing Configuration', () => {
     cy.getIframeBody()
       .contains('a', CONFIG_MAP_NAME)
       .click({ force: true });
+
+    cy.getIframeBody()
+      .contains(CONFIG_MAP_NAME)
+      .should('be.visible');
   });
 
   it('Test a Secret', () => {
@@ -77,5 +83,124 @@ context('Busola - Testing Configuration', () => {
     cy.getIframeBody()
       .contains('a', SECRET_NAME)
       .click({ force: true });
+
+    cy.getIframeBody()
+      .contains(SECRET_NAME)
+      .should('be.visible');
+  });
+
+  it('Test a Roles list view', () => {
+    getLeftNav()
+      .find('[data-testid=roles_roles]')
+      .click()
+      .wait(1000);
+
+    cy.getIframeBody()
+      .contains('h3', 'Roles')
+      .should('be.visible');
+  });
+
+  it('Test a Role Binding', () => {
+    getLeftNav()
+      .find('[data-testid=role-bindings_rolebindings]')
+      .click()
+      .wait(1000);
+
+    cy.getIframeBody()
+      .contains('Create Role Binding')
+      .click();
+
+    cy.getIframeBody()
+      .find('[placeholder="User name"]')
+      .clear()
+      .type(USER_NAME);
+
+    cy.getIframeBody()
+      .find('[placeholder="Choose role..."]')
+      .type(ROLE_NAME);
+
+    cy.getIframeBody()
+      .find('[role="dialog"]')
+      .contains('button', 'Create')
+      .click();
+
+    cy.wait(1000);
+
+    cy.getIframeBody()
+      .contains('a', `${USER_NAME}-view`)
+      .click({ force: true });
+
+    cy.getIframeBody()
+      .contains(`${USER_NAME}-view`)
+      .should('be.visible');
+  });
+
+  it('Test a Cluster Roles list view', () => {
+    getLeftNav()
+      .contains('Namespaces')
+      .click();
+
+    getLeftNav()
+      .contains('Administration')
+      .click();
+
+    getLeftNav()
+      .find('[data-testid=cluster-roles_clusterroles]')
+      .click()
+      .wait(1000);
+
+    cy.getIframeBody()
+      .contains('h3', 'Cluster Roles')
+      .should('be.visible');
+
+    cy.getIframeBody()
+      .find('tbody tr')
+      .its('length')
+      .should('be.gte', 1);
+  });
+
+  it('Test a Cluster Role Binding', () => {
+    getLeftNav()
+      .find('[data-testid=cluster-role-bindings_clusterrolebindings]')
+      .click()
+      .wait(1000);
+
+    cy.getIframeBody()
+      .contains('Create Cluster Role Binding')
+      .click();
+
+    cy.getIframeBody()
+      .find('[placeholder="User name"]')
+      .clear()
+      .type(USER_NAME);
+
+    cy.getIframeBody()
+      .find('[placeholder="Choose role..."]')
+      .type(ROLE_NAME);
+
+    cy.getIframeBody()
+      .find('[role="dialog"]')
+      .contains('button', 'Create')
+      .click();
+
+    cy.wait(1000);
+
+    cy.getIframeBody()
+      .find('[role="search"] [aria-label="search-input"]')
+      .type(`${USER_NAME}-view`, { force: true }); // use force to skip clicking (the table could re-render between the click and the typing)
+
+    cy.getIframeBody()
+      .contains('tbody tr td a', `${USER_NAME}-view`)
+      .click({ force: true });
+
+    cy.getIframeBody()
+      .contains(`${USER_NAME}-view`)
+      .should('be.visible');
+
+    cy.getIframeBody()
+      .contains('button', 'Delete')
+      .click();
+
+    cy.get('[data-testid=luigi-modal-confirm]').click();
   });
 });
