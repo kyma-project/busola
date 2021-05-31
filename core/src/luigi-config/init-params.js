29f656b52d2c2bd8c18610a574722441fac9d911
Wawrzyn321 <Wawrzyn321@gmail.com>
2021-05-31 10:37:30 +0200

    Review fix

--- core/src/luigi-config/init-params.js
+++ core/src/luigi-config/init-params.js
@@ -30,48 +30,57 @@ function getResponseParams(usePKCE = true) {
 export async function saveInitParamsIfPresent() {
   const encodedParams = new URL(location).searchParams.get('init');
   if (encodedParams) {
-    const decoded = await encoder.decompress(encodedParams);
-
-    const isKubeconfigPresent = !!Object.keys(decoded.kubeconfig || {}).length;
-    const kubeconfigUser =
-      decoded.kubeconfig?.users && decoded.kubeconfig?.users[0].user;
-    const isOidcAuthPresent = decoded.config?.auth;
-
-    const requireMoreInput =
-      !isKubeconfigPresent ||
-      (!isOidcAuthPresent && !hasKubeconfigAuth(kubeconfigUser));
-
-    if (requireMoreInput) {
-      navigateToAddCluster(encodedParams);
-      return;
+    try {
+      await setupFromParams(encodedParams);
+    } catch (e) {
+      alert('Error loading init params, configuration not changed.');
+      console.warn(e);
     }
-
-    const params = {
-      ...decoded,
-      config: {
-        ...decoded.config,
-        modules: { ...DEFAULT_MODULES, ...(decoded.config?.modules || {}) },
-      },
-      currentContext: {
-        cluster: decoded?.kubeconfig?.clusters[0],
-        user: decoded?.kubeconfig?.users[0],
-      },
-    };
-
-    if (params.config.auth && !hasKubeconfigAuth(kubeconfigUser)) {
-      // no auth in kubeconfig, setup OIDC auth
-      params.config.auth = {
-        ...params.config.auth,
-        ...getResponseParams(params.config.auth.usePKCE),
-      };
-    }
-
-    saveClusterParams(params);
-
-    const clusterName = params.currentContext.cluster.name;
-    saveActiveClusterName(clusterName);
-    saveLocation(`/cluster/${encodeURIComponent(clusterName)}/namespaces`);
   }
+}
+
+async function setupFromParams(encodedParams) {
+  const decoded = await encoder.decompress(encodedParams);
+
+  const isKubeconfigPresent = !!Object.keys(decoded.kubeconfig || {}).length;
+  const kubeconfigUser =
+    decoded.kubeconfig?.users && decoded.kubeconfig?.users[0].user;
+  const isOidcAuthPresent = decoded.config?.auth;
+
+  const requireMoreInput =
+    !isKubeconfigPresent ||
+    (!isOidcAuthPresent && !hasKubeconfigAuth(kubeconfigUser));
+
+  if (requireMoreInput) {
+    navigateToAddCluster(encodedParams);
+    return;
+  }
+
+  const params = {
+    ...decoded,
+    config: {
+      ...decoded.config,
+      modules: { ...DEFAULT_MODULES, ...(decoded.config?.modules || {}) },
+    },
+    currentContext: {
+      cluster: decoded.kubeconfig.clusters[0],
+      user: decoded.kubeconfig.users[0],
+    },
+  };
+
+  if (params.config.auth && !hasKubeconfigAuth(kubeconfigUser)) {
+    // no auth in kubeconfig, setup OIDC auth
+    params.config.auth = {
+      ...params.config.auth,
+      ...getResponseParams(params.config.auth.usePKCE),
+    };
+  }
+
+  saveClusterParams(params);
+
+  const clusterName = params.currentContext.cluster.name;
+  saveActiveClusterName(clusterName);
+  saveLocation(`/cluster/${encodeURIComponent(clusterName)}/namespaces`);
 }
 
 function navigateToAddCluster(encodedParams) {
