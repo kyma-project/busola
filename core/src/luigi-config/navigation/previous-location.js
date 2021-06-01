aa34f9b3039945afd9e4a1022f2a02956ecc37ec
Wawrzyn321 <Wawrzyn321@gmail.com>
2021-06-01 17:06:58 +0200

    Don't save location if with init params

--- core/src/luigi-config/navigation/previous-location.js
+++ core/src/luigi-config/navigation/previous-location.js
@@ -1,24 +1,23 @@
 const PREVIOUS_LOCATION_KEY = 'busola.location';
 
 export const saveLocation = location => {
-  console.log('save', location);
   localStorage.setItem(PREVIOUS_LOCATION_KEY, location);
 };
 
 export const saveCurrentLocation = () => {
-  // && window.location.pathname !== '/'
-  if (!window.location.hash) {
+  const hasInitParams = [...window.location.searchParams.keys()].includes(
+    'init',
+  );
+  if (!window.location.hash && !hasInitParams) {
     const location = window.location.pathname;
     const params = window.location.search;
     saveLocation(location + params);
-    console.log('save curr', location + params);
   }
 };
 
 export const tryRestorePreviousLocation = () => {
   const prevLocation = localStorage.getItem(PREVIOUS_LOCATION_KEY);
   if (prevLocation) {
-    console.log('load', prevLocation);
     localStorage.removeItem(PREVIOUS_LOCATION_KEY);
     Luigi.navigation().navigate(prevLocation);
   }
