fe5074510f07af7c463f0383fdf235395db58f9a
Wojciech Sołtys <74361703+Sawthis@users.noreply.github.com>
2021-06-01 08:43:57 +0200

    Fix for invalid-kubeconfig flow (#179)
    * Enable smoke tests for namespace details
    
    * Fix for invalid-kubeconfig flow
    
    * Unskip create a function flow

--- tests/tests/invalid-kubeconfig.spec.js
+++ tests/tests/invalid-kubeconfig.spec.js
@@ -1,19 +1,18 @@
 /// <reference types="cypress" />
-import 'cypress-file-upload';
 import config from '../config';
 
 context('Invalid kubeconfig', () => {
   it('Use wrong kubeconfig', () => {
     cy.visit(config.clusterAddress);
-    cy.get('[data-testid=app-switcher]')
-      .click()
-      .get('[data-testid=addcluster]')
-      .click();
+    cy.get('[data-testid=app-switcher]').click();
+
+    cy.get('[data-testid=addcluster]').click();
 
     cy.getIframeBody()
       .find('#textarea-kubeconfig')
-      .type('wrong_kubeconfig')
-      .getIframeBody()
+      .type('wrong_kubeconfig');
+
+    cy.getIframeBody()
       .contains('Apply kubeconfig')
       .click();
 
