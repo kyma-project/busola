3c6b973524e9837acfbfd3c18a56258c226a3330
Wawrzyn321 <Wawrzyn321@gmail.com>
2021-06-11 12:55:49 +0200

    Adjust paths on cluster

--- shared/utils/setupMonaco.js
+++ shared/utils/setupMonaco.js
@@ -1,4 +1,9 @@
+const isLocalDev = location.hostname.startsWith('localhost');
+
+console.log(isLocalDev);
+console.log('so path: ', isLocalDev ? '/vs' : '/assets/libs/vs');
+
 export function setupMonaco(monaco) {
   // monaco editor - load from static files instead of from CDN
-  monaco.config({ paths: { vs: '/vs' } });
+  monaco.config({ paths: { vs: isLocalDev ? '/vs' : '/assets/libs/vs' } });
 }
