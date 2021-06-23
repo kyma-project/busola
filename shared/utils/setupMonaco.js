dfb769e0cd03a4d6ac9de0a05b5d4500bf5014e5
Wawrzyn321 <Wawrzyn321@gmail.com>
2021-06-23 08:21:44 +0200

    Fix monaco

--- shared/utils/setupMonaco.js
+++ shared/utils/setupMonaco.js
@@ -1,4 +1,5 @@
-const isLocalDev = location.hostname.startsWith('localhost');
+const isLocalDev =
+  location.hostname.startsWith('localhost') && location.port !== '3001'; // todo
 
 export function setupMonaco(monaco) {
   // monaco editor - load from static files instead of from CDN
