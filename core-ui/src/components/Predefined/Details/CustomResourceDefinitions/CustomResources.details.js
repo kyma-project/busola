f7b288774a417f3d84873a7377af8129fabc4038
dariadomagala <daria.domagala@sap.com>
2021-06-25 08:19:36 +0200

    Add Additional Printer Columns to the list

--- core-ui/src/components/Predefined/Details/CustomResourceDefinitions/CustomResources.details.js
+++ core-ui/src/components/Predefined/Details/CustomResourceDefinitions/CustomResources.details.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import LuigiClient from '@luigi-project/client';
 
-import { ResourceDetails } from 'react-shared';
+import {
+  ResourceDetails,
+  useGet,
+  EMPTY_TEXT_PLACEHOLDER,
+  GenericList,
+} from 'react-shared';
 
 export function CustomResource({ params }) {
   const namespace = LuigiClient.getContext().namespaceId;
@@ -10,6 +15,45 @@ export function CustomResource({ params }) {
     resourceVersion,
     resourceName,
   } = params;
+  const { data, error, loading } = useGet(
+    `/apis/apiextensions.k8s.io/v1/customresourcedefinitions/${customResourceDefinitionName}`,
+    {
+      pollingInterval: null,
+    },
+  );
+
+  if (loading) return 'loading'; //change!
+
+  const versions = data?.spec?.versions;
+  const version = versions?.find(version => version.name === resourceVersion);
+
+  const AdditionalPrinterColumns = resource => {
+    if (!version.additionalPrinterColumns) return null;
+
+    const getJsonPath = (resource, jsonPath) => {
+      return (
+        jsonPath
+          ?.substring(1)
+          .split('.')
+          .reduce((obj, i) => obj[i], resource) || EMPTY_TEXT_PLACEHOLDER
+      );
+    };
+    const headerRenderer = () => ['Name', 'Value'];
+    const rowRenderer = entry => [
+      entry.name,
+      getJsonPath(resource, entry.jsonPath),
+    ];
+
+    return (
+      <GenericList
+        title="Additional Printer Columns"
+        entries={version.additionalPrinterColumns || []}
+        headerRenderer={headerRenderer}
+        rowRenderer={rowRenderer}
+      />
+    );
+  };
+
   const crdName = customResourceDefinitionName.split('.')[0];
   const crdGroup = customResourceDefinitionName.replace(`${crdName}.`, '');
   const resourceUrl = `/apis/${crdGroup}/${resourceVersion}/${
@@ -37,6 +81,7 @@ export function CustomResource({ params }) {
       resourceName={resourceName}
       namespace={namespace}
       breadcrumbs={breadcrumbs}
+      customComponents={[AdditionalPrinterColumns]}
     />
   );
 }
