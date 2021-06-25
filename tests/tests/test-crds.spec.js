8cd64509a5489b647d0241b47e3907964032b117
dariadomagala <daria.domagala@sap.com>
2021-06-25 15:22:58 +0200

    Add more tests

--- tests/tests/test-crds.spec.js
+++ tests/tests/test-crds.spec.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 import 'cypress-file-upload';
 
-context('Test Custom Resource Definitions', () => {
+context('Test Custom Resource Definitions (Namespace Wide)', () => {
   before(() => {
     cy.loginAndSelectCluster();
     cy.goToNamespaceDetails();
@@ -42,7 +42,7 @@ context('Test Custom Resource Definitions', () => {
 
     cy.getIframeBody()
       .find('[data-testid=crd-custom-resources]')
-      .find('tbody tr td a')
+      .find('tbody tr td a', { timeout: 7000 })
       .first()
       .click({ force: true });
   });
@@ -51,6 +51,28 @@ context('Test Custom Resource Definitions', () => {
     cy.getIframeBody()
       .find('[data-testid=cr-additional-printer-columns]')
       .contains('td', 'Age', { timeout: 7000 })
+      .should('be.visible');
+  });
+
+  it('Test Custom Resource Definitions list (Cluster Wide)', () => {
+    cy.getLeftNav()
+      .contains('Namespaces')
+      .click();
+
+    cy.getLeftNav()
+      .contains('Configuration')
+      .click();
+
+    cy.getLeftNav()
+      .find('[data-testid=customresourcedefinitions_customresourcedefinitions]')
+      .click();
+
+    cy.getIframeBody()
+      .find('[role="search"] [aria-label="search-input"]')
+      .type('applications', { force: true }); // use force to skip clicking (the table could re-render between the click and the typing)
+
+    cy.getIframeBody()
+      .contains('tbody tr td a', 'applications')
       .should('be.visible');
   });
 });
