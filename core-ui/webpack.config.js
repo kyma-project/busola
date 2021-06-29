974559743b8e5519b7a8ff07c7b987e3c7469a53
Wawrzyn321 <Wawrzyn321@gmail.com>
2021-06-29 13:48:03 +0200

    Remove MonacoWebpackPlugin

--- core-ui/webpack.config.js
+++ core-ui/webpack.config.js
@@ -1,16 +1,9 @@
-const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 const path = require('path');
 
 const APP_DIR = path.resolve(__dirname, './src');
 const MONACO_DIR = path.resolve(__dirname, './node_modules/monaco-editor');
 
 module.exports = {
-  plugins: [
-    new MonacoWebpackPlugin({
-      // available options are documented at https://github.com/Microsoft/monaco-editor-webpack-plugin#options
-      languages: ['json', 'javascript', 'typescript', 'python'],
-    }),
-  ],
   rules: [
     {
       test: /\.css$/,
