c721b793444edf9395274cc8c2fa3c6b769d2f37
Piotr Wawrzyńczyk <Wawrzyn321@gmail.com>
2021-07-13 09:17:39 +0200

    Restyle the drawer (#274)
    * Add filtering button, setup handle for resizing
    
    * Resizing the drawer
    
    * Bump
    
    * Title, buttons
    
    * Adjustments
    
    * Close search on editor
    
    * Review fixes
    
    * Review fixes 2

---
+++ shared/contexts/YamlEditorContext/EditorActions.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Tooltip } from '../../components/Tooltip/Tooltip';
+import { Button } from 'fundamental-react';
+import copyToCliboard from 'copy-to-clipboard';
+import { saveAs } from 'file-saver';
+import './EditorActions.scss';
+
+const ButtonWithTooltip = ({
+  tooltipContent,
+  glyph,
+  onClick,
+  className,
+  disabled = false,
+}) => {
+  return (
+    <Tooltip className={className} content={tooltipContent} position="top">
+      <Button
+        option="transparent"
+        glyph={glyph}
+        onClick={onClick}
+        disabled={disabled}
+        className="circle-button"
+      />
+    </Tooltip>
+  );
+};
+
+export function EditorActions({ val, editor, title, onSave, saveDisabled }) {
+  const openSearch = () => {
+    // focus is required for search control to appear
+    editor.focus();
+    editor.trigger('', 'actions.find');
+  };
+
+  const download = () => {
+    const blob = new Blob([val], {
+      type: 'application/yaml;charset=utf-8',
+    });
+    saveAs(blob, title || 'spec.yaml');
+  };
+
+  return (
+    <section className="editor-actions fd-margin-bottom--sm">
+      <ButtonWithTooltip
+        tooltipContent="Search"
+        glyph="filter"
+        onClick={openSearch}
+        className="fd-margin-end--sm"
+      />
+      <ButtonWithTooltip
+        tooltipContent="Save"
+        glyph="save"
+        onClick={onSave}
+        disabled={saveDisabled}
+      />
+      <ButtonWithTooltip
+        tooltipContent="Copy to clipboard"
+        glyph="copy"
+        onClick={() => copyToCliboard(val)}
+      />
+      <ButtonWithTooltip
+        tooltipContent="Download"
+        glyph="download"
+        onClick={download}
+      />
+    </section>
+  );
+}
