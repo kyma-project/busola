c9f80eac255a6e44683d59cef9529a8cc2035962
Jakub Łukomski <dev@tenku.eu>
2021-07-29 15:14:15 +0200

    change collapse option

--- core/src/luigi-config/settings.js
+++ core/src/luigi-config/settings.js
@@ -2,7 +2,7 @@ import { setTheme, getTheme } from './utils/theme';
 
 export function createSettings(params) {
   return {
-    responsiveNavigation: 'simpleMobileOnly',
+    responsiveNavigation: 'semiCollapsed',
     sideNavFooterText: '',
     header: {
       logo: getTheme() === 'hcw' ? 'assets/logo-black.svg' : 'assets/logo.svg',
