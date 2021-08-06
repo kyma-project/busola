bc726730fe5e4e6719a0550dd22a19470c186b9b
dariadomagala <daria.domagala@sap.com>
2021-08-06 14:28:40 +0200

    Remove images from the list view

--- core-ui/src/components/Predefined/List/DaemonSets.list.js
+++ core-ui/src/components/Predefined/List/DaemonSets.list.js
@@ -5,14 +5,6 @@ import { Labels, StatusBadge } from 'react-shared';
 export const DaemonSetsList = ({ DefaultRenderer, ...otherParams }) => {
   const { t } = useTranslation();
 
-  const getImages = daemonSet => {
-    const images =
-      daemonSet.spec.template.spec.containers?.map(
-        container => container.image,
-      ) || [];
-    return images;
-  };
-
   const isStatusOk = daemonSet => {
     const allPods =
       daemonSet.status.numberReady + (daemonSet.status.numberUnavailable || 0);
@@ -31,11 +23,13 @@ export const DaemonSetsList = ({ DefaultRenderer, ...otherParams }) => {
 
   const customColumns = [
     {
-      header: t('daemon-sets.images'),
-      value: resource => {
-        const images = getImages(resource);
-        return images?.map(image => <p>{image}</p>);
-      },
+      header: t('daemon-sets.node-selector'),
+      value: resource => (
+        <Labels
+          labels={resource.spec.template.spec.nodeSelector}
+          shortenLongLabels
+        />
+      ),
     },
     {
       header: t('daemon-sets.pods'),
@@ -44,15 +38,6 @@ export const DaemonSetsList = ({ DefaultRenderer, ...otherParams }) => {
         const statusType = getStatusType(resource);
         return <StatusBadge type={statusType}>{podsCount}</StatusBadge>;
       },
-    },
-    {
-      header: t('daemon-sets.node-selector'),
-      value: resource => (
-        <Labels
-          labels={resource.spec.template.spec.nodeSelector}
-          shortenLongLabels
-        />
-      ),
     },
   ];
 
