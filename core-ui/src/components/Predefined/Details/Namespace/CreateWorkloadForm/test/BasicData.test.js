cc2dc30147f6e57d24245a7e6c720c95e018fd84
Wojciech Sołtys <wojciech.soltys@sap.com>
2021-08-09 10:07:11 +0200

    Fix BasicData test

--- core-ui/src/components/Predefined/Details/Namespace/CreateWorkloadForm/test/BasicData.test.js
+++ core-ui/src/components/Predefined/Details/Namespace/CreateWorkloadForm/test/BasicData.test.js
@@ -1,23 +1,49 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import BasicData from '../BasicData';
+import { shallow } from 'enzyme';
+
+jest.mock('react-i18next', () => ({
+  // this mock makes sure any components using the translate hook can use it without a warning being shown
+  useTranslation: () => {
+    return {
+      t: str => str,
+      i18n: {
+        changeLanguage: () => new Promise(() => {}),
+        options: {},
+      },
+    };
+  },
+}));
+
+jest.mock('react-shared', () => ({
+  K8sNameInput: () => <input role="name-input" />,
+  Tooltip: () => null,
+}));
+
+jest.mock('components/Lambdas/components', () => ({
+  LabelsInput: () => null,
+}));
 
 describe('BasicData', () => {
   it('Updates name and docker image', () => {
     const setDeploymentMock = jest.fn();
-    const { getByLabelText } = render(
+    const component = shallow(
       <BasicData deployment={{}} setDeployment={setDeploymentMock} />,
     );
-    fireEvent.change(getByLabelText(/Name/), {
-      target: { value: 'test-name' },
-    });
 
+    const nameEvent = {
+      preventDefault() {},
+      target: { value: 'test-name' },
+    };
+    component.find('K8sNameInput').simulate('change', nameEvent);
     expect(setDeploymentMock).toHaveBeenCalledWith({ name: 'test-name' });
 
-    fireEvent.change(getByLabelText(/Docker image/), {
+    const imageEvent = {
+      preventDefault() {},
       target: { value: 'test-image' },
-    });
-
+    };
+    component.find('FormInput').simulate('change', imageEvent);
     expect(setDeploymentMock).toHaveBeenCalledWith({
       dockerImage: 'test-image',
     });
