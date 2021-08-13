d5a1184b7620b8354ac845ede87268ceffdf6268
Piotr Wawrzyńczyk <Wawrzyn321@gmail.com>
2021-08-13 08:33:25 +0200

    New Preferences look (#341)
    * New Prefs look, some new /shared components
    
    * Bump, move components to shared
    
    * Smol fixes
    
    * Finish EN translations
    
    * PL translations, adjust keys
    
    * Center the text on TileButtons
    
    * Update core/src/i18n/en.yaml
    
    * Update core/src/i18n/en.yaml
    
    * Reorder words
    
    Co-authored-by: Nina Hingerl <76950046+NHingerl@users.noreply.github.com>

--- core-ui/src/components/Preferences/NamespaceSettings.js
+++ core-ui/src/components/Preferences/NamespaceSettings.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import LuigiClient from '@luigi-project/client';
 import { useMicrofrontendContext, useFeatureToggle } from 'react-shared';
-import { LayoutPanel, Switch } from 'fundamental-react';
+import { Switch } from 'fundamental-react';
+import { useTranslation } from 'react-i18next';
 
 export default function NamespaceSettings() {
+  const { t } = useTranslation();
   const { groups } = useMicrofrontendContext();
   const [showHiddenNamespaces, setShowHiddenNamespaces] = useFeatureToggle(
     'showHiddenNamespaces',
@@ -26,20 +28,19 @@ export default function NamespaceSettings() {
 
   return (
     shouldShowNamespaceSettings() && (
-      <LayoutPanel className="fd-margin--tiny fd-margin-top--md">
-        <LayoutPanel.Header>
-          <LayoutPanel.Head title="Namespace settings" />
-          <LayoutPanel.Actions>
-            Show Hidden Namespaces
-            <Switch
-              inputProps={{ 'aria-label': 'toggle-hidden-namespaces' }}
-              className="fd-has-display-inline-block fd-margin-begin--tiny"
-              checked={showHiddenNamespaces}
-              onChange={toggleVisibility}
-            />
-          </LayoutPanel.Actions>
-        </LayoutPanel.Header>
-      </LayoutPanel>
+      <div className="preferences-row">
+        <span className="fd-has-color-status-4">
+          {t('settings.clusters.showHiddenNamespaces')}
+        </span>
+        <div>
+          <Switch
+            inputProps={{ 'aria-label': 'toggle-hidden-namespaces' }}
+            className="fd-has-display-inline-block fd-margin-begin--tiny"
+            checked={showHiddenNamespaces}
+            onChange={toggleVisibility}
+          />
+        </div>
+      </div>
     )
   );
 }
