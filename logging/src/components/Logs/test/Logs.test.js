c0a6d279ddaf6535b99e3222c342697efda6a6da
Wojciech Sołtys <wojciech.soltys@sap.com>
2021-08-20 14:47:31 +0200

    Delete commented tests

--- logging/src/components/Logs/test/Logs.test.js
+++ logging/src/components/Logs/test/Logs.test.js
@@ -2,11 +2,6 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Logs from '../Logs';
 
-// @kyma-project/common is deprecated
-// jest.mock('@kyma-project/common', () => ({
-//   getApiUrl: () => 'kyma.local',
-// }));
-
 describe('Logs', () => {
   it('Shows message if no labels are present', () => {
     const { queryByText } = render(
