6e362c67e64b3329cf36b10a23b9a8c1f4f31956
Jakub Łukomski <dev@tenku.eu>
2021-08-22 18:28:33 +0200

    support excluded domains

--- core-ui/src/components/Predefined/Details/Issuer/IssuerDomains.js
+++ core-ui/src/components/Predefined/Details/Issuer/IssuerDomains.js
@@ -5,14 +5,25 @@ import { GenericList } from 'react-shared';
 export function IssuerDomains(issuer) {
   const { t } = useTranslation();
   return (
-    <GenericList
-      key="domains"
-      title={t('issuers.domains')}
-      showSearchField={false}
-      headerRenderer={() => []}
-      rowRenderer={domain => [domain]}
-      showHeader={false}
-      entries={issuer.spec.acme?.domains.include || []}
-    />
+    <>
+      <GenericList
+        key="domains"
+        title={t('issuers.domains.included')}
+        showSearchField={false}
+        headerRenderer={() => []}
+        rowRenderer={domain => [domain]}
+        showHeader={false}
+        entries={issuer.spec.acme?.domains.include || []}
+      />
+      <GenericList
+        key="domains"
+        title={t('issuers.domains.excluded')}
+        showSearchField={false}
+        headerRenderer={() => []}
+        rowRenderer={domain => [domain]}
+        showHeader={false}
+        entries={issuer.spec.acme?.domains.exclude || []}
+      />
+    </>
   );
 }
