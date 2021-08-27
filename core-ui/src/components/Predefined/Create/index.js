faa9c72ef465c573cfeb85071c0f8a4825b7c1d8
dariadomagala <daria.domagala@sap.com>
2021-08-27 10:38:58 +0200

    Remove old components

--- core-ui/src/components/Predefined/Create/index.js
+++ core-ui/src/components/Predefined/Create/index.js
@@ -2,5 +2,4 @@ export * from './Namespaces/Namespaces.create.js';
 export * from './AddonsConfigurations/AddonsConfigurations.create.js';
 export * from './RoleBindings/RoleBindings.create.js';
 export * from './OAuthClients/OAuthClients.create.js';
-export * from './Secrets/Secrets.create.js';
 export * from './ConfigMaps.create.js';
