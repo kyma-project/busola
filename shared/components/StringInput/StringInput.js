2823d28c2a7501c854aa80acb68b18405079317b
dariadomagala <daria.domagala@sap.com>
2021-09-03 14:58:22 +0200

    Add i18n to string input

--- shared/components/StringInput/StringInput.js
+++ shared/components/StringInput/StringInput.js
@@ -1,26 +1,32 @@
 import React, { useState, useRef, useEffect } from 'react';
 import classNames from 'classnames';
 import { Token } from 'fundamental-react';
+import { useTranslation } from 'react-i18next';
 
-export const SingleString = ({ text, onClick }) => (
-  <Token
-    title="Click to remove"
-    className="label-selector__label"
-    onClick={onClick}
-    buttonLabel=""
-  >
-    {text}
-  </Token>
-);
+export const SingleString = ({ text, onClick, i18n }) => {
+  const { t } = useTranslation(null, { i18n });
+  return (
+    <Token
+      title={t('components.string-input.remove')}
+      className="label-selector__label"
+      onClick={onClick}
+      buttonLabel=""
+    >
+      {text}
+    </Token>
+  );
+};
 
 export const StringInput = ({
   stringList = {},
   onChange,
   regexp = /^[A-z_-]+$/,
   required,
-  placeholder = 'Enter multiple values separated by comma',
+  placeholder = 'components.string-input.placeholder',
+  i18n,
   ...props
 }) => {
+  const { t } = useTranslation(null, { i18n });
   const [isValid, setValid] = useState(true);
   const inputRef = useRef(null);
 
@@ -72,13 +78,18 @@ export const StringInput = ({
       >
         {!!stringList.length &&
           stringList.map(s => (
-            <SingleString key={s} text={s} onClick={() => deleteString(s)} />
+            <SingleString
+              key={s}
+              text={s}
+              onClick={() => deleteString(s)}
+              i18n={i18n}
+            />
           ))}
         <input
           ref={inputRef}
           className="fd-input label-selector__input"
           type="text"
-          placeholder={placeholder}
+          placeholder={t(placeholder)}
           onKeyDown={handleKeyDown}
           onBlur={handleOutOfFocus}
           data-ignore-visual-validation
