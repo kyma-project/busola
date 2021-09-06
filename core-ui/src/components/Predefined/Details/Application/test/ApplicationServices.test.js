a0b034890e0dd67ba98787547d79bcfed455f321
Wojciech Sołtys <wojciech.soltys@sap.com>
2021-09-06 10:52:20 +0200

    Add react-i18next mocks

--- core-ui/src/components/Predefined/Details/Application/test/ApplicationServices.test.js
+++ core-ui/src/components/Predefined/Details/Application/test/ApplicationServices.test.js
@@ -2,6 +2,19 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import ApplicationServices from '../ApplicationServices';
 
+jest.mock('react-i18next', () => ({
+  // this mock makes sure any components using the translate hook can use it without a warning being shown
+  useTranslation: () => {
+    return {
+      t: str => str,
+      i18n: {
+        changeLanguage: () => new Promise(() => {}),
+        options: {},
+      },
+    };
+  },
+}));
+
 describe('ApplicationServices', () => {
   it('Renders list', () => {
     const spec = {
--- core-ui/src/components/Predefined/Details/Application/test/ServicesBoundModal.test.js
+++ core-ui/src/components/Predefined/Details/Application/test/ServicesBoundModal.test.js
@@ -2,6 +2,19 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import ServicesBoundModal from '../ServicesBoundModal';
 
+jest.mock('react-i18next', () => ({
+  // this mock makes sure any components using the translate hook can use it without a warning being shown
+  useTranslation: () => {
+    return {
+      t: str => str,
+      i18n: {
+        changeLanguage: () => new Promise(() => {}),
+        options: {},
+      },
+    };
+  },
+}));
+
 describe('ServicesBoundModal', () => {
   const namespace = 'test-namespace';
   const binding = {
