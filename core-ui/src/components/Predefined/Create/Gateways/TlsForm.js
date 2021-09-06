3a1fa05541d1362f8bdaad8f7aab1028edb9b18b
Agata Kucharska <akucharska@users.noreply.github.com>
2021-09-06 12:21:50 +0200

    Create gateway (#379)
    * Progress
    
    * Work in progress
    
    * Image
    
    * Something start to work
    
    * More problems
    
    * Almost
    
    * Remove translation temporarily
    
    * Reapply translations
    
    * Finally
    
    * Review changes
    
    * Review changes... fix hosts not being applied from simple to advanced view
    
    * Language review
    
    Co-authored-by: Aleksandra Simeonova <aleksandra.simeonova@sap.com>
    Co-authored-by: Wawrzyn321 <Wawrzyn321@gmail.com>

---
+++ core-ui/src/components/Predefined/Create/Gateways/TlsForm.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import { CreateForm } from 'shared/components/CreateForm/CreateForm';
+import { Checkbox, Select, FormLabel, FormInput } from 'fundamental-react';
+import { useTranslation } from 'react-i18next';
+import { TSL_MODES } from './helpers';
+
+const setTlsValue = (server, variableName, value, servers, setServers) => {
+  if (value === null) {
+    delete server.port[variableName];
+  } else {
+    if (!server.tls) {
+      server.tls = {};
+    }
+    server.tls[variableName] = value;
+  }
+  setServers([...servers]);
+};
+
+export const switchTLS = (server, hasTLS, servers, setServers) => {
+  if (hasTLS) {
+    server.tls = {};
+  } else {
+    delete server.tls;
+  }
+
+  setServers([...servers]);
+};
+
+export const TlsForm = ({ disabled = false, server, servers, setServers }) => {
+  const { t } = useTranslation();
+
+  const resourceOptions = TSL_MODES.map(mode => ({
+    key: mode,
+    text: mode,
+  }));
+  return (
+    <CreateForm.CollapsibleSection
+      title={t('gateways.create-modal.advanced.tls.tls')}
+      defaultOpen
+      disabled={disabled}
+      actions={
+        <Checkbox
+          compact
+          checked={!!server.tls}
+          onChange={(_, checked) =>
+            switchTLS(server, checked, servers, setServers)
+          }
+          dir="rtl"
+        >
+          {t('gateways.create-modal.advanced.enable-tls')}
+        </Checkbox>
+      }
+    >
+      <CreateForm.FormField
+        label={
+          <FormLabel required={!!server.tls}>
+            {t('gateways.create-modal.advanced.tls.mode')}
+          </FormLabel>
+        }
+        input={
+          <Select
+            compact
+            required={!!server.tls}
+            onSelect={(_, selected) => {
+              setTlsValue(server, 'mode', selected.key, servers, setServers);
+            }}
+            selectedKey={server.tls?.mode || ''}
+            options={resourceOptions}
+            disabled={!server.tls}
+            placeholder={t(
+              'gateways.create-modal.advanced.placeholders.tls.mode',
+            )}
+          ></Select>
+        }
+      />
+      <CreateForm.FormField
+        label={
+          <FormLabel required={!!server.tls}>
+            {t('gateways.create-modal.advanced.tls.credentialName')}
+          </FormLabel>
+        }
+        input={
+          <FormInput
+            type="text"
+            disabled={!server.tls}
+            required={!!server.tls}
+            compact
+            placeholder={t(
+              'gateways.create-modal.advanced.placeholders.tls.credentialName',
+            )}
+            value={server.tls?.credentialName || ''}
+            onChange={e =>
+              setTlsValue(
+                server,
+                'credentialName',
+                e.target.value || '',
+                servers,
+                setServers,
+              )
+            }
+          />
+        }
+      />
+    </CreateForm.CollapsibleSection>
+  );
+};
