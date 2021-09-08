14c71eba021995f77e84efe4b855f95f71deb3bf
Wawrzyn321 <Wawrzyn321@gmail.com>
2021-09-08 12:32:08 +0200

    WIP so hard again

---
+++ core-ui/src/components/Predefined/Create/Deployments/ResourceForm.js
@@ -0,0 +1,140 @@
+import React from 'react';
+import LuigiClient from '@luigi-project/client';
+import { FormInput, FormLabel } from 'fundamental-react';
+import { Tooltip, usePost, useNotification } from 'react-shared';
+import classnames from 'classnames';
+import { CollapsibleSection2 } from 'shared/components/CreateForm/CollapsibleSection/CollapsibleSection';
+import * as jp from 'jsonpath';
+import pluralize from 'pluralize';
+import { ModeSelector } from 'shared/components/CreateForm/ModeSelector/ModeSelector';
+import { Editor } from 'shared/components/CreateForm/Editor/Editor';
+import 'shared/components/CreateForm/CreateForm.scss';
+
+ResourceForm.Label = ({ required, tooltipContent, children }) => {
+  const label = <FormLabel required={required}>{children}</FormLabel>;
+
+  if (tooltipContent) {
+    return <Tooltip content={tooltipContent}>{label}</Tooltip>;
+  } else {
+    return label;
+  }
+};
+
+ResourceForm.Input = ({ value, setValue, required, props }) => (
+  <FormInput
+    compact
+    required={required}
+    value={value}
+    onChange={e => setValue(e.target.value)}
+    {...props}
+  />
+);
+
+ResourceForm.CollapsibleSection = CollapsibleSection2;
+
+ResourceForm.FormField = function({
+  label,
+  input,
+  className,
+  required,
+  tooltipContent,
+  value,
+  setValue,
+}) {
+  return (
+    <div className={classnames('fd-row form-field', className)}>
+      <div className="fd-col fd-col-md--4 form-field__label">
+        <ResourceForm.Label required={required} tooltipContent={tooltipContent}>
+          {label}
+        </ResourceForm.Label>
+      </div>
+      <div className="fd-col fd-col-md--7">{input(value, setValue)}</div>
+    </div>
+  );
+};
+
+export function ResourceForm({
+  kind, //todo
+  resource,
+  setResource,
+  onCreate,
+  onChange,
+  formElementRef,
+  children,
+  apiGroup,
+  namespaceId,
+}) {
+  const postRequest = usePost();
+  const notification = useNotification();
+  const [mode, setMode] = React.useState(ModeSelector.MODE_SIMPLE);
+
+  if (!onCreate) {
+    onCreate = async () => {
+      try {
+        await postRequest(
+          `${apiGroup}/${namespaceId}/${pluralize(kind)}/`,
+          resource,
+        );
+        notification.notifySuccess({
+          content: kind + 'created',
+        });
+        LuigiClient.linkManager()
+          .fromContext('namespace')
+          .navigate(`/${pluralize(kind)}/details/${resource.metadata.name}`);
+      } catch (e) {
+        console.error(e);
+        notification.notifyError({
+          content: 'cannot create' + kind + ': ' + e.message,
+        });
+        return false;
+      }
+    };
+  }
+
+  const renderFormChildren = (children, isAdvanced) =>
+    React.Children.map(children, child => {
+      if (child.props.simple && isAdvanced) {
+        return null;
+      }
+      if (child.props.advanced && !isAdvanced) {
+        return null;
+      }
+      if (!child.props.yamlPath) {
+        return child;
+      }
+      return React.cloneElement(child, {
+        value: jp.value(resource, child.props.yamlPath),
+        setValue: value => {
+          jp.value(resource, child.props.yamlPath, value);
+          setResource({ ...resource });
+        },
+      });
+    });
+
+  return (
+    <div className="create-form">
+      <ModeSelector mode={mode} setMode={setMode} />
+      {mode === ModeSelector.MODE_SIMPLE && (
+        <div onChange={onChange} className="simple-form">
+          {renderFormChildren(children, false)}
+        </div>
+      )}
+      {mode === ModeSelector.MODE_YAML && (
+        <>
+          <Editor resource={resource} setResource={setResource} />
+        </>
+      )}
+      {/* always keep the advanced form to ensure validation */}
+      <form
+        className={classnames('advanced-form', {
+          hidden: mode !== ModeSelector.MODE_ADVANCED,
+        })}
+        onChange={onChange}
+        ref={formElementRef}
+        onSubmit={onCreate}
+      >
+        {renderFormChildren(children, true)}
+      </form>
+    </div>
+  );
+}
