43711f11e14d163c469392fc791cfcd08b43baa3
Wawrzyn321 <Wawrzyn321@gmail.com>
2021-09-13 11:04:00 +0200

    Review fixes

--- core-ui/src/shared/components/ResourceRef/ExternalResourceRef.js
+++ core-ui/src/shared/components/ResourceRef/ExternalResourceRef.js
@@ -1,39 +1,56 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Dropdown } from 'react-shared';
+import { FormInput, ComboboxInput } from 'fundamental-react';
+
+import './ExternalResourceRef.scss';
 
 export function ExternalResourceRef({ resourceRef, onChange, resources }) {
-  const { t, i18n } = useTranslation();
-
-  const format = resource => {
-    if (!resource) return null;
-    return `${resource.namespace}/${resource.name}`;
-  };
-
+  const { t } = useTranslation();
   const options = (resources || []).map(resource => ({
-    key: format(resource.metadata),
-    text: format(resource.metadata),
-    resource,
+    key: `${resource.metadata.namespace}/${resource.metadata.name}`,
+    text: resource.metadata.name,
+    namespace: resource.metadata.namespace,
   }));
 
   const selectResource = (e, selected) => {
-    onChange(e, {
-      name: selected.resource.metadata.name,
-      namespace: selected.resource.metadata.namespace,
-    });
+    console.log(selected.key);
+    if (selected.key !== -1) {
+      onChange(e, { name: selected.text, namespace: selected.namespace });
+    }
   };
 
   return (
     <div className="external-resource-ref">
-      <Dropdown
+      <ComboboxInput
+        id="external-resource-ref"
+        ariaLabel={t('common.labels.name')}
         compact
-        onSelect={selectResource}
+        onChange={e => {
+          onChange(e, { ...resourceRef, name: e.target.value });
+        }}
+        onSelectionChange={selectResource}
         placeholder={t('common.labels.name')}
-        selectedKey={format(resourceRef)}
+        formItemProps={{ className: 'name' }}
+        inputProps={{ value: resourceRef.name }}
         options={options}
-        fullWidth
-        i18n={i18n}
+        optionRenderer={resource => (
+          <>
+            <span className="fd-list__title">{resource.text}</span>
+            <span className="fd-list__secondary">{resource.namespace}</span>
+          </>
+        )}
       />
+      <div className="namespace fd-input-group--control fd-input-group">
+        <FormInput
+          compact
+          className="fd-input-group__input"
+          onChange={e =>
+            onChange(e, { ...resourceRef, namespace: e.target.value })
+          }
+          placeholder={t('common.labels.namespace')}
+          value={resourceRef.namespace}
+        />
+      </div>
     </div>
   );
 }
