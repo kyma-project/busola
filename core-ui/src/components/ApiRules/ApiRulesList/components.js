c7237ec59bd9babc083070a7b72fe1ac93ac6370
Agata Kucharska <akucharska@users.noreply.github.com>
2021-09-15 11:40:46 +0200

    When api rule has simple host get data from gateway (#405)
    * Work in progress
    
    * When simple host get data from gateway
    
    * Bump image
    
    * Review changes

--- core-ui/src/components/ApiRules/ApiRulesList/components.js
+++ core-ui/src/components/ApiRules/ApiRulesList/components.js
@@ -2,7 +2,7 @@ import React from 'react';
 import LuigiClient from '@luigi-project/client';
 
 import { Link } from 'fundamental-react';
-import { CopiableLink } from 'react-shared';
+import { CopiableLink, useGet } from 'react-shared';
 import { useTranslation } from 'react-i18next';
 
 import AccessStrategies from 'components/ApiRules/AccessStrategies/AccessStrategies';
@@ -19,6 +19,14 @@ function navigateToService(apiRule) {
     .navigate(`services/details/${apiRule.spec.service.name}`);
 }
 
+function getGatewayHost(gateway) {
+  const properServer = gateway.spec.servers.filter(
+    server => server.port.protocol === 'HTTPS',
+  );
+  if (!properServer?.length || !properServer[0].hosts?.length) return null;
+  return properServer[0].hosts[0].replace('*.', '');
+}
+
 export function GoToApiRuleDetails({ apiRule }) {
   return (
     <Link className="fd-link" onClick={() => goToApiRuleDetails(apiRule)}>
@@ -28,10 +36,23 @@ export function GoToApiRuleDetails({ apiRule }) {
 }
 
 export function CopiableApiRuleHost({ apiRule }) {
-  const { i18n } = useTranslation();
-  return (
-    <CopiableLink url={`https://${apiRule.spec.service.host}`} i18n={i18n} />
-  );
+  const { t, i18n } = useTranslation();
+  let hostname = apiRule.spec.service.host;
+  // regex is explained here: https://stackoverflow.com/questions/17986371/regular-expression-to-validate-fqdn-in-c-sharp-and-javascript
+  const regex = /^(?=.{1,254}$)((?=[a-z0-9-]{1,63}\.)(xn--+)?[a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,63}$/i;
+  const isFQDN = hostname.match(regex);
+  const [gatewayName, gatewayNamespace] = apiRule.spec.gateway.split('.', 2);
+  const gatewayUrl = `/apis/networking.istio.io/v1alpha3/namespaces/${gatewayNamespace}/gateways/${gatewayName}`;
+
+  const { data: gateway, error, loading } = useGet(gatewayUrl, {});
+
+  if (!isFQDN) {
+    if (loading) return t('common.headers.loading');
+    if (error) return `${t('common.tooltips.error')} ${error.message}`;
+    hostname = `${hostname}.${getGatewayHost(gateway)}`;
+  }
+
+  return <CopiableLink url={`https://${hostname}`} i18n={i18n} />;
 }
 
 export function ApiRuleServiceInfo({ apiRule, withName = true }) {
