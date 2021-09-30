c5110759e0a62194084b91899f25eaf0ee71e150
Agata Kucharska <akucharska@users.noreply.github.com>
2021-09-30 16:34:26 +0200

    Issuers (#428)
    * wip
    
    * Distractions
    
    * I don't remember
    
    * Bump
    
    * Fix tls form
    
    * A little bulletproofing
    
    * Make sure preset Services have valid ports
    
    * Add TLS only for HTTPS, not HTTPS & TCP
    
    * AAAAAA
    
    * Workaround for CI flaky test?
    
    * YOU HAD ONE JOB
    
    * Compact everywhere
    
    * Add delay to tooltips
    
    * Enable other protocols, force TLS in HTTPS
    
    * Expand the list on add
    
    * Make Select full width customization
    
    * Let's pause this for a bit
    
    * portse
    
    * wip
    
    * Fix ModalWithForm translations
    
    * Add secrets with filtering
    
    * Gettin' there
    
    * Fix
    
    * Drawing towards close
    
    * Self review
    
    * screaming snakes
    
    * That was scary
    
    * Refactor
    
    * Fix non-https validation
    
    * Make fields on basic collapsible, but expanded
    
    * Unambiguousise Gateway description
    
    * Update core/src/i18n/en.yaml
    
    Co-authored-by: Nina Hingerl <76950046+NHingerl@users.noreply.github.com>
    
    * Update core/src/i18n/en.yaml
    
    Co-authored-by: Nina Hingerl <76950046+NHingerl@users.noreply.github.com>
    
    * Add TLS!
    
    * Update core/src/i18n/en.yaml
    
    Co-authored-by: Nina Hingerl <76950046+NHingerl@users.noreply.github.com>
    
    * Update core/src/i18n/en.yaml
    
    Co-authored-by: Nina Hingerl <76950046+NHingerl@users.noreply.github.com>
    
    * Update core/src/i18n/en.yaml
    
    Co-authored-by: Nina Hingerl <76950046+NHingerl@users.noreply.github.com>
    
    * Update core/src/i18n/en.yaml
    
    Co-authored-by: Nina Hingerl <76950046+NHingerl@users.noreply.github.com>
    
    * Partial comment apply
    
    * Update translations
    
    * Almost done
    
    * Fix auto registration
    
    * Small fix
    
    * Bump image
    
    * Placeholders, tooltips etc
    
    * Small fix
    
    * Delete unused files
    
    * Use new SecretRef / IssuerRef everywhere
    
    * Suggestions from code review
    
    * Update en.yaml
    
    I think Kubernetes says renewal too: https://kubernetes.io/docs/tasks/administer-cluster/kubeadm/kubeadm-certs/
    
    * Update core/src/i18n/en.yaml
    
    * Update core/src/i18n/en.yaml
    
    * Update core/src/i18n/en.yaml
    
    * Make private key ref required when it needs to. Rename labels of secretRef and IssuerRef
    
    * Apply suggestions from code review
    
    Co-authored-by: Nina Hingerl <76950046+NHingerl@users.noreply.github.com>
    
    * Make sections open by default on simple view and closed on advanced, add other small adjustments
    
    * Rework domains translations
    
    * Update core/src/i18n/en.yaml
    
    * To much for example
    
    * Fix console errors
    
    * Add validation, fix secretRet field, other review changes
    
    * Small fix
    
    * Fix validation for externalAccountBinding
    
    Co-authored-by: Wawrzyn321 <Wawrzyn321@gmail.com>
    Co-authored-by: Nina Hingerl <76950046+NHingerl@users.noreply.github.com>

--- core-ui/src/components/Predefined/Create/Certificates/SimpleForm.js
+++ core-ui/src/components/Predefined/Create/Certificates/SimpleForm.js
@@ -13,6 +13,7 @@ import { K8sNameInput } from 'react-shared';
 import { CreateForm } from 'shared/components/CreateForm/CreateForm';
 import { IssuerRef } from 'shared/components/ResourceRef/IssuerRef';
 import { base64Decode, base64Encode } from 'shared/helpers';
+import * as jp from 'jsonpath';
 
 import './SimpleForm.scss';
 
@@ -157,19 +158,17 @@ export function SimpleForm({ certificate, setCertificate }) {
           />
         </FormFieldset>
         {(certificate.withCSR && csrFields) || commonNameFields}
-        <FormFieldset>
-          <CreateForm.FormField
-            label={<FormLabel>{t('certificates.issuer')}</FormLabel>}
-            input={
-              <IssuerRef
-                resourceRef={certificate.issuerRef}
-                onChange={(e, issuerRef) =>
-                  setCertificate({ ...certificate, issuerRef })
-                }
-              />
-            }
-          />
-        </FormFieldset>
+        <IssuerRef
+          className={'fd-margin-top--sm'}
+          id="issuerRef"
+          defaultOpen
+          resourceRef={jp.value(certificate, '$.issuerRef') || {}}
+          title={t('certificates.issuer')}
+          onChange={issuerRef => {
+            jp.value(certificate, '$.issuerRef', issuerRef);
+            setCertificate({ ...certificate, issuerRef });
+          }}
+        />
       </CreateForm.Section>
     </>
   );
--- core-ui/src/components/Predefined/Create/Issuers/helpers.js
+++ core-ui/src/components/Predefined/Create/Issuers/helpers.js
@@ -1,112 +1,43 @@
 import * as jp from 'jsonpath';
 
-export function toYaml(issuer) {
-  let spec = {
-    requestsPerDayQuota: issuer.requestsPerDayQuota || undefined,
-  };
-  if (issuer.type === 'ca') {
-    spec = {
-      ...spec,
-      ca: {
-        privateKeySecretRef: issuer.privateKeySecretRef,
-      },
-    };
-  } else if (issuer.type === 'acme') {
-    const includeDomains = issuer.includeDomains.filter(domain => !!domain);
-    const excludeDomains = issuer.excludeDomains.filter(domain => !!domain);
-    const hasDomains = includeDomains.length || excludeDomains.length;
-    const hasExternalAccount =
-      issuer.externalAccountKeyId || issuer.externalAccountSecretRef.name;
-    spec = {
-      ...spec,
-      acme: {
-        server: issuer.server,
-        email: issuer.email,
-        autoRegistration: issuer.autoRegistration,
-        privateKeySecretRef: !issuer.autoRegistration
-          ? issuer.privateKeySecretRef
-          : undefined,
-        domains: hasDomains
-          ? {
-              include: includeDomains.length ? includeDomains : undefined,
-              exclude: excludeDomains.length ? excludeDomains : undefined,
-            }
-          : undefined,
-        skipDNSChallengeValidation:
-          issuer.skipDNSChallengeValidation || undefined,
-        externalAccountBinding: hasExternalAccount
-          ? {
-              keyID: issuer.externalAccountKeyId,
-              keySecretRef: issuer.externalAccountSecretRef,
-            }
-          : undefined,
-      },
-    };
+export function validateIssuer(issuer, issuerType) {
+  const isRequestSet = jp.value(issuer, '$.spec.requestsPerDayQuota') >= 0;
+  if (issuerType === 'ca') {
+    const isSecretRefSet =
+      jp.value(issuer, '$.spec.ca.privateKeySecretRef.name') &&
+      jp.value(issuer, '$.spec.ca.privateKeySecretRef.namespace');
+
+    return isRequestSet && isSecretRefSet;
   }
-
-  return {
-    apiVersion: 'cert.gardener.cloud/v1alpha1',
-    kind: 'Issuer',
-    metadata: {
-      name: issuer.name,
-      namespace: issuer.namespace,
-    },
-    spec,
-  };
-}
-
-export function fromYaml(yaml, prevIssuer) {
-  let type;
-  let privateKeySecretRef;
-  if (jp.value(yaml, '$.spec.ca')) {
-    type = 'ca';
-    privateKeySecretRef = {
-      name: jp.value(yaml, '$.spec.ca.privateKeySecretRef.name'),
-      namespace: jp.value(yaml, '$.spec.ca.privateKeySecretRef.namespace'),
-    };
-  } else if (jp.value(yaml, '$.spec.acme')) {
-    type = 'acme';
-    privateKeySecretRef = {
-      name: jp.value(yaml, '$.spec.acme.privateKeySecretRef.name'),
-      namespace: jp.value(yaml, '$.spec.acme.privateKeySecretRef.namespace'),
-    };
+  if (issuerType === 'acme') {
+    const isServerSet = jp.value(issuer, '$.spec.acme.server');
+    const isEmailSet = jp.value(issuer, '$.spec.acme.email');
+    const isAutoRegistrationSet = jp.value(
+      issuer,
+      '$.spec.acme.autoRegistration',
+    );
+    const isSecretRefSet = isAutoRegistrationSet
+      ? isAutoRegistrationSet
+      : jp.value(issuer, '$.spec.acme.privateKeySecretRef.name') &&
+        jp.value(issuer, '$.spec.acme.privateKeySecretRef.namespace');
+    const isExternalAccountBindingValid =
+      (jp.value(issuer, '$.spec.acme.externalAccountBinding.keyID') &&
+        jp.value(
+          issuer,
+          '$.spec.acme.externalAccountBinding.keySecretRef.name',
+        ) &&
+        jp.value(
+          issuer,
+          '$.spec.acme.externalAccountBinding.keySecretRef.namespace',
+        )) ||
+      !jp.value(issuer, '$.spec.acme.externalAccountBinding');
+    return (
+      isRequestSet &&
+      isServerSet &&
+      isEmailSet &&
+      isSecretRefSet &&
+      isExternalAccountBindingValid
+    );
   }
-
-  return {
-    name: jp.value(yaml, '$.metadata.name') || '',
-    namespace: jp.value(yaml, '$.metadata.namespace') || '',
-    type,
-    requestsPerDayQuota: jp.value(yaml, '$.spec.requestsPerDayQuota'),
-    server: jp.value(yaml, '$.spec.acme.server') || '',
-    email: jp.value(yaml, '$.spec.acme.email') || '',
-    includeDomains: jp.value(yaml, '$.spec.acme.domains.include') || [],
-    excludeDomains: jp.value(yaml, '$.spec.acme.domains.exclude') || [],
-    skipDNSChallengeValidation:
-      jp.value(yaml, '$.spec.acme.skipDNSChallengeValidation') || false,
-    privateKeySecretRef,
-  };
-}
-
-export function createTemplate(namespace) {
-  return {
-    name: '',
-    namespace,
-    type: undefined,
-    server: '',
-    email: '',
-    autoRegistration: true,
-    privateKeySecretRef: {
-      name: '',
-      namespace: '',
-    },
-    requestsPerDayQuota: 0,
-    skipDNSChallengeValidation: false,
-    includeDomains: [],
-    excludeDomains: [],
-    externalAccountKeyId: '',
-    externalAccountSecretRef: {
-      name: '',
-      namespace: '',
-    },
-  };
+  return false;
 }
