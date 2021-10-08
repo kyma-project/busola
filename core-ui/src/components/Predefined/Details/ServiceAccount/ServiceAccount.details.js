3e25c16d2739f81f1ac586b894fdf59b47241f89
Jakub Lyczko <jakub.lyczko1@gmail.com>
2021-10-08 12:16:51 +0200

    Modified service accounts list based on generic component

--- core-ui/src/components/Predefined/Details/ServiceAccount/ServiceAccount.details.js
+++ core-ui/src/components/Predefined/Details/ServiceAccount/ServiceAccount.details.js
@@ -1,56 +1,41 @@
 import React from 'react';
-import { ComponentForList } from 'shared/getComponents';
 import { useTranslation } from 'react-i18next';
 import { StatusBadge } from 'react-shared';
+import { GenericSecrets } from './GenericSecrets';
 
 const ServiceAccountSecrets = serviceAccount => {
   const namespace = serviceAccount.metadata.namespace;
-  const secretsUrl = `/api/v1/namespaces/${namespace}/secrets`;
+  const listKey = 'service-account-secrets';
+  const title = 'secretsList';
   const filterBySecret = secret =>
     serviceAccount.secrets.find(
       ({ name: secretName }) => secret.metadata.name === secretName,
     );
 
   return (
-    <ComponentForList
-      name="secretsList"
-      key="service-account-secrets"
-      params={{
-        hasDetailsView: true,
-        fixedPath: true,
-        resourceUrl: secretsUrl,
-        resourceType: 'secrets',
-        namespace,
-        isCompact: true,
-        showTitle: true,
-        filter: filterBySecret,
-      }}
+    <GenericSecrets
+      namespace={namespace}
+      filter={filterBySecret}
+      listKey={listKey}
+      title={title}
     />
   );
 };
 const ServiceAccountImagePullSecrets = serviceAccount => {
   const namespace = serviceAccount.metadata.namespace;
-  const serviceAccountsUrl = `/api/v1/namespaces/${namespace}/serviceaccounts/${serviceAccount.name}/imagePullSecrets`;
+  const listKey = 'service-account-imagepullsecrets';
+  const title = 'imagePullSecretsList';
+  const filterBySecret = secret =>
+    serviceAccount.imagePullSecrets.find(
+      ({ name: secretName }) => secret.metadata.name === secretName,
+    );
 
-  const displayImagePullSecret = serviceAccount => {
-    if (!serviceAccount.imagePullSecrets) return;
-    console.log(serviceAccount.imagePullSecrets);
-    return serviceAccount.imagePullSecrets.map(obj => obj.name);
-  };
   return (
-    <ComponentForList
-      name="secrets"
-      key="service-account-imagepullsecrets"
-      params={{
-        hasDetailsView: true,
-        fixedPath: true,
-        resourceUrl: serviceAccountsUrl,
-        resourceType: 'imagePullSecrets',
-        namespace,
-        isCompact: true,
-        showTitle: true,
-        filter: displayImagePullSecret,
-      }}
+    <GenericSecrets
+      namespace={namespace}
+      filter={filterBySecret}
+      listKey={listKey}
+      title={title}
     />
   );
 };
