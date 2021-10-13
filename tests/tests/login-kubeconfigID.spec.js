ad4a834a51517db4b8313bfe267495f3eb95006b
Wawrzyn321 <Wawrzyn321@gmail.com>
2021-10-13 15:15:18 +0200

    Add e2e test

--- tests/tests/login-kubeconfigID.spec.js
+++ tests/tests/login-kubeconfigID.spec.js
@@ -3,10 +3,11 @@ import 'cypress-file-upload';
 import config from '../config';
 import { loadKubeconfig } from '../support/loadKubeconfigFile';
 
+const kubeconfigIdAddress =
+  'https://kyma-env-broker.cp.dev.kyma.cloud.sap/kubeconfig';
+
 context('Login - kubeconfigID', () => {
   it('Adds cluster by kubeconfigID', () => {
-    const kubeconfigIdAddress =
-      'https://kyma-env-broker.cp.dev.kyma.cloud.sap/kubeconfig';
     cy.wrap(loadKubeconfig()).then(kubeconfig => {
       cy.intercept(
         {
@@ -21,8 +22,6 @@ context('Login - kubeconfigID', () => {
   });
 
   it('Gracefully fails on invalid input', () => {
-    const kubeconfigIdAddress =
-      'https://kyma-env-broker.cp.dev.kyma.cloud.sap/kubeconfig';
     cy.intercept(
       {
         method: 'GET',
@@ -35,5 +34,31 @@ c:d`,
     Cypress.on('window:alert', alertContent =>
       expect(alertContent).to.include('Error loading kubeconfig ID'),
     );
+  });
+
+  it('Authenticated but not authorized', () => {
+    const requestData = {
+      method: 'POST',
+      url: '/backend/apis/authorization.k8s.io/v1/selfsubjectrulesreviews',
+    };
+    cy.intercept(requestData, {
+      status: {
+        resourceRules: [],
+      },
+    });
+
+    cy.wrap(loadKubeconfig()).then(kubeconfig => {
+      cy.intercept(
+        {
+          method: 'GET',
+          url: `${kubeconfigIdAddress}/*`,
+        },
+        kubeconfig,
+      );
+      cy.visit(`${config.clusterAddress}/clusters?kubeconfigID=tests`);
+      cy.getIframeBody()
+        .contains(/It looks like you have no role assigned./)
+        .should('be.visible');
+    });
   });
 });
