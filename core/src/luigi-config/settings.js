b98dbb46eb1d440699a68cc097bd99ffce479dfd
Piotr Wawrzyńczyk <Wawrzyn321@gmail.com>
2021-10-14 15:02:31 +0200

    Legal stuff links in footer (#459)
    * WIP
    
    * Bump
    
    * Finalizing
    
    * Disable sso login
    
    * Add workaround for OIDC
    
    * Minifix
    
    * Fix double footer
    
    * Open links in new tab
    
    * Simplify!
    
    * Optimize MultiInput
    
    * Restore en.yaml
    
    * ?
    
    * Bump backend
    
    * Damn
    
    * Remove line width limit
    
    * Add line width limit
    
    * Force changes
    
    * Force changes for PL
    
    * Set unlimited width
    
    * First sort, then prettify?

--- core/src/luigi-config/settings.js
+++ core/src/luigi-config/settings.js
@@ -1,16 +1,9 @@
 import { getTheme } from './utils/theme';
 
-async function getBusolaVersion() {
-  return await fetch('/assets/version.json')
-    .then(response => response.json())
-    .then(json => json.version)
-    .catch(() => 'unknown');
-}
-
-export async function createSettings(params) {
+export function createSettings(params) {
   return {
     responsiveNavigation: 'Fiori3',
-    sideNavFooterText: 'Version: ' + (await getBusolaVersion()),
+    sideNavFooterText: ' ', // init empty footer
     header: {
       logo: getTheme() === 'hcw' ? 'assets/logo-black.svg' : 'assets/logo.svg',
       title: params?.currentContext.cluster.name || '',
