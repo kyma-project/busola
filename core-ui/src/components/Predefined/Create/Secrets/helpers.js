b097d9e63f550cb959a190f5c072aa1c4af0e5eb
Jakub Łukomski <dev@tenku.eu>
2021-10-15 13:11:28 +0200

    Refactor secrets (#454)
    * secrets wip
    
    * secrets wip
    
    * secrets wip
    
    * secrets wip - still a mess
    
    * complete secrets refactor
    
    * clean up code
    
    * fix generating secrets and add missing defs
    
    * minor tweaks
    
    * fix targets ref css
    
    * recover strangely merged i18n
    
    * fix i18n again
    
    * and yet again

--- core-ui/src/components/Predefined/Create/Secrets/helpers.js
+++ core-ui/src/components/Predefined/Create/Secrets/helpers.js
@@ -1,184 +1,61 @@
-import * as jp from 'jsonpath';
+import * as components from '../';
 
-import { base64Encode } from 'shared/helpers';
+export function getSecretDefs(t, context) {
+  return Object.values(components)
+    .filter(component => component.secrets)
+    .map(component => component.secrets(t, context))
+    .flat();
+}
+
+export function readFromFile() {
+  return new Promise(resolve => {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.addEventListener('change', e => {
+      const file = e.target.files[0];
+      if (!file) resolve(null);
+      const reader = new FileReader();
+      reader.readAsText(file);
+      reader.onload = e =>
+        resolve({ name: file.name, content: e.target.result });
+    });
+    input.click();
+  });
+}
 
 export const mapObjectValues = (fn, obj) =>
   Object.fromEntries(
     Object.entries(obj).map(([key, value]) => [key, fn(value)]),
   );
 
-export function secretToYaml({ secret, isEncoded }) {
-  return {
-    apiVersion: 'v1',
-    kind: 'Secret',
-    type: secret.type,
-    metadata: {
-      name: secret.name,
-      namespace: secret.namespace,
-      labels: secret.labels,
-      annotations: secret.annotations,
-    },
-    data: isEncoded ? secret.data : mapObjectValues(base64Encode, secret.data),
-  };
-}
-
-export function yamlToSecret(yaml) {
-  return {
-    name: jp.value(yaml, '$.metadata.name') || '',
-    namespace: jp.value(yaml, '$.metadata.namespace') || '',
-    type: jp.value(yaml, '$.type') || '',
-    labels: jp.value(yaml, '$.metadata.labels') || {},
-    annotations: jp.value(yaml, '$.metadata.annotations') || {},
-    data: jp.value(yaml, '$.data') || '',
-  };
-}
-
 export function createSecretTemplate(namespaceId) {
   return {
-    name: '',
-    namespace: namespaceId,
     type: 'Opaque',
-    labels: {},
-    annotations: {},
+    metadata: {
+      name: '',
+      namespace: namespaceId,
+      labels: {},
+      annotations: {},
+    },
     data: {},
   };
 }
 
-export function createPresets(namespaceId, translate, DNSExist) {
-  return DNSExist
-    ? [
-        {
-          name: translate('secrets.create-modal.presets.default'),
-          value: createSecretTemplate(namespaceId),
-        },
-        {
-          name: 'Amazon Route53',
-          value: {
-            name: 'amazon-route53',
-            namespace: namespaceId,
-            type: 'Opaque',
-            labels: {},
-            annotations: {},
-            data: {
-              AWS_ACCESS_KEY_ID: '',
-              AWS_SECRET_ACCESS_KEY: '',
-            },
-          },
-        },
-        {
-          name: 'GoogleCloud DNS',
-          value: {
-            name: 'google-cloud-dns',
-            namespace: namespaceId,
-            type: 'Opaque',
-            labels: {},
-            annotations: {},
-            data: {
-              'serviceaccount.json': '',
-            },
-          },
-        },
-        {
-          name: 'AliCloud DNS',
-          value: {
-            name: 'ali-cloud-dns',
-            namespace: namespaceId,
-            type: 'Opaque',
-            labels: {},
-            annotations: {},
-            data: {
-              ACCESS_KEY_ID: '',
-              SECRET_ACCESS_KEY: '',
-            },
-          },
-        },
-        {
-          name: 'Azure DNS',
-          value: {
-            name: 'azure-dns',
-            namespace: namespaceId,
-            type: 'Opaque',
-            labels: {},
-            annotations: {},
-            data: {
-              AZURE_SUBSCRIPTION_ID: '',
-              AZURE_TENANT_ID: '',
-              AZURE_CLIENT_ID: '',
-              AZURE_CLIENT_SECRET: '',
-            },
-          },
-        },
-        {
-          name: 'OpenStack Designate',
-          value: {
-            name: 'openstack-designate',
-            namespace: namespaceId,
-            type: 'Opaque',
-            labels: {},
-            annotations: {},
-            data: {
-              OS_AUTH_URL: '',
-              OS_DOMAIN_NAME: '',
-              OS_PROJECT_NAME: '',
-              OS_USERNAME: '',
-              OS_PASSWORD: '',
-              OS_PROJECT_ID: '',
-              OS_REGION_NAME: '',
-              OS_TENANT_NAME: '',
-              OS_APPLICATION_CREDENTIAL_ID: '',
-              OS_APPLICATION_CREDENTIAL_NAME: '',
-              OS_APPLICATION_CREDENTIAL_SECRET: '',
-              OS_DOMAIN_ID: '',
-              OS_USER_DOMAIN_NAME: '',
-              OS_USER_DOMAIN_ID: '',
-            },
-          },
-        },
-        {
-          name: 'Cloudflare DNS',
-          value: {
-            name: 'cloudflare-dns',
-            namespace: namespaceId,
-            type: 'Opaque',
-            labels: {},
-            annotations: {},
-            data: {
-              CLOUDFLARE_API_TOKEN: '',
-            },
-          },
-        },
-        {
-          name: 'Infoblox',
-          value: {
-            name: 'infoblox',
-            namespace: namespaceId,
-            type: 'Opaque',
-            labels: {},
-            annotations: {},
-            data: {
-              USERNAME: '',
-              PASSWORD: '',
-            },
-          },
-        },
-        {
-          name: 'Netlify DNS',
-          value: {
-            name: 'netlify-dns',
-            namespace: namespaceId,
-            type: 'Opaque',
-            labels: {},
-            annotations: {},
-            data: {
-              NETLIFY_AUTH_TOKEN: '',
-            },
-          },
-        },
-      ]
-    : [
-        {
-          name: translate('secrets.create-modal.presets.default'),
-          value: createSecretTemplate(namespaceId),
-        },
-      ];
+export function createPresets(secretDefs, namespaceId, t) {
+  return [
+    {
+      name: t('secrets.create-modal.presets.default'),
+      value: createSecretTemplate(namespaceId),
+    },
+    ...secretDefs.map(({ title, name, type, data, ...value }) => ({
+      name: title || name || type,
+      value: {
+        ...createSecretTemplate(namespaceId),
+        ...value,
+        name,
+        type,
+        data: data?.reduce((acc, key) => ({ ...acc, [key]: '' }), {}),
+      },
+    })),
+  ];
 }
