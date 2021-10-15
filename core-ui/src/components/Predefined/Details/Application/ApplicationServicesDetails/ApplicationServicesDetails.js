8b8551408ae2706ac4a48fc211cfa6231137d6fc
dariadomagala <daria.domagala@sap.com>
2021-10-15 08:28:52 +0200

    Add apis and events list

--- core-ui/src/components/Predefined/Details/Application/ApplicationServicesDetails/ApplicationServicesDetails.js
+++ core-ui/src/components/Predefined/Details/Application/ApplicationServicesDetails/ApplicationServicesDetails.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 import {
+  GenericList,
   PageHeader,
   YamlEditorProvider,
   useGet,
@@ -9,6 +10,7 @@ import {
   getErrorMessage,
   Spinner,
   useWindowTitle,
+  EMPTY_TEXT_PLACEHOLDER,
 } from 'react-shared';
 
 export function ApplicationServiceDetails({ applicationName, serviceName }) {
@@ -16,7 +18,7 @@ export function ApplicationServiceDetails({ applicationName, serviceName }) {
   const { i18n } = useTranslation();
   const resourceUrl = `/apis/applicationconnector.kyma-project.io/v1alpha1/applications/${applicationName}`;
 
-  const { loading = true, error, data: resource } = useGet(resourceUrl, {
+  const { loading = true, error, data: application } = useGet(resourceUrl, {
     pollingInterval: 3000,
   });
 
@@ -55,13 +57,50 @@ export function ApplicationServiceDetails({ applicationName, serviceName }) {
     );
   }
 
+  const service = application?.spec.services?.find(
+    service => service.name === serviceName,
+  );
+  const APIs = service?.entries?.filter(t => t.type === 'API');
+  const events = service?.entries?.filter(t => t.type === 'Events');
+
+  const headerRenderer = () => ['Name', 'Access Label', 'Central Gateway URL'];
+  const rowRenderer = e => [
+    e.name || EMPTY_TEXT_PLACEHOLDER,
+    e.accessLabel || EMPTY_TEXT_PLACEHOLDER,
+    e.centralGatewayUrl || EMPTY_TEXT_PLACEHOLDER,
+  ];
+
   return (
     <YamlEditorProvider i18n={i18n}>
-      {resource && (
-        <PageHeader
-          title={serviceName}
-          breadcrumbItems={breadcrumbItems}
-        ></PageHeader>
+      {application && (
+        <>
+          <PageHeader
+            title={service?.displayName || serviceName}
+            breadcrumbItems={breadcrumbItems}
+          ></PageHeader>
+
+          <GenericList
+            key="application-service-apis"
+            title={'APIs'}
+            textSearchProperties={['name', 'accessLabel', 'centralGatewayUrl']}
+            entries={APIs}
+            headerRenderer={headerRenderer}
+            rowRenderer={rowRenderer}
+            notFoundMessage={'Not Found'}
+            i18n={i18n}
+          />
+
+          <GenericList
+            key="application-service-events"
+            title={'Events'}
+            textSearchProperties={['name', 'accessLabel', 'centralGatewayUrl']}
+            entries={events}
+            headerRenderer={headerRenderer}
+            rowRenderer={rowRenderer}
+            notFoundMessage={'Not Found'}
+            i18n={i18n}
+          />
+        </>
       )}
     </YamlEditorProvider>
   );
