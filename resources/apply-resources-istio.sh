15cfcdaf3ee292836df708554f41f9a82a4a6a2c
Wawrzyn321 <Wawrzyn321@gmail.com>
2021-10-18 08:42:05 +0200

    Use envsubst

--- resources/apply-resources-istio.sh
+++ resources/apply-resources-istio.sh
@@ -3,19 +3,13 @@ if [ -z "$1" ] ; then
     exit 1
 fi
 
-namespace=${2:-busola}
+export DOMAIN=$1
+export NAMESPACE=${2:-busola}
 
 mkdir -p ../temp/resources
-
 cp -rf . ../temp/resources
 
-for i in ../temp/resources/**{/*,}.yaml; do
-    sed -i '' "s/%DOMAIN%/$1/g" $i
-done
+envsubst < ../temp/resources/istio/virtualservice-busola-template.yaml > ../temp/resources/istio/virtualservice-busola.yaml
 
-for i in ../temp/resources/**{/*,}.yaml; do
-    sed -i '' "s/%NAMESPACE%/$2/g" $i
-done
-
-kubectl apply -k ../temp/resources/istio --namespace=$namespace
+kubectl apply -k ../temp/resources/istio --namespace=$NAMESPACE
 
