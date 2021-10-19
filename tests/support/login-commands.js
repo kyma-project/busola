f705ad63e0abdeb2311d2dc46c9eec11f8459df6
Wojciech Sołtys <wojciech.soltys@sap.com>
2021-10-19 14:46:53 +0200

    Revert changes in login commands

--- tests/support/login-commands.js
+++ tests/support/login-commands.js
@@ -1,58 +1,8 @@
 import config from '../config';
 
 Cypress.Commands.add('loginAndSelectCluster', () => {
-  cy.visit(`${config.clusterAddress}/clusters`);
-  cy.wait(10000);
-
-  cy.getIframeBody().then($body => {
-    //OIDC login flow
-    if ($body.find('span[role="presentation"]').length > 0) {
-      cy.getIframeBody()
-        .find('span[role="presentation"]')
-        .click();
-
-      cy.getIframeBody()
-        .find('input[aria-label="issuer-url"]')
-        .type('https://apskyxzcl.accounts400.ondemand.com');
-      //.type('https://kymatest.accounts400.ondemand.com');
-
-      cy.getIframeBody()
-        .find('input[aria-label="client-id"]')
-        .type('d0316c58-b0fe-45cd-9960-0fea0708355a');
-      //.type('9bd05ed7-a930-44e6-8c79-e6defeb7dec9');
-
-      cy.getIframeBody()
-        .find('input[aria-label="scopes"]')
-        .type('openid');
-
-      cy.getIframeBody()
-        .contains('Next Step')
-        .click();
-
-      cy.getIframeBody()
-        .contains('Add Cluster')
-        .click();
-
-      //it doesn't load in Cypress
-      cy.getIframeBody()
-        .find('input[id="j_username"]')
-        .type('kyma_hasselhoff@sap.com');
-
-      cy.getIframeBody()
-        .find('input[id="j_password"]')
-        .type('password');
-
-      cy.getIframeBody()
-        .find('button[id="logOnFormSubmit"]')
-        .click();
-    } else {
-      cy.getIframeBody()
-        .contains('Add Cluster')
-        .click();
-    }
-  });
-
-  cy.getIframeBody()
+  cy.visit(`${config.clusterAddress}/clusters`)
+    .getIframeBody()
     .contains('Add a Cluster')
     .click();
 
@@ -61,7 +11,11 @@ Cypress.Commands.add('loginAndSelectCluster', () => {
     .attachFile('kubeconfig.yaml', { subjectType: 'drag-n-drop' });
 
   cy.getIframeBody()
-    .contains('Next Step')
+    .contains('Next')
+    .click();
+
+  cy.getIframeBody()
+    .contains('Add Cluster')
     .click();
 
   cy.url().should('match', /namespaces$/);
