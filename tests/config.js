20858a5e3f39535c5b0a4c1fae83b7b7c4d42a28
Wojciech Sołtys <wojciech.soltys@sap.com>
2021-10-20 14:28:32 +0200

    Change Cyoress config

--- tests/config.js
+++ tests/config.js
@@ -4,5 +4,5 @@ const domain = env.DOMAIN || 'local.kyma.dev';
 export default {
   domain: domain,
   localDev: env.LOCAL_DEV || false,
-  clusterAddress: env.LOCAL_DEV ? `http://localhost:8080` : `https://${domain}`,
+  clusterAddress: env.LOCAL_DEV ? `http://localhost:3001` : `https://${domain}`,
 };
