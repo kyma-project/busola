75cd45f0409a0f4a190dc911f5c63386bbe888ce
Wojciech Sołtys <wojciech.soltys@sap.com>
2021-10-20 14:44:39 +0200

    Change cypress config

--- tests/config.js
+++ tests/config.js
@@ -1,8 +1,8 @@
 const env = Cypress.env();
-const domain = env.DOMAIN || 'local.kyma.dev';
+const domain = env.DOMAIN || 'https://local.kyma.dev';
 
 export default {
   domain: domain,
   localDev: env.LOCAL_DEV || false,
-  clusterAddress: env.LOCAL_DEV ? `http://localhost:3001` : `https://${domain}`,
+  clusterAddress: env.LOCAL_DEV ? `http://localhost:8080` : `${domain}`,
 };
