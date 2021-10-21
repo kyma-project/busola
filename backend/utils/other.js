6920a3f5c7364997aece3c852fb19ae64992ab54
Wojciech Sołtys <wojciech.soltys@sap.com>
2021-10-21 14:44:17 +0200

    Add small improvements

--- backend/utils/other.js
+++ backend/utils/other.js
@@ -2,7 +2,6 @@ export function requestLogger(httpModule) {
   var original = httpModule.request;
   httpModule.request = function(options, callback) {
     console.log('Outgoing HTTP request with options', options);
-
     return original(options, callback);
   };
 }
