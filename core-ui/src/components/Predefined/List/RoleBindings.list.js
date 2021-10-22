e2eaeafd9f2b3a3a38034ca5ea5d9c6acf941598
dariadomagala <daria.domagala@sap.com>
2021-10-22 07:46:54 +0200

    Add link to tole

--- core-ui/src/components/Predefined/List/RoleBindings.list.js
+++ core-ui/src/components/Predefined/List/RoleBindings.list.js
@@ -1,13 +1,40 @@
 import React from 'react';
+import LuigiClient from '@luigi-project/client';
 import { useTranslation } from 'react-i18next';
+import { Link } from 'fundamental-react';
+
+const navigateToClusterRole = roleName =>
+  LuigiClient.linkManager()
+    .fromContext('cluster')
+    .navigate(`/clusterroles/details/${roleName}`);
+
+const navigateToRole = roleName =>
+  LuigiClient.linkManager()
+    .fromContext('namespace')
+    .navigate(`/roles/details/${roleName}`);
 
 export const RoleBindingsList = ({ DefaultRenderer, ...otherParams }) => {
   const { t } = useTranslation();
 
+  const navigate = (roleName, kind) => {
+    if (kind === 'ClusterRole') {
+      navigateToClusterRole(roleName);
+    } else {
+      navigateToRole(roleName);
+    }
+  };
+
   const customColumns = [
     {
       header: t('role-bindings.headers.role-name'),
-      value: binding => binding.roleRef.name,
+      value: binding => (
+        <Link
+          className="fd-link"
+          onClick={() => navigate(binding.roleRef.name, binding.roleRef.kind)}
+        >
+          {binding.roleRef.name}
+        </Link>
+      ),
     },
   ];
 
@@ -22,7 +49,14 @@ export const ClusterRoleBindingsList = ({
   const customColumns = [
     {
       header: t('role-bindings.headers.role-name'),
-      value: binding => binding.roleRef.name,
+      value: binding => (
+        <Link
+          className="fd-link"
+          onClick={() => navigateToClusterRole(binding.roleRef.name)}
+        >
+          {binding.roleRef.name}
+        </Link>
+      ),
     },
   ];
 
