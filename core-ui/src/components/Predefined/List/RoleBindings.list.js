058b345773fe6be1b4dd34d7e909bc4f288a2958
dariadomagala <daria.domagala@sap.com>
2021-10-22 12:57:39 +0200

    Simplify the code, fix navigation

--- core-ui/src/components/Predefined/List/RoleBindings.list.js
+++ core-ui/src/components/Predefined/List/RoleBindings.list.js
@@ -4,61 +4,61 @@ import { useTranslation } from 'react-i18next';
 import { Link } from 'fundamental-react';
 import { Tooltip } from 'react-shared';
 
-const navigateToClusterRole = roleName =>
-  LuigiClient.linkManager()
-    .fromContext('cluster')
-    .navigate(`/clusterroles/details/${roleName}`);
+const BindingsList = ({ DefaultRenderer, ...otherParams }) => {
+  const { t } = useTranslation();
 
-const navigateToRole = roleName =>
-  LuigiClient.linkManager()
-    .fromContext('namespace')
-    .navigate(`/roles/details/${roleName}`);
+  const navigateToRole = role => {
+    if (role.kind === 'ClusterRole') {
+      LuigiClient.linkManager()
+        .fromContext('cluster')
+        .navigate(`/clusterroles/details/${role.name}`);
+    } else {
+      LuigiClient.linkManager()
+        .fromContext('namespace')
+        .navigate(`/roles/details/${role.name}`);
+    }
+  };
 
-const navigate = (roleName, kind) => {
-  if (kind === 'ClusterRole') {
-    navigateToClusterRole(roleName);
-  } else {
-    navigateToRole(roleName);
-  }
-};
-
-const getSubject = subject => (
-  <div>
-    {subject.name}{' '}
-    <Tooltip content={subject.kind}>({subject.kind?.slice(0, 1)})</Tooltip>
-  </div>
-);
-
-const getSubjectWithLink = subject => (
-  <div>
-    <Link
-      className="fd-link"
-      onClick={() =>
-        LuigiClient.linkManager()
-          .fromContext('namespaces')
-          .navigate(`/serviceaccounts/details/${subject}`)
-      } //fix
-    >
-      {subject.name}
-    </Link>
-    <Tooltip content={subject.kind}> (SA)</Tooltip>
-  </div>
-);
-
-const getAllSubjects = binding => {
-  return binding.subjects?.map(s =>
-    s.kind === 'ServiceAccount' ? getSubjectWithLink(s) : getSubject(s),
+  const getSubject = subject => (
+    <div>
+      {subject.name}{' '}
+      <Tooltip content={subject.kind}>({subject.kind?.slice(0, 1)})</Tooltip>
+    </div>
   );
-};
 
-const getColumns = t => {
-  return [
+  const getSubjectWithLink = subject => (
+    <div>
+      <Link
+        className="fd-link"
+        onClick={() =>
+          LuigiClient.linkManager()
+            .fromContext('cluster')
+            .navigate(
+              `/namespaces/${subject.namespace}/serviceaccounts/details/${subject.name}`,
+            )
+        }
+      >
+        {subject.name}
+      </Link>
+      <Tooltip content={subject.kind}> (SA)</Tooltip>
+    </div>
+  );
+
+  const getAllSubjects = binding => {
+    return binding.subjects?.map(subject =>
+      subject.kind === 'ServiceAccount'
+        ? getSubjectWithLink(subject)
+        : getSubject(subject),
+    );
+  };
+
+  const customColumns = [
     {
       header: t('role-bindings.headers.role-name'),
       value: binding => (
         <Link
           className="fd-link"
-          onClick={() => navigate(binding.roleRef.name, binding.roleRef.kind)}
+          onClick={() => navigateToRole(binding.roleRef)}
         >
           {binding.roleRef.name}
         </Link>
@@ -69,22 +69,16 @@ const getColumns = t => {
       value: binding => getAllSubjects(binding),
     },
   ];
-};
 
-const textSearchProperties = [
-  'roleRef.name',
-  (entry, query) => {
-    const matchingSubject = entry.subjects?.find(subject =>
-      subject.name.toLowerCase().includes(query.toLowerCase()),
-    );
-    return matchingSubject?.name || null;
-  },
-];
-
-export const RoleBindingsList = ({ DefaultRenderer, ...otherParams }) => {
-  const { t } = useTranslation();
-
-  const customColumns = getColumns(t);
+  const textSearchProperties = [
+    'roleRef.name',
+    (entry, query) => {
+      const matchingSubject = entry.subjects?.find(subject =>
+        subject.name.toLowerCase().includes(query.toLowerCase()),
+      );
+      return matchingSubject?.name || null;
+    },
+  ];
 
   return (
     <DefaultRenderer
@@ -93,21 +87,15 @@ export const RoleBindingsList = ({ DefaultRenderer, ...otherParams }) => {
       {...otherParams}
     />
   );
+};
+
+export const RoleBindingsList = ({ DefaultRenderer, ...otherParams }) => {
+  return <BindingsList DefaultRenderer={DefaultRenderer} {...otherParams} />;
 };
 
 export const ClusterRoleBindingsList = ({
   DefaultRenderer,
   ...otherParams
 }) => {
-  const { t } = useTranslation();
-
-  const customColumns = getColumns(t);
-
-  return (
-    <DefaultRenderer
-      customColumns={customColumns}
-      textSearchProperties={textSearchProperties}
-      {...otherParams}
-    />
-  );
+  return <BindingsList DefaultRenderer={DefaultRenderer} {...otherParams} />;
 };
