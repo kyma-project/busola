3b8f5f7e11f9fe3c389918fe398780edcddc8600
Wawrzyn321 <Wawrzyn321@gmail.com>
2021-10-25 10:13:52 +0200

    Add svc catalog

--- pages.sh
+++ pages.sh
@@ -5,9 +5,9 @@ npm run build --prefix=core
 echo 'Build core-ui'
 npm run build --prefix=core-ui
 echo 'Build service-catalog'
-# npm run build --prefix=service-catalog-ui
+npm run build --prefix=service-catalog-ui
 
 mkdir build
 cp -r core/src/* build
 cp -r core-ui/build build/core-ui
-# cp -r service-catalog-ui/build build/service-catalog-ui
+cp -r service-catalog-ui/build build/service-catalog-ui
