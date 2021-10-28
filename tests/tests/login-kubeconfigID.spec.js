7de5eedaa8eabd5eb1ac400bc851798a11417f5e
Wawrzyn321 <Wawrzyn321@gmail.com>
2021-10-28 08:24:23 +0200

    Add e2e test with custm domain

--- tests/tests/login-kubeconfigID.spec.js
+++ tests/tests/login-kubeconfigID.spec.js
@@ -20,6 +20,38 @@ context('Login - kubeconfigID', () => {
     });
   });
 
+  it('Uses custom kubeconfig URL', () => {
+    const customKubeconfigUrl = 'http://example.com/kubeconfig';
+    const requestData = {
+      method: 'GET',
+      url: '/assets/config/config.json*',
+    };
+    const configMock = JSON.stringify({
+      config: {
+        features: {
+          KUBECONFIG_ID: {
+            config: {
+              kubeconfigUrl: customKubeconfigUrl,
+            },
+          },
+        },
+      },
+    });
+    cy.intercept(requestData, configMock);
+
+    cy.wrap(loadKubeconfig()).then(kubeconfig => {
+      cy.intercept(
+        {
+          method: 'GET',
+          url: `${customKubeconfigUrl}/*`,
+        },
+        kubeconfig,
+      );
+      cy.visit(`${config.clusterAddress}/clusters?kubeconfigID=tests`);
+      cy.url().should('match', /namespaces$/);
+    });
+  });
+
   it('Gracefully fails on invalid input', () => {
     cy.intercept(
       {
