fe5708c7bf159f864d5bad4fa26c6deda1e56cf1
Agata Kucharska <agata.kucharska@sap.com>
2021-11-10 10:52:47 +0100

    Create replica set form

--- core-ui/src/shared/components/K8sResourceSelect.js
+++ core-ui/src/shared/components/K8sResourceSelect.js
@@ -98,6 +98,7 @@ export function K8sResourceSelect({
         })}
         id="k8s-resource-dropdown"
         ariaLabel={t('common.messages.choose', { value: resourceType })}
+        arrowLabel="Combobox input arrow"
         options={options}
         onChange={onChange}
         onSelectionChange={(_, selected) => onSelect(selected.text)}
