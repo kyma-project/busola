92c51b87e7d081bd7a352adc00864d30ed193893
Wawrzyn321 <Wawrzyn321@gmail.com>
2021-11-12 09:13:11 +0100

    Handle multiple AccessStrategies

--- core-ui/src/components/ApiRules/AccessStrategies/AccessStrategies.js
+++ core-ui/src/components/ApiRules/AccessStrategies/AccessStrategies.js
@@ -2,39 +2,30 @@ import React from 'react';
 import classNames from 'classnames';
 
 import { GenericList } from 'react-shared';
-import { InfoLabel, Icon } from 'fundamental-react';
+import { InfoLabel } from 'fundamental-react';
 import { useTranslation } from 'react-i18next';
 
-import accessStrategyTypes, { usesMethods } from '../accessStrategyTypes';
+import accessStrategyTypes from '../accessStrategyTypes';
 
 import { ACCESS_STRATEGIES_PANEL } from 'components/ApiRules/constants';
 
 import './AccessStrategies.scss';
 
-const headerRenderer = () => ['Path', 'Type', 'Methods'];
+const headerRenderer = () => ['Path', 'Types', 'Methods'];
 const textSearchProperties = ['path', 'accessStrategies', 'methods'];
 const rowRenderer = strategy => {
-  const selectedType = strategy.accessStrategies[0].handler;
   return [
     <span>{strategy.path}</span>,
-    <InfoLabel modifier="filled">
-      <Icon
-        ariaLabel={accessStrategyTypes[selectedType].displayName}
-        glyph={
-          selectedType === accessStrategyTypes.noop.value ||
-          selectedType === accessStrategyTypes.allow.value
-            ? 'unlocked'
-            : 'locked'
-        }
-        size="s"
-      />
-      {accessStrategyTypes[selectedType].displayName}
-    </InfoLabel>,
-    <ul
-      className={classNames('methods', {
-        'fd-hidden': !usesMethods(selectedType),
-      })}
-    >
+    <ul className="tokens">
+      {strategy.accessStrategies.map(ac => (
+        <li key={ac.handler}>
+          <InfoLabel modifier="filled">
+            {accessStrategyTypes[ac.handler].displayName}
+          </InfoLabel>
+        </li>
+      ))}
+    </ul>,
+    <ul className="tokens">
       {strategy.methods
         .sort()
         .reverse()
