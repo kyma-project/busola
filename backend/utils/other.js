18cdb7b3a3b890b732803311085312bf16946f81
Wojciech Sołtys <wojciech.soltys@sap.com>
2021-11-15 11:22:14 +0100

    Dummy change

--- backend/utils/other.js
+++ backend/utils/other.js
@@ -2,6 +2,7 @@ export function requestLogger(httpModule) {
   var original = httpModule.request;
   httpModule.request = function(options, callback) {
     console.log('Outgoing HTTP request with options', options);
+    console.log('Outgoing HTTP request with options', options);
     return original(options, callback);
   };
 }
