377c54f1155e0284f90aa37f818b177e37e29394
grego952 <grzegorz.karaluch@sap.com>
2021-11-18 16:24:50 +0100

    add defintion of namespace config items (#573)
    * add defintion of namespace config items
    
    * update deployment.yaml
    
    * Update core/src/i18n/en.yaml
    
    Co-authored-by: Nina Hingerl <76950046+NHingerl@users.noreply.github.com>
    
    * Update core/src/i18n/en.yaml
    
    Co-authored-by: Klaudia Grzondziel <35192450+klaudiagrz@users.noreply.github.com>
    
    * Update core/src/i18n/en.yaml
    
    Co-authored-by: Nina Hingerl <76950046+NHingerl@users.noreply.github.com>
    
    * Update core/src/i18n/en.yaml
    
    Co-authored-by: Nina Hingerl <76950046+NHingerl@users.noreply.github.com>
    
    * Update core/src/i18n/en.yaml
    
    Co-authored-by: Nina Hingerl <76950046+NHingerl@users.noreply.github.com>
    
    * Update core/src/i18n/en.yaml
    
    Co-authored-by: Małgorzata Świeca <mitorajmalgorzata@gmail.com>
    
    * update certificate defintion
    
    * correct links for Config Map and Secret
    
    Co-authored-by: Nina Hingerl <76950046+NHingerl@users.noreply.github.com>
    Co-authored-by: Klaudia Grzondziel <35192450+klaudiagrz@users.noreply.github.com>
    Co-authored-by: Małgorzata Świeca <mitorajmalgorzata@gmail.com>

--- core-ui/src/components/Predefined/List/DNSProviders.list.js
+++ core-ui/src/components/Predefined/List/DNSProviders.list.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StatusBadge } from 'react-shared';
 import { useTranslation } from 'react-i18next';
+import { Link } from 'react-shared';
+import { Trans } from 'react-i18next';
 
 export const DNSProvidersList = ({ DefaultRenderer, ...otherParams }) => {
   const { t } = useTranslation();
@@ -21,9 +23,19 @@ export const DNSProvidersList = ({ DefaultRenderer, ...otherParams }) => {
     },
   ];
 
+  const description = (
+    <Trans i18nKey="dnsproviders.description">
+      <Link
+        className="fd-link"
+        url="https://kyma-project.io/docs/kyma/latest/03-tutorials/00-api-exposure/apix-01-own-domain"
+      />
+    </Trans>
+  );
+
   return (
     <DefaultRenderer
       customColumns={customColumns}
+      description={description}
       resourceName="DNS Providers"
       {...otherParams}
     />
--- core-ui/src/components/Predefined/List/OAuth2Clients.list.js
+++ core-ui/src/components/Predefined/List/OAuth2Clients.list.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { OAuth2ClientStatus } from 'shared/components/OAuth2ClientStatus/OAuth2ClientStatus';
 import { useTranslation } from 'react-i18next';
+import { Link } from 'react-shared';
+import { Trans } from 'react-i18next';
 
 export const OAuth2ClientsList = ({ DefaultRenderer, ...otherParams }) => {
   const { t } = useTranslation();
@@ -17,9 +19,19 @@ export const OAuth2ClientsList = ({ DefaultRenderer, ...otherParams }) => {
     },
   ];
 
+  const description = (
+    <Trans i18nKey="oauth2-clients.description">
+      <Link
+        className="fd-link"
+        url="https://kyma-project.io/docs/kyma/latest/03-tutorials/00-api-exposure/apix-03-expose-and-secure-service#register-an-o-auth2-client-and-get-tokens"
+      />
+    </Trans>
+  );
+
   return (
     <DefaultRenderer
       customColumns={customColumns}
+      description={description}
       resourceName={t('oauth2-clients.title')}
       {...otherParams}
     />
