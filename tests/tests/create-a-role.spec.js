1be12e99a3f8e0052a977ae325785bb92759ab97
dariadomagala <daria.domagala@sap.com>
2021-11-18 16:51:02 +0100

    Add a test for role creation

--- tests/tests/create-a-role.spec.js
+++ tests/tests/create-a-role.spec.js
@@ -2,6 +2,8 @@
 import 'cypress-file-upload';
 
 const ROLE_NAME = 'test-role';
+const API_GROUP = '(core)';
+const RESOURCE = 'namespaces';
 
 context('Create a Role', () => {
   before(() => {
@@ -10,6 +12,10 @@ context('Create a Role', () => {
   });
 
   it('Create a Role', () => {
+    cy.getLeftNav()
+      .contains('Configuration')
+      .click();
+
     cy.getLeftNav()
       .find('[data-testid=roles_roles]')
       .click();
@@ -21,5 +27,55 @@ context('Create a Role', () => {
     cy.getIframeBody()
       .contains('Create Role')
       .click();
+
+    cy.getIframeBody()
+      .find('[placeholder="Role Name"]')
+      .filter(':visible', { log: false })
+      .type(ROLE_NAME)
+      .click();
+
+    cy.getIframeBody()
+      .find('[placeholder="Start typing to select API Groups from the list."]')
+      .filter(':visible', { log: false })
+      .type(API_GROUP)
+      .click();
+
+    cy.getIframeBody()
+      .find('[placeholder="Start typing to select Resources from the list."]')
+      .filter(':visible', { log: false })
+      .type(RESOURCE)
+      .click();
+
+    cy.getIframeBody()
+      .find('[placeholder="Start typing to select Verbs from the list."]')
+      .filter(':visible', { log: false })
+      .type('get')
+      .click();
+
+    cy.getIframeBody()
+      .find('[placeholder="Start typing to select Verbs from the list."]')
+      .filter(':visible', { log: false })
+      .eq(1)
+      .type('post')
+      .click();
+
+    cy.getIframeBody()
+      .contains('button', /^Create$/)
+      .click();
+  });
+  it('Check the Role details', () => {
+    cy.getIframeBody()
+      .contains('h3', ROLE_NAME, { timeout: 7000 })
+      .should('be.visible');
+
+    cy.getIframeBody()
+      .find('td')
+      .eq(0)
+      .should('not.have.text', '-');
+
+    cy.getIframeBody()
+      .find('td')
+      .eq(2)
+      .should('have.text', '-');
   });
 });
