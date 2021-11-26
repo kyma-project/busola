db5bc8a8802ba80a048674436fc603bba175e8ec
mwisnia97 <mwisnia97@gmail.com>
2021-11-26 10:52:39 +0100

    Added tests

---
+++ tests/tests/test-service-accounts.spec.js
@@ -0,0 +1,128 @@
+/// <reference types="cypress" />
+
+const SERVICE_NAME = 'test-sa-name';
+
+context('Test Service Accounts', () => {
+  before(() => {
+    cy.loginAndSelectCluster();
+    cy.goToNamespaceDetails();
+  });
+
+  it('Navigate to Service Accounts', () => {
+    cy.getLeftNav()
+      .contains('Configuration')
+      .click();
+
+    cy.getLeftNav()
+      .contains('Service Accounts')
+      .click();
+  });
+
+  it('Create a Client', () => {
+    cy.getIframeBody()
+      .contains('Create Service Account')
+      .click();
+
+    cy.getIframeBody()
+      .contains('Advanced')
+      .click();
+
+    cy.getIframeBody()
+      .find('[placeholder="Service Account Name"]')
+      .clear()
+      .type(SERVICE_NAME);
+
+    cy.getIframeBody()
+      .contains('Image Pull Secrets')
+      .click();
+
+    cy.getIframeBody()
+      .find(
+        '[placeholder="Start typing to select Image Pull Secrets from the list."]',
+      )
+      .clear()
+      .type('default');
+
+    cy.getIframeBody()
+      .contains('default-token')
+      .click();
+
+    cy.getIframeBody()
+      .find('[role="presentation"]')
+      .click();
+
+    cy.getIframeBody()
+      .find('[role="dialog"]')
+      .contains('button', 'Create')
+      .click();
+  });
+
+  it('Checking details', () => {
+    cy.getIframeBody()
+      .contains(SERVICE_NAME)
+      .should('be.visible');
+
+    cy.getIframeBody()
+      .contains(`${SERVICE_NAME}-token`)
+      .should('be.visible');
+
+    cy.getIframeBody()
+      .contains('default-token')
+      .should('be.visible');
+
+    cy.getIframeBody()
+      .contains('ENABLED')
+      .should('be.visible');
+  });
+
+  it('Edit', () => {
+    cy.getIframeBody()
+      .contains('Edit')
+      .click();
+
+    cy.getIframeBody()
+      .contains('Advanced')
+      .click();
+
+    cy.getIframeBody()
+      .find('[role="document"]')
+      .contains('Labels')
+      .click();
+
+    cy.getIframeBody()
+      .find('[placeholder="Enter Key"]')
+      .type('test.key');
+
+    cy.getIframeBody()
+      .find('[index="1"]')
+      .clear()
+      .type('test-value');
+
+    cy.getIframeBody()
+      .find('[role="presentation"]')
+      .click();
+
+    cy.getIframeBody()
+      .find('[role="dialog"]')
+      .contains('button', 'Update')
+      .click();
+  });
+
+  it('Checking updated details', () => {
+    cy.getIframeBody()
+      .contains('DISABLED')
+      .should('be.visible');
+
+    cy.getIframeBody()
+      .contains('test.key=test-value')
+      .should('be.visible');
+  });
+
+  it('Delete Service Account', () => {
+    cy.getIframeBody()
+      .contains('button', 'Delete')
+      .click();
+
+    cy.get('[data-testid=luigi-modal-confirm]').click();
+  });
+});
