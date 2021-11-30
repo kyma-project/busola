a64e93bab20931082176b1b9aa740db98303b12c
wchrapka <wojciech.chrapka@connect.is>
2021-11-30 17:41:00 +0100

    fix dns entries test

--- tests/tests/test-dns-entries.spec.js
+++ tests/tests/test-dns-entries.spec.js
@@ -47,6 +47,10 @@ context('Create a DNS Entry', () => {
       .type(DNS_NAME)
       .click();
 
+    cy.getIframeBody()
+      .contains('Advanced')
+      .click();
+
     // target
     cy.getIframeBody()
       .find('[aria-label^="Enter the A record"]:visible')
