5811b857fb59c2ab74c5bdd8efd032aa8b672d2f
Jakub Łukomski <dev@tenku.eu>
2021-12-01 11:24:18 +0100

    update tests for new i18n

--- tests/tests/test-protected-resources.js
+++ tests/tests/test-protected-resources.js
@@ -100,7 +100,10 @@ context('Test Protected Resources', () => {
       .click();
 
     cy.getModalIframeBody()
-      .contains('.preferences-row', 'Allow modification of protected resources')
+      .contains(
+        '.preferences-row',
+        'Allow for modification of protected resources',
+      )
       .find('.fd-switch')
       .click();
 
