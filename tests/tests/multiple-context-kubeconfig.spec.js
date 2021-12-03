508a20a8328a4f6b3f245ef4665b0843d24da625
dariadomagala <daria.domagala@sap.com>
2021-12-03 10:08:52 +0100

    Select second context from the list

--- tests/tests/multiple-context-kubeconfig.spec.js
+++ tests/tests/multiple-context-kubeconfig.spec.js
@@ -4,38 +4,60 @@ import config from '../config';
 import { loadMultipleContextKubeconfig } from '../support/loadKubeconfigFile';
 import jsyaml from 'js-yaml';
 
-it('User can add cluster via kubeconfig ID + does not display "Add cluster on overview', () => {
-  cy.wrap(loadMultipleContextKubeconfig()).then(kubeconfig => {
-    console.log(kubeconfig);
+context('Multiple context kubeconfig', () => {
+  it('User can choose different context with the multiple context kubeconfig', () => {
+    cy.wrap(loadMultipleContextKubeconfig()).then(kubeconfig => {
+      console.log(kubeconfig);
 
-    cy.visit(`${config.clusterAddress}/clusters`);
+      cy.visit(`${config.clusterAddress}/clusters`);
 
-    cy.getIframeBody()
-      .contains('Add a Cluster')
-      .click();
+      cy.getIframeBody()
+        .contains('Add a Cluster')
+        .click();
 
-    cy.getIframeBody()
-      .contains('Drag file here')
-      .attachFile(
-        {
-          fileContent: jsyaml.dump(kubeconfig),
-          filePath: 'kubeconfig.yaml',
-        },
-        {
-          subjectType: 'drag-n-drop',
-        },
-      );
+      cy.getIframeBody()
+        .contains('Drag file here')
+        .attachFile(
+          {
+            fileContent: jsyaml.dump(kubeconfig),
+            filePath: 'kubeconfig.yaml',
+          },
+          {
+            subjectType: 'drag-n-drop',
+          },
+        );
 
-    cy.getIframeBody()
-      .contains('Next')
-      .click();
+      cy.getIframeBody()
+        .contains('Next')
+        .click();
 
-    cy.getIframeBody()
-      .contains('Next')
-      .click();
+      cy.getIframeBody()
+        .find('[role=combobox]')
+        .within(() => {
+          cy.contains(kubeconfig['current-context']).click();
+        });
 
-    cy.getIframeBody()
-      .contains('Add Cluster')
-      .click();
+      cy.getIframeBody()
+        .find('[role=option]')
+        .within(() => {
+          cy.contains(kubeconfig.contexts[1].name).click();
+        });
+
+      cy.getIframeBody()
+        .contains('Next')
+        .click();
+
+      cy.getIframeBody()
+        .contains('Add Cluster')
+        .click();
+
+      cy.getIframeBody()
+        .contains('Namespaces')
+        .should('exist');
+
+      cy.get('[data-testid=luigi-topnav-title]')
+        .contains(kubeconfig.contexts[1].context.cluster)
+        .should('exist');
+    });
   });
 });
