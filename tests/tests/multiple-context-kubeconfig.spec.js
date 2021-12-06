d5bd39403be592096a47cf47690462148e7797a7
Daria Domagała <40663133+dariadomagala@users.noreply.github.com>
2021-12-06 09:49:24 +0100

    Test multiple contexts kubeconfig (#652)
    * Fix form validation
    
    * Fist multiple context test
    
    * Bump deployment
    
    * Select second context from the list
    
    * Update tests/tests/multiple-context-kubeconfig.spec.js
    
    Co-authored-by: Piotr Wawrzyńczyk <Wawrzyn321@gmail.com>
    
    * Fix linter
    
    Co-authored-by: Jakub Łukomski <dev@tenku.eu>
    Co-authored-by: Piotr Wawrzyńczyk <Wawrzyn321@gmail.com>

---
+++ tests/tests/multiple-context-kubeconfig.spec.js
@@ -0,0 +1,61 @@
+/// <reference types="cypress" />
+import 'cypress-file-upload';
+import config from '../config';
+import { loadMultipleContextKubeconfig } from '../support/loadKubeconfigFile';
+import jsyaml from 'js-yaml';
+
+context('Multiple context kubeconfig', () => {
+  it('User can choose different context with the multiple context kubeconfig', () => {
+    cy.wrap(loadMultipleContextKubeconfig()).then(kubeconfig => {
+      cy.visit(`${config.clusterAddress}/clusters`);
+
+      cy.getIframeBody()
+        .contains('Add a Cluster')
+        .click();
+
+      cy.getIframeBody()
+        .contains('Drag file here')
+        .attachFile(
+          {
+            fileContent: jsyaml.dump(kubeconfig),
+            filePath: 'kubeconfig.yaml',
+          },
+          {
+            subjectType: 'drag-n-drop',
+          },
+        );
+
+      cy.getIframeBody()
+        .contains('Next')
+        .click();
+
+      cy.getIframeBody()
+        .find('[role=combobox]')
+        .within(() => {
+          cy.contains(kubeconfig['current-context']).click();
+        });
+
+      cy.getIframeBody()
+        .find('[role=option]')
+        .within(() => {
+          cy.contains(kubeconfig.contexts[1].name).click();
+        });
+
+      cy.getIframeBody()
+        .contains('Next')
+        .click();
+
+      cy.getIframeBody()
+        .contains('Add Cluster')
+        .click();
+
+      cy.getIframeBody()
+        .contains('Namespaces')
+        .should('exist');
+
+      cy.get('[data-testid=luigi-topnav-title]')
+        .contains(kubeconfig.contexts[1].context.cluster)
+        .should('exist');
+    });
+  });
+});
