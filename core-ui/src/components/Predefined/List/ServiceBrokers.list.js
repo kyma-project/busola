f291d6c9e5678e3afae88b3abef9b4910d251e56
Wawrzyn321 <Wawrzyn321@gmail.com>
2021-12-07 14:40:53 +0100

    Dedupe translations

--- core-ui/src/components/Predefined/List/ServiceBrokers.list.js
+++ core-ui/src/components/Predefined/List/ServiceBrokers.list.js
@@ -21,7 +21,6 @@ export const ServiceBrokersList = ({ DefaultRenderer, ...otherParams }) => {
             autoResolveType
             additionalContent={lastCondition?.message}
             i18n={i18n}
-            resourceKind="brokers"
           >
             {lastCondition?.type || status.lastConditionState}
           </StatusBadge>
