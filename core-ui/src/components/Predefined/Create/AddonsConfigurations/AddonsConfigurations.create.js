5906fb1ee84000b954fb957233d8bbe73c26eead
Jakub Lyczko <jakub.lyczko1@gmail.com>
2021-12-08 11:39:24 +0100

    Fix placeholders names in create addons configurations modals

--- core-ui/src/components/Predefined/Create/AddonsConfigurations/AddonsConfigurations.create.js
+++ core-ui/src/components/Predefined/Create/AddonsConfigurations/AddonsConfigurations.create.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import { AddonsConfigurations } from './AddonsConfigurations.js';
+import { useTranslation } from 'react-i18next';
 
-export const AddonsConfigurationsCreate = ({ resourceType, ...props }) => (
-  <AddonsConfigurations resourceType={resourceType} {...props} />
-);
+export const AddonsConfigurationsCreate = ({ resourceType, ...props }) => {
+  const { t } = useTranslation();
+
+  return (
+    <AddonsConfigurations
+      resourceType={resourceType}
+      kind={t('addons.configuration')}
+      {...props}
+    />
+  );
+};
 
 export const ClusterAddonsConfigurationsCreate = ({
   resourceType,
   ...props
-}) => <AddonsConfigurations resourceType={resourceType} {...props} />;
+}) => {
+  const { t } = useTranslation();
+
+  return (
+    <AddonsConfigurations
+      resourceType={resourceType}
+      kind={t('cluster-addons.configuration')}
+      {...props}
+    />
+  );
+};
