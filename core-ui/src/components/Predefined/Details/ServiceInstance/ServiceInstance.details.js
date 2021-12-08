a2dbcde47a9444a3dd99dbd9e7b4718eb6a881de
Piotr Wawrzyńczyk <Wawrzyn321@gmail.com>
2021-12-08 09:29:25 +0100

    Migrate service catalog and service management views to new translatable labels (#674)
    * Handle brokers
    
    * Bump
    
    * Handle BTP stuff
    
    * Handle Catalog
    
    * Dedupe BTP bindings
    
    * Dedupe translations
    
    * Update core/src/i18n/en.yaml
    
    Co-authored-by: Aleksandra Simeonova <aleksandra.simeonova@sap.com>
    
    * Update core/src/i18n/en.yaml
    
    Co-authored-by: Aleksandra Simeonova <aleksandra.simeonova@sap.com>
    
    Co-authored-by: Aleksandra Simeonova <aleksandra.simeonova@sap.com>

--- core-ui/src/components/Predefined/Details/ServiceInstance/ServiceInstance.details.js
+++ core-ui/src/components/Predefined/Details/ServiceInstance/ServiceInstance.details.js
@@ -27,7 +27,12 @@ export const ServiceInstancesDetails = ({
   const customColumns = [
     {
       header: t('common.headers.status'),
-      value: resource => <BTPResourceStatus status={resource.status} />,
+      value: resource => (
+        <BTPResourceStatus
+          status={resource.status}
+          resourceKind="btp-instances"
+        />
+      ),
     },
   ];
 
