47e68598102f019638b9ab4e3a2a3b181351e21a
dariadomagala <daria.domagala@sap.com>
2021-12-13 11:41:09 +0100

    Fix lint

--- core-ui/src/components/Predefined/List/Deployments.list.js
+++ core-ui/src/components/Predefined/List/Deployments.list.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import { ControlledByKind, StatusBadge } from 'react-shared';
-import { useTranslation } from 'react-i18next';
-import { Link } from 'react-shared';
-import { Trans } from 'react-i18next';
-import { DeploymentsCreate } from '../Create';
+import { ControlledByKind, Link } from 'react-shared';
+import { useTranslation, Trans } from 'react-i18next';
 import { DeploymentStatus } from '../Details/Deployment/DeploymentStatus';
 
 const getImages = deployment => {
@@ -14,25 +11,8 @@ const getImages = deployment => {
   return images;
 };
 
-const isStatusOk = deployment => {
-  return deployment.status.readyReplicas === deployment.status.replicas;
-};
-
-const getStatus = deployment => {
-  return isStatusOk(deployment) ? 'running' : 'error';
-};
-
-const getStatusType = deployment => {
-  return isStatusOk(deployment) ? 'success' : 'error';
-};
-
-const getPodsCount = deployment => {
-  return `${deployment.status.readyReplicas || 0} / ${deployment.status
-    .replicas || 0}`;
-};
-
 export const DeploymentsList = ({ DefaultRenderer, ...otherParams }) => {
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
 
   const customColumns = [
     {
