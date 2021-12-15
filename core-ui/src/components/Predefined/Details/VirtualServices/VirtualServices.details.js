92da863a7b37b2e0befc3e733f4dec416e96647f
Jakub Łukomski <dev@tenku.eu>
2021-12-15 08:02:56 +0100

    clean up code

--- core-ui/src/components/Predefined/Details/VirtualServices/VirtualServices.details.js
+++ core-ui/src/components/Predefined/Details/VirtualServices/VirtualServices.details.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { useTranslation } from 'react-i18next';
 
 import { ServiceGateways } from './ServiceGateways';
 import { ServiceHosts } from './ServiceHosts';
@@ -8,16 +7,8 @@ import { TlsRoutes } from './TlsRoutes';
 import { TcpRoutes } from './TcpRoutes';
 
 export function VirtualServicesDetails({ DefaultRenderer, ...otherParams }) {
-  const customColumns = [
-    // {
-    // // header: t('gateways.selector'),
-    // // value: gateway => <GatewaySelector key="selector" gateway={gateway} />,
-    // },
-  ];
-
   return (
     <DefaultRenderer
-      customColumns={customColumns}
       customComponents={[
         ServiceGateways,
         ServiceHosts,
