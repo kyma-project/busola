149847157115d6ac288e54c8b51694735d82b39c
Jakub Lyczko <jakub.lyczko1@gmail.com>
2021-12-16 13:48:02 +0100

    remove needless props

--- core-ui/src/components/Predefined/Create/EventSubscriptions/EventSubscriptions.create.js
+++ core-ui/src/components/Predefined/Create/EventSubscriptions/EventSubscriptions.create.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { useTranslation } from 'react-i18next';
 import { createEventSubscriptionTemplate } from './templates';
 
 import { ResourceForm } from 'shared/ResourceForm';
@@ -9,24 +8,19 @@ const SubscriptionsCreate = ({
   onChange,
   formElementRef,
   namespace,
-  resource: initialEventSubscription,
   resourceUrl,
 }) => {
-  const t = useTranslation();
-
   const [eventSubscription, setEventSubscription] = useState(
     createEventSubscriptionTemplate(namespace),
   );
 
   return (
     <ResourceForm
-      pluralKind="subscriptions"
+      pluralKind="eventsubscriptions"
       resource={eventSubscription}
-      singularName="eventsubscription"
       setResource={setEventSubscription}
       onChange={onChange}
       formElementRef={formElementRef}
-      initialResource={initialEventSubscription}
       createUrl={resourceUrl}
       onlyYaml
     />
