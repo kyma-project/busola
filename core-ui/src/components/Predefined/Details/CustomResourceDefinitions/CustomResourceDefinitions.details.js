7d52960d202aca38ec37aeb840991f167e571abc
wchrapka <wojciech.chrapka@connect.is>
2021-12-21 16:27:50 +0100

    Add form to create any CR based on the definition

--- core-ui/src/components/Predefined/Details/CustomResourceDefinitions/CustomResourceDefinitions.details.js
+++ core-ui/src/components/Predefined/Details/CustomResourceDefinitions/CustomResourceDefinitions.details.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CustomResourceDefinitionVersions } from './CustomResourceDefinitionVersions';
-
-import { GenericList, EMPTY_TEXT_PLACEHOLDER } from 'react-shared';
 import { useTranslation } from 'react-i18next';
+import {
+  GenericList,
+  EMPTY_TEXT_PLACEHOLDER,
+  ModalWithForm,
+} from 'react-shared';
+import { Button } from 'fundamental-react';
+
+import { CRCreate } from './CRCreate';
 
 export const CustomResourceDefinitionsDetails = ({
   DefaultRenderer,
@@ -44,11 +50,41 @@ export const CustomResourceDefinitionsDetails = ({
     );
   };
 
+  const [showEditDialog, setShowEditDialog] = useState(false);
+
   return (
     <DefaultRenderer
       customColumns={customColumns}
       customComponents={[ResourceNames, CustomResourceDefinitionVersions]}
+      resourceHeaderActions={[
+        crd => {
+          return (
+            <>
+              <ModalWithForm
+                title={t('components.resources-list.create', {
+                  resourceType: crd.spec.names.kind,
+                })}
+                opened={showEditDialog}
+                confirmText={t('common.buttons.create')}
+                id={`add-${crd.spec.names.kind}-modal`}
+                className="modal-size--l create-resource-modal"
+                renderForm={props => <CRCreate crd={crd} {...props} />}
+                i18n={i18n}
+                modalOpeningComponent={<></>}
+                customCloseAction={() => setShowEditDialog(false)}
+              />
+              <Button
+                option="default"
+                onClick={() => setShowEditDialog(true)}
+                className="fd-margin-end--tiny"
+              >
+                {t('common.buttons.create')} {crd.spec.names.kind}
+              </Button>
+            </>
+          );
+        },
+      ]}
       {...otherParams}
-    ></DefaultRenderer>
+    />
   );
 };
