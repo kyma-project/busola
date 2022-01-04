3e62e2baf97fb24a84630587a4400010f73bbde2
dariadomagala <daria.domagala@sap.com>
2022-01-04 07:47:10 +0100

    Fix

--- core/src/luigi-config/nav-footer.js
+++ core/src/luigi-config/nav-footer.js
@@ -3,17 +3,12 @@ import { getBusolaClusterParams } from './busola-cluster-params';
 
 function elementReady(selector) {
   return new Promise(resolve => {
-    let el = document.querySelector(selector);
-    if (el) {
-      resolve(el);
-      return;
-    }
     new MutationObserver((_, observer) => {
-      // Query for elements matching the specified selector
-      Array.from(document.querySelectorAll(selector)).forEach(element => {
+      const element = document.querySelector(selector);
+      if (element) {
         resolve(element);
         observer.disconnect();
-      });
+      }
     }).observe(document.documentElement, {
       childList: true,
       subtree: true,
@@ -23,27 +18,27 @@ function elementReady(selector) {
 
 // setting sideNavFooterText of Luigi settings won't allow HTML
 export async function setNavFooterText() {
-  const targetElement = await elementReady('.lui-side-nav__footer--text');
+  elementReady('.lui-side-nav__footer--text').then(async targetElement => {
+    const language = i18next.language;
+    const version = await getBusolaVersion();
+    const feature = (await getBusolaClusterParams()).config?.features
+      ?.LEGAL_LINKS;
 
-  const language = i18next.language;
-  const version = await getBusolaVersion();
-  const feature = (await getBusolaClusterParams()).config?.features
-    ?.LEGAL_LINKS;
+    const versionText = i18next.t('common.labels.version');
+    const versionLink = checkVersionLink(version);
 
-  const versionText = i18next.t('common.labels.version');
-  const versionLink = checkVersionLink(version);
-
-  targetElement.innerHTML = `
-    <ul>
-      ${Object.entries(feature?.config || {})
-        .map(([key, value]) => {
-          const text = i18next.t('legal.' + key);
-          const link = value[language] || value.default;
-          return `<li><a href="${link}" target="_blank" rel="noopener noreferrer">${text}</a></li>`;
-        })
-        .join('')}
-    </ul>
-    <p>${versionText}:</p><a href="${versionLink}" target="_blank" rel="noopener noreferrer" data-test-id="version-link">${version}</a>`;
+    targetElement.innerHTML = `
+      <ul>
+        ${Object.entries(feature?.config || {})
+          .map(([key, value]) => {
+            const text = i18next.t('legal.' + key);
+            const link = value[language] || value.default;
+            return `<li><a href="${link}" target="_blank" rel="noopener noreferrer">${text}</a></li>`;
+          })
+          .join('')}
+      </ul>
+      <p>${versionText}:</p><a href="${versionLink}" target="_blank" rel="noopener noreferrer" data-test-id="version-link">${version}</a>`;
+  });
 }
 
 async function getBusolaVersion() {
