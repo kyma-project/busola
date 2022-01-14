35bcfcf1ae316c4bd4bad4551063cb42a6b3be9d
wchrapka <wojciech.chrapka@sap.com>
2022-01-14 16:19:07 +0100

    webpack config test

--- core-ui/config-overrides.js
+++ core-ui/config-overrides.js
@@ -2,11 +2,11 @@ module.exports = function(config, env) {
   if (env === 'development') {
     config.output.filename = 'static/js/[name]bundle.js';
   }
-  //
-  // config.module.rules.push({
-  //   test: /\.worker\.js$/,
-  //   use: { loader: 'worker-loader' },
-  // });
+
+  config.module.rules.unshift({
+    test: /\.worker\.js$/,
+    use: { loader: 'worker-loader' },
+  });
 
   return config;
 };
