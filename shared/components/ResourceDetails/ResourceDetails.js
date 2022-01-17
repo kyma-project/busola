f7df8dafdc9701c6056288e69d479b20c85e27ec
mwisnia97 <mwisnia97@gmail.com>
2022-01-17 14:23:57 +0100

    Changed delete modal

--- shared/components/ResourceDetails/ResourceDetails.js
+++ shared/components/ResourceDetails/ResourceDetails.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import jsyaml from 'js-yaml';
-import { Button } from 'fundamental-react';
 import { useTranslation } from 'react-i18next';
+import { Button, Checkbox, MessageBox, MessageStrip } from 'fundamental-react';
 import { createPatch } from 'rfc6902';
+import LuigiClient from '@luigi-project/client';
 
 import {
   PageHeader,
@@ -23,8 +24,11 @@ import {
   Spinner,
 } from '../..';
 import CustomPropTypes from '../../typechecking/CustomPropTypes';
-import { handleDelete } from '../GenericList/actionHandlers/simpleDelete';
-import { useWindowTitle, useProtectedResources } from '../../hooks';
+import {
+  useWindowTitle,
+  useProtectedResources,
+  useFeatureToggle,
+} from '../../hooks';
 import { ModalWithForm } from '../ModalWithForm/ModalWithForm';
 
 ResourceDetails.propTypes = {
@@ -33,7 +37,7 @@ ResourceDetails.propTypes = {
   customComponents: PropTypes.arrayOf(PropTypes.func),
   resourceUrl: PropTypes.string.isRequired,
   resourceType: PropTypes.string.isRequired,
-  resourceName: PropTypes.string.isRequired,
+  resourceName: PropTypes.string,
   namespace: PropTypes.string,
   headerActions: PropTypes.node,
   resourceHeaderActions: PropTypes.arrayOf(PropTypes.func),
@@ -120,7 +124,6 @@ function Resource({
   createResourceForm: CreateResourceForm,
   customColumns,
   customComponents,
-  deleteResourceMutation,
   editActionLabel,
   headerActions,
   i18n,
@@ -128,7 +131,6 @@ function Resource({
   readOnly,
   resource,
   resourceHeaderActions,
-  resourceName,
   resourceType,
   resourceUrl,
   silentRefetch,
@@ -136,12 +138,20 @@ function Resource({
   windowTitle,
   resourceTitle,
 }) {
+  const { t } = useTranslation(['translation'], { i18n });
   useWindowTitle(
     windowTitle || resourceTitle || prettifyNamePlural(null, resourceType),
   );
   const { isProtected, protectedResourceWarning } = useProtectedResources(i18n);
 
-  const { t } = useTranslation(['translation'], { i18n });
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+  const [activeResource, setActiveResource] = useState(null);
+  const deleteResourceMutation = useDelete(resourceUrl);
+  const [dontConfirmDelete, setDontConfirmDelete] = useFeatureToggle(
+    'dontConfirmDelete',
+  );
+  const withoutQueryString = path => path.split('?')[0];
+
   const { setEditedYaml: setEditedSpec } = useYamlEditor();
   const notification = useNotification();
 
@@ -222,7 +232,7 @@ function Resource({
       {resourceHeaderActions.map(resourceAction => resourceAction(resource))}
       <Button
         disabled={protectedResource}
-        onClick={handleResourceDelete}
+        onClick={() => handleResourceDelete(resource)}
         option="transparent"
         type="negative"
       >
@@ -263,16 +273,46 @@ function Resource({
     }
   };
 
-  async function handleResourceDelete() {
-    return handleDelete(
-      resourceTitle || resourceType,
-      null,
-      resourceName,
-      notification,
-      () => deleteResourceMutation(resourceUrl),
-      () => navigateToList(resourceType),
-      t,
-    );
+  const performDelete = async resource => {
+    const url = withoutQueryString(resourceUrl);
+
+    LuigiClient.sendCustomMessage({
+      id: 'busola.dontConfirmDelete',
+      value: dontConfirmDelete,
+    });
+    try {
+      await deleteResourceMutation(url);
+      notification.notifySuccess({
+        content: t('components.resources-list.messages.delete.success', {
+          resourceType: prettifiedResourceName,
+        }),
+      });
+      navigateToList(resourceType);
+    } catch (e) {
+      console.error(e);
+      notification.notifyError({
+        content: t('components.resources-list.messages.delete.failure', {
+          resourceType: prettifiedResourceName,
+          error: e.message,
+        }),
+      });
+      throw e;
+    }
+  };
+
+  const closeDeleteDialog = () => {
+    LuigiClient.uxManager().removeBackdrop();
+    setShowDeleteDialog(false);
+  };
+
+  async function handleResourceDelete(resource) {
+    if (dontConfirmDelete) {
+      performDelete(resource);
+    } else {
+      LuigiClient.uxManager().addBackdrop();
+      setActiveResource(resource);
+      setShowDeleteDialog(true);
+    }
   }
 
   return (
@@ -302,6 +342,44 @@ function Resource({
           </PageHeader.Column>
         ))}
       </PageHeader>
+      <MessageBox
+        type="warning"
+        title={t('common.delete-dialog.title', {
+          name: activeResource?.metadata.name,
+        })}
+        actions={[
+          <Button
+            data-testid="delete-confirmation"
+            type="negative"
+            compact
+            onClick={() => performDelete(activeResource)}
+          >
+            {t('common.buttons.delete')}
+          </Button>,
+          <Button onClick={() => setDontConfirmDelete(false)} compact>
+            {t('common.buttons.cancel')}
+          </Button>,
+        ]}
+        show={showDeleteDialog}
+        onClose={closeDeleteDialog}
+      >
+        <p>
+          {t('common.delete-dialog.message', {
+            type: prettifiedResourceName,
+            name: activeResource?.metadata.name,
+          })}
+        </p>
+        <div className="fd-margin-top--sm">
+          <Checkbox onChange={e => setDontConfirmDelete(e.target.checked)}>
+            {t('common.delete-dialog.delete-confirm')}
+          </Checkbox>
+        </div>
+        {dontConfirmDelete && (
+          <MessageStrip type="information" className="fd-margin-top--sm">
+            {t('common.delete-dialog.information')}
+          </MessageStrip>
+        )}
+      </MessageBox>
 
       {customComponents.map(component => component(resource, resourceUrl))}
 
