83ad66e20b6a1be5b45f634a4a6d9cd1d35dac4b
Jakub Lyczko <jakub.lyczko@sap.com>
2022-01-18 11:50:04 +0100

    Introduce Persistent Volume Claims (#808)
    * remove needless resourcetile
    
    * Add PVC to the navigation, add creation functionality, add basic translations
    
    * Add pvc list that includes storage class and status info
    
    * Add capacity column
    
    * Add basic information to the details view
    
    * Add storage tab in navigation, modify status colours
    
    * change icon
    
    * Add pods to the details view and small improvements in ui
    
    * Add deep linking and improve details view
    
    * Remove needless information when no init containers
    
    * Add related pods list and improve displaying match labels and expressions
    
    * add tests
    
    * Change way we display storageclasses on the details view
    
    * bump
    
    * Add translations, change link navigation
    
    * translation fix
    
    * Change displaying match labels
    
    * improve translations and unlock list view
    
    * remve enter
    
    * Add deep links, move files from shared
    
    * Add a test for deep-linking
    
    * Fix tests, adjust texts
    
    * Lint fix
    
    Co-authored-by: dariadomagala <daria.domagala@sap.com>

--- core-ui/src/components/Predefined/Details/StorageClasses/PersistentVolumeClaimsList.js
+++ core-ui/src/components/Predefined/Details/StorageClasses/PersistentVolumeClaimsList.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import LuigiClient from '@luigi-project/client';
+
 import { ComponentForList } from 'shared/getComponents';
 
 export function PersistentVolumeClaimsList(storageclass) {
+  const navigateFn = resource => {
+    LuigiClient.linkManager()
+      .fromContext('cluster')
+      .navigate(
+        `namespaces/${resource.metadata.namespace}/persistentvolumeclaims/details/${resource.metadata.name}`,
+      );
+  };
+
   const params = {
-    // Uncoment and check flow after resolve issue #531
-    // hasDetailsView: true,
-    fixedPath: true,
+    hasDetailsView: true,
+    navigateFn,
     resourceUrl: `/api/v1/persistentvolumeclaims`,
     resourceType: 'PersistentVolumeClaims',
     isCompact: true,
