6344e82b9b08cfd18a116be312a8804753884aeb
Wawrzyn321 <Wawrzyn321@gmail.com>
2022-01-25 12:40:59 +0100

    Add translations for statuses

--- core-ui/src/components/Predefined/Details/Secret/helm/ReleaseStatus.js
+++ core-ui/src/components/Predefined/Details/Secret/helm/ReleaseStatus.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { StatusBadge } from 'react-shared';
 
 export function ReleaseStatus({ release }) {
+  const { i18n } = useTranslation();
   const status = release.info.status;
 
   const type = status === 'deployed' ? 'success' : 'info';
 
   return (
-    <StatusBadge type={type} noTooltip>
+    <StatusBadge resourceKind="secrets.helm" i18n={i18n} type={type} noTooltip>
       {status}
     </StatusBadge>
   );
