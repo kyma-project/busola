c27ec396f39452290405c06d89d210d4d6ea1dda
Piotr Wawrzyńczyk <Wawrzyn321@gmail.com>
2022-01-26 12:23:02 +0100

    Handle deep linking with kubeconfig ID, figz #841 (#859)
    * Handle deep linking with kubeconfig ID
    
    * Bump, fix a bug
    
    * Adjust e2e tests
    
    * Adjust add-cluster-disabled suite
    
    Co-authored-by: Agata Kucharska <akucharska@users.noreply.github.com>

--- tests/support/login-commands.js
+++ tests/support/login-commands.js
@@ -1,31 +1,41 @@
 import config from '../config';
 
-Cypress.Commands.add(
-  'loginAndSelectCluster',
-  (fileName = 'kubeconfig.yaml', expectedLocation = /overview$/) => {
-    cy.visit(`${config.clusterAddress}/clusters`)
-      .getIframeBody()
-      .contains('Connect a cluster')
+Cypress.Commands.add('loginAndSelectCluster', params => {
+  const defaults = {
+    fileName: 'kubeconfig.yaml',
+    expectedLocation: /overview$/,
+    storage: null,
+  };
+  const { fileName, expectedLocation, storage } = { ...defaults, ...params };
+
+  cy.visit(`${config.clusterAddress}/clusters`)
+    .getIframeBody()
+    .contains('Connect a cluster')
+    .click();
+
+  cy.getIframeBody()
+    .contains('Drag file here')
+    .attachFile(fileName, { subjectType: 'drag-n-drop' });
+
+  cy.getIframeBody()
+    .contains('Next')
+    .click();
+
+  if (storage) {
+    cy.getIframeBody()
+      .contains(storage)
       .click();
+  }
 
-    cy.getIframeBody()
-      .contains('Drag file here')
-      .attachFile(fileName, { subjectType: 'drag-n-drop' });
+  cy.getIframeBody()
+    .find('[role="dialog"]')
+    .contains('button', 'Connect a cluster')
+    .click();
 
-    cy.getIframeBody()
-      .contains('Next')
-      .click();
+  cy.url().should('match', expectedLocation);
+  cy.getIframeBody()
+    .find('thead')
+    .should('be.visible'); //wait for the namespaces XHR request to finish to continue running the tests. There's no <thead> while the request is pending.
 
-    cy.getIframeBody()
-      .find('[role="dialog"]')
-      .contains('button', 'Connect a cluster')
-      .click();
-
-    cy.url().should('match', expectedLocation);
-    cy.getIframeBody()
-      .find('thead')
-      .should('be.visible'); //wait for the namespaces XHR request to finish to continue running the tests. There's no <thead> while the request is pending.
-
-    return cy.end();
-  },
-);
+  return cy.end();
+});
