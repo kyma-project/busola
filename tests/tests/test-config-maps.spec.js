b84950a8728b38729bc50a8af2e036cc1ed81787
Jakub Łukomski <dev@tenku.eu>
2022-02-03 10:33:59 +0100

    improve config map tests

--- tests/tests/test-config-maps.spec.js
+++ tests/tests/test-config-maps.spec.js
@@ -2,8 +2,11 @@ const CONFIG_MAP_NAME = `test-config-map-${Math.floor(Math.random() * 9999) +
   1000}`;
 const CLONE_NAME = `${CONFIG_MAP_NAME}-clone`;
 
-const ENTRY_KEY = 'foo';
-const ENTRY_VALUE = 'bar';
+const ENTRY_KEY = 'config-map-key';
+const ENTRY_VALUE = 'config-map-value';
+
+const ENTRY_KEY2 = 'config-map-alt-key';
+const ENTRY_VALUE2 = 'config-map-alt-value';
 
 context('Test Config Maps', () => {
   Cypress.skipAfterFail();
@@ -54,11 +57,37 @@ context('Test Config Maps', () => {
       .contains(ENTRY_VALUE);
   });
 
-  it('Clone the secret', () => {
+  it('Edit the Config MAp', () => {
+    cy.getIframeBody()
+      .contains('Edit')
+      .click();
+
+    cy.getIframeBody()
+      .find('[placeholder="Enter Key"]:visible')
+      .eq(1)
+      .type(`${ENTRY_KEY2}{enter}{backspace}${ENTRY_VALUE2}`);
+
+    cy.getIframeBody()
+      .find('[role=dialog]')
+      .contains('button', 'Update')
+      .click();
+  });
+
+  it('Inspect the updated Config Map', () => {
+    cy.getIframeBody()
+      .contains('.fd-layout-panel', ENTRY_KEY2)
+      .contains(ENTRY_VALUE2);
+  });
+
+  it('Inspect list', () => {
     cy.getLeftNav()
       .contains('Config Maps')
       .click();
 
+    cy.getIframeBody().contains('tr', CONFIG_MAP_NAME);
+  });
+
+  it('Clone the secret', () => {
     cy.getIframeBody()
       .contains('.fd-table__row', CONFIG_MAP_NAME)
       .find('button[data-testid="clone"]')
@@ -80,5 +109,9 @@ context('Test Config Maps', () => {
     cy.getIframeBody()
       .contains('.fd-layout-panel', ENTRY_KEY)
       .contains(ENTRY_VALUE);
+
+    cy.getIframeBody()
+      .contains('.fd-layout-panel', ENTRY_KEY2)
+      .contains(ENTRY_VALUE2);
   });
 });
