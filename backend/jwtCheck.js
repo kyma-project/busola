81aaf663247add0370bbd4305f4df8c54f807ab4
Wawrzyn321 <Wawrzyn321@gmail.com>
2022-02-09 13:17:43 +0100

    Remove debugs

--- backend/jwtCheck.js
+++ backend/jwtCheck.js
@@ -15,16 +15,12 @@ const jwtCheck = ({ issuer, jwksUri }) =>
   });
 
 export function setupJWTCheck(app) {
-  console.log('start setup');
   try {
-    console.log('fse', fs.existsSync('./config/config.json'));
-    const a = JSON.parse(fs.readFileSync('./config/config.json'));
-    console.log(a);
-    const jwtConfig = a.config.features.JWT_CHECK_CONFIG;
-    console.log('config', jwtConfig);
-    if (jwtConfig.isEnabled) {
-      app.use(jwtCheck(jwtConfig.config));
-      console.log('done');
+    const { isEnabled, config } = JSON.parse(
+      fs.readFileSync('./config/config.json'),
+    ).config.features.JWT_CHECK_CONFIG;
+    if (isEnabled) {
+      app.use(jwtCheck(config));
     }
   } catch (e) {
     console.log(e);
