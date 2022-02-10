9159a9dae7fc0741d78b72d7b2df9e3298e93c84
Agata Kucharska <akucharska@users.noreply.github.com>
2022-02-10 09:11:04 +0100

    Add possibility to disable sidecars for jobs and cronjobs (#940)
    * Add possibility to disable sidecars for jobs and cronjobs
    
    * Bump image
    
    * Review changes
    
    * Remove duplicated translation
    
    * Fix deleting annotation
    
    * Get rid of useEffect
    
    * Review change
    
    * Review change

--- core-ui/src/components/Predefined/Create/Jobs/Jobs.create.js
+++ core-ui/src/components/Predefined/Create/Jobs/Jobs.create.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import * as jp from 'jsonpath';
 import { useTranslation } from 'react-i18next';
+import { Switch } from 'fundamental-react';
+import { useMicrofrontendContext } from 'react-shared';
+
 import { cloneDeep } from 'lodash';
 
 import { ResourceForm } from 'shared/ResourceForm';
@@ -10,6 +13,9 @@ import { createJobTemplate, createJobPresets } from './templates';
 import { JobSpecSection } from './SpecSection';
 import { ContainerSection, ContainersSection } from './ContainersSection';
 import { MessageStrip } from 'fundamental-react';
+
+const SIDECAR_INJECTION_LABEL = 'sidecar.istio.io/inject';
+const SIDECAR_INJECTION_VALUE = 'false';
 
 function isJobValid(job) {
   const isNameValid = jp.value(job, '$.metadata.name');
@@ -30,14 +36,51 @@ function JobsCreate({
   resourceUrl,
 }) {
   const { t } = useTranslation();
+  const { features } = useMicrofrontendContext();
+  const istioEnabled = features.ISTIO?.isEnabled;
+  const defaultSidecarAnnotations = initialJob
+    ? initialJob?.spec.template.metadata.annotations
+    : istioEnabled
+    ? { [SIDECAR_INJECTION_LABEL]: SIDECAR_INJECTION_VALUE }
+    : {};
 
   const [job, setJob] = useState(
-    initialJob ? cloneDeep(initialJob) : createJobTemplate(namespace),
+    initialJob
+      ? cloneDeep(initialJob)
+      : createJobTemplate(namespace, defaultSidecarAnnotations),
   );
 
   useEffect(() => {
     setCustomValid(isJobValid(job));
   }, [job, setCustomValid]);
+
+  const onSwitchChange = () => {
+    const isSidecar =
+      jp.value(
+        job,
+        `$.spec.template.metadata.annotations["${SIDECAR_INJECTION_LABEL}"]`,
+      ) !== SIDECAR_INJECTION_VALUE;
+    if (isSidecar) {
+      jp.value(
+        job,
+        `$.spec.template.metadata.annotations["${SIDECAR_INJECTION_LABEL}"]`,
+        SIDECAR_INJECTION_VALUE,
+      );
+      setJob({ ...job });
+    } else {
+      const templateAnnotations =
+        job.spec.template?.metadata?.annotations || {};
+      delete templateAnnotations[SIDECAR_INJECTION_LABEL];
+
+      jp.value(
+        job,
+        '$.spec.template.metadata.annotations',
+        templateAnnotations,
+      );
+
+      setJob({ ...job });
+    }
+  };
 
   return (
     <ResourceForm
@@ -61,7 +104,21 @@ function JobsCreate({
         }}
         readOnly={!!initialJob}
       />
-
+      <ResourceForm.FormField
+        label={t('jobs.create-modal.disable-sidecar')}
+        input={() => (
+          <Switch
+            compact
+            onChange={onSwitchChange}
+            checked={
+              jp.value(
+                job,
+                `$.spec.template.metadata.annotations["${SIDECAR_INJECTION_LABEL}"]`,
+              ) === SIDECAR_INJECTION_VALUE
+            }
+          />
+        )}
+      />
       <KeyValueField
         advanced
         propertyPath="$.metadata.labels"
