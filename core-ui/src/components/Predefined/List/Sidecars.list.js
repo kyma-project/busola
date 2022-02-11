2229d41e7d54cbf3129db04202b65c0cfc8f0d97
Jakub Lyczko <jakub.lyczko@sap.com>
2022-02-11 14:26:34 +0100

    Add labels to the list view

--- core-ui/src/components/Predefined/List/Sidecars.list.js
+++ core-ui/src/components/Predefined/List/Sidecars.list.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Link, EMPTY_TEXT_PLACEHOLDER } from 'react-shared';
+import { Link, EMPTY_TEXT_PLACEHOLDER, Labels } from 'react-shared';
 import { Trans } from 'react-i18next';
 
 export function SidecarsList({ DefaultRenderer, ...otherParams }) {
@@ -11,6 +11,12 @@ export function SidecarsList({ DefaultRenderer, ...otherParams }) {
       header: t('sidecars.headers.outbound-traffic-policy'),
       value: sidecar =>
         sidecar.spec?.outboundTrafficPolicy?.mode || EMPTY_TEXT_PLACEHOLDER,
+    },
+    {
+      header: t('sidecars.headers.workload-selector-labels'),
+      value: sidecar => (
+        <Labels labels={sidecar.spec?.workloadSelector?.labels} />
+      ),
     },
   ];
 
