9b371ffb801de586a04169984b8f22d5eced31eb
Wawrzyn321 <Wawrzyn321@gmail.com>
2022-02-15 16:46:21 +0100

    tests wip

---
+++ tests/tests/test-resource-upload.spec.js
@@ -0,0 +1,22 @@
+// /// <reference types="cypress" />
+// import 'cypress-file-upload';
+
+// context('Test resource upload', () => {
+//   Cypress.skipAfterFail();
+
+//   before(() => {
+//     cy.loginAndSelectCluster();
+//   });
+
+//   it('', () => {
+//     cy.getLeftNav()
+//       .contains('Cluster Overview')
+//       .click();
+
+//     cy.getIframeBody()
+//       .contains('Upload YAML')
+//       .click();
+
+//     cy.pasteIntoMonaco
+//   });
+// });
