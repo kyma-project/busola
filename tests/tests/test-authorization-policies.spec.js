d007d17a641a089b2512fe51af84e60e4e1b26cc
Mateusz Wisniewski <mwisnia97@gmail.com>
2022-02-18 08:41:41 +0100

    Introduce Authorization Policies view (#966)
    * Added create modal for ap and added ap to command pallete
    
    * Added simple details view
    
    * Added importing namespace name to yaml
    
    * Repair small problems
    
    * Added Action field in details
    
    * Added Rule fields
    
    * Update headers in translation
    
    * If there is no rule don't display it
    
    * Clean up code
    
    * Added related workloads
    
    * Added action field in list view
    
    * Repair action in list and add simple ap yaml
    
    * Added test for ap
    
    * bump
    
    * Remove unnecesary things
    
    * Remove nav
    
    * Repair en and apply changes
    
    * Added ap to resource-name
    
    * Added deeplink for sa
    
    * Prettier
    
    * Prettier
    
    * Facilitate custom columns
    
    * Removed cos
    
    * Update core/src/i18n/en.yaml
    
    Co-authored-by: Grzegorz Karaluch <grzegorz.karaluch@sap.com>
    
    Co-authored-by: Jakub Lyczko <jakub.lyczko@sap.com>
    Co-authored-by: Grzegorz Karaluch <grzegorz.karaluch@sap.com>

---
+++ tests/tests/test-authorization-policies.spec.js
@@ -0,0 +1,73 @@
+/// <reference types="cypress" />
+import 'cypress-file-upload';
+import { loadFile } from '../support/loadFile';
+
+context('Test Authorization Policies', () => {
+  Cypress.skipAfterFail();
+
+  before(() => {
+    cy.loginAndSelectCluster();
+    cy.goToNamespaceDetails();
+  });
+
+  it('Create Authorization Policy', () => {
+    cy.navigateTo('Istio', 'Authorization Policies');
+
+    cy.getIframeBody()
+      .contains('Create Authorization Policy')
+      .click();
+
+    cy.wrap(loadFile('test-authorization-policies.yaml')).then(AP_CONFIG => {
+      const AP = JSON.stringify(AP_CONFIG);
+
+      cy.pasteToMonaco(AP);
+    });
+
+    cy.getIframeBody()
+      .find('[role="dialog"]')
+      .contains('button', 'Create')
+      .click();
+  });
+
+  it('Checking details', () => {
+    cy.getIframeBody()
+      .contains('test-ap')
+      .should('be.visible');
+
+    cy.getIframeBody()
+      .contains('app=myapi')
+      .should('be.visible');
+
+    cy.getIframeBody()
+      .contains('To')
+      .should('be.visible');
+
+    cy.getIframeBody()
+      .contains('Operation')
+      .should('be.visible');
+
+    cy.getIframeBody()
+      .contains('GET')
+      .should('be.visible');
+
+    cy.getIframeBody()
+      .contains('/user/profile/*')
+      .should('be.visible');
+  });
+
+  it('Inspect list and delete Authorization Policy', () => {
+    cy.getIframeBody()
+      .contains('Authorization Policies')
+      .click();
+
+    cy.getIframeBody()
+      .find('[role="search"] [aria-label="search-input"]')
+      .type('test-ap', { force: true });
+
+    cy.getIframeBody()
+      .contains('tbody tr td a', 'test-ap')
+      .click({ force: true });
+
+    cy.deleteInDetails();
+  });
+});
