ab6195e5a48a5bf800d22e671d1aad59e72435f4
Jakub Lyczko <jakub.lyczko@sap.com>
2022-02-18 12:30:45 +0100

    Introduce Istio Service Entries (#968)
    * Add serviceentries to the luigi navigation
    
    * create view
    
    * list view
    
    * fix luigi navigation
    
    * create view
    
    * first iteration of list and details views
    
    * Improve details view and translations
    
    * working on displaying the endpoints
    
    * improve details view, modify translations, add workload selector component
    
    * polishing
    
    * tests
    
    * change location of the workloadselector
    
    * add linking to service accounts
    
    * add linking to service accounts
    
    * fix en yaml
    
    * pull main
    
    * Improve arrays as tokens, add info when exportTo is empty, fix sidecars
    
    * fix translations
    
    * bump
    
    * Add missing networkpolicies to the command pallete
    
    * fix translations
    
    * fix tests
    
    * nav fix
    
    * Remove needless keys, add key to workloads component, improve details test

---
+++ tests/tests/test-service-entries.spec.js
@@ -0,0 +1,96 @@
+/// <reference types="cypress" />
+import 'cypress-file-upload';
+import { loadFile } from '../support/loadFile';
+
+const FILE_NAME = 'test-service-entry.yaml';
+
+const SE_NAME =
+  'test-' +
+  Math.random()
+    .toString()
+    .substr(2, 8);
+
+const RESOLUTION = 'STATIC';
+const LOCATION = 'MESH_EXTERNAL';
+const HOST = 'test.com';
+const ADDRESS = '192.192.192.192/24';
+const WORKLOAD_SELECTOR_LABEL = 'test=test';
+
+async function loadSE(name, namespace, fileName) {
+  const resource = await loadFile(fileName);
+  const newResource = { ...resource };
+
+  newResource.metadata.name = name;
+  newResource.metadata.namespace = namespace;
+
+  return newResource;
+}
+
+context('Test Service Entries', () => {
+  Cypress.skipAfterFail();
+
+  before(() => {
+    cy.loginAndSelectCluster();
+    cy.goToNamespaceDetails();
+  });
+
+  it('Create a Service Entry', () => {
+    cy.navigateTo('Istio', 'Service Entries');
+
+    cy.getIframeBody()
+      .contains('Create Service Entry')
+      .click();
+
+    cy.wrap(loadSE(SE_NAME, Cypress.env('NAMESPACE_NAME'), FILE_NAME)).then(
+      SE_CONFIG => {
+        const SE = JSON.stringify(SE_CONFIG);
+        cy.pasteToMonaco(SE);
+      },
+    );
+
+    cy.getIframeBody()
+      .find('[role="dialog"]')
+      .contains('button', 'Create')
+      .click();
+
+    cy.getIframeBody()
+      .contains('h3', SE_NAME)
+      .should('be.visible');
+  });
+
+  it('Check the Service Entry details', () => {
+    cy.getIframeBody()
+      .should('include.text', RESOLUTION)
+      .and('include.text', LOCATION)
+      .and('include.text', HOST)
+      .and('include.text', ADDRESS)
+      .and('include.text', WORKLOAD_SELECTOR_LABEL);
+  });
+
+  it('Check the Service Entries list', () => {
+    cy.getLeftNav()
+      .contains('Service Entries')
+      .click();
+
+    cy.getIframeBody()
+      .contains(SE_NAME)
+      .parent()
+      .getIframeBody()
+      .contains(RESOLUTION);
+  });
+
+  it('Delete a Service Entry', () => {
+    cy.getIframeBody()
+      .contains('.fd-table__row', SE_NAME)
+      .find('button[data-testid="delete"]')
+      .click();
+
+    cy.getIframeBody()
+      .contains('button', 'Delete')
+      .click();
+
+    cy.getIframeBody()
+      .contains('.fd-table__row', SE_NAME)
+      .should('not.exist');
+  });
+});
