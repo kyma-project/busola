6c9d1f4a0314097284b9d7cc478387b958fa2892
Wawrzyn321 <Wawrzyn321@gmail.com>
2022-02-21 07:57:13 +0100

    Fix resource upload

--- core-ui/src/components/Predefined/Details/Namespace/YamlUpload/YamlUpload.js
+++ core-ui/src/components/Predefined/Details/Namespace/YamlUpload/YamlUpload.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { MessageStrip } from 'fundamental-react';
 import { useTranslation } from 'react-i18next';
-import { ControlledEditor, useTheme } from 'react-shared';
+import { MonacoEditor, useTheme } from 'react-shared';
 import jsyaml from 'js-yaml';
 
 import { YamlFileUploader } from './YamlFileUploader';
@@ -23,6 +23,7 @@ export function YamlUpload({ resourcesData, setResourcesData }) {
   const updateYamlContent = text => {
     try {
       const files = jsyaml.loadAll(text);
+      console.log(text);
       if (files.some(file => typeof file !== 'object')) {
         setError(t('clusters.wizard.not-an-object'));
       } else if (files.some(file => !isK8sResource(file))) {
@@ -43,7 +44,7 @@ export function YamlUpload({ resourcesData, setResourcesData }) {
       <p className="editor-label fd-margin-bottom--sm fd-margin-top--sm">
         {t('upload-yaml.or-paste-here')}
       </p>
-      <ControlledEditor
+      <MonacoEditor
         height="400px"
         language="yaml"
         theme={editorTheme}
@@ -51,7 +52,7 @@ export function YamlUpload({ resourcesData, setResourcesData }) {
         editorDidMount={(getValue, editor) =>
           editor.onDidBlurEditorWidget(() => updateYamlContent(getValue()))
         }
-        onChange={(_, value) => updateYamlContent(value)}
+        onChange={updateYamlContent}
         options={{ scrollbar: { alwaysConsumeMouseWheel: false } }}
       />
       {error && (
