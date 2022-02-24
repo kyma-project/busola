32b61111f4fd9c225e7f07526c0a94d4ddbb3d11
Karaluch <grzegorz.karaluch@sap.com>
2022-02-24 07:58:26 +0100

    tests

--- tests/tests/run-before.spec.js
+++ tests/tests/run-before.spec.js
@@ -28,6 +28,7 @@ context('Create Namespace', () => {
 
     cy.getIframeBody()
       .find('[role=dialog]')
+      .find('input[placeholder]:visible')
       .type(Cypress.env('NAMESPACE_NAME'));
 
     cy.getIframeBody()
