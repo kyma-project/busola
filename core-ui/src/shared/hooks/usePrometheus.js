e548dbfcdd15467b59a9c6f8c9f914f920f7e268
dariadomagala <daria.domagala@sap.com>
2022-03-02 10:13:14 +0100

    Code cleanup

--- core-ui/src/shared/hooks/usePrometheus.js
+++ core-ui/src/shared/hooks/usePrometheus.js
@@ -1,78 +1,75 @@
 import { useEffect, useState } from 'react';
 import { useGet } from 'react-shared';
 
-const getPrometheusSelector = (type, data) => {
-  if (type === 'cluster') {
-    return `cluster="", container!=""`;
-  } else if (type === 'pod') {
-    return `cluster="", container!="", namespace="${data.namespace}", pod="${data.pod}"`;
-  } else {
-    return '';
-  }
+const getPrometheusSelector = data => {
+  return `cluster="", container!="", namespace="${data.namespace}", pod="${data.pod}"`;
 };
 
-const getPrometheusCPUQuery = (type, selector, step) => {
+const getPrometheusCPUQuery = (type, data, step) => {
   if (type === 'cluster') {
     return `count(node_cpu_seconds_total{mode="idle"}) - sum(rate(node_cpu_seconds_total{mode="idle"}[${step}s]))`;
   } else if (type === 'pod') {
-    return `sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_rate{${selector}})`;
+    return `sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_rate{${getPrometheusSelector(
+      data,
+    )}})`;
   } else {
     return '';
   }
 };
 
-const getPrometheusMemoryQuery = (type, selector) => {
+const getPrometheusMemoryQuery = (type, data) => {
   if (type === 'cluster') {
     return `sum(node_memory_MemTotal_bytes - node_memory_MemFree_bytes)`;
   } else if (type === 'pod') {
-    return `sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_rate{${selector}})`;
+    return `sum(node_namespace_pod_container:container_cpu_usage_seconds_total:sum_rate{${getPrometheusSelector(
+      data,
+    )}})`;
   } else {
     return '';
   }
 };
 
-const getPrometheusNetworkReceivedQuery = (type, selector, step) => {
+const getPrometheusNetworkReceivedQuery = (type, data, step) => {
   if (type === 'cluster') {
     return `sum(rate(node_network_receive_bytes_total{device!="lo"}[${step}s]))`;
   } else if (type === 'pod') {
-    return `sum(irate(container_network_receive_bytes_total{${selector}}[${step}s]))`;
+    return `sum(irate(container_network_receive_bytes_total{${getPrometheusSelector(
+      data,
+    )}}[${step}s]))`;
   } else {
     return '';
   }
 };
 
-const getPrometheusNetworkTransmittedQuery = (type, selector, step) => {
+const getPrometheusNetworkTransmittedQuery = (type, data, step) => {
   if (type === 'cluster') {
     return `sum(rate(node_network_transmit_bytes_total{device!="lo"}[${step}s]))`;
   } else if (type === 'pod') {
-    return `sum(irate(container_network_receive_bytes_total{${selector}}[${step}s]))`;
+    return `sum(irate(container_network_receive_bytes_total{${getPrometheusSelector(
+      data,
+    )}}[${step}s]))`;
   } else {
     return '';
   }
 };
 
 export function getMetric(type, metric, { step, ...data }) {
-  const selector = getPrometheusSelector(type, data);
   const metrics = {
     cpu: {
-      prometheusQuery: getPrometheusCPUQuery(type, selector, step),
+      prometheusQuery: getPrometheusCPUQuery(type, data, step),
       unit: '',
     },
     memory: {
-      prometheusQuery: getPrometheusMemoryQuery(type, selector),
+      prometheusQuery: getPrometheusMemoryQuery(type, data),
       binary: true,
       unit: 'B',
     },
     'network-down': {
-      prometheusQuery: getPrometheusNetworkReceivedQuery(type, selector, step),
+      prometheusQuery: getPrometheusNetworkReceivedQuery(type, data, step),
       unit: 'B/s',
     },
     'network-up': {
-      prometheusQuery: getPrometheusNetworkTransmittedQuery(
-        type,
-        selector,
-        step,
-      ),
+      prometheusQuery: getPrometheusNetworkTransmittedQuery(type, data, step),
       unit: 'B/s',
     },
   };
