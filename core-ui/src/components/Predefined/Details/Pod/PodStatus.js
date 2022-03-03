d3e21c609cb2560aa61468b4305bf7f7802fd216
mwisnia97 <mwisnia97@gmail.com>
2022-03-03 12:04:51 +0100

    Return pod status

--- core-ui/src/components/Predefined/Details/Pod/PodStatus.js
+++ core-ui/src/components/Predefined/Details/Pod/PodStatus.js
@@ -2,12 +2,43 @@ import React from 'react';
 import { StatusBadge } from 'react-shared';
 import { useTranslation } from 'react-i18next';
 
+const calculatePodState = pod => {
+  const containerStatuses = pod?.status?.containerStatuses;
+  if (containerStatuses?.length > 0) {
+    const waitingStatus = containerStatuses
+      .reverse()
+      .find(element => element.state.waiting);
+    if (waitingStatus) {
+      return {
+        status: waitingStatus.state.waiting.reason || 'Waiting',
+        message: waitingStatus.state.waiting.message,
+      };
+    } else {
+      const terminatedStatus = containerStatuses
+        .reverse()
+        .find(element => element.state.terminated);
+      if (terminatedStatus) {
+        return {
+          status: terminatedStatus.state.terminated.reason || 'Terminated',
+          message: terminatedStatus.state.terminated.message,
+        };
+      }
+    }
+  }
+  return { status: 'Running' };
+};
+
 const badgeType = status => {
   switch (status) {
     case 'Running':
     case 'Succeeded':
+    case 'Completed':
       return 'success';
+    case 'Terminated':
     case 'Pending':
+    case 'Terminating':
+    case 'PodInitializing':
+    case 'ContainerCreating':
       return 'info';
     case 'Unknown':
       return undefined;
@@ -19,14 +50,16 @@ const badgeType = status => {
 export function PodStatus({ pod }) {
   const { i18n } = useTranslation();
 
+  const podState = calculatePodState(pod);
+
   return (
     <StatusBadge
       i18n={i18n}
+      additionalContent={podState.message}
       resourceKind="pods"
-      type={badgeType(pod.status.phase)}
-      noTooltip
+      type={badgeType(podState.status)}
     >
-      {pod.status.phase}
+      {podState.status}
     </StatusBadge>
   );
 }
