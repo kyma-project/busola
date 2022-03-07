89db9692a70f77526603cb5e463396bc5a2c60b1
wchrapka <94044629+wchrapka@users.noreply.github.com>
2022-03-07 15:58:15 +0100

    merge Busola's defaultConfig with ConfigMap (#1034)
    * merge Busola's defaultConfig with ConfigMap
    
    * split the defaultConfig.json
    
    * Apply suggestions from code review
    
    proofreading corrections
    
    Co-authored-by: Grzegorz Karaluch <grzegorz.karaluch@sap.com>
    
    * Update docs/features.md
    
    Co-authored-by: Grzegorz Karaluch <grzegorz.karaluch@sap.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Grzegorz Karaluch <grzegorz.karaluch@sap.com>
    
    * proofread correction
    
    Co-authored-by: Grzegorz Karaluch <grzegorz.karaluch@sap.com>

--- core/src/luigi-config/busola-cluster-params.js
+++ core/src/luigi-config/busola-cluster-params.js
@@ -1,11 +1,22 @@
+import { merge } from 'lodash';
+
 let params = null;
 
 export async function getBusolaClusterParams() {
   if (!params) {
     try {
       const cacheBuster = '?cache-buster=' + Date.now();
-      const response = await fetch('/assets/config/config.json' + cacheBuster);
-      params = await response.json();
+      const defaultConfigResponse = await fetch(
+        '/assets/defaultConfig.json' + cacheBuster,
+      );
+      const configMapResponse = await fetch(
+        '/assets/config/config.json' + cacheBuster,
+      );
+
+      const defaultParams = await defaultConfigResponse.json();
+      const mapParams = await configMapResponse.json();
+
+      params = merge(defaultParams, mapParams);
     } catch (e) {
       console.warn('Cannot load cluster params: ', e);
       params = {};
