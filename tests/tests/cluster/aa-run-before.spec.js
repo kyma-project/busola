cee69cb62ac5f0ebb1aa5a3adf4856715b008501
dariadomagala <daria.domagala@sap.com>
2022-03-08 11:08:36 +0100

    Fix cluster tests

--- tests/tests/cluster/aa-run-before.spec.js
+++ tests/tests/cluster/aa-run-before.spec.js
@@ -5,66 +5,7 @@ context('Create Namespace', () => {
 
   before(cy.loginAndSelectCluster);
 
-  it('Create Namespace', () => {
-    cy.getLeftNav()
-      .contains('Namespaces')
-      .click();
-
-    cy.getIframeBody()
-      .contains('Create Namespace')
-      .click();
-
-    cy.getIframeBody()
-      .contains('Advanced')
-      .click();
-
-    cy.getIframeBody()
-      .contains('Create Resource Quota')
-      .click();
-
-    cy.getIframeBody()
-      .contains('Create Limit Range')
-      .click();
-
-    cy.getIframeBody()
-      .find('[role=dialog]')
-      .find('input[ariaLabel="Namespace name"]:visible')
-      .type(Cypress.env('NAMESPACE_NAME'));
-
-    cy.getIframeBody()
-      .find('[role=dialog]')
-      .contains('Advanced')
-      .click();
-
-    cy.getIframeBody()
-      .find('[role=dialog]')
-      .contains('button', 'Create')
-      .click();
-
-    cy.getIframeBody()
-      .contains('h3', Cypress.env('NAMESPACE_NAME'))
-      .should('be.visible');
-
-    cy.getIframeBody()
-      .contains('button', 'Edit')
-      .click();
-
-    cy.getIframeBody()
-      .find('[role=dialog]')
-      .contains('Advanced')
-      .click();
-
-    cy.getIframeBody()
-      .find('[role=dialog]')
-      .contains('button', 'Update')
-      .click();
-  });
-
   it('Create application', () => {
-    cy.getLeftNav()
-      .contains('Back to Cluster Overview')
-      .click();
-
     cy.navigateTo('Integration', 'Applications');
 
     cy.getIframeBody()
@@ -74,7 +15,7 @@ context('Create Namespace', () => {
     cy.getIframeBody()
       .find('[role=dialog]')
       .find("input[ariaLabel='Application name']:visible")
-      .type(`test-mock-app-${Cypress.env('NAMESPACE_NAME')}`);
+      .type(Cypress.env('APP_NAME'));
 
     cy.getIframeBody()
       .find('[role="dialog"]')
@@ -82,7 +23,7 @@ context('Create Namespace', () => {
       .click();
 
     cy.getIframeBody()
-      .contains(`test-mock-app-${Cypress.env('NAMESPACE_NAME')}`)
+      .contains(Cypress.env('APP_NAME'))
       .should('be.visible');
   });
 });
