9c952b48995d9566b79668b0bfaa969d818f07cf
wchrapka <94044629+wchrapka@users.noreply.github.com>
2022-03-21 13:35:57 +0100

    Merge shared repo (#1095)
    * shared repo copied
    
    * minimal setup
    
    * remove shared files
    
    * wip merging shared
    
    * clean package.json
    
    * remove shared from config, address eslint errors
    
    * address eslint warnings
    
    * remove inputWithSuffix
    
    * fix cypress tests
    
    * skip useGet.test

---
+++ core-ui/src/shared-repo/utils/setupMonaco.js
@@ -0,0 +1,6 @@
+const isLocalDev = window.location.hostname.startsWith('localhost');
+
+export function setupMonaco(loader) {
+  // monaco editor - load from static files instead of from CDN
+  loader.config({ paths: { vs: isLocalDev ? '/vs' : '/assets/libs/vs' } });
+}
