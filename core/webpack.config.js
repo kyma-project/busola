f3491793bf8e913b70904ca965df6e976a78cc9f
Wawrzyn321 <Wawrzyn321@gmail.com>
2022-03-23 11:47:20 +0100

    Handle CSS, cleanup files WIP

--- core/webpack.config.js
+++ core/webpack.config.js
@@ -6,6 +6,7 @@ const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = env => {
   return {
@@ -33,12 +34,23 @@ module.exports = env => {
           test: /\.css$/,
           use: [MiniCssExtractPlugin.loader, 'css-loader'],
         },
+        {
+          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'fonts/',
+              },
+            },
+          ],
+        },
       ],
     },
     plugins: [
-      new MiniCssExtractPlugin({
-        filename: './src/assets/libs/luigi-core/luigi.[contenthash].css',
-      }),
+      // new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin({ filename: 'index.[contenthash].css' }),
       new CopyWebpackPlugin([
         {
           from:
