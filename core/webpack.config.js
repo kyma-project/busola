44db6c0050aa5662bb6c29db3c626a716af3bae3
Jakub Lyczko <jakub.lyczko@sap.com>
2022-03-23 17:04:29 +0100

    remove import

--- core/webpack.config.js
+++ core/webpack.config.js
@@ -3,7 +3,6 @@ const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = env => {
   return {
