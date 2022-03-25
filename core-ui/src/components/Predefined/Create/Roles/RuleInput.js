dbb3e2abb4c3f3cfaf30e6e62fe31832443c708b
Jakub Łukomski <dev@tenku.eu>
2022-03-25 09:34:30 +0200

    remove unnecessary bit of code

--- core-ui/src/components/Predefined/Create/Roles/RuleInput.js
+++ core-ui/src/components/Predefined/Create/Roles/RuleInput.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Button, BusyIndicator } from 'fundamental-react';
 import * as jp from 'jsonpath';
