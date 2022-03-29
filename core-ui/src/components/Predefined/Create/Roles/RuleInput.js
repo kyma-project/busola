19a3e444d73e1ff32858da3d340e8ae4dde4f26e
Jakub Łukomski <dev@tenku.eu>
2022-03-29 11:16:40 +0200

    All api groups (#1123)
    * basic profiles
    
    * read resources on groups change
    
    * cache a bit more aggressively
    
    * bump image version
    
    * add "add all" buttons
    
    * add loading indicator
    
    * add icon to buttons
    
    * introduce manual loading of api group resources
    
    * implement new multiinput modes
    
    * strongly rework cache filling and resource fetching
    
    * remove unnecessary bit of code
    
    * fix not being able to type
    
    * get rid of a stray console.log
    
    * protect api groups agains empty values
    
    * add even more security
    
    * fix tests
    
    * fix more tests
    
    Co-authored-by: Daria Domagała <40663133+dariadomagala@users.noreply.github.com>

--- core-ui/src/components/Predefined/Create/Roles/RuleInput.js
+++ core-ui/src/components/Predefined/Create/Roles/RuleInput.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { Button, BusyIndicator } from 'fundamental-react';
+import * as jp from 'jsonpath';
 import { useMicrofrontendContext } from 'react-shared';
 import { ResourceForm } from 'shared/ResourceForm';
 import { ComboboxArrayInput, TextArrayInput } from 'shared/ResourceForm/fields';
 import { InvalidRoleError } from './InvalidRoleError';
 import { useResourcesForApiGroups } from './useResourcesForApiGroups';
-
-function unique(arr) {
-  return [...new Set(arr)];
-}
+import {
+  EMPTY_API_GROUP_KEY,
+  getApiGroupInputOptions,
+  unique,
+} from './helpers';
 
 const nonResourceUrls = [
   '/healthz/ready',
@@ -37,38 +40,61 @@ const verbs = [
   '*',
 ];
 
-const extractApiGroup = groupVersion => {
-  // handle core ('') group
-  if (groupVersion === 'v1') return '';
-  const [apiGroup] = groupVersion.split('/');
-  return apiGroup;
-};
-
 export function RuleInput({ rule, rules, setRules, isAdvanced }) {
   const { namespaceId, groupVersions } = useMicrofrontendContext();
   const { t } = useTranslation();
 
   // dictionary of pairs (apiGroup: resources in that apiGroup)
   const apiRules = rule?.apiGroups?.flat();
-  const { cache: resourcesCache, fetchResources } = useResourcesForApiGroups(
-    apiRules ? [...new Set(apiRules)] : [],
-  );
-  const EMPTY_STRING_KEY = 'core-api-group';
+  const {
+    cache: resourcesCache,
+    fetchResources,
+    loadable,
+    loading,
+  } = useResourcesForApiGroups(apiRules ? [...new Set(apiRules)] : []);
   // introduce special option for '' apiGroup - Combobox doesn't accept empty string key
-  const apiGroupsInputOptions = unique(
-    groupVersions.map(extractApiGroup),
-  ).map(g =>
-    g === '' ? { key: EMPTY_STRING_KEY, text: '(core)' } : { key: g, text: g },
-  );
+  const apiGroupsInputOptions = getApiGroupInputOptions(groupVersions);
 
   // there's no endpoint for "all resources" - add just a '*' and specific resources
   // for already choosen apiGroups
-  const availableResources = unique([
-    ...(rule.apiGroups
-      ?.flatMap(apiGroup => resourcesCache[apiGroup] || [])
-      .map(r => r.name) || []),
-    '*',
-  ]);
+  const getAvailableResources = resourcesCache =>
+    unique([
+      ...(rule.apiGroups
+        ?.flatMap(apiGroup => resourcesCache[apiGroup] || [])
+        .map(r => r.name) || []),
+      '*',
+    ]);
+  const availableResources = getAvailableResources(resourcesCache);
+
+  const addAllApiGroups = () => {
+    jp.value(
+      rule,
+      '$.apiGroups',
+      apiGroupsInputOptions.map(g => g.key),
+    );
+    setRules([...rules]);
+  };
+
+  const addAllResources = () => {
+    fetchResources().then(resourcesCache => {
+      const availableResources = getAvailableResources(resourcesCache);
+      jp.value(
+        rule,
+        '$.resources',
+        availableResources.filter(r => r !== '*'),
+      );
+      setRules([...rules]);
+    });
+  };
+
+  const addAllVerbs = () => {
+    jp.value(
+      rule,
+      '$.verbs',
+      verbs.filter(r => r !== '*'),
+    );
+    setRules([...rules]);
+  };
 
   return (
     <ResourceForm.Wrapper
@@ -77,24 +103,58 @@ export function RuleInput({ rule, rules, setRules, isAdvanced }) {
       setResource={() => setRules([...rules])}
     >
       <ComboboxArrayInput
+        noEdit
+        filterOptions
         title={t('roles.headers.api-groups')}
         propertyPath="$.apiGroups"
         options={apiGroupsInputOptions}
-        emptyStringKey={EMPTY_STRING_KEY}
+        emptyStringKey={EMPTY_API_GROUP_KEY}
         defaultOpen
-        onBlur={fetchResources}
+        actions={
+          <Button compact glyph="add" onClick={addAllApiGroups}>
+            {t('roles.add-all')}
+          </Button>
+        }
       />
       <ComboboxArrayInput
+        noEdit
+        filterOptions
         title={t('roles.headers.resources')}
         propertyPath="$.resources"
         options={availableResources.map(i => ({ key: i, text: i }))}
         defaultOpen
+        actions={[
+          <BusyIndicator size="s" show={loading} />,
+          <Button
+            compact
+            glyph="refresh"
+            onClick={fetchResources}
+            disabled={!loadable || loading}
+          >
+            {t('roles.load')}
+          </Button>,
+          <Button
+            compact
+            glyph="add"
+            onClick={addAllResources}
+            disabled={loading}
+          >
+            {t('roles.add-all')}
+          </Button>,
+        ]}
       />
       <ComboboxArrayInput
+        noEdit
+        filterOptions
         title={t('roles.headers.verbs')}
         propertyPath="$.verbs"
         options={verbs.map(i => ({ key: i, text: i }))}
         defaultOpen
+        actions={[
+          <Button compact glyph="add" onClick={addAllVerbs}>
+            {t('roles.add-all')}
+          </Button>,
+        ]}
       />
       {isAdvanced && (
         <TextArrayInput
