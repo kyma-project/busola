083d43203d262bac79e26940ab4b6bda1e623edd
Jakub Lyczko <jakub.lyczko@sap.com>
2022-03-29 15:19:46 +0200

    Move ExpirationDate component to the separate folder

--- core-ui/src/components/Predefined/Details/Secret/CertificatePanel.js
+++ core-ui/src/components/Predefined/Details/Secret/CertificatePanel.js
@@ -1,98 +1,12 @@
 import React from 'react';
-import { Icon, LayoutPanel } from 'fundamental-react';
+import { LayoutPanel } from 'fundamental-react';
 import { LayoutPanelRow } from 'shared/components/LayoutPanelRow/LayoutPanelRow';
 import { useTranslation } from 'react-i18next';
-
-import { Tooltip, FormattedDatetime } from 'react-shared';
-
-const ExpirationDate = ({ date, lang }) => {
-  const { t } = useTranslation();
-  const currentDate = Date.now();
-  const dayInMilliseconds = 1000 * 60 * 60 * 24;
-  const dateDifference = (Date.parse(date) - currentDate) / dayInMilliseconds;
-  const EXPIRATION_LIMIT = 30;
-
-  if (dateDifference > EXPIRATION_LIMIT)
-    return <FormattedDatetime date={date} lang={lang} />;
-
-  let certificateDetails = {};
-
-  if (dateDifference < 0) {
-    certificateDetails = {
-      tooltipContent: t('certificates.tooltips.expired'),
-      ariaLabel: 'Error',
-      glyph: 'message-warning',
-      colorIndex: '3',
-    };
-  } else if (dateDifference < EXPIRATION_LIMIT && dateDifference > 0) {
-    let tooltipContent;
-
-    if (dateDifference < 1)
-      tooltipContent = t('certificates.tooltips.close-expiration');
-    else if (Math.floor(dateDifference) === 1)
-      tooltipContent = t('certificates.tooltips.will-expire', {
-        daysToExpire: 1,
-        daysForm: 'day',
-      });
-    else {
-      tooltipContent = t('certificates.tooltips.will-expire', {
-        daysToExpire: Math.floor(dateDifference),
-        daysForm: 'days',
-      });
-    }
-
-    certificateDetails = {
-      tooltipContent,
-      ariaLabel: 'Warning',
-      glyph: 'message-warning',
-      colorIndex: '2',
-    };
-  }
-  return (
-    <div
-      style={{
-        display: 'flex',
-      }}
-    >
-      <p
-        style={{
-          marginRight: '5px',
-        }}
-      >
-        <FormattedDatetime date={date} lang={lang} />
-      </p>
-      {Object.keys(certificateDetails).length > 0 ? (
-        <Tooltip content={certificateDetails.tooltipContent}>
-          <Icon
-            ariaLabel={certificateDetails.ariaLabel}
-            glyph={certificateDetails.glyph}
-            size="s"
-            className={`fd-has-color-status-${certificateDetails.colorIndex} has-tooltip`}
-          />
-        </Tooltip>
-      ) : null}
-    </div>
-  );
-};
+import { ExpirationDate } from 'shared/components/ExpirationDate/ExpirationDate';
 
 export function CertificatePanel({ name, certificate }) {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
   const { format: formatDate } = new Intl.DateTimeFormat('en');
-  console.log(Date.parse(certificate.notAfter));
-  // const expirationWarning = certificate.notAfter < new Date() && (
-  //   <Tooltip
-  //     delay={0}
-  //     position="right"
-  //     content={t('secrets.certificate-panel.expiration-warning')}
-  //   >
-  //     <Icon
-  //       className="fd-has-color-status-2"
-  //       ariaLabel="Warning"
-  //       glyph="message-warning"
-  //       size="s"
-  //     />
-  //   </Tooltip>
-  // );
 
   return (
     <LayoutPanel className="fd-margin--md">
@@ -116,12 +30,9 @@ export function CertificatePanel({ name, certificate }) {
         />
         <LayoutPanelRow
           name={t('secrets.certificate-panel.expires')}
-          // value={
-          //   <p>
-          //     {formatDate(certificate.notAfter)} {expirationWarning}
-          //   </p>
-          // }
-          value={<ExpirationDate lang={'en'} date={certificate.notAfter} />}
+          value={
+            <ExpirationDate lang={i18n.language} date={certificate.notAfter} />
+          }
         />
       </LayoutPanel.Body>
     </LayoutPanel>
---
+++ core-ui/src/shared/components/ExpirationDate/ExpirationDate.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Icon } from 'fundamental-react';
+import { useTranslation } from 'react-i18next';
+import { Tooltip, FormattedDatetime } from 'react-shared';
+
+export function ExpirationDate({ date, lang }) {
+  const { t } = useTranslation();
+  const currentDate = Date.now();
+  const dayInMilliseconds = 1000 * 60 * 60 * 24;
+  const dateDifference = (Date.parse(date) - currentDate) / dayInMilliseconds;
+  const EXPIRATION_LIMIT = 30;
+
+  if (dateDifference >= EXPIRATION_LIMIT)
+    return <FormattedDatetime date={date} lang={lang} />;
+
+  let certificateDetails = {};
+
+  if (dateDifference > 0) {
+    certificateDetails = {
+      tooltipContent: t('certificates.tooltips.expired'),
+      ariaLabel: 'Error',
+      glyph: 'message-warning',
+      colorIndex: '3',
+    };
+  } else if (dateDifference < EXPIRATION_LIMIT && dateDifference > 0) {
+    let tooltipContent;
+
+    if (dateDifference < 1)
+      tooltipContent = t('certificates.tooltips.close-expiration');
+    else if (Math.floor(dateDifference) === 1)
+      tooltipContent = t('certificates.tooltips.will-expire', {
+        daysToExpire: 1,
+        daysForm: 'day',
+      });
+    else {
+      tooltipContent = t('certificates.tooltips.will-expire', {
+        daysToExpire: Math.floor(dateDifference),
+        daysForm: 'days',
+      });
+    }
+
+    certificateDetails = {
+      tooltipContent,
+      ariaLabel: 'Warning',
+      glyph: 'message-warning',
+      colorIndex: '2',
+    };
+  }
+  return (
+    <div
+      style={{
+        display: 'flex',
+      }}
+    >
+      <p
+        style={{
+          marginRight: '5px',
+        }}
+      >
+        <FormattedDatetime date={date} lang={lang} />
+      </p>
+      {Object.keys(certificateDetails).length > 0 ? (
+        <Tooltip content={certificateDetails.tooltipContent}>
+          <Icon
+            ariaLabel={certificateDetails.ariaLabel}
+            glyph={certificateDetails.glyph}
+            size="s"
+            className={`fd-has-color-status-${certificateDetails.colorIndex} has-tooltip`}
+          />
+        </Tooltip>
+      ) : null}
+    </div>
+  );
+}
