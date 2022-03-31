c20bbc887f2d5f401bdba4d75ffba2df21f9a00a
Jakub Lyczko <jakub.lyczko1@gmail.com>
2022-03-31 09:12:39 +0200

    Add expiration info in the Certificates (#1169)
    * Add certificate expiration icon
    
    * Add logic to the certificate expiration
    
    * test expiration date with secrets certificates
    
    * Move ExpirationDate component to the separate folder
    
    * Change name of the component and cleaning
    
    * bump deployment
    
    * Add styling to the component
    
    * remove needless if
    
    * unknown date translation
    
    * fix import
    
    * fix import
    
    * fix import once again
    
    * Update core/src/i18n/en.yaml
    
    add dot in translation
    
    Co-authored-by: Grzegorz Karaluch <grzegorz.karaluch@sap.com>
    
    * Update core/src/i18n/en.yaml
    
    add dot to translation
    
    Co-authored-by: Grzegorz Karaluch <grzegorz.karaluch@sap.com>
    
    Co-authored-by: Daria Domagała <40663133+dariadomagala@users.noreply.github.com>
    Co-authored-by: Grzegorz Karaluch <grzegorz.karaluch@sap.com>

--- core-ui/src/components/Predefined/List/Certificates.list.js
+++ core-ui/src/components/Predefined/List/Certificates.list.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { ResourcesList } from 'shared/components/ResourcesList/ResourcesList';
 import { ResourceStatus } from 'shared/components/ResourceStatus/ResourceStatus';
-import { FormattedDatetime } from 'shared/components/FormattedDate/FormattedDate';
 import { Link } from 'shared/components/Link/Link';
 import { Trans } from 'react-i18next';
 import { CertificatesCreate } from '../Create/Certificates/Certificates.create';
 import { IssuerLink } from '../Details/Certificate/IssuerLink';
+import { CertificateDate } from 'shared/components/CertificateDate/CertificateDate';
+import { EMPTY_TEXT_PLACEHOLDER } from 'shared/constants';
 
 const CertificatesList = props => {
   const { t, i18n } = useTranslation();
@@ -14,7 +15,8 @@ const CertificatesList = props => {
   const customColumns = [
     {
       header: t('certificates.common-name'),
-      value: certificate => certificate.status?.commonName || '-',
+      value: certificate =>
+        certificate.status?.commonName || EMPTY_TEXT_PLACEHOLDER,
     },
     {
       header: t('certificates.issuer'),
@@ -26,12 +28,12 @@ const CertificatesList = props => {
       header: t('certificates.expiration-date'),
       value: certificate =>
         certificate.status?.expirationDate ? (
-          <FormattedDatetime
+          <CertificateDate
             date={certificate.status.expirationDate}
             lang={i18n.language}
           />
         ) : (
-          '-'
+          EMPTY_TEXT_PLACEHOLDER
         ),
     },
     {
