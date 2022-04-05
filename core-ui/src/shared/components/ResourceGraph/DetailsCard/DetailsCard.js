784f2547299adba68c73941ef366c0f0f048d25d
Jakub Lyczko <jakub.lyczko@sap.com>
2022-04-05 10:12:49 +0200

    remove comments and add translation

--- core-ui/src/shared/components/ResourceGraph/DetailsCard/DetailsCard.js
+++ core-ui/src/shared/components/ResourceGraph/DetailsCard/DetailsCard.js
@@ -5,15 +5,11 @@ import { navigateToResource } from 'shared/hooks/navigate';
 import { useTranslation } from 'react-i18next';
 
 export function DetailsCard({ resource, handleCloseModal }) {
-  console.log(resource);
-
   const { t } = useTranslation();
   return (
     <div className="details-card-wrapper">
       <header className="details-card-header">
-        {/* <div className="name-wrapper"> */}
         <p className="resource-name">{resource?.metadata?.name}</p>
-        {/* </div> */}
         <p className="resource-kind">{resource?.kind}</p>
       </header>
       <section className="details-content"></section>
