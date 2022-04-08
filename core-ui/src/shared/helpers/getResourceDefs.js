bb1b06b09e6e20133daf8cddf0052889d5426dff
Jakub Łukomski <dev@tenku.eu>
2022-04-08 08:49:41 +0200

    remove console.logs

--- core-ui/src/shared/helpers/getResourceDefs.js
+++ core-ui/src/shared/helpers/getResourceDefs.js
@@ -2,7 +2,6 @@ import pluralize from 'pluralize';
 import { resources } from 'resources';
 
 export function getResourceDefs(defType, t, context) {
-  console.log('getResourceDefs', { defType });
   return Object.values(resources)
     .filter(resource => resource[defType])
     .map(resource => resource[defType](t, context))
@@ -10,26 +9,11 @@ export function getResourceDefs(defType, t, context) {
 }
 
 export function getPerResourceDefs(defType, t, context) {
-  console.log('getPerResourceDefs', { defType });
-  console.log(
-    Object.fromEntries(
-      Object.entries(resources)
-        .filter(([, resource]) => resource[defType])
-        .map(([resourceName, resource]) => {
-          // ResourcesCreate -> Resource
-          // const kind = pluralize(resourceName.replace('Create', ''), 1);
-          const kind = pluralize(resourceName, 1);
-          const value = resource[defType](t, context);
-          return [kind, value];
-        }),
-    ),
-  );
   return Object.fromEntries(
     Object.entries(resources)
       .filter(([, resource]) => resource[defType])
       .map(([resourceName, resource]) => {
         // ResourcesCreate -> Resource
-        // const kind = pluralize(resourceName.replace('Create', ''), 1);
         const kind = pluralize(resourceName, 1);
         const value = resource[defType](t, context);
         return [kind, value];
