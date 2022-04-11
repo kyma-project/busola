184abc43372c3d3b0653716f08111e714bc2b369
dariadomagala <daria.domagala@sap.com>
2022-04-11 11:13:57 +0200

    Get crd schema

--- core-ui/src/components/Extensibility/extensibilityCreate.js
+++ core-ui/src/components/Extensibility/extensibilityCreate.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { ResourceForm } from 'shared/ResourceForm';
+import { useGet } from 'shared/hooks/BackendAPI/useGet';
+import { Spinner } from 'shared/components/Spinner/Spinner';
 
 export function ExtensibilityCreate({
   formElementRef,
@@ -7,8 +9,18 @@ export function ExtensibilityCreate({
   setCustomValid,
   resourceType,
   resourceUrl,
+  resource: createResource,
 }) {
+  const api = createResource?.nav?.resource;
   const [resource, setResource] = useState({});
+  const { data, loading = true } = useGet(
+    `/apis/apiextensions.k8s.io/v1/customresourcedefinitions/${api.kind}.${api.group}`,
+  );
+  if (loading) return <Spinner />;
+
+  const versions = data?.spec?.versions || [];
+  const schema = versions.find(v => v.name === api.version)?.schema || {};
+  console.log(schema);
 
   return (
     <ResourceForm
