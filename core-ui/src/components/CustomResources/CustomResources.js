b412e88a2691bc78f062fe37e39600ae96a0a00a
mwisnia97 <mwisnia97@gmail.com>
2022-04-12 13:00:27 +0200

    Allow for detailed Custom Resources lists

--- core-ui/src/components/CustomResources/CustomResources.js
+++ core-ui/src/components/CustomResources/CustomResources.js
@@ -56,6 +56,7 @@ export function CustomResources({
     showNamespace,
     hideCreateOption,
     createResourceForm: props => <CRCreate {...props} crd={crd} />,
+    alowSlashShortcut: true,
   };
 
   return <ResourcesList {...params} />;
