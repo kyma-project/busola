645043bf21b670e0c94e2698da68a00f5e7b3aa3
Jakub Lyczko <jakub.lyczko@sap.com>
2022-04-15 13:20:26 +0200

    Fix DNSProviders and Isssuers Details' page layout (#1247)
    * Add file where panel-grid class exists
    
    * Move panel-grid class to the index.scss
    
    * bump deployment

--- core-ui/src/resources/Issuers/IssuerDomains.js
+++ core-ui/src/resources/Issuers/IssuerDomains.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+
 import { GenericList } from 'shared/components/GenericList/GenericList';
 
 export function IssuerDomains(issuer) {
