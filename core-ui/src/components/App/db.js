6f7075a5aa9737686978575df0a389819285e784
Wawrzyn321 <Wawrzyn321@gmail.com>
2022-05-02 12:24:11 +0200

    Revert3

--- core-ui/src/components/App/db.js
+++
@@ -1,9 +0,0 @@
-import Dexie from 'dexie';
-
-export const db = new Dexie('busola.cache');
-
-window.db = db;
-
-// db.version(1).stores({
-//   paths: '[cluster+path], cluster, path, items',
-// });
