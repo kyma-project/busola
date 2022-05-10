cccb0abaf17ae7e3b78051d655c967c58dd3c41f
dariadomagala <daria.domagala@sap.com>
2022-05-10 07:54:51 +0200

    Change log to error

--- core-ui/src/resources/Namespaces/YamlUpload/YamlUploadDialog.js
+++ core-ui/src/resources/Namespaces/YamlUpload/YamlUploadDialog.js
@@ -64,7 +64,7 @@ export function YamlUploadDialog({ show, onCancel }) {
       setResourcesWithStatuses(resourcesWithStatus);
       oldYaml.current = yaml;
     } catch ({ message }) {
-      console.log(message);
+      console.error(message);
     }
   };
 
