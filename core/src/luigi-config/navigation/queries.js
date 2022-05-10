be2378cf6a55c42998c72199c464a828469cb7eb
Wawrzyn321 <Wawrzyn321@gmail.com>
2022-05-10 15:32:15 +0200

    Bump

--- core/src/luigi-config/navigation/queries.js
+++ core/src/luigi-config/navigation/queries.js
@@ -1,3 +1,4 @@
+import { fetchQueue } from 'fetch-queue';
 import { config } from './../config';
 import { getActiveCluster } from './../cluster-management/cluster-management';
 import { getSSOAuthData } from '../auth/sso';
@@ -52,7 +53,7 @@ export async function failFastFetch(input, auth, init = {}) {
 
   init.headers = await createHeaders(auth, input);
 
-  return await fetch(input, init);
+  return await fetchQueue(input, init);
 }
 
 export async function checkIfClusterRequiresCA(auth) {
