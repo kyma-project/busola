11c29d76620dc0dbceb1ad10fd3da8b5381e1047
mwisnia97 <mwisnia97@gmail.com>
2022-05-12 09:31:08 +0200

    First implementation with egenriclist and table data

---
+++ core-ui/src/components/Extensibility/ds/widgets-details/GenericListRenderer.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { GenericList } from 'shared/components/GenericList/GenericList';
+import { useTranslation } from 'react-i18next';
+
+export function GenericListRenderer({
+  onChange,
+  onKeyDown,
+  value,
+  schema,
+  storeKeys,
+  required,
+}) {
+  const { t, i18n } = useTranslation();
+
+  return (
+    <GenericList
+      title={t(ACCESS_STRATEGIES_PANEL.LIST.TITLE)}
+      showSearchField={''}
+      textSearchProperties={''}
+      showSearchSuggestion={false}
+      entries={''}
+      headerRenderer={''}
+      rowRenderer={''}
+      noSearchResultMessage={''}
+      i18n={i18n}
+    />
+  );
+}
+{
+  /* <GenericList
+  textSearchProperties={textSearchProperties}
+  showSearchSuggestion={false}
+  entries={entries}
+  headerRenderer={headerRenderer}
+  rowRenderer={rowRenderer}
+  actions={actions}
+  extraHeaderContent={extraHeaderContent}
+  noSearchResultMessage={t('clusters.list.no-clusters-found')}
+  i18n={i18n}
+  allowSlashShortcut
+/>; */
+}
