9250159649a1a8098a3992ad3335d587e8990f61
Jakub Łukomski <dev@tenku.eu>
2022-05-19 08:27:45 +0200

    support enums in StringRenderer

--- core-ui/src/components/Extensibility/ds/widgets-form/StringRenderer.js
+++ core-ui/src/components/Extensibility/ds/widgets-form/StringRenderer.js
@@ -12,9 +12,31 @@ export function StringRenderer({
   schema,
   storeKeys,
   required,
+  ...props
 }) {
-  if (schema.enum) {
-    return 'ENUM';
+  if (schema.get('enum')) {
+    const options = schema
+      .get('enum')
+      .toArray()
+      .map(key => ({ key, text: key }));
+    return (
+      <ResourceForm.FormField
+        value={value}
+        setValue={value => {
+          onChange({
+            storeKeys,
+            scopes: ['value'],
+            type: 'set',
+            schema,
+            required,
+            data: { value },
+          });
+        }}
+        label={<TransTitle schema={schema} storeKeys={storeKeys} />}
+        input={Inputs.ComboboxInput}
+        options={options}
+      />
+    );
   } else {
     return (
       <ResourceForm.FormField
