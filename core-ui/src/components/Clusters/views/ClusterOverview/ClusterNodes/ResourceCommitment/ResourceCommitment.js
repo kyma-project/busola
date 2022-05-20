95a24853015e99cf4d72d19a7ddfa846b28c2b79
Wawrzyn321 <Wawrzyn321@gmail.com>
2022-05-20 13:39:00 +0200

    Make less queries

--- core-ui/src/components/Clusters/views/ClusterOverview/ClusterNodes/ResourceCommitment/ResourceCommitment.js
+++ core-ui/src/components/Clusters/views/ClusterOverview/ClusterNodes/ResourceCommitment/ResourceCommitment.js
@@ -1,166 +1,127 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useFeature } from 'shared/hooks/useFeature';
-import { useGet } from 'shared/hooks/BackendAPI/useGet';
-import { Tooltip } from 'shared/components/Tooltip/Tooltip';
 import { LayoutPanel } from 'fundamental-react';
+import { Dropdown } from 'shared/ResourceForm/inputs';
+import { useTranslation } from 'react-i18next';
+import { useCurrentQuery } from './queries';
 import './ResourceCommitment.scss';
+import { Spinner } from 'shared/components/Spinner/Spinner';
 
-function useMiniQuery(serviceUrl, query, time) {
-  query = encodeURIComponent(query);
-  const url = `${serviceUrl}/query?query=${query}&time=${time}`;
-  const { data, loading, error } = useGet(url, {
-    pollingInterval: 0,
-    skip: !serviceUrl,
+function useMetricQuery({ serviceUrl, time }) {
+  const { t } = useTranslation();
+
+  const CPU_QUERY = {
+    text: t('graphs.resource-commitment.cpu'),
+    key: 'cpu',
+  };
+
+  const MEMORY_QUERY = {
+    text: t('graphs.resource-commitment.memory'),
+    key: 'memory',
+  };
+
+  const [queryType, setQueryType] = useState(CPU_QUERY.key);
+  const queryResults = useCurrentQuery({
+    serviceUrl,
+    time,
+    queryType,
   });
 
-  // eslint-disable-next-line no-unused-vars
-  const [_date, value] = data?.data?.result[0]?.value || [];
-  return { value: parseFloat(value), loading, error };
-}
+  const QueryDropdown = (
+    <Dropdown
+      compact={false}
+      fullWidth={false}
+      className="query-dropdown"
+      selectedKey={queryType}
+      onSelect={(_, { key }) => {
+        setQueryType(key);
+      }}
+      options={[CPU_QUERY, MEMORY_QUERY]}
+    />
+  );
 
-function flattenRequests(requests) {
   return {
-    data: requests.map(request => ({
-      name: request.name,
-      value: request.query.value,
-    })),
-    loading: requests.some(query => query.loading), // any loading=true
-    error: requests.find(query => query.error), // find first error
+    QueryDropdown,
+    queryResults,
+    queryType,
   };
 }
 
-function useMemoryQueries(serviceUrl, time) {
-  const queries = [
+function CommitmentGraph({ data }) {
+  data = [
+    ...data,
     {
-      name: 'allocated',
-      query: useMiniQuery(
-        serviceUrl,
-        '1 - sum(:node_memory_MemAvailable_bytes:sum{cluster=""}) / sum(node_memory_MemTotal_bytes{job="node-exporter",cluster=""})',
-        time,
-      ),
-    },
-    {
-      name: 'requests',
-      query: useMiniQuery(
-        serviceUrl,
-        'sum(namespace_memory:kube_pod_container_resource_requests:sum{cluster=""}) / sum(kube_node_status_allocatable{job="kube-state-metrics",resource="memory",cluster=""})',
-        time,
-      ),
-    },
-    {
-      name: 'limits',
-      query: useMiniQuery(
-        serviceUrl,
-        'sum(namespace_memory:kube_pod_container_resource_limits:sum{cluster=""}) / sum(kube_node_status_allocatable{job="kube-state-metrics",resource="memory",cluster=""})',
-        time,
-      ),
+      name: 'allocatable',
+      value: 1,
     },
   ];
+  data.sort((a, b) => a.value - b.value);
 
-  return flattenRequests(queries);
+  const max = data.at(-1).value;
+
+  data.forEach((v, i) => {
+    v.width = data[i - 1] ? data[i].value - data[i - 1].value : data[i].value;
+  });
+
+  return (
+    <div className="graph">
+      {data.map((v, i) => {
+        return (
+          <div
+            key={v.name}
+            className={`box--${v.name}`}
+            style={{
+              width: `${(v.width / max) * 100}%`,
+              zIndex: data.length - i,
+            }}
+          ></div>
+        );
+      })}
+    </div>
+  );
 }
 
-function useCpuQueries(serviceUrl, time) {
-  const queries = [
-    {
-      name: 'allocated',
-      query: useMiniQuery(
-        serviceUrl,
-        '1 - sum(avg by (mode) (rate(node_cpu_seconds_total{job="node-exporter", mode=~"idle|iowait|steal", cluster=""}[150s])))',
-        time,
-      ),
-    },
-    {
-      name: 'limits',
-      query: useMiniQuery(
-        serviceUrl,
-        'sum(namespace_cpu:kube_pod_container_resource_limits:sum{cluster=""}) / sum(kube_node_status_allocatable{job="kube-state-metrics",resource="cpu",cluster=""})',
-        time,
-      ),
-    },
-    {
-      name: 'requests',
-      query: useMiniQuery(
-        serviceUrl,
-        'sum(namespace_cpu:kube_pod_container_resource_requests:sum{cluster=""}) / sum(kube_node_status_allocatable{job="kube-state-metrics",resource="cpu",cluster=""})',
-        time,
-      ),
-    },
-  ];
+function ResourceCommitmentComponent({ serviceUrl }) {
+  const { t } = useTranslation();
+  const [time, setTime] = useState(Math.floor(Date.now() / 1000));
+  const { QueryDropdown, queryResults, queryType } = useMetricQuery({
+    serviceUrl,
+    time,
+  });
 
-  return flattenRequests(queries);
+  const { data, loading, error } = queryResults;
+
+  useEffect(() => {
+    const REFETCH_RATE_MS = 30 * 1000;
+    const id = setInterval(
+      () => setTime(Math.floor(Date.now() / 1000)),
+      REFETCH_RATE_MS,
+    );
+    return () => clearInterval(id);
+  }, [queryType]);
+
+  const content = () => {
+    if (loading) {
+      return <Spinner />;
+    } else if (error) {
+      return <p>{t('common.messages.error', { error: error.message })}</p>;
+    } else {
+      return <CommitmentGraph data={data} />;
+    }
+  };
+
+  return (
+    <LayoutPanel className="commitment-graph">
+      <LayoutPanel.Header>
+        <LayoutPanel.Filters>{QueryDropdown}</LayoutPanel.Filters>
+      </LayoutPanel.Header>
+      <LayoutPanel.Body className="graph-wrapper">{content()}</LayoutPanel.Body>
+    </LayoutPanel>
+  );
 }
 
 export function ResourceCommitment() {
   const { isEnabled, serviceUrl } = useFeature('PROMETHEUS');
-  const [time] = useState(Math.floor(Date.now() / 1000));
-
-  const memoryQueries = useMemoryQueries(serviceUrl, time);
-  const cpuQueries = useCpuQueries(serviceUrl, time);
-  if (memoryQueries.loading || cpuQueries.loading) {
-    return null;
-  }
-  const { data: memory } = memoryQueries;
-  const { data: cpu } = cpuQueries;
-
-  memory.push({
-    name: 'allocatable',
-    value: 1,
-  });
-  cpu.push({
-    name: 'allocatable',
-    value: 1,
-  });
-
-  memory.sort((a, b) => a.value - b.value);
-  cpu.sort((a, b) => a.value - b.value);
-
   if (!isEnabled) return null;
-
-  const Sub = ({ title, data }) => {
-    const max = data.at(-1).value;
-
-    data.forEach((v, i) => {
-      v.width = data[i - 1] ? data[i].value - data[i - 1].value : data[i].value;
-    });
-
-    return (
-      <>
-        <p>{title}</p>
-        <div className="main">
-          {data.map((v, i) => {
-            return (
-              <div
-                key={v.name}
-                className={`box--${v.name}`}
-                style={{
-                  width: `${(v.width / max) * 100}%`,
-                  zIndex: data.length - i,
-                }}
-              >
-                <Tooltip
-                  position="bottom"
-                  content={`${v.name}, val: ${v.value.toFixed(2)}`}
-                >
-                  <div className="in" />
-                </Tooltip>
-              </div>
-            );
-          })}
-        </div>
-      </>
-    );
-  };
-
-  return (
-    <LayoutPanel>
-      <LayoutPanel.Header>
-        <LayoutPanel.Head title="Resource Commitment" />
-      </LayoutPanel.Header>
-      <LayoutPanel.Body>
-        <Sub title="CPU" data={cpu} />
-        <Sub title="Memory" data={memory} />
-      </LayoutPanel.Body>
-    </LayoutPanel>
-  );
+  return <ResourceCommitmentComponent serviceUrl={serviceUrl} />;
 }
---
+++ core-ui/src/components/Clusters/views/ClusterOverview/ClusterNodes/ResourceCommitment/queries.js
@@ -0,0 +1,58 @@
+import { useGet } from 'shared/hooks/BackendAPI/useGet';
+
+function useMiniQuery(serviceUrl, query, time, skip) {
+  query = encodeURIComponent(query);
+  const url = `${serviceUrl}/query?query=${query}&time=${time}`;
+  const { data, loading, error } = useGet(url, {
+    pollingInterval: 0,
+    skip: !serviceUrl || skip,
+  });
+
+  // eslint-disable-next-line no-unused-vars
+  const [_date, value] = data?.data?.result[0]?.value || [];
+  return { value: parseFloat(value) || null, loading, error };
+}
+
+export function useCurrentQuery({ serviceUrl, time, queryType }) {
+  const flattenRequests = requests => {
+    return {
+      data: requests.map(request => ({
+        name: request.name,
+        value: request.query.value,
+      })),
+      loading: requests.some(request => request.query.loading), // any loading=true
+      error: requests.find(request => request.query.error)?.query?.error, // find first error
+    };
+  };
+
+  const queries = {
+    cpu: {
+      allocated:
+        '1 - sum(avg by (mode) (rate(node_cpu_seconds_total{job="node-exporter", mode=~"idle|iowait|steal", cluster=""}[150s])))',
+      limits:
+        'sum(namespace_cpu:kube_pod_container_resource_requests:sum{cluster=""}) / sum(kube_node_status_allocatable{job="kube-state-metrics",resource="cpu",cluster=""})',
+      requests:
+        'sum(namespace_cpu:kube_pod_container_resource_limits:sum{cluster=""}) / sum(kube_node_status_allocatable{job="kube-state-metrics",resource="cpu",cluster=""})',
+    },
+    memory: {
+      allocated:
+        '1 - sum(:node_memory_MemAvailable_bytes:sum{cluster=""}) / sum(node_memory_MemTotal_bytes{job="node-exporter",cluster=""})',
+      limits:
+        'sum(namespace_memory:kube_pod_container_resource_limits:sum{cluster=""}) / sum(kube_node_status_allocatable{job="kube-state-metrics",resource="memory",cluster=""})',
+      requests:
+        'sum(namespace_memory:kube_pod_container_resource_requests:sum{cluster=""}) / sum(kube_node_status_allocatable{job="kube-state-metrics",resource="memory",cluster=""})',
+    },
+  }[queryType];
+
+  return flattenRequests([
+    {
+      name: 'allocated',
+      query: useMiniQuery(serviceUrl, queries.allocated, time),
+    },
+    { name: 'limits', query: useMiniQuery(serviceUrl, queries.limits, time) },
+    {
+      name: 'requests',
+      query: useMiniQuery(serviceUrl, queries.requests, time),
+    },
+  ]);
+}
