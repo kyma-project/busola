7e143cee87f8dd6a01cccc73c5c8f22b1dbe320a
Wawrzyn321 <Wawrzyn321@gmail.com>
2022-05-25 09:20:56 +0200

    WRONG

--- core-ui/src/components/Clusters/views/ClusterOverview/ClusterNodes/ResourceCommitment/ResourceCommitment.js
+++ core-ui/src/components/Clusters/views/ClusterOverview/ClusterNodes/ResourceCommitment/ResourceCommitment.js
@@ -6,6 +6,7 @@ import { useTranslation } from 'react-i18next';
 import { useCurrentQuery } from './queries';
 import './ResourceCommitment.scss';
 import { Spinner } from 'shared/components/Spinner/Spinner';
+import { Tooltip } from 'shared/components/Tooltip/Tooltip';
 
 function useMetricQuery({ serviceUrl, time }) {
   const { t } = useTranslation();
@@ -48,36 +49,69 @@ function useMetricQuery({ serviceUrl, time }) {
 }
 
 function CommitmentGraph({ data }) {
+  const { t } = useTranslation();
   data = [
-    ...data,
     {
       name: 'allocatable',
       value: 1,
     },
+    ...data,
   ];
+  // make sure legend data is not sorted
+  const legendData = [...data];
   data.sort((a, b) => a.value - b.value);
 
   const max = data.at(-1).value;
 
+  let accumulatedX = 0;
   data.forEach((v, i) => {
     v.width = data[i - 1] ? data[i].value - data[i - 1].value : data[i].value;
+    v.xPos = accumulatedX / max;
+    accumulatedX += v.width * 100;
   });
 
+  const allocated = data.find(d => d.name === 'allocated').value;
+  const requests = data.find(d => d.name === 'requests').value;
+  const limits = data.find(d => d.name === 'limits').value;
+
+  // console.log({ allocated, requests, limits });
+  // console.log(
+  //   'allocated should be between ',
+  //   allocated * 0.75,
+  //   '(requests) and ',
+  //   allocated * 1.5 + '(limits)',
+  // );
+  // console.log('req:', (requests / allocated) * 100);
+  // console.log('lim:', (limits / allocated) * 100);
+
   return (
-    <div className="graph">
-      {data.map((v, i) => {
-        return (
-          <div
-            key={v.name}
-            className={`box--${v.name}`}
-            style={{
-              width: `${(v.width / max) * 100}%`,
-              zIndex: data.length - i,
-            }}
-          ></div>
-        );
-      })}
-    </div>
+    <>
+      <svg viewBox="0 0 100 8" xmlns="http://www.w3.org/2000/svg">
+        {data.map(v => {
+          return (
+            <rect
+              key={v.name}
+              width={(v.width / max) * 100}
+              height="100"
+              x={v.xPos}
+              className={`graph-box--${v.name}`}
+            />
+          );
+        })}
+      </svg>
+      <legend className="graph-legend">
+        {legendData.map(v => (
+          <div key={v.name}>
+            <Tooltip
+              content={t(`graphs.resource-commitment.tooltips.${v.name}`)}
+            >
+              <div className={`legend-box graph-box--${v.name}`}></div>
+              <span>{t(`graphs.resource-commitment.legend.${v.name}`)}</span>
+            </Tooltip>
+          </div>
+        ))}
+      </legend>
+    </>
   );
 }
 
@@ -92,7 +126,7 @@ function ResourceCommitmentComponent({ serviceUrl }) {
   const { data, loading, error } = queryResults;
 
   useEffect(() => {
-    const REFETCH_RATE_MS = 30 * 1000;
+    const REFETCH_RATE_MS = 60 * 1000;
     const id = setInterval(
       () => setTime(Math.floor(Date.now() / 1000)),
       REFETCH_RATE_MS,
