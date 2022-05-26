ce001e21c15058f1826f16c6010ae26da3cc5333
Wawrzyn321 <Wawrzyn321@gmail.com>
2022-05-26 16:28:11 +0200

    Fix naming

--- core-ui/src/components/Clusters/views/ClusterOverview/ClusterNodes/ResourceCommitment/queries.js
+++ core-ui/src/components/Clusters/views/ClusterOverview/ClusterNodes/ResourceCommitment/queries.js
@@ -1,11 +1,11 @@
 import { useGet } from 'shared/hooks/BackendAPI/useGet';
 
-function useMiniQuery(serviceUrl, query, time, skip) {
+function usePrometheusQuery(serviceUrl, query, time) {
   query = encodeURIComponent(query);
   const url = `${serviceUrl}/query?query=${query}&time=${time}`;
   const { data, loading, error } = useGet(url, {
     pollingInterval: 0,
-    skip: !serviceUrl || skip,
+    skip: !serviceUrl,
   });
 
   // eslint-disable-next-line no-unused-vars
@@ -27,31 +27,34 @@ export function useCurrentQuery({ serviceUrl, time, queryType }) {
   const queries = {
     cpu: {
       utilized:
-        '1 - sum(avg by (mode) (rate(node_cpu_seconds_total{job="node-exporter", mode=~"idle|iowait|steal", cluster=""}[150s])))',
+        '1 - sum(avg by (mode) (rate(node_cpu_seconds_total{ mode=~"idle|iowait|steal", }[150s])))',
       limits:
-        'sum(namespace_cpu:kube_pod_container_resource_limits:sum{cluster=""}) / sum(kube_node_status_allocatable{job="kube-state-metrics",resource="cpu",cluster=""})',
+        'sum(namespace_cpu:kube_pod_container_resource_limits:sum{}) / sum(kube_node_status_allocatable{job="kube-state-metrics",resource="cpu"})',
       requests:
-        'sum(namespace_cpu:kube_pod_container_resource_requests:sum{cluster=""}) / sum(kube_node_status_allocatable{job="kube-state-metrics",resource="cpu",cluster=""})',
+        'sum(namespace_cpu:kube_pod_container_resource_requests:sum{}) / sum(kube_node_status_allocatable{job="kube-state-metrics",resource="cpu"})',
     },
     memory: {
       utilized:
-        '1 - sum(:node_memory_MemAvailable_bytes:sum{cluster=""}) / sum(node_memory_MemTotal_bytes{job="node-exporter",cluster=""})',
+        '1 - sum(:node_memory_MemAvailable_bytes:sum{}) / sum(node_memory_MemTotal_bytes{})',
       limits:
-        'sum(namespace_memory:kube_pod_container_resource_limits:sum{cluster=""}) / sum(kube_node_status_allocatable{job="kube-state-metrics",resource="memory",cluster=""})',
+        'sum(namespace_memory:kube_pod_container_resource_limits:sum{}) / sum(kube_node_status_allocatable{job="kube-state-metrics",resource="memory"})',
       requests:
-        'sum(namespace_memory:kube_pod_container_resource_requests:sum{cluster=""}) / sum(kube_node_status_allocatable{job="kube-state-metrics",resource="memory",cluster=""})',
+        'sum(namespace_memory:kube_pod_container_resource_requests:sum{}) / sum(kube_node_status_allocatable{job="kube-state-metrics",resource="memory"})',
     },
   }[queryType];
 
   return flattenRequests([
     {
       name: 'utilized',
-      query: useMiniQuery(serviceUrl, queries.utilized, time),
+      query: usePrometheusQuery(serviceUrl, queries.utilized, time),
     },
-    { name: 'limits', query: useMiniQuery(serviceUrl, queries.limits, time) },
+    {
+      name: 'limits',
+      query: usePrometheusQuery(serviceUrl, queries.limits, time),
+    },
     {
       name: 'requests',
-      query: useMiniQuery(serviceUrl, queries.requests, time),
+      query: usePrometheusQuery(serviceUrl, queries.requests, time),
     },
   ]);
 }
