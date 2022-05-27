689b3745fcbd590b12e25ea30f74d917344143c5
dariadomagala <daria.domagala@sap.com>
2022-05-27 07:26:30 +0200

    Change the graph color

--- core-ui/src/components/Clusters/views/ClusterOverview/ClusterNodes/ClusterNodes.js
+++ core-ui/src/components/Clusters/views/ClusterOverview/ClusterNodes/ClusterNodes.js
@@ -96,7 +96,7 @@ export function ClusterNodes() {
               percentage: cpu.percentage,
             }),
             position: 'bottom',
-            color: 'var(--sapIndicationColor_5)',
+            color: 'var(--sapIndicationColor_7)',
           }}
         />
       ) : (
