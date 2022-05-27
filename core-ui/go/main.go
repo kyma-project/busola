50e1c8b291538a1fff2c58813d61a0cb410789d3
Jakub Lyczko <jakub.lyczko1@gmail.com>
2022-05-27 11:22:51 +0200

    Importing wasm file works

--- core-ui/go/main.go
+++ core-ui/go/main.go
@@ -1,31 +1,71 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"github.com/datreeio/datree/pkg/jsonSchemaValidator"
+// 	"syscall/js"
+// )
+
+// func ValidateUserYaml() js.Func {
+// 	jsv := jsonSchemaValidator.New()
+
+// 	x :=
+// 		`napiVersion: v1
+// 	 policies:
+// 	  - name: "policy1"
+// 		isDefault: true
+// 	  - name: "policy1"`
+
+// 	schema := `type: object
+// 	  properties:
+// 		apiVersion:
+// 		  type: string
+// 	  required:
+// 		- apiVersion`
+
+// 	_, err := jsv.ValidateYamlSchema(schema, x)
+// 	if err != nil {
+// 		fmt.Printf("err %v", err)
+// 	}
+// 	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+// 		return "hello"
+// 	})
+// }
+
+// func registerCallbacks() {
+// 	js.Global().Set("validateUserYaml", ValidateUserYaml)
+// }
+
+// func main() {
+
+// 	c := make(chan struct{})
+
+// 	println("WASM Go Initialized")
+// 	// register functions
+// 	registerCallbacks()
+// 	<-c
+// }
+
 package main
 
 import (
 	"fmt"
-
-	"github.com/datreeio/datree/pkg/jsonSchemaValidator"
+	"syscall/js"
 )
 
+var htmlString = `<h4>Hello, I'm an HTML snippet from Go!</h4>`
+
+func GetHtml() js.Func {
+	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		return htmlString
+	})
+}
+
 func main() {
-	jsv := jsonSchemaValidator.New()
 
-	x := 
-	`napiVersion: v1
-	 policies:
-	  - name: "policy1"
-		isDefault: true
-	  - name: "policy1"`
+	ch := make(chan struct{}, 0)
+	fmt.Printf("Hello Web Assembly from Go hahaha!\n")
 
-	  schema := `type: object
-	  properties:
-		apiVersion:
-		  type: string
-	  required:
-		- apiVersion`
-
-	_, err := jsv.ValidateYamlSchema(schema, x)
-	if err != nil {
-		fmt.Printf("err %v", err)
-	}
-
+	js.Global().Set("getHtml", GetHtml())
+	<-ch
 }
--- core-ui/src/components/Clusters/views/ClusterOverview/ClusterOverview.js
+++ core-ui/src/components/Clusters/views/ClusterOverview/ClusterOverview.js
@@ -4,6 +4,13 @@ import { ClusterNodes } from './ClusterNodes/ClusterNodes';
 import './ClusterOverview.scss';
 
 export function ClusterOverview() {
+  const goWasm = new window.Go();
+
+  WebAssembly.instantiateStreaming(
+    fetch('main.wasm'),
+    goWasm.importObject,
+  ).then(result => goWasm.run(result.instance));
+
   return (
     <>
       <ClusterOverviewHeader />
