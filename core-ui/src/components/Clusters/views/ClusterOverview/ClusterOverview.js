66cabfabf3b846fe1c9dd04fa42df8d3716dec6b
Jakub Lyczko <jakub.lyczko1@gmail.com>
2022-06-03 15:01:44 +0200

    Get rid off golang and wasm

--- core-ui/src/components/Clusters/views/ClusterOverview/ClusterOverview.js
+++ core-ui/src/components/Clusters/views/ClusterOverview/ClusterOverview.js
@@ -1,16 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ClusterOverviewHeader } from './ClusterOverviewHeader';
 import { ClusterNodes } from './ClusterNodes/ClusterNodes';
 import './ClusterOverview.scss';
 
 export function ClusterOverview() {
-  const goWasm = new window.Go();
-
-  WebAssembly.instantiateStreaming(
-    fetch('main.wasm'),
-    goWasm.importObject,
-  ).then(result => goWasm.run(result.instance));
-
   return (
     <>
       <ClusterOverviewHeader />
