d666c4d4b72fef5a3b4cf4169dbda3fae8c9a043
Jakub Lyczko <jakub.lyczko1@gmail.com>
2022-06-10 15:21:39 +0200

    Add after created fn

--- core-ui/src/components/Extensibility/ExtensibilityCreate.js
+++ core-ui/src/components/Extensibility/ExtensibilityCreate.js
@@ -5,6 +5,8 @@ import { useMicrofrontendContext } from 'shared/contexts/MicrofrontendContext';
 
 import { createTemplate } from './helpers';
 import { ResourceSchema } from './ResourceSchema';
+import { useNotification } from 'shared/contexts/NotificationContext';
+import { useTranslation } from 'react-i18next';
 
 export function ExtensibilityCreate({
   formElementRef,
@@ -12,27 +14,54 @@ export function ExtensibilityCreate({
   resourceType,
   resourceUrl,
   resource: createResource,
+  toggleFormFn,
+  ...props
 }) {
   const { namespaceId: namespace } = useMicrofrontendContext();
+  const notification = useNotification();
+  const { t } = useTranslation();
   const api = createResource?.resource || {};
 
   const [resource, setResource] = useState(
     createResource?.template ||
       createTemplate(api, namespace, createResource?.navigation?.scope),
   );
+
   //TODO filter schema based on form configuration
   const schema = createResource?.schema;
+
+  console.log('props', props);
+  const afterCreatedFn = async defaultAfterCreatedFn => {
+    console.log('api', api);
+    console.log('createres', createResource);
+    if (createResource?.details) {
+      defaultAfterCreatedFn();
+    } else {
+      toggleFormFn(false);
+      notification.notifySuccess({
+        content: t(
+          !props?.item //how to check if it is edit/create mode
+            ? 'common.create-form.messages.patch-success'
+            : 'common.create-form.messages.create-success',
+          {
+            resourceType: api?.kind,
+          },
+        ),
+      });
+    }
+  };
 
   return (
     <ResourceForm
       pluralKind={resourceType}
-      singularName={resourceType}
+      singularName={api?.kind}
       resource={resource}
       setResource={setResource}
       formElementRef={formElementRef}
       createUrl={resourceUrl}
       setCustomValid={setCustomValid}
       onlyYaml={!schema}
+      afterCreatedFn={afterCreatedFn}
     >
       <ResourceSchema
         simple
