253f91a810d393012e84e9073eb96b4ca13bc83e
wchrapka <wojciech.chrapka@sap.com>
2022-06-10 15:20:30 +0200

    add SecretRef component

---
+++ core-ui/src/components/Extensibility/components/SecretRefs.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Link } from 'fundamental-react';
+
+import { useGetTranslation } from '../helpers';
+import { GenericList } from 'shared/components/GenericList/GenericList';
+import { useTranslation } from 'react-i18next';
+import { navigateToResource } from 'shared/helpers/universalLinks';
+
+export function SecretRefs({ value, structure, schema }) {
+  const { t, i18n } = useTranslation();
+  const { widgetT } = useGetTranslation();
+
+  //kyma logpipeline api can return object in place of array wrongly, if only one record is defined
+  const sanitizedValue =
+    !Array.isArray(value) && value?.name && value?.namespace ? [value] : value;
+
+  const headerRenderer = () => [
+    t('common.headers.link'),
+    t('common.headers.namespace'),
+    t('common.headers.name'),
+  ];
+
+  const rowRenderer = ({ name, namespace }) => [
+    <Link
+      onClick={() =>
+        navigateToResource({
+          kind: 'Secret',
+          name,
+          namespace,
+        })
+      }
+    >
+      {namespace}/{name}
+    </Link>,
+    namespace,
+    name,
+  ];
+
+  return (
+    <GenericList
+      title={widgetT(structure)}
+      entries={sanitizedValue || []}
+      headerRenderer={headerRenderer}
+      rowRenderer={rowRenderer}
+      i18n={i18n}
+      showSearchField={false}
+    />
+  );
+}
