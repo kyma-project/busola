6b13c33521d8897767915371ac94542aaf55f91d
dariadomagala <daria.domagala@sap.com>
2022-06-20 14:10:05 +0200

    Add simpledetails

--- core-ui/src/components/Extensibility/components/ControlledBy.js
+++ core-ui/src/components/Extensibility/components/ControlledBy.js
@@ -11,7 +11,6 @@ export function ControlledBy({ value, structure, schema }) {
   return isNil(value) ? EMPTY_TEXT_PLACEHOLDER : <CB ownerReferences={value} />;
 }
 export function ControlledByKind({ value, structure, schema }) {
-  console.log(value);
   return isNil(value) ? (
     EMPTY_TEXT_PLACEHOLDER
   ) : (
