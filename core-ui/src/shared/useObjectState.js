36f311c86b400a5465fb7fab77e58ec52351954f
Wawrzyn321 <Wawrzyn321@gmail.com>
2022-06-27 08:33:09 +0200

    Rename useObjectState

--- core-ui/src/shared/useObjectState.js
+++ core-ui/src/shared/useObjectState.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export function useResourceCache() {
+export function useObjectState() {
   const [resourceCache, setResourceCache] = useState({});
 
   const updateResourceCache = (key, value) => {
