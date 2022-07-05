decdd8b6cf6a6a8c8181fe742c154d5242d73eb9
Agata Kucharska <akucharska@users.noreply.github.com>
2022-07-05 10:49:50 +0200

    Add widget that display joined array entries (#1529)
    * Add widget that joins array entries
    
    * Bump image
    
    * Add documentation
    
    * Update docs/extensibility/display-widgets.md
    
    Co-authored-by: Jakub Łukomski <dev@tenku.eu>
    
    * Review fixes
    
    * Revert Widget.js
    
    * Apply suggestions from code review
    
    Co-authored-by: Maja Szostok <maja.szostok@sap.com>
    
    * use better png
    
    * Apply suggestions from code review
    
    Co-authored-by: Jakub Łukomski <dev@tenku.eu>
    
    * Fix error
    
    Co-authored-by: Jakub Łukomski <dev@tenku.eu>
    Co-authored-by: Maja Szostok <maja.szostok@sap.com>

---
+++ core-ui/src/components/Extensibility/components/JoinedArray.js
@@ -0,0 +1,20 @@
+import { isNil } from 'lodash';
+import { EMPTY_TEXT_PLACEHOLDER } from 'shared/constants';
+import { useTranslation } from 'react-i18next';
+
+export function JoinedArray({ value, structure, schema }) {
+  const { t } = useTranslation();
+  if (isNil(value)) {
+    return EMPTY_TEXT_PLACEHOLDER;
+  } else if (
+    !Array.isArray(value) ||
+    value.some(item => typeof item === Object || typeof item === Array)
+  ) {
+    return t('extensibility.widgets.joined-array.error');
+  }
+
+  return value.join(structure.separator ? structure.separator : ', ');
+}
+
+JoinedArray.array = true;
+JoinedArray.inline = true;
