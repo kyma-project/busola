509f6ef6e927b05591feead2fd7ae96cbd996bc5
Jakub Lyczko <jakub.lyczko1@gmail.com>
2022-07-06 09:57:07 +0200

    testing

--- core-ui/src/shared/components/GraphLegend/GraphLegend.js
+++ core-ui/src/shared/components/GraphLegend/GraphLegend.js
@@ -2,22 +2,39 @@ import React, { useState, useRef, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
+import { useTheme } from 'shared/contexts/ThemeContext';
 import './GraphLegend.scss';
 
 export function GraphLegend({ values, isStatsPanel = true }) {
   const { t } = useTranslation();
-  const css = useRef();
+  // const css = useRef();
   const [color, setColor] = useState('');
-  const barColors = color.trim().split(/ +/);
+  // const [barColors, setBarColors] = useState([]);
+  const barColors = color?.trim().split(/ +/);
+  const { theme } = useTheme();
+
+  // console.log('theme', theme);
 
   useEffect(() => {
     const canvas = document.querySelector('canvas.stats-graph');
-
     if (!canvas) return;
-    css.current = getComputedStyle(canvas);
-    const barColor = css.current.getPropertyValue('--bar-color');
+    // console.log('hi');
+    // const handleCss = () => {
+
+    const css = getComputedStyle(canvas);
+    const barColor = css.getPropertyValue('--bar-color');
+    console.log(barColor);
     setColor(barColor);
-  }, [values]);
+    // };
+
+    // const cssObserver = setInterval(handleCss, 100);
+
+    // return () => clearInterval(cssObserver);
+  }, [theme, values]);
+
+  // useEffect(() => {
+  //   setBarColors(color?.trim().split(/ +/));
+  // }, [theme, color]);
 
   return (
     <legend className="commitment-graph__legend">
