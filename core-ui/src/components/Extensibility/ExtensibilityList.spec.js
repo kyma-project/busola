62bec31a7330b0b2fbf6394c8fdac17993b07d70
dariadomagala <daria.domagala@sap.com>
2022-07-07 13:44:57 +0200

    Add fist draft test pt3

--- core-ui/src/components/Extensibility/ExtensibilityList.spec.js
+++ core-ui/src/components/Extensibility/ExtensibilityList.spec.js
@@ -2,28 +2,40 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { ExtensibilityList } from './ExtensibilityList';
 
+const resourcePath = 'myCustomPath';
+const resourceKind = 'MyCustomResource';
+const resourceUrl = 'mycustomresources';
+const translations = {
+  name: 'MyResource',
+  description: 'This is my resource',
+};
+
+// those mocks have to start with `mock`
+const mockLocation = new URL('https://example.com');
+const mockUseGetCRbyPath = jest.fn();
+jest.mock('./useGetCRbyPath', () => ({
+  useGetCRbyPath: y => mockUseGetCRbyPath(y),
+}));
+
 jest.mock('react-i18next', () => ({
   useTranslation: () => ({
-    t: key => key,
+    t: key => {
+      const translationKey = key.split('::')[1];
+      if (translationKey) return translations[translationKey];
+      return key;
+    },
   }),
 }));
 
 jest.mock('./ExtensibilityCreate', () => ({
   ExtensibilityCreate: data => {
-    return JSON.stringify(data);
+    return (
+      <>
+        <p>Create form for with properties: </p>
+        Url: {data.resourceUrl}
+      </>
+    );
   },
-}));
-
-jest.mock('./useGetCRbyPath', () => ({
-  useGetCRbyPath: () => ({
-    resource: {
-      path: 'myCustomPath',
-      kind: 'myCustomKind',
-      name: 'My custom name',
-    },
-    schema: {},
-    relations: {},
-  }),
 }));
 
 jest.mock('./components/Widget', () => ({
@@ -38,15 +50,50 @@ jest.mock('shared/components/ResourcesList/ResourcesList', () => ({
     return (
       <>
         <CreateForm {...data} />
-        {JSON.stringify(data)}
+        <p>List of resources with properties: </p>
+        Name: {data.resourceName}
+        Description: {data.description}
+        Type: {data.resourceType}
+        Custom columns:{' '}
+        {data.customColumns?.length
+          ? 'has custom columns'
+          : "doesn't have custom columns"}
       </>
     );
   },
 }));
 
 describe('ExtensibilityList', () => {
-  it("Doesn't render for empty data", () => {
+  it('Renders a simple list for only required data', () => {
+    mockUseGetCRbyPath.mockImplementationOnce(() => ({
+      resource: {
+        path: resourcePath,
+        kind: resourceKind,
+      },
+    }));
     const { getByText } = render(<ExtensibilityList />);
-    expect(getByText(new RegExp('myCustomPath', 'i'))).toBeVisible();
+    expect(getByText(new RegExp(resourcePath, 'i'))).toBeVisible();
+    expect(getByText(new RegExp(translations.name, 'i'))).toBeVisible();
+    expect(getByText(new RegExp(translations.description, 'i'))).toBeVisible();
+    expect(getByText(new RegExp(resourceUrl, 'i'))).toBeVisible();
+    expect(
+      getByText(new RegExp("doesn't have custom columns", 'i')),
+    ).toBeVisible();
+  });
+
+  it('Renders a complex list for more complex data', () => {
+    mockUseGetCRbyPath.mockImplementationOnce(() => ({
+      resource: {
+        path: `${resourcePath}?hasDetailsView=true`,
+        kind: resourceKind,
+      },
+      list: [{ path: 'spec.description' }],
+    }));
+    const { getByText } = render(<ExtensibilityList />);
+    expect(getByText(new RegExp(resourcePath, 'i'))).toBeVisible();
+    expect(getByText(new RegExp(translations.name, 'i'))).toBeVisible();
+    expect(getByText(new RegExp(translations.description, 'i'))).toBeVisible();
+    expect(getByText(new RegExp(resourceUrl, 'i'))).toBeVisible();
+    expect(getByText(new RegExp('has custom columns', 'i'))).toBeVisible();
   });
 });
