2a496477d0d515885a98bfc9281c2935c5b992f0
wchrapka <wojciech.chrapka@sap.com>
2022-07-07 14:35:01 +0200

    bug fixes

--- core-ui/src/components/Extensibility/components/Text.js
+++ core-ui/src/components/Extensibility/components/Text.js
@@ -1,11 +1,15 @@
 import { useGetPlaceholder } from 'components/Extensibility/helpers';
+import { isNil } from 'lodash';
+import { stringifyIfBoolean } from 'shared/utils/helpers';
 
 export const Text = ({ value, schema, structure }) => {
   const { emptyLeafPlaceholder } = useGetPlaceholder(structure);
+
   if (typeof value === 'object') {
     return JSON.stringify(value);
   }
 
-  return value || emptyLeafPlaceholder;
+  const sanitizedValue = stringifyIfBoolean(value);
+  return isNil(value) ? emptyLeafPlaceholder : sanitizedValue;
 };
 Text.inline = true;
