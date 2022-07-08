5927a7282382399887a33de6df785b4f03100c90
dariadomagala <daria.domagala@sap.com>
2022-07-08 08:39:50 +0200

    Adjust tests

--- core-ui/src/components/Extensibility/components/tests/ControlledBy.spec.js
+++ core-ui/src/components/Extensibility/components/tests/ControlledBy.spec.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import { render } from '@testing-library/react';
 import { ControlledBy, ControlledByKind } from '../ControlledBy';
+import {
+  ControlledBy as CB,
+  ControlledByKind as CBK,
+} from 'shared/components/ControlledBy/ControlledBy';
 
 describe('ControlledBy', () => {
-  it('Renders owners', () => {
+  it('Renders ControlledBy component', () => {
     const owners = [
       {
         apiVersion: 'v1',
@@ -17,14 +22,21 @@ describe('ControlledBy', () => {
       },
     ];
 
+    const component = shallow(<ControlledBy value={owners} />);
+    const cards = component.find(CB);
+    expect(cards).toHaveLength(1);
+  });
+
+  it('Renders empty owners for incorrect data', () => {
+    const owners = '';
+
     const { getByText } = render(<ControlledBy value={owners} />);
-    expect(getByText(new RegExp('Pod', 'i'))).toBeVisible();
-    expect(getByText(new RegExp('Function', 'i'))).toBeVisible();
+    expect(getByText(new RegExp('-', 'i'))).toBeVisible();
   });
 });
 
 describe('ControlledByKind', () => {
-  it('Renders owners', () => {
+  it('Renders ControlledByKind component', () => {
     const owners = [
       {
         apiVersion: 'v1',
@@ -38,8 +50,15 @@ describe('ControlledByKind', () => {
       },
     ];
 
+    const component = shallow(<ControlledByKind value={owners} />);
+    const cards = component.find(CBK);
+    expect(cards).toHaveLength(1);
+  });
+
+  it('Renders empty owners for incorrect data', () => {
+    const owners = '';
+
     const { getByText } = render(<ControlledByKind value={owners} />);
-    expect(getByText(new RegExp('Pod', 'i'))).toBeVisible();
-    expect(getByText(new RegExp('Function', 'i'))).toBeVisible();
+    expect(getByText(new RegExp('-', 'i'))).toBeVisible();
   });
 });
