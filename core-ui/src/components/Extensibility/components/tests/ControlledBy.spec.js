f68c2971146029324f8c68897adfdd4c6ce69bcf
dariadomagala <daria.domagala@sap.com>
2022-07-08 07:59:13 +0200

    Add a controlledby test

---
+++ core-ui/src/components/Extensibility/components/tests/ControlledBy.spec.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { ControlledBy, ControlledByKind } from '../ControlledBy';
+
+jest.mock('../Widget', () => ({
+  Widget: () => 'xxx',
+}));
+
+describe('ControlledBy', () => {
+  it('Renders owners', () => {
+    const owners = [
+      {
+        apiVersion: 'v1',
+        kind: 'Pod',
+        name: 'pod-name',
+      },
+      {
+        apiVersion: 'v1',
+        kind: 'Function',
+        name: 'function-name',
+      },
+    ];
+
+    const { getByText } = render(<ControlledBy value={owners} />);
+    expect(getByText(new RegExp('Pod', 'i'))).toBeVisible();
+    expect(getByText(new RegExp('Function', 'i'))).toBeVisible();
+  });
+});
+
+describe('ControlledByKind', () => {
+  it('Renders owners', () => {
+    const owners = [
+      {
+        apiVersion: 'v1',
+        kind: 'Pod',
+        name: 'pod-name',
+      },
+      {
+        apiVersion: 'v1',
+        kind: 'Function',
+        name: 'function-name',
+      },
+    ];
+
+    const { getByText } = render(<ControlledByKind value={owners} />);
+    expect(getByText(new RegExp('Pod', 'i'))).toBeVisible();
+    expect(getByText(new RegExp('Function', 'i'))).toBeVisible();
+  });
+});
