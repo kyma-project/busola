1a5c57bbbdfef097e9b3bc65012f753e235388a2
wchrapka <wojciech.chrapka@sap.com>
2022-07-08 14:50:11 +0200

    get around luigi asyncs

--- core-ui/src/components/App/useLoginWithKubeconfigID.js
+++ core-ui/src/components/App/useLoginWithKubeconfigID.js
@@ -14,23 +14,35 @@ export const useLoginWithKubeconfigID = () => {
       return;
     }
 
-    // // remove old clusters
-    // Object.keys(clusters).forEach(clusterName =>
-    //   LuigiClient.sendCustomMessage({
-    //     id: 'busola.deleteCluster',
-    //     clusterName,
-    //   }),
-    // );
-
     // add new clusters
     const onlyOneCluster = getKubeconfigId.contexts.length === 1;
+
     getKubeconfigId.contexts.forEach(context => {
-      addByContext(getKubeconfigId, context, onlyOneCluster);
+      const previousStorageMethod = clusters[context?.name]?.config?.storage;
+
+      if (previousStorageMethod) {
+        LuigiClient.sendCustomMessage({
+          id: 'busola.deleteCluster',
+          clusterName: context.name,
+        });
+      }
+      // LUIGI workaround: 'busola.deleteCluster' cannot be waited for, but has plenty of asyncs inside, invoke after the cluster is removed
+      setTimeout(() => {
+        addByContext(
+          getKubeconfigId,
+          context,
+          onlyOneCluster,
+          previousStorageMethod,
+        );
+      });
     });
+
     if (!onlyOneCluster) {
-      // Luigi doesn't reload when we don't choose an active cluster
-      LuigiClient.sendCustomMessage({
-        id: 'busola.refreshClusters',
+      // LUIGI workaround: 'Luigi doesn't reload when we don't choose an active cluster. 'addByContext' cannot be waited for as it calls a luigi function with asyncs inside
+      setTimeout(() => {
+        LuigiClient.sendCustomMessage({
+          id: 'busola.refreshClusters',
+        });
       });
     }
   }, [getKubeconfigId, clusters]);
