9cd4595cbca554c1f71b168499193cbe08bf7c0f
wchrapka <wojciech.chrapka@sap.com>
2022-07-12 13:35:43 +0200

    clean luigi

--- core/src/luigi-config/kubeconfig-id/index.js
+++ core/src/luigi-config/kubeconfig-id/index.js
@@ -1,33 +1,10 @@
-import {
-  saveClusterParams,
-  saveActiveClusterName,
-} from '../cluster-management/cluster-management';
-import * as constants from './../constants';
 import { loadKubeconfigById } from './loadKubeconfigById';
 import i18next from 'i18next';
-import * as clusterStorage from './../cluster-management/clusters-storage';
-
-function getContext(kubeconfig) {
-  const contexts = kubeconfig.contexts;
-  const currentContextName = kubeconfig['current-context'];
-  if (contexts.length === 0 || !currentContextName) {
-    // no contexts or no context chosen, just take first cluster and user
-    return {
-      cluster: kubeconfig.clusters[0],
-      user: kubeconfig.users[0],
-    };
-  } else {
-    const { context } = contexts.find(c => c.name === currentContextName);
-    return {
-      cluster: kubeconfig.clusters.find(c => c.name === context.cluster),
-      user: kubeconfig.users.find(u => u.name === context.user),
-    };
-  }
-}
 
 export async function handleKubeconfigIdIfPresent() {
   const searchParams = new URL(location).searchParams;
   const kubeconfigId = searchParams.get('kubeconfigID');
+
   try {
     const kubeconfig = await loadKubeconfigById(kubeconfigId);
     if (!kubeconfig) {
