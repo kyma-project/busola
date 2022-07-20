b910899b1d4da8847ac65d6d542c5cecafd3a669
mwisnia97 <mwisnia97@gmail.com>
2022-07-20 11:17:11 +0200

    add secretName and change grandTypes

--- core-ui/src/resources/OAuth2Clients/helpers.js
+++ core-ui/src/resources/OAuth2Clients/helpers.js
@@ -7,8 +7,9 @@ export function createOAuth2ClientTemplate(namespace) {
       namespace,
     },
     spec: {
-      grantTypes: [],
+      grantTypes: [''],
       scope: '',
+      secretName: '',
       tokenEndpointAuthMethod: 'client_secret_basic',
     },
   };
