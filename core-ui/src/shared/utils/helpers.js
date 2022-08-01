dc2e9668676496e383da465900fca2e4cf6fc0ab
Lyczeq <lyczeq@gmail.com>
2022-08-01 15:27:20 +0200

    Added suggestion

--- core-ui/src/shared/utils/helpers.js
+++ core-ui/src/shared/utils/helpers.js
@@ -96,7 +96,7 @@ const capitalize = str => {
 
 const splitName = name => {
   const nameArray = name?.match(/[A-Z][a-z]+/g);
-  return nameArray.join(' ');
+  return (nameArray || []).join(' ');
 };
 
 export const prettifyNamePlural = (resourceName, resourceType) => {
