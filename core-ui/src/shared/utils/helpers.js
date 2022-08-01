6907a6af9385aa0bedf1c4391c04c62189bf7829
Lyczeq <lyczeq@gmail.com>
2022-08-01 13:05:30 +0200

    fix match is not a function

--- core-ui/src/shared/utils/helpers.js
+++ core-ui/src/shared/utils/helpers.js
@@ -95,7 +95,7 @@ const capitalize = str => {
 };
 
 const splitName = name => {
-  const nameArray = name.match(/[A-Z][a-z]+/g);
+  const nameArray = name?.match(/[A-Z][a-z]+/g);
   return nameArray.join(' ');
 };
 
