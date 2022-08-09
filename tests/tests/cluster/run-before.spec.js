f2bcb27661473d7847db9b11dbb101fe68c6bf70
Wawrzyn321 <Wawrzyn321@gmail.com>
2022-08-09 13:46:37 +0200

    add mock test

--- tests/tests/cluster/run-before.spec.js
+++ tests/tests/cluster/run-before.spec.js
@@ -1,29 +1,9 @@
 /// <reference types="cypress" />
 
 context('Create Application', () => {
-  Cypress.skipAfterFail({ skipAllSuits: true });
-
-  before(cy.loginAndSelectCluster);
-
-  it('Create Application', () => {
-    cy.navigateTo('Integration', 'Applications');
-
-    cy.getIframeBody()
-      .contains('Create Application')
-      .click();
-
-    cy.getIframeBody()
-      .find('[role=dialog]')
-      .find("input[ariaLabel='Application name']:visible")
-      .type(Cypress.env('APP_NAME'));
-
-    cy.getIframeBody()
-      .find('[role="dialog"]')
-      .contains('button', 'Create')
-      .click();
-
-    cy.getIframeBody()
-      .contains(Cypress.env('APP_NAME'))
-      .should('be.visible');
+  it('aha!', () => {
+    cy.fixture(
+      'examples/pizzas/configuration/pizzas-configmap.yaml',
+    ).then(file => expect(file).to.equal('kotek'));
   });
 });
