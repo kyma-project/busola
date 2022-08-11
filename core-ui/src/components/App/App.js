5d505089bebfbe50c95a90a50df3545f07a3b7eb
Lyczeq <lyczeq@gmail.com>
2022-08-11 12:32:43 +0200

    Removed needless useMemmo import

--- core-ui/src/components/App/App.js
+++ core-ui/src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo } from 'react';
+import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
