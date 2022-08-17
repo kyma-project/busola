90aa10ab7b24cfeb4f61e1a5d8a9f47211b0c57f
mwisnia97 <mwisnia97@gmail.com>
2022-08-17 15:45:49 +0200

    added form config and fix gatewaySelector

--- core-ui/src/resources/Gateways/GatewaySelector.js
+++ core-ui/src/resources/Gateways/GatewaySelector.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Token } from 'fundamental-react';
+import { EMPTY_TEXT_PLACEHOLDER } from 'shared/constants';
 
 export function GatewaySelector({ gateway }) {
-  return Object.entries(gateway.spec.selector).map(([key, value]) => (
-    <Token readOnly key={key} buttonLabel="">
-      {key}={value}
-    </Token>
-  ));
+  return gateway.spec.selector
+    ? Object.entries(gateway.spec.selector).map(([key, value]) => (
+        <Token readOnly key={key} buttonLabel="">
+          {key}={value}
+        </Token>
+      ))
+    : EMPTY_TEXT_PLACEHOLDER;
 }
