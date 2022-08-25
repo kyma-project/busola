e7b39b7b3d8ae86e09a164545c322f5de352c665
wchrapka <wojciech.chrapka@sap.com>
2022-08-25 10:08:10 +0200

    fix joined array unit test

--- core-ui/src/components/Extensibility/components/tests/JoinedArray.test.js
+++ core-ui/src/components/Extensibility/components/tests/JoinedArray.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { JoinedArray } from '../JoinedArray';
 
+jest.mock('components/Extensibility/components', () => []);
+
 describe('JoinedArray', () => {
   it('Renders joined array', () => {
     const value = ['a', 'b', 'c'];
