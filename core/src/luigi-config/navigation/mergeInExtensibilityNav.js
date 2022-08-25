17941050323f42b78b67829420fa87d5e1a5b80d
wchrapka <wojciech.chrapka@sap.com>
2022-08-25 11:45:23 +0200

    code refactor

--- core/src/luigi-config/navigation/mergeInExtensibilityNav.js
+++ core/src/luigi-config/navigation/mergeInExtensibilityNav.js
@@ -1,26 +1,16 @@
 import { partial } from 'lodash';
 
-const isNodeASwapCandidate = node => {
-  if (!node.label || !node.pathSegment) {
-    return true;
-  }
-};
+const isLabelAndPathDefined = node => node.label && node.pathSegment;
 
-const findNodeWithSameLabelAndPath = (busolaNode, extNode) => {
-  if (isNodeASwapCandidate(busolaNode)) {
-    return false;
-  }
-  return (
-    busolaNode.label === extNode.label &&
-    busolaNode.pathSegment === extNode.pathSegment
-  );
-};
+const isTheSameLabelAndPath = (busolaNode, extNode) =>
+  busolaNode.label === extNode.label &&
+  busolaNode.pathSegment === extNode.pathSegment;
 
 const replaceOrAddNode = (nodeList, node, index) => {
-  if (index > -1) {
-    nodeList.splice(index, 1, node);
-  } else {
+  if (index === -1) {
     nodeList.push(node);
+  } else {
+    nodeList.splice(index, 1, node);
   }
 };
 
@@ -28,16 +18,13 @@ export const mergeInExtensibilityNav = (nodes, extensionNodes) => {
   const busolaNodeList = [...nodes];
 
   extensionNodes.forEach(extNode => {
-    if (isNodeASwapCandidate(extNode)) {
-      return;
+    if (isLabelAndPathDefined(extNode)) {
+      const busolaNodeSameAsExtNodeFn = partial(isTheSameLabelAndPath, extNode);
+      const elToBeReplacedIndex = busolaNodeList.findIndex(
+        busolaNodeSameAsExtNodeFn,
+      );
+      replaceOrAddNode(busolaNodeList, extNode, elToBeReplacedIndex);
     }
-
-    const findSameLabelAndPath = partial(findNodeWithSameLabelAndPath, extNode);
-    const elementToBeReplacedIndex = busolaNodeList.findIndex(
-      findSameLabelAndPath,
-    );
-
-    replaceOrAddNode(busolaNodeList, extNode, elementToBeReplacedIndex);
   });
 
   return busolaNodeList;
