68cdfddfc97184e709ab7e718a6794d0015356a1
wchrapka <wojciech.chrapka@sap.com>
2022-08-29 13:18:28 +0200

    wip

---
+++ core-ui/src/components/Extensibility/tempRes/outlierDetection.js
@@ -0,0 +1,48 @@
+export const outlierDetection = prefix => ({
+  source: prefix + 'outlierDetection',
+  name: 'outlierDetection',
+  visibility: '$exists($.data)',
+  widget: 'Panel',
+  children: [
+    {
+      source: prefix + 'outlierDetection.splitExternalLocalOriginErrors',
+      name: 'splitExternalLocalOriginErrors',
+      visibility: '$exists($.data)',
+    },
+    {
+      source: prefix + 'outlierDetection.consecutiveLocalOriginFailures',
+      name: 'consecutiveLocalOriginFailures',
+      visibility: '$exists($.data)',
+    },
+    {
+      source: prefix + 'outlierDetection.consecutiveGatewayErrors',
+      name: 'consecutiveGatewayErrors',
+      visibility: '$exists($.data)',
+    },
+    {
+      source: prefix + 'outlierDetection.consecutive5xxErrors',
+      name: 'consecutive5xxErrors',
+      visibility: '$exists($.data)',
+    },
+    {
+      source: prefix + 'outlierDetection.interval',
+      name: 'interval',
+      visibility: '$exists($.data)',
+    },
+    {
+      source: prefix + 'outlierDetection.baseEjectionTime',
+      name: 'baseEjectionTime',
+      visibility: '$exists($.data)',
+    },
+    {
+      source: prefix + 'outlierDetection.maxEjectionPercent',
+      name: 'maxEjectionPercent',
+      visibility: '$exists($.data)',
+    },
+    {
+      source: prefix + 'outlierDetection.minHealthPercent',
+      name: 'minHealthPercent',
+      visibility: '$exists($.data)',
+    },
+  ],
+});
