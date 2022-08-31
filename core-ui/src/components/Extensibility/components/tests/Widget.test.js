280991ce80748d6e9281299c2506a16d19eacefd
Wawrzyn321 <Wawrzyn321@gmail.com>
2022-08-31 18:48:49 +0200

    Adjust tests

--- core-ui/src/components/Extensibility/components/tests/Widget.test.js
+++ core-ui/src/components/Extensibility/components/tests/Widget.test.js
@@ -3,9 +3,6 @@ import { mount } from 'enzyme';
 import { waitFor } from '@testing-library/react';
 import { Widget } from '../Widget';
 
-jest.mock('shared/components/MonacoEditorESM/Editor', () => ({
-  'monaco-editor': () => 'monaco-editor',
-}));
 jest.mock('components/Extensibility/useJsonata', () => ({
   useJsonata: () => 'test-value',
 }));
