85ebf501ba37e6a46ca9fa7c4cf4ab4386f80a17
Wawrzyn321 <Wawrzyn321@gmail.com>
2022-09-15 12:56:29 +0200

    Fix RoleBinding form

--- core-ui/src/resources/RoleBindings/RoleForm.js
+++ core-ui/src/resources/RoleBindings/RoleForm.js
@@ -4,27 +4,53 @@ import { Spinner } from 'shared/components/Spinner/Spinner';
 import { ComboboxInput } from 'fundamental-react';
 import { ResourceForm } from 'shared/ResourceForm';
 import { useTranslation } from 'react-i18next';
+import { Dropdown } from 'shared/ResourceForm/inputs';
+import { ResourceFormWrapper } from 'shared/ResourceForm/components/Wrapper';
+import { useMicrofrontendContext } from 'shared/contexts/MicrofrontendContext';
 
 export const RoleForm = ({
   loading,
   error,
-  allRoles,
+  roles,
+  clusterRoles,
   binding,
-  handleRoleChange,
+  setBinding,
 }) => {
   const { t } = useTranslation();
+  const { namespaceId: namespace } = useMicrofrontendContext();
+
   if (loading) return <Spinner compact={true} />;
   if (error) return error.message;
 
-  const selectedRole =
-    binding?.roleRef?.kind && binding?.roleRef?.name
-      ? `${binding?.roleRef?.kind?.toLowerCase()}-${binding?.roleRef?.name}`
-      : undefined;
+  const roleTypeDropdown = (
+    <ResourceForm.FormField
+      required
+      label={t('role-bindings.create-modal.role-type')}
+      propertyPath="$.roleRef.kind"
+      input={props => (
+        <Dropdown
+          fullWidth={false}
+          selectedKey={props.value}
+          options={['Role', 'ClusterRole'].map(v => ({ key: v, text: v }))}
+          {...props}
+          disabled={!namespace}
+        />
+      )}
+    />
+  );
 
-  return (
+  const rolesForCurrentType =
+    binding.roleRef.kind === 'ClusterRole' ? clusterRoles : roles;
+  const options = (rolesForCurrentType || []).map(r => ({
+    key: r.metadata.name,
+    text: r.metadata.name,
+  }));
+
+  const roleNameInput = (
     <ResourceForm.FormField
       required
       label={t('role-bindings.create-modal.role')}
+      propertyPath="$.roleRef.name"
       input={props => (
         <ComboboxInput
           id="role"
@@ -35,15 +61,23 @@ export const RoleForm = ({
           showAllEntries
           searchFullString
           placeholder={t('common.messages.type-to-select', {
-            value: t('role-bindings.name_singular'),
+            value: t('common.headers.resource'),
           })}
-          options={allRoles}
-          selectedKey={selectedRole}
+          options={options}
+          selectedKey={props.value}
+          typedValue={props.value}
           selectionType="manual"
-          onSelectionChange={(_, selected) => handleRoleChange(selected)}
+          onSelectionChange={(_, selected) => props.setValue(selected.text)}
           {...props}
         />
       )}
     />
+  );
+
+  return (
+    <ResourceFormWrapper resource={binding} setResource={setBinding}>
+      {roleTypeDropdown}
+      {roleNameInput}
+    </ResourceFormWrapper>
   );
 };
