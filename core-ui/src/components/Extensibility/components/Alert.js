60ee48e9951726db0dc57ae368cf92336eee8626
mwisnia97 <mwisnia97@gmail.com>
2022-09-21 10:31:37 +0200

    allow alertFormula for jsonata

--- core-ui/src/components/Extensibility/components/Alert.js
+++ core-ui/src/components/Extensibility/components/Alert.js
@@ -3,19 +3,37 @@ import React from 'react';
 import { MessageStrip } from 'fundamental-react';
 import { useGetTranslation } from 'components/Extensibility/helpers';
 import classNames from 'classnames';
+import { jsonataWrapper } from '../helpers/jsonataWrapper';
 
-export const Alert = ({ value, schema, structure }) => {
+export const Alert = ({ value, schema, structure, ...props }) => {
   const { t: tExt } = useGetTranslation();
   const alert = structure.alert || value;
+  const alertFormula = structure.alertFormula;
   const schemaType = structure.type || 'information';
 
   const messageClassNames = classNames({
     'fd-margin--md': !structure.disableMargin,
   });
 
+  function alertJsonata(alertFormula) {
+    try {
+      const expression = jsonataWrapper(alertFormula);
+
+      expression.assign('root', props.originalResource);
+      expression.assign('item', value);
+
+      return expression.evaluate();
+    } catch (e) {
+      console.warn('Widget::shouldBeVisible error:', e);
+      return { error: e };
+    }
+  }
+
   return (
     <div className={messageClassNames}>
-      <MessageStrip type={schemaType}>{tExt(alert)}</MessageStrip>
+      <MessageStrip type={schemaType}>
+        {alertFormula ? alertJsonata(alertFormula) : tExt(alert)}
+      </MessageStrip>
     </div>
   );
 };
