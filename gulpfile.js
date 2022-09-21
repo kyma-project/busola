aeb508b9e06eabed8fc0ccd35de3352005c2668a
Wawrzyn321 <Wawrzyn321@gmail.com>
2022-09-21 12:30:14 +0200

    nice

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,10 @@ const childProcess = require('child_process');
 const log = require('fancy-log');
 const clc = require('cli-color');
 const { promisify } = require('util');
+const through2 = require('through2');
+const jsyaml = require('js-yaml');
+const { mapValues } = require('lodash');
+const concat = require('gulp-concat');
 
 const exec = promisify(childProcess.exec);
 
@@ -60,4 +64,54 @@ gulp.task('copy-themes', function() {
         .pipe(gulp.dest(`./core-ui/public/themes/@sap-theming`)),
     ),
   );
+});
+
+gulp.task('pack-extensions', function() {
+  const convertYamlToObject = yamlString => {
+    return jsyaml.load(yamlString, { json: true });
+  };
+
+  const loadExtensions = through2.obj(function(file, _, cb) {
+    const { data } = jsyaml.load(file.contents.toString());
+    file.contents = Buffer.from(
+      jsyaml.dump(mapValues(data, convertYamlToObject)),
+    );
+    cb(null, file);
+  });
+
+  const intoConfigMap = through2.obj(function(file, _, cb) {
+    const addIndent = str =>
+      str
+        .split('\n')
+        .map(s => `    ${s}`)
+        .join('\n');
+
+    const extensions = addIndent(file.contents.toString());
+
+    const configMapHeader = `kind: ConfigMap
+apiVersion: v1
+metadata:
+  name: busola-builtin-resource-extensions
+  labels:
+    app.kubernetes.io/name: busola-builtin-resource-extensions
+    busola.io/extension: builtin-resources
+data:
+  extensions.yaml: |-`;
+
+    file.contents = Buffer.from(`${configMapHeader}
+${extensions}
+  `);
+    cb(null, file);
+  });
+
+  return gulp
+    .src('extensions/**/*.yaml')
+    .pipe(loadExtensions)
+    .pipe(
+      concat('builtin-resource-extensions.configmap.yaml', {
+        newLine: '---\n',
+      }),
+    )
+    .pipe(intoConfigMap)
+    .pipe(gulp.dest('./resources/web/'));
 });
