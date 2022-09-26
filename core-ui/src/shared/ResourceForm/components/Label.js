e5d3e12f4313c202121c37f859afea3ca6195b69
Jakub Łukomski <dev@tenku.eu>
2022-09-26 14:43:45 +0200

    Update core-ui/src/shared/ResourceForm/components/Label.js
    Co-authored-by: Piotr Wawrzyńczyk <Wawrzyn321@gmail.com>

--- core-ui/src/shared/ResourceForm/components/Label.js
+++ core-ui/src/shared/ResourceForm/components/Label.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { FormLabel } from 'fundamental-react';
 import './Label.scss';
 
-export function Label({ required, tooltipContent, children }) {
+export function Label({ required, children }) {
   return (
     <>
       <FormLabel required={required} includeColon>
