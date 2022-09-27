760ad59d3c872eb3c9e8b5d60086468e70361720
mwisnia97 <mwisnia97@gmail.com>
2022-09-27 10:57:44 +0200

    added jsonata to alert

--- core-ui/src/components/Extensibility/components-form/AlertRenderer.js
+++ core-ui/src/components/Extensibility/components-form/AlertRenderer.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { useGetTranslation } from 'components/Extensibility/helpers';
 import { MessageStrip } from 'fundamental-react';
+import { jsonataWrapper } from '../helpers/jsonataWrapper';
 
 export function AlertRenderer({ value, schema, storeKeys, compact, ...props }) {
   const { t: tExt } = useGetTranslation();
@@ -9,9 +10,23 @@ export function AlertRenderer({ value, schema, storeKeys, compact, ...props }) {
   const alertFormula = schema.get('alertFormula');
   const schemaType = schema.get('type') || 'information';
 
+  function alertJsonata(alertFormula) {
+    try {
+      const expression = jsonataWrapper(alertFormula);
+
+      expression.assign('root', props.originalResource);
+      expression.assign('item', value);
+
+      return expression.evaluate();
+    } catch (e) {
+      console.warn('Widget::shouldBeVisible error:', e);
+      return { visible: false, error: e };
+    }
+  }
+
   return (
     <MessageStrip type={schemaType} className="fd-margin-top--sm">
-      {alertFormula || tExt(alert)}
+      {alertFormula ? alertJsonata(alertFormula) : tExt(alert)}
     </MessageStrip>
   );
 }
