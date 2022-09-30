5f3c7204318dd75abe5d29f7249b6b63f91ff8b6
Lyczeq <lyczeq@gmail.com>
2022-09-30 14:57:15 +0200

    prettify useJsonata and useJsonataEvaluate

--- core-ui/src/components/Extensibility/components/hooks/useJsonataEvaluate.js
+++ core-ui/src/components/Extensibility/components/hooks/useJsonataEvaluate.js
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 
 import { jsonataWrapper } from '../../helpers/jsonataWrapper';
 import { DataSourcesContext } from '../../contexts/DataSources';
-
+import { mapValues } from 'lodash';
 export function useJsonataEvaluate(root) {
   const {
     dataSources,
@@ -10,15 +10,12 @@ export function useJsonataEvaluate(root) {
     requestRelatedResource,
   } = useContext(DataSourcesContext);
 
-  const dataSourceFetchers = Object.fromEntries(
-    Object.entries(dataSources).map(([id, def]) => [
-      id,
-      () => {
-        requestRelatedResource(root, id);
-        return dataSourceStore?.[id]?.data;
-      },
-    ]),
-  );
+  const dataSourceFetchers = mapValues(dataSources, (_, id) => {
+    return () => {
+      requestRelatedResource(root, id);
+      return dataSourceStore?.[id]?.data;
+    };
+  });
 
   return (query, extras) =>
     jsonataWrapper(query).evaluate(root, {
