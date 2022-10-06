4a75211def224bc46af1f08474bec37552b52334
mwisnia97 <mwisnia97@gmail.com>
2022-10-06 10:21:01 +0200

    added subscription.yaml and jsonata function for timestamp

--- core-ui/src/components/Extensibility/helpers/jsonataWrapper.js
+++ core-ui/src/components/Extensibility/helpers/jsonataWrapper.js
@@ -1,5 +1,6 @@
 import jsonata from 'jsonata';
 import { isEqual } from 'lodash';
+import { EMPTY_TEXT_PLACEHOLDER } from 'shared/constants';
 
 export function jsonataWrapper(expression) {
   const exp = jsonata(expression);
@@ -26,6 +27,39 @@ export function jsonataWrapper(expression) {
 
   exp.registerFunction('compareStrings', (first, second) => {
     return first?.localeCompare(second) ?? 1;
+  });
+
+  exp.registerFunction('readableTimestamp', timestamp => {
+    if (!timestamp) return EMPTY_TEXT_PLACEHOLDER;
+
+    function getDayDifference(time1, time2) {
+      return (time1 - time2) / (24 * 60 * 60 * 1000);
+    }
+
+    function getHourDifference(time1, time2) {
+      return (time1 - time2) / (60 * 60 * 1000);
+    }
+
+    function getMinuteDifference(time1, time2) {
+      return (time1 - time2) / (60 * 1000);
+    }
+
+    const rtf = new Intl.RelativeTimeFormat('en', {
+      localeMatcher: 'best fit', // other values: "lookup"
+      numeric: 'auto', // other values: "auto"
+      style: 'long', // other values: "short" or "narrow"
+    });
+
+    const now = new Date();
+    const createdAt = new Date(timestamp);
+    const dayDifference = getDayDifference(createdAt, now);
+    if (dayDifference < -1) return rtf.format(Math.ceil(dayDifference), 'day');
+
+    const hourDifference = getHourDifference(createdAt, now);
+    if (hourDifference < -1)
+      return rtf.format(Math.ceil(hourDifference), 'hour');
+
+    return rtf.format(Math.ceil(getMinuteDifference(createdAt, now)), 'minute');
   });
 
   return exp;
