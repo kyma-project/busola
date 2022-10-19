aa0663fb5ab27d6c0b3d477618057d5e8438c809
Lyczeq <lyczeq@gmail.com>
2022-10-19 08:10:07 +0200

    add state keyword to the key

--- core-ui/src/state/disableResourceProtectionAtom.ts
+++ core-ui/src/state/disableResourceProtectionAtom.ts
@@ -6,7 +6,7 @@ const DISABLE_RESOURCE_PROTECTION_STORAGE_KEY =
 const DEFAULT_DISABLE_RESOURCE_PROTECTION = false;
 
 export const disableResourceProtectionState = atom<boolean>({
-  key: 'disableResourceProtection',
+  key: 'disableResourceProtectionState',
   default: DEFAULT_DISABLE_RESOURCE_PROTECTION,
   effects: [
     localStorageEffect<boolean>(DISABLE_RESOURCE_PROTECTION_STORAGE_KEY),
--- core-ui/src/state/dontConfirmDeleteAtom.ts
+++ core-ui/src/state/dontConfirmDeleteAtom.ts
@@ -5,7 +5,7 @@ const DONT_CONFIRM_DELETE_STORAGE_KEY = 'busola.dontConfirmDelete';
 const DEFAULT_DONT_CONFIRM_DELETE = false;
 
 export const dontConfirmDeleteState = atom<boolean>({
-  key: 'dontConfirmDelete',
+  key: 'dontConfirmDeleteState',
   default: DEFAULT_DONT_CONFIRM_DELETE,
   effects: [localStorageEffect<boolean>(DONT_CONFIRM_DELETE_STORAGE_KEY)],
 });
