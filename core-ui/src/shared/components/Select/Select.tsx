7a4c9b23b11d204237d9018a1aad3e91877c183b
Lyczeq <lyczeq@gmail.com>
2022-10-19 10:13:39 +0200

    feat: add types to OtherSettings and Select components

---
+++ core-ui/src/shared/components/Select/Select.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Select as FdSelect } from 'fundamental-react';
+import { SelectProps as FdSelectProps } from 'fundamental-react/lib/Select/Select';
+
+interface SelectProps extends FdSelectProps {
+  fullWidth?: boolean;
+}
+
+export function Select({ fullWidth, ...props }: SelectProps) {
+  return (
+    <FdSelect
+      {...props}
+      className={fullWidth ? 'select-full-width' : undefined}
+    />
+  );
+}
