dbd8036cce37ed5d21c32cce3d337a4ad600571f
Lyczeq <lyczeq@gmail.com>
2022-10-25 08:31:49 +0200

    fix: busola github links

--- core-ui/src/sidebar/Footer/useGetBusolaVersion.ts
+++ core-ui/src/sidebar/Footer/useGetBusolaVersion.ts
@@ -2,9 +2,11 @@ import { useState, useEffect } from 'react';
 import { useTranslation, TFunction } from 'react-i18next';
 import jsyaml from 'js-yaml';
 
-const BUSOLA_GITHUB_REPOSITORY_LINK = 'https://github.com/kyma-project/busola';
-const BUSOLA_GITHUB_PULLS_LINK = 'https://github.com/kyma-project/busola';
-const BUSOLA_GITHUB_COMMMITS_LINK = 'https://github.com/kyma-project/busola';
+const BUSOLA_GITHUB_LINKS = {
+  REPOSITORY: 'https://github.com/kyma-project/busola',
+  PULLS: 'https://github.com/kyma-project/busola/pull',
+  COMMITS: 'https://github.com/kyma-project/busola/commit',
+};
 
 function createGithubLink(version: string): string {
   const devVersion = 'dev';
@@ -12,12 +14,12 @@ function createGithubLink(version: string): string {
 
   if (version !== devVersion && version !== unknownVersion) {
     if (version.startsWith('PR-')) {
-      return `${BUSOLA_GITHUB_PULLS_LINK}/${version.slice(3)}`;
+      return `${BUSOLA_GITHUB_LINKS.PULLS}/${version.slice(3)}`;
     }
-    return `${BUSOLA_GITHUB_COMMMITS_LINK}/${version}`;
+    return `${BUSOLA_GITHUB_LINKS.COMMITS}/${version}`;
   }
 
-  return BUSOLA_GITHUB_REPOSITORY_LINK;
+  return BUSOLA_GITHUB_LINKS.REPOSITORY;
 }
 
 async function getBusolaVersion(t: TFunction): Promise<string> {
