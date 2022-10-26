cf4c3d48542fa75d93f4edee780a7f5e6b5a7493
Lyczeq <lyczeq@gmail.com>
2022-10-26 17:45:10 +0200

    feat: moved changing css files logic from ThemeContext to atom

--- core-ui/src/state/preferences/themeAtom.ts
+++ core-ui/src/state/preferences/themeAtom.ts
@@ -6,6 +6,33 @@ type Theme = 'dark' | 'light' | 'light_dark' | 'hcw' | 'hcb';
 const THEME_STORAGE_KEY = 'busola.theme';
 const DEFAULT_THEME = 'light_dark';
 
+function applyThemeToLinkNode(name: Theme, publicUrl: string): any {
+  const link = document.querySelector('head #_theme') as HTMLLinkElement;
+  if (name === 'light' && link) {
+    link.parentNode?.removeChild(link);
+  }
+  if (!link) {
+    addLinkNode();
+    return applyThemeToLinkNode(name, publicUrl);
+  }
+
+  link.href = `${publicUrl || ''}/themes/${name}.css`;
+}
+
+function addLinkNode() {
+  const newLink = document.createElement('link');
+  newLink.id = '_theme';
+  newLink.rel = 'stylesheet';
+  document.head.appendChild(newLink);
+}
+type AddLinkEffect = () => AtomEffect<Theme>;
+export const addLinkEffect: AddLinkEffect = () => ({ onSet }) => {
+  onSet(newTheme => {
+    if (typeof process.env.PUBLIC_URL === 'undefined') return;
+    applyThemeToLinkNode(newTheme, process.env.PUBLIC_URL);
+  });
+};
+
 export const themeState: RecoilState<Theme> = atom<Theme>({
   key: 'themeState',
   default: DEFAULT_THEME,
