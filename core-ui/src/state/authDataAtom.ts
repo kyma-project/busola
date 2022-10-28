dc2b589e00f6d45726a9b4202823531654c96bbd
wchrapka <wojciech.chrapka@sap.com>
2022-10-28 15:21:27 +0200

    add authData null type

--- core-ui/src/state/authDataAtom.ts
+++ core-ui/src/state/authDataAtom.ts
@@ -4,7 +4,8 @@ export type AuthDataState =
   | { 'client-certificate-data': string; 'client-key-data': string }
   | {
       token: string;
-    };
+    }
+  | null;
 
 export const authDataState: RecoilState<AuthDataState> = atom<AuthDataState>({
   key: 'authDataState',
