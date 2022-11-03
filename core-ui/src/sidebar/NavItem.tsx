e20a3982f3bc14913aa777f5931d73a344353553
Wawrzyn321 <Wawrzyn321@gmail.com>
2022-11-03 15:41:56 +0100

    Handle item highlight

--- core-ui/src/sidebar/NavItem.tsx
+++ core-ui/src/sidebar/NavItem.tsx
@@ -13,12 +13,16 @@ export function NavItem({ node }: NavItemProps) {
   const namespaceId = useRecoilValue(activeNamespaceIdState);
   const { t } = useTranslation();
 
-  const pathSegments = window.location.pathname?.split('/') || [];
-  const currentPath = pathSegments[pathSegments.length - 1];
+  const isNodeSelected = () => {
+    const { pathname } = window.location;
+    const namespacePart = namespaceId ? `/namespaces/${namespaceId}` : '';
+    const nodePathName = `${namespacePart}/${node.resourceType}`;
+    return pathname.startsWith(nodePathName);
+  };
 
   return (
     <SideNav.ListItem
-      selected={currentPath === node.pathSegment}
+      selected={isNodeSelected()}
       key={node.pathSegment}
       id={node.pathSegment}
       name={t(node.label, { defaultValue: node.label })}
