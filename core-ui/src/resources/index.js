7725d6b3c57d9ce7264640a52b78c93bf43b4089
mrCherry97 <mwisnia97@gmail.com>
2022-11-03 10:39:22 +0100

    remove gateways

--- core-ui/src/resources/index.js
+++ core-ui/src/resources/index.js
@@ -21,7 +21,6 @@ import * as NetworkPolicies from './NetworkPolicies';
 import * as Issuers from './Issuers';
 import * as Ingresses from './Ingresses';
 import * as HorizontalPodAutoscalers from './HorizontalPodAutoscalers';
-import * as Gateways from './Gateways';
 import * as Events from './Events';
 import * as DnsProviders from './DnsProviders';
 import * as DnsEntries from './DnsEntries';
@@ -61,7 +60,6 @@ export const resources = [
   DaemonSets,
   // istio
   VirtualServices,
-  Gateways,
   Sidecars,
   ServiceEntries,
   DestinationRules,
