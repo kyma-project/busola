5acf0c21e807b089464f277f2007ea70c8c95026
mrCherry97 <mwisnia97@gmail.com>
2022-11-03 10:42:19 +0100

    add mockExtension function from #1991

--- tests/support/mockConfig.js
+++ tests/support/mockConfig.js
@@ -1,3 +1,5 @@
+const { load } = require('js-yaml');
+
 const requestData = {
   method: 'GET',
   url: '/backend/api/v1/namespaces/kube-public/configmaps/busola-config',
@@ -19,4 +21,19 @@ Cypress.Commands.add('setBusolaFeature', (featureName, isEnabled) => {
   };
 
   cy.intercept(requestData, configMock);
+});
+
+Cypress.Commands.add('mockExtension', (featureName, extensionPath) => {
+  cy.log(`Set Busola feature: ${featureName}`);
+
+  const requestData = {
+    method: 'GET',
+    url:
+      '/backend/api/v1/configmaps?labelSelector=busola.io/extension=resource',
+  };
+
+  cy.fixture(extensionPath).then(fileContent => {
+    const extensionConfig = load(fileContent);
+    cy.intercept(requestData, JSON.stringify({ items: [extensionConfig] }));
+  });
 });
