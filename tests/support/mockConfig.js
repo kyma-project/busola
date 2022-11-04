9e2fa173eb3e08d85942dbb3acc8523c150c1393
Mateusz Wisniewski <mwisnia97@gmail.com>
2022-11-04 09:01:30 +0100

    remove old implementation of sidecars (#1991)
    * remove sidecars
    
    * align tests
    
    * bump
    
    * remove sidecars from navigation
    
    * fix test
    
    * fix test
    
    * really dump fix
    
    * fix tests
    
    * fix tests
    
    * remove unnecesary tests from cypress
    
    * fix tests
    
    * fix tests one more time
    
    * fix tests one more time second time
    
    * fix tests one more time second time one more time
    
    * fix tests one more time second time one more time second time
    
    * fix tests one more time second time one more time second time one more time
    
    * fix tests one more time second time one more time second time one more time second time
    
    * fix tests one more time second time one more time second time one more time second time one more time
    
    * fix tests one more time second time one more time second time one more time second time one more time second time
    
    * final fix but debug second problem
    
    * debug
    
    * debug
    
    * fix findLastIndex
    
    * remove debugging and some unnecesary code after fixing fix after finxing fixes
    
    * stop making stupid thigs and blaming tests
    
    * remove sidecars from translations
    
    * fix linter
    
    * aplied suggestion

--- tests/support/mockConfig.js
+++ tests/support/mockConfig.js
@@ -1,10 +1,12 @@
-const requestData = {
-  method: 'GET',
-  url: '/backend/api/v1/namespaces/kube-public/configmaps/busola-config',
-};
+import { load } from 'js-yaml';
 
 Cypress.Commands.add('setBusolaFeature', (featureName, isEnabled) => {
   cy.log(`Set Busola feature: ${featureName} -> ${isEnabled}`);
+
+  const requestData = {
+    method: 'GET',
+    url: '/backend/api/v1/namespaces/kube-public/configmaps/busola-config',
+  };
 
   const configMock = {
     data: {
@@ -19,4 +21,19 @@ Cypress.Commands.add('setBusolaFeature', (featureName, isEnabled) => {
   };
 
   cy.intercept(requestData, configMock);
+});
+
+Cypress.Commands.add('mockExtension', (featureName, extensionPath) => {
+  cy.log(`Set Busola feature: ${featureName}`);
+
+  const requestData = {
+    method: 'GET',
+    url:
+      '/backend/api/v1/configmaps?labelSelector=busola.io/extension=resource',
+  };
+
+  cy.fixture(extensionPath).then(fileContent => {
+    const extensionConfig = load(fileContent);
+    cy.intercept(requestData, JSON.stringify({ items: [extensionConfig] }));
+  });
 });
