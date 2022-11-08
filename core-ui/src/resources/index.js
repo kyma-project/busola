6dda021ff2eac4f310f8a3f28823b0eba3a665bd
mrCherry97 <mwisnia97@gmail.com>
2022-11-08 15:01:03 +0100

    remove ap

--- core-ui/src/resources/index.js
+++ core-ui/src/resources/index.js
@@ -28,7 +28,6 @@ import * as Deployments from './Deployments';
 import * as DaemonSets from './DaemonSets';
 import * as ConfigMaps from './ConfigMaps';
 import * as Certificates from './Certificates';
-import * as AuthorizationPolicies from './AuthorizationPolicies';
 import * as APIRules from './APIRules';
 import * as Subscriptions from './Subscriptions';
 import * as ServiceAccounts from './ServiceAccounts';
@@ -61,7 +60,6 @@ export const resources = [
   VirtualServices,
   ServiceEntries,
   DestinationRules,
-  AuthorizationPolicies,
   // discovery and network
   Services,
   Ingresses,
