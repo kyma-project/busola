f9574f55c208c6668a1499c64bfa8076d030e1f8
Piotr Wawrzyńczyk <Wawrzyn321@gmail.com>
2022-11-10 09:38:43 +0100

    Update core-ui/src/components/App/useInitTheme.ts
    Co-authored-by: Iwona Langer <iwona.langer@sap.com>

--- core-ui/src/components/App/useInitTheme.ts
+++ core-ui/src/components/App/useInitTheme.ts
@@ -1,7 +1,7 @@
 import { useRecoilValue } from 'recoil';
 import { themeState } from 'state/preferences/themeAtom';
 
-// we have to initially load the themeState to
+// you must initially load the themeState to
 //let the App know what theme to choose
 export const useInitTheme = () => {
   useRecoilValue(themeState);
