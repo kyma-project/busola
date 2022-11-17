e75b67dd6902bf3892c670e640496d5bba0e3a8f
mrCherry97 <mwisnia97@gmail.com>
2022-11-17 10:56:10 +0100

    Adjust tests

--- tests/tests/namespace/test-virtual-services.spec.js
+++ tests/tests/namespace/test-virtual-services.spec.js
@@ -1,21 +1,32 @@
-import { loadFile } from '../../support/loadFile';
-
 const SERVICE_NAME = `test-virtual-service-${Math.floor(Math.random() * 9999) +
   1000}`;
+const MATCH_NAME = 'test-match';
+const URI_KEY = 'prefix';
+const URI_PREFIX = '/wpcatalog';
 
-async function loadVirtualService() {
-  const service = await loadFile('test-virtual-service.yaml');
-  const newService = { ...service };
-  service.metadata.name = SERVICE_NAME;
-  service.metadata.namespace = Cypress.env('NAMESPACE_NAME');
+const HEADER_KEY = 'exact';
+const HEADER_KEY1 = 'header';
+const HEADER_VALUE = 'foo';
 
-  return newService;
-}
+const REDIRECT_URI = '/v1/bookRatings';
+const REDIRECT_AUTHORITY = 'newratings.default.svc.cluster.local';
+
+// to edit
+const GATEWAY =
+  'kyma-gateway-application-connector.kyma-system.svc.cluster.local';
+const HOST1 = 'host1.example.com';
+const HOST2 = 'host2.example.com';
 
 context('Test Virtual Services', () => {
   Cypress.skipAfterFail();
 
   before(() => {
+    cy.setBusolaFeature('EXTENSIBILITY', true);
+    cy.mockExtension(
+      'GATEWAYS',
+      'examples/resources/istio/virtual-services.yaml',
+    );
+
     cy.loginAndSelectCluster();
     cy.goToNamespaceDetails();
   });
@@ -27,10 +38,96 @@ context('Test Virtual Services', () => {
       .contains('Create Virtual Service')
       .click();
 
-    cy.wrap(loadVirtualService()).then(VS_CONFIG => {
-      const VS = JSON.stringify(VS_CONFIG);
-      cy.pasteToMonaco(VS);
-    });
+    // name
+    cy.getIframeBody()
+      .find('[arialabel="VirtualService name"]:visible', { log: false })
+      .type(SERVICE_NAME);
+
+    // HTTP
+    cy.getIframeBody()
+      .find('[aria-label="expand HTTP"]:visible', { log: false })
+      .contains('Add')
+      .click();
+
+    // Matches
+    cy.getIframeBody()
+      .find('[aria-label="expand Matches"]:visible', { log: false })
+      .contains('Add')
+      .click();
+
+    cy.getIframeBody()
+      .find('[data-testid="spec.http.0.match.0.name"]:visible')
+      .type(MATCH_NAME);
+
+    // URIs
+    cy.getIframeBody()
+      .find('[aria-label="Combobox input"]:visible', { log: false })
+      .first()
+      .click();
+
+    cy.getIframeBody()
+      .contains(URI_KEY)
+      .click();
+
+    cy.getIframeBody()
+      .find('[placeholder="Enter value"]:visible', { log: false })
+      .filterWithNoValue()
+      .first()
+      .type(URI_PREFIX);
+
+    // Headers
+    cy.getIframeBody()
+      .find('[aria-label="expand URI"]:visible', { log: false })
+      .click();
+
+    cy.getIframeBody()
+      .find('[aria-label="expand Scheme"]:visible', { log: false })
+      .click();
+
+    cy.getIframeBody()
+      .find('[aria-label="expand Method"]:visible', { log: false })
+      .click();
+
+    cy.getIframeBody()
+      .find('[aria-label="expand Authority"]:visible', { log: false })
+      .click();
+
+    cy.getIframeBody()
+      .find('[placeholder="Enter key"]:visible', { log: false })
+      .filterWithNoValue()
+      .type(HEADER_KEY);
+
+    cy.getIframeBody()
+      .find('[aria-label="Combobox input"]:visible', { log: false })
+      .first()
+      .click();
+
+    cy.getIframeBody()
+      .contains(HEADER_KEY1)
+      .click();
+
+    cy.getIframeBody()
+      .find('[placeholder="Enter value"]:visible', { log: false })
+      .filterWithNoValue()
+      .first()
+      .type(HEADER_VALUE);
+
+    cy.getIframeBody()
+      .find('[aria-label="expand Matches"]:visible', { log: false })
+      .click();
+
+    // REDIRECT
+    cy.getIframeBody()
+      .find('[aria-label="expand Redirect"]:visible', { log: false })
+      .click();
+
+    cy.getIframeBody()
+      .find('[data-testid="spec.http.0.redirect.uri"]:visible')
+      .type(REDIRECT_URI);
+
+    cy.getIframeBody()
+      .find('[data-testid="spec.http.0.redirect.authority"]:visible')
+      .type(REDIRECT_AUTHORITY);
 
     cy.getIframeBody()
       .find('[role="dialog"]')
@@ -45,20 +142,72 @@ context('Test Virtual Services', () => {
 
   it('Inspect Virtual Service', () => {
     cy.getIframeBody().contains('h3', SERVICE_NAME);
-    cy.getIframeBody().contains(
-      'kyma-gateway-application-connector.kyma-system.svc.cluster.local',
-    );
-    cy.getIframeBody().contains('host1.example.com');
-    cy.getIframeBody().contains('matches ^/uri-*regex?');
-    cy.getIframeBody().contains('is exactly exact-uri');
-    cy.getIframeBody().contains('starts with prefixed-uri (ignore case)');
-    cy.getIframeBody().contains('TLS Route');
-    cy.getIframeBody().contains('SNI Hosts');
-    cy.getIframeBody().contains('TCP Route');
-    cy.getIframeBody().contains('Destinations');
-    cy.getIframeBody().contains('CORS Policy');
-    cy.getIframeBody().contains('Redirect');
-    cy.getIframeBody().contains('Delegate');
+
+    cy.getIframeBody()
+      .find('[data-testid="collapse-button-close"]', { timeout: 10000 })
+      .click();
+
+    cy.getIframeBody().contains(MATCH_NAME);
+    cy.getIframeBody().contains(`${URI_KEY}=${URI_PREFIX}`);
+    cy.getIframeBody().contains(HEADER_KEY);
+    cy.getIframeBody().contains(HEADER_KEY1);
+    cy.getIframeBody().contains(HEADER_VALUE);
+    cy.getIframeBody().contains(REDIRECT_URI);
+    cy.getIframeBody().contains(REDIRECT_AUTHORITY);
+    // cy.getIframeBody().contains(
+    //   'kyma-gateway-application-connector.kyma-system.svc.cluster.local',
+    // );
+    // cy.getIframeBody().contains('host1.example.com');
+    // cy.getIframeBody().contains('matches ^/uri-*regex?');
+    // cy.getIframeBody().contains('is exactly exact-uri');
+    // cy.getIframeBody().contains('starts with prefixed-uri (ignore case)');
+    // cy.getIframeBody().contains('TLS Route');
+    // cy.getIframeBody().contains('SNI Hosts');
+    // cy.getIframeBody().contains('TCP Route');
+    // cy.getIframeBody().contains('Destinations');
+    // cy.getIframeBody().contains('CORS Policy');
+    // cy.getIframeBody().contains('Redirect');
+    // cy.getIframeBody().contains('Delegate');
+  });
+
+  it('Edit VS and check updates', () => {
+    cy.getIframeBody()
+      .contains('Edit')
+      .click();
+
+    // Hosts
+    cy.getIframeBody()
+      .find('[aria-label="expand Hosts"]:visible', {
+        log: false,
+      })
+      .click();
+
+    cy.getIframeBody()
+      .find('[data-testid="spec.hosts.0"]:visible')
+      .clear()
+      .type(HOST1);
+
+    cy.getIframeBody()
+      .find('[data-testid="spec.hosts.1"]:visible')
+      .clear()
+      .type(HOST2);
+
+    // Gateways
+    cy.getIframeBody()
+      .find('[aria-label="expand Gateways"]:visible', {
+        log: false,
+      })
+      .click();
+
+    cy.getIframeBody()
+      .find('[data-testid="spec.gateways.0"]:visible')
+      .clear()
+      .type(GATEWAY);
+
+    // Changed details
+    // cy.getIframeBody().contains(HOST1);
+    // cy.getIframeBody().contains(HOST2);
+    cy.getIframeBody().contains(GATEWAY);
   });
 
   it('Inspect service list', () => {
