8576b1f39d0af510de3f110e2fed8300c29dac1e
chriskari <chriskari@gmx.de>
2022-11-21 15:29:44 +0100

    who needs tests?

--- tests/config.js
+++
@@ -1,8 +0,0 @@
-const env = Cypress.env();
-const domain = env.DOMAIN || 'https://local.kyma.dev';
-
-export default {
-  domain: domain,
-  localDev: env.LOCAL_DEV || false,
-  clusterAddress: env.LOCAL_DEV ? `http://localhost:8080` : `${domain}`,
-};
--- tests/support/helpers.js
+++
@@ -1,9 +0,0 @@
-export function chooseComboboxOption(selector, optionText) {
-  cy.getIframeBody()
-    .find(selector)
-    .filterWithNoValue()
-    .type(optionText);
-  cy.getIframeBody()
-    .contains(optionText)
-    .click();
-}
