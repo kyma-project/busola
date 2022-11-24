4128b2ffbbd0ef863687cf16037407c860ec7ed9
Wawrzyn321 <Wawrzyn321@gmail.com>
2022-11-24 22:35:26 +0100

    Address PR changes

--- core-ui/src/components/Preferences/NamespaceSettings.tsx
+++ core-ui/src/components/Preferences/NamespaceSettings.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useRecoilState } from 'recoil';
 import { Switch } from 'fundamental-react';
--- core-ui/src/components/Preferences/test/NamespaceSettings.test.tsx
+++ core-ui/src/components/Preferences/test/NamespaceSettings.test.tsx
@@ -1,23 +1,15 @@
 import LuigiClient from '@luigi-project/client';
-import { RecoilRoot } from 'recoil';
 import { showHiddenNamespacesState } from 'state/preferences/showHiddenNamespacesAtom';
 import { render, fireEvent } from 'testing/reactTestingUtils';
 import NamespaceSettings from '../NamespaceSettings';
 
-jest.mock('shared/contexts/MicrofrontendContext', () => ({
-  useFeatureToggle: () => [true, () => null],
-}));
-
 describe('NamespaceSettings', () => {
   it('Sends custom message on toggle', () => {
     const spy = jest.spyOn(LuigiClient, 'sendCustomMessage');
-    const { getByLabelText } = render(
-      <RecoilRoot
-        initializeState={state => state.set(showHiddenNamespacesState, true)}
-      >
-        <NamespaceSettings />
-      </RecoilRoot>,
-    );
+    const { getByLabelText } = render(<NamespaceSettings />, {
+      initializeState: snapshot =>
+        snapshot.set(showHiddenNamespacesState, true),
+    });
 
     const toggleElement = getByLabelText(
       'settings.clusters.showHiddenNamespaces',
