c989324bad89861b89a7c9bbf5791126dbdc5c24
dariadomagala <daria.domagala@sap.com>
2022-11-30 12:21:28 +0100

    Add target cluster config

--- core-ui/src/state/configFeatures/configFeaturesSelector.ts
+++ core-ui/src/state/configFeatures/configFeaturesSelector.ts
@@ -1,7 +1,9 @@
-import { selector, RecoilValue } from 'recoil';
-import { ConfigFeatureList } from '../types';
 import jsyaml from 'js-yaml';
+import { selector, RecoilValue } from 'recoil';
 import { merge } from 'lodash';
+
+import { ConfigFeatureList } from '../types';
+import { getFetchFn } from '../utils/getFetchFn';
 
 type Config = {
   config?: {
@@ -9,28 +11,55 @@ type Config = {
   };
 };
 
+type ConfigMapResponse =
+  | {
+      data: {
+        config: string;
+      };
+    }
+  | undefined;
+
 type ConfigFeaturesState = ConfigFeatureList | null;
 
 export const configFeaturesState: RecoilValue<ConfigFeaturesState> = selector<
   ConfigFeaturesState
 >({
   key: 'configFeaturesState',
-  get: async () => {
+  get: async ({ get }) => {
+    const fetchFn = getFetchFn(get);
+    if (!fetchFn) {
+      return null;
+    }
+
     try {
       const cacheBuster = '?cache-buster=' + Date.now();
+
       const defaultConfigResponse = await fetch(
         '/config/defaultConfig.yaml' + cacheBuster,
       );
-      const configMapResponse = await fetch(
-        '/config/config.yaml' + cacheBuster,
-      );
+
+      const configResponse = await fetch('/config/config.yaml' + cacheBuster);
+
+      let configMapResponse: ConfigMapResponse;
+      try {
+        const response = await fetchFn({
+          relativeUrl:
+            '/api/v1/namespaces/kube-public/configmaps/busola-config',
+        });
+        configMapResponse = await response.json();
+      } catch (e) {
+        console.warn('Cannot load cluster params from the target cluster: ', e);
+      }
 
       const defaultParams = jsyaml.load(
         await defaultConfigResponse.text(),
       ) as Config;
-      const mapParams = jsyaml.load(await configMapResponse.text()) as Config;
+      const configParams = jsyaml.load(await configResponse.text()) as Config;
+      const mapParams = configMapResponse?.data?.config
+        ? (jsyaml.load(configMapResponse.data.config) as Config)
+        : {};
 
-      return merge(defaultParams, mapParams) as ConfigFeatureList;
+      return merge(defaultParams, configParams, mapParams) as ConfigFeatureList;
     } catch (e) {
       console.warn('Cannot load cluster params: ', e);
       return null;
