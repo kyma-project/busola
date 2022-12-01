1ef6370a96232d9dbce62661e6102a15502e79f4
Mateusz Wisniewski <mwisnia97@gmail.com>
2022-12-01 14:54:54 +0100

    Remove SSO (#2093)
    * remove sso
    
    * remove jwt
    
    * fix pull
    
    * remove jwks-rsa

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,6 @@ import { makeHandleRequest, serveStaticApp, serveMonaco } from './common';
 import { handleTracking } from './tracking.js';
 import jsyaml from 'js-yaml';
 //import { requestLogger } from './utils/other'; //uncomment this to log the outgoing traffic
-const { setupJWTCheck } = require('./jwtCheck');
 
 global.config = {};
 
@@ -50,7 +49,6 @@ if (gzipEnabled)
 if (process.env.NODE_ENV === 'development') {
   app.use(cors({ origin: '*' }));
 }
-setupJWTCheck(app);
 
 let server = null;
 
