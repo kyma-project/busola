982666d56f390da369589da429b779dd96330013
mrCherry97 <mwisnia97@gmail.com>
2022-12-02 14:51:24 +0100

    bring back undefined

--- core-ui/src/hooks/useFeature.ts
+++ core-ui/src/hooks/useFeature.ts
@@ -4,7 +4,7 @@ import { ConfigFeature, ConfigFeaturesNames } from 'state/types';
 
 export function useFeature<T extends ConfigFeature>(
   featureName: ConfigFeaturesNames,
-): T {
+): T | undefined {
   const configuration = useRecoilValue(configurationAtom);
   const feature = configuration?.features?.[featureName] ?? {
     isEnabled: false,
