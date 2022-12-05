78c1dc32568e0afdf7ba4665bb82b8b3abaf1175
Jakub Łukomski <dev@tenku.eu>
2022-12-05 11:52:00 +0100

    add resourceUrl to useUrl

--- core-ui/src/hooks/useUrl.ts
+++ core-ui/src/hooks/useUrl.ts
@@ -2,6 +2,11 @@ import { useMatch } from 'react-router';
 
 import { UrlGenerators } from 'state/types';
 
+export interface UrlOverrides {
+  cluster?: string;
+  namespace?: string;
+}
+
 export const useUrl: () => UrlGenerators = () => {
   const cluster =
     useMatch({ path: '/cluster/:cluster', end: false })?.params?.cluster ?? '';
@@ -9,18 +14,27 @@ export const useUrl: () => UrlGenerators = () => {
     useMatch({ path: '/cluster/:cluster/namespaces/:namespace', end: false })
       ?.params?.namespace ?? '';
 
-  const clusterUrl = (path: string) => {
-    return `/cluster/${cluster}/${path}`;
+  const clusterUrl = (path: string, overrides: UrlOverrides = {}) => {
+    return `/cluster/${overrides.cluster ?? cluster}/${path}`;
   };
-  const namespaceUrl = (path: string) => {
-    return `/cluster/${cluster}/namespaces/${namespace}/${path}`;
+
+  const namespaceUrl = (path: string, overrides: UrlOverrides = {}) => {
+    return `/cluster/${overrides.cluster ??
+      cluster}/namespaces/${overrides.namespace ?? namespace}/${path}`;
   };
-  const scopedUrl = (path: string) => {
-    if (namespace) {
-      return namespaceUrl(path);
+
+  const scopedUrl = (path: string, overrides: UrlOverrides = {}) => {
+    if (overrides.namespace ?? namespace) {
+      return namespaceUrl(path, overrides);
     } else {
-      return clusterUrl(path);
+      return clusterUrl(path, overrides);
     }
+  };
+
+  const resourceUrl = (resource: any) => {
+    return scopedUrl(resource.metadata.name, {
+      namespace: resource.metadata.namespace,
+    });
   };
 
   return {
@@ -29,5 +43,6 @@ export const useUrl: () => UrlGenerators = () => {
     clusterUrl,
     namespaceUrl,
     scopedUrl,
+    resourceUrl,
   };
 };
