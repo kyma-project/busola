7a86b069da099a11d1bfde2be6c40073ef2b38b4
mrCherry97 <mwisnia97@gmail.com>
2022-12-06 15:55:10 +0100

    adjust cluster role bindings

--- tests/tests/cluster/test-cluster-role-bindings.spec.js
+++ tests/tests/cluster/test-cluster-role-bindings.spec.js
@@ -14,104 +14,61 @@ context('Test Cluster Role Bindings', () => {
   it('Create a ClusterRoleBinding', () => {
     cy.navigateTo('Configuration', 'Cluster Role Bindings');
 
-    cy.getIframeBody()
-      .contains('Create Cluster Role Binding')
-      .click();
+    cy.contains('Create Cluster Role Binding').click();
 
-    cy.getIframeBody()
-      .contains('Advanced')
-      .click();
+    cy.contains('Advanced').click();
 
-    cy.getIframeBody()
-      .find('[ariaLabel="ClusterRoleBinding name"]')
-      .type(CRB_NAME);
+    cy.get('[ariaLabel="ClusterRoleBinding name"]').type(CRB_NAME);
 
-    cy.getIframeBody()
-      .find('[placeholder="Start typing to select ClusterRole from the list"]')
-      .type('admin');
+    cy.get(
+      '[placeholder="Start typing to select ClusterRole from the list"]',
+    ).type('admin');
 
-    cy.getIframeBody()
-      .contains('li', 'cluster-admin')
-      .click();
+    cy.contains('li', 'cluster-admin').click();
 
-    cy.getIframeBody()
-      .find('[ariaLabel="User name"]')
+    cy.get('[ariaLabel="User name"]')
       .clear()
       .type(USER_NAME);
 
-    cy.getIframeBody()
-      .find('[role="dialog"]')
-      .contains('button', 'Create')
-      .click();
+    cy.contains('[role="dialog"]button', 'Create').click();
   });
 
   it('Checking details', () => {
-    cy.getIframeBody()
-      .contains(CRB_NAME)
-      .should('be.visible');
+    cy.contains(CRB_NAME).should('be.visible');
 
-    cy.getIframeBody()
-      .contains('User')
-      .should('be.visible');
+    cy.contains('User').should('be.visible');
 
-    cy.getIframeBody()
-      .contains(USER_NAME)
-      .should('be.visible');
+    cy.contains(USER_NAME).should('be.visible');
 
-    cy.getIframeBody()
-      .contains('cluster-admin')
-      .should('be.visible');
+    cy.contains('cluster-admin').should('be.visible');
   });
 
   it('Edit', () => {
-    cy.getIframeBody()
-      .contains('Edit')
-      .click();
+    cy.contains('Edit').click();
 
-    cy.getIframeBody()
-      .find('[role="document"]')
-      .contains('User')
-      .click();
+    cy.contains('[role="document"]', 'User').click();
 
-    cy.getIframeBody()
-      .contains('ServiceAccount')
-      .click();
+    cy.contains('ServiceAccount').click();
 
-    cy.getIframeBody()
-      .contains('Service Account Namespace')
-      .should('be.visible');
+    cy.contains('Service Account Namespace').should('be.visible');
 
-    cy.getIframeBody()
-      .contains('Service Account Name')
-      .should('be.visible');
+    cy.contains('Service Account Name').should('be.visible');
 
-    cy.getIframeBody()
-      .contains('ServiceAccount')
-      .click();
+    cy.contains('ServiceAccount').click();
 
-    cy.getIframeBody()
-      .contains('Group')
-      .click();
+    cy.contains('Group').click();
 
-    cy.getIframeBody()
-      .find('[ariaLabel="Group name"]')
+    cy.get('[ariaLabel="Group name"]')
       .clear()
       .type('test-group');
 
-    cy.getIframeBody()
-      .find('[role="dialog"]')
-      .contains('button', 'Update')
-      .click();
+    cy.contains('[role="dialog"] button', 'Update').click();
   });
 
   it('Checking updates details', () => {
-    cy.getIframeBody()
-      .contains('Group')
-      .should('be.visible');
+    cy.contains('Group').should('be.visible');
 
-    cy.getIframeBody()
-      .contains('test-group')
-      .should('be.visible');
+    cy.contains('test-group').should('be.visible');
   });
 
   it('Delete Cluster Role Binding', () => {
