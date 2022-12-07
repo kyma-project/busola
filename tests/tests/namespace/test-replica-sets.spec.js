fae2bb4a0358a8ff958394e8876baed9235b15d3
mrCherry97 <mwisnia97@gmail.com>
2022-12-07 15:52:46 +0100

    adjust replicasets

--- tests/tests/namespace/test-replica-sets.spec.js
+++ tests/tests/namespace/test-replica-sets.spec.js
@@ -18,49 +18,39 @@ context('Test Replica Sets', () => {
   it('Creates a Replica Set', () => {
     cy.navigateTo('Workloads', 'Replica Sets');
 
-    cy.getIframeBody()
-      .contains('Create Replica Set')
-      .click();
+    cy.contains('Create Replica Set').click();
 
-    cy.getIframeBody()
-      .contains('Advanced')
-      .click();
+    cy.contains('Advanced').click();
 
-    cy.getIframeBody()
-      .find('[ariaLabel="ReplicaSet name"]')
+    cy.get('[ariaLabel="ReplicaSet name"]')
       .clear()
       .type(REPLICA_SET_NAME);
 
-    cy.getIframeBody()
-      .find('[placeholder="Replicas"]')
+    cy.get('[placeholder="Replicas"]')
       .clear()
       .type(REPLICAS_AMOUNT)
       .should('have.value', REPLICAS_AMOUNT);
 
-    cy.getIframeBody()
-      .find(
-        '[placeholder="Enter the Docker image tag, for example, bitnami/nginx"]',
-      )
+    cy.get(
+      '[placeholder="Enter the Docker image tag, for example, bitnami/nginx"]',
+    )
       .clear()
       .type(DOCKER_IMAGE_TAG)
       .should('have.value', DOCKER_IMAGE_TAG);
 
-    cy.getIframeBody()
-      .find('[role="document"]')
+    cy.get('[role="document"]')
       .contains('button', 'Create')
       .click();
   });
 
   it('Checks the details view', () => {
-    cy.getIframeBody()
-      .contains(`${REPLICA_SET_NAME}-`)
-      .click();
+    cy.contains(`${REPLICA_SET_NAME}-`).click();
 
-    cy.getIframeBody().contains(REPLICA_SET_NAME);
+    cy.contains(REPLICA_SET_NAME);
 
-    cy.getIframeBody().contains('Always');
+    cy.contains('Always');
 
-    cy.getIframeBody().contains(`Image${DOCKER_IMAGE_TAG}`);
+    cy.contains(`Image${DOCKER_IMAGE_TAG}`);
   });
 
   it('Checks the list view', () => {
@@ -68,54 +58,42 @@ context('Test Replica Sets', () => {
       .contains('Replica Sets')
       .click();
 
-    cy.getIframeBody()
-      .contains(REPLICA_SET_NAME)
-      .click();
+    cy.contains(REPLICA_SET_NAME).click();
 
-    cy.getIframeBody().contains(REPLICA_SET_NAME);
+    cy.contains(REPLICA_SET_NAME);
   });
 
   it('Edits the Docker image and Replicas amount in the Replica set', () => {
-    cy.getIframeBody()
-      .contains('Edit')
-      .click();
+    cy.contains('Edit').click();
 
-    cy.getIframeBody()
-      .find(
-        '[placeholder="Enter the Docker image tag, for example, bitnami/nginx"]',
-      )
+    cy.get(
+      '[placeholder="Enter the Docker image tag, for example, bitnami/nginx"]',
+    )
       .clear()
       .type(EDITED_DOCKER_IMAGE_TAG)
       .should('have.value', EDITED_DOCKER_IMAGE_TAG);
 
-    cy.getIframeBody()
-      .find('[placeholder="Replicas"]')
+    cy.get('[placeholder="Replicas"]')
       .clear()
       .type(EDITED_REPLICAS_AMOUNT)
       .should('have.value', EDITED_REPLICAS_AMOUNT);
 
-    cy.getIframeBody()
-      .find('[role="document"]')
+    cy.get('[role="document"]')
       .contains('button', 'Update')
       .click();
   });
 
   it('Checks the new Docker image', () => {
-    cy.getIframeBody()
-      .contains('Replica Sets')
-      .click();
+    cy.contains('Replica Sets').click();
 
-    cy.getIframeBody().contains(EDITED_DOCKER_IMAGE_TAG);
+    cy.contains(EDITED_DOCKER_IMAGE_TAG);
   });
 
   it('Checks the new amout of Replicas', () => {
-    cy.getIframeBody()
-      .contains(REPLICA_SET_NAME)
-      .click();
+    cy.contains(REPLICA_SET_NAME).click();
 
-    cy.getIframeBody().contains(
-      `${EDITED_REPLICAS_AMOUNT} / ${EDITED_REPLICAS_AMOUNT}`,
-      { timeout: 15 * 1000 },
-    );
+    cy.contains(`${EDITED_REPLICAS_AMOUNT} / ${EDITED_REPLICAS_AMOUNT}`, {
+      timeout: 15 * 1000,
+    });
   });
 });
