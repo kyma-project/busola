578c14e563d41f33ae4f307db4473235bb00cc26
mrCherry97 <mwisnia97@gmail.com>
2022-12-08 10:40:11 +0100

    adjust SE test

--- tests/tests/namespace/test-service-entries.spec.js
+++ tests/tests/namespace/test-service-entries.spec.js
@@ -27,81 +27,62 @@ context('Test Service Entries', () => {
   it('Create a Service Entry', () => {
     cy.navigateTo('Istio', 'Service Entries');
 
-    cy.getIframeBody()
-      .contains('Create Service Entry')
-      .click();
+    cy.contains('Create Service Entry').click();
 
-    cy.getIframeBody()
-      .contains('Advanced')
-      .click();
+    cy.contains('Advanced').click();
 
     // Name
-    cy.getIframeBody()
-      .find('[arialabel="ServiceEntry name"]:visible', { log: false })
-      .type(SE_NAME);
+    cy.get('[arialabel="ServiceEntry name"]:visible', { log: false }).type(
+      SE_NAME,
+    );
 
     // Hosts
-    cy.getIframeBody()
-      .find('[aria-label="expand Hosts"]:visible', { log: false })
-      .click();
+    cy.get('[aria-label="expand Hosts"]:visible', { log: false }).click();
 
-    cy.getIframeBody()
-      .find('[data-testid="spec.hosts.0"]:visible')
-      .type(HOST);
+    cy.get('[data-testid="spec.hosts.0"]:visible').type(HOST);
 
-    cy.getIframeBody()
-      .find('[aria-label="Combobox input"]:visible', { log: false })
+    cy.get('[aria-label="Combobox input"]:visible', { log: false })
       .eq(1)
       .type(RESOLUTION);
 
-    cy.getIframeBody()
-      .find('[aria-label="Combobox input"]:visible', { log: false })
+    cy.get('[aria-label="Combobox input"]:visible', { log: false })
       .first()
       .type(LOCATION);
 
-    cy.getIframeBody()
-      .find('[aria-label="expand Addresses"]:visible', { log: false })
-      .click();
+    cy.get('[aria-label="expand Addresses"]:visible', { log: false }).click();
 
-    cy.getIframeBody()
-      .find('[placeholder="For example, 127.0.0.1"]:visible', {
-        log: false,
-      })
-      .type(ADDRESS);
+    cy.get('[placeholder="For example, 127.0.0.1"]:visible', {
+      log: false,
+    }).type(ADDRESS);
 
-    cy.getIframeBody()
-      .find('[placeholder="Enter key"]:visible', { log: false })
+    cy.get('[placeholder="Enter key"]:visible', { log: false })
       .filterWithNoValue()
       .type('test');
 
-    cy.getIframeBody()
-      .find('[placeholder="Enter value"]:visible', { log: false })
+    cy.get('[placeholder="Enter value"]:visible', { log: false })
       .filterWithNoValue()
       .first()
       .type('selector-value');
 
-    cy.getIframeBody()
-      .find('[role="dialog"]')
+    cy.get('[role="dialog"]')
       .contains('button', 'Create')
       .click();
 
-    cy.getIframeBody()
-      .contains('h3', SE_NAME)
-      .should('be.visible');
+    cy.contains('h3', SE_NAME).should('be.visible');
   });
 
   it('Check the Service Entry details', () => {
-    cy.getIframeBody()
+    cy.get('#content-wrap')
       .should('include.text', RESOLUTION)
       .and('include.text', LOCATION)
       .and('include.text', HOST)
       .and('include.text', ADDRESS);
 
-    cy.getIframeBody().contains(WORKLOAD_SELECTOR_LABEL);
+    cy.contains(WORKLOAD_SELECTOR_LABEL);
   });
 
   it('Check the Service Entries list', () => {
     cy.inspectList('Service Entries', SE_NAME);
-    cy.getIframeBody().contains(RESOLUTION);
+    cy.contains(RESOLUTION);
   });
 });
