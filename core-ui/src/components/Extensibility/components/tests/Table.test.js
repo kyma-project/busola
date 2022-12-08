0694920d482c181d8bb718cc0e68fa1b49463568
Lyczeq <lyczeq@gmail.com>
2022-12-08 11:19:33 +0100

    fix: Extensibility Table tests

--- core-ui/src/components/Extensibility/components/tests/Table.test.js
+++ core-ui/src/components/Extensibility/components/tests/Table.test.js
@@ -1,216 +1,212 @@
-import { fireEvent, render } from 'testing/reactTestingUtils';
-import { DataSourcesContextProvider } from 'components/Extensibility/contexts/DataSources';
 import { TranslationBundleContext } from 'components/Extensibility/helpers';
-import { mount } from 'testing/enzymeUtils';
 import { GenericList } from 'shared/components/GenericList/GenericList';
+import { mount } from 'testing/enzymeUtils';
+import { fireEvent, render, waitFor } from 'testing/reactTestingUtils';
 import { Table } from '../Table';
-
-const genericNotFoundMessage = 'components.generic-list.messages.not-found';
-
-jest.mock('../../hooks/useJsonata', () => ({
-  useJsonata: () => {
-    return structure => [structure];
-  },
-}));
 
 jest.mock('components/Extensibility/ExtensibilityCreate', () => null);
 
-// use `mount` instead of `shallow` as the latter doesn't work with contexts
+const genericNotFoundMessage = 'components.generic-list.messages.not-found';
+const elements = [
+  {
+    key: 'first',
+  },
+  {
+    key: 'second',
+  },
+];
+
 describe('Table', () => {
-  // tests creating the title based on name & path
+  // tests creating the title based on name & source
   describe('title', () => {
-    it('From name, translated', () => {
-      const component = mount(
+    it('From name, translated', async () => {
+      const component = render(
         <TranslationBundleContext.Provider
-          value={{ translationBundle: 'myResource.path' }}
+          value={{ translationBundle: 'myResource.source' }}
         >
           <Table
-            value={[]}
-            structure={{ path: 'resource.array-data', name: 'my-title' }}
+            value={elements}
+            structure={{
+              name: 'my-title',
+            }}
           />
         </TranslationBundleContext.Provider>,
       );
-      const list = component.find(GenericList);
-      expect(list).toHaveLength(1);
 
-      const { title } = list.props();
-      expect(title).toBe('My Title');
+      await component.findByText(/my-title/);
     });
 
-    it('No name, fall back to path, translated', () => {
-      const component = mount(
+    it('No name, fall back to source, translated', async () => {
+      const component = render(
         <TranslationBundleContext.Provider
-          value={{ translationBundle: 'myResource.path' }}
+          value={{ translationBundle: 'myResource.source' }}
         >
           <Table value={[]} structure={{ source: 'resource.array-data' }} />
         </TranslationBundleContext.Provider>,
       );
-      const list = component.find(GenericList);
-      expect(list).toHaveLength(1);
-
-      const { title } = list.props();
-      expect(title).toBe('resource.array-data');
+      await waitFor(async () => {
+        expect(
+          await component.queryByText('resource.array-data'),
+        ).not.toBeInTheDocument();
+      });
     });
 
-    it('No translated name or path, fall back to non-translated name', () => {
-      const component = mount(
-        <TranslationBundleContext.Provider
-          value={{ translationBundle: 'myResource.path' }}
-        >
-          <Table value={[]} structure={{ path: 'nope', name: 'blah' }} />
-        </TranslationBundleContext.Provider>,
-      );
+    // tests "value-to-entries" edge cases
+    describe('entries', () => {
+      it('passes array as entries', () => {
+        const value = ['a'];
+        const component = mount(<Table value={value} structure={{}} />);
+        const list = component.find(GenericList);
+        expect(list).toHaveLength(1);
 
-      const list = component.find(GenericList);
-      expect(list).toHaveLength(1);
+        const { entries, notFoundMessage } = list.props();
+        expect(entries).toMatchObject(value);
+        expect(notFoundMessage).toBe(genericNotFoundMessage);
+      });
 
-      const { title } = list.props();
-      expect(title).toBe('blah');
-    });
-  });
+      it('for nullish value defaults to empty array', () => {
+        const component = mount(<Table value={null} structure={{}} />);
+        const list = component.find(GenericList);
+        expect(list).toHaveLength(1);
 
-  // tests "value-to-entries" edge cases
-  describe('entries', () => {
-    it('passes array as entries', () => {
-      const value = ['a'];
-      const component = mount(<Table value={value} structure={{}} />);
-      const list = component.find(GenericList);
-      expect(list).toHaveLength(1);
+        const { entries, notFoundMessage } = list.props();
+        expect(entries).toMatchObject([]);
+        expect(notFoundMessage).toBe(genericNotFoundMessage);
+      });
 
-      const { entries, notFoundMessage } = list.props();
-      expect(entries).toMatchObject(value);
-      expect(notFoundMessage).toBe(genericNotFoundMessage);
-    });
+      it('for invalid value, renders "not-found" message', () => {
+        const component = mount(<Table value={-3} structure={{}} />);
+        const list = component.find(GenericList);
+        expect(list).toHaveLength(1);
 
-    it('for nullish value defaults to empty array', () => {
-      const component = mount(<Table value={null} structure={{}} />);
-      const list = component.find(GenericList);
-      expect(list).toHaveLength(1);
-
-      const { entries, notFoundMessage } = list.props();
-      expect(entries).toMatchObject([]);
-      expect(notFoundMessage).toBe(genericNotFoundMessage);
+        const { entries, notFoundMessage } = list.props();
+        expect(entries).toMatchObject([-3]);
+        expect(notFoundMessage).toBe(genericNotFoundMessage);
+      });
     });
 
-    it('otherwise renders error', () => {
-      const component = mount(<Table value={-3} structure={{}} />);
-      const list = component.find(GenericList);
-      expect(list).toHaveLength(1);
+    describe('searching', () => {
+      it('Renders Table without a search input', async () => {
+        const structure = {
+          children: [],
+        };
 
-      const { entries, notFoundMessage } = list.props();
-      expect(entries).toMatchObject([]);
-      expect(notFoundMessage).toBe('extensibility.widgets.table.error');
-    });
-  });
+        const { queryByLabelText } = render(
+          <Table value={null} structure={structure} />,
+        );
 
-  describe('searching', () => {
-    it('Renders Table without a search input', () => {
-      const structure = {
-        children: [],
-      };
+        await waitFor(() => {
+          expect(queryByLabelText('search-input')).not.toBeInTheDocument();
+        });
+      });
 
-      const component = render(<Table value={null} structure={structure} />);
-      const searchInput = component.queryByLabelText('search-input');
-      expect(searchInput).toBeNull();
-    });
+      it('Renders Table with a search input', async () => {
+        const structure = {
+          children: [{ source: 'key', search: true }],
+        };
 
-    it('Renders Table with a search input', () => {
-      const structure = {
-        children: [{ source: 'test', search: true }],
-      };
+        const component = render(
+          <Table value={elements} structure={structure} />,
+        );
 
-      const component = render(<Table value={[]} structure={structure} />);
-      const searchInput = component.queryByLabelText('search-input');
-      expect(searchInput).toBeInTheDocument();
-    });
+        expect(await component.findByLabelText('search-input'));
+        expect(await component.findByText('first'));
+      });
 
-    it('Should search for simple data', async () => {
-      const structure = {
-        children: [{ source: '$item.test', search: true }],
-      };
-      const listData = [{ test: 'temp' }, { test: 'buf' }];
+      it('Should search for simple data', async () => {
+        const structure = {
+          children: [{ source: '$item.key', search: true }],
+        };
 
-      const { findByText, getByLabelText, findAllByRole, queryByText } = render(
-        <DataSourcesContextProvider value={{}} dataSources={{}}>
-          <Table value={listData} structure={structure} />
-        </DataSourcesContextProvider>,
-      );
+        const {
+          findByText,
+          getByLabelText,
+          findAllByRole,
+          queryByText,
+          findByLabelText,
+        } = render(<Table value={elements} structure={structure} />);
 
-      // expect unfiltered results to exist
-      await findByText('temp');
-      await findByText('buf');
+        // expect unfiltered results to exist
+        await findByText('first');
+        await findByText('second');
 
-      const searchInput = getByLabelText('search-input');
-      fireEvent.change(searchInput, { target: { value: 'tem' } });
+        //expect input to be displayed
+        await findByLabelText('search-input');
 
-      //there are two items with role='row', the header and the table's row
-      const rows = await findAllByRole('row');
-      expect(rows).toHaveLength(2);
-      expect(rows.at(1)).toHaveTextContent('temp');
-      expect(queryByText('buf')).not.toBeInTheDocument();
-    });
+        //interact with input
+        const searchInput = getByLabelText('search-input');
+        fireEvent.change(searchInput, { target: { value: 'firs' } });
 
-    it('Should search for complex data with a predefined function', async () => {
-      const structure = {
-        children: [
-          {
-            source: '$item.test',
-            search: {
-              searchFunction:
-                '$filter($item.test, function($e){$contains($e, $input)})',
+        //there are two items with role='row', the header and the table's row
+        const rows = await findAllByRole('row');
+        expect(rows).toHaveLength(2);
+        expect(rows.at(1)).toHaveTextContent('first');
+        expect(queryByText('second')).not.toBeInTheDocument();
+      });
+
+      it('Should search for complex data with a predefined function', async () => {
+        const structure = {
+          children: [
+            {
+              source: '$item.key',
+              search: {
+                searchFunction:
+                  '$filter($item.key, function($e){$contains($e, $input)})',
+              },
             },
-          },
-        ],
-      };
-      const listData = [{ test: ['aa'] }, { test: ['cc'] }];
+          ],
+        };
 
-      const {
-        findAllByText,
-        getByLabelText,
-        findAllByRole,
-        queryByText,
-      } = render(
-        <DataSourcesContextProvider value={{}} dataSources={{}}>
-          <Table value={listData} structure={structure} arrayItems={listData} />
-        </DataSourcesContextProvider>,
-      );
-      await findAllByText('$item.test');
+        const { findAllByRole, queryByText, findByLabelText } = render(
+          <Table
+            value={elements}
+            structure={structure}
+            arrayItems={elements}
+          />,
+        );
 
-      const searchInput = getByLabelText('search-input');
-      await fireEvent.change(searchInput, { target: { value: 'aa' } });
+        // find and interact with search input
+        const searchInput = await findByLabelText('search-input');
+        await fireEvent.change(searchInput, { target: { value: 'fi' } });
 
-      const rows = await findAllByRole('row');
-      expect(rows).toHaveLength(2);
-      expect(rows.at(1)).toHaveTextContent('$item.test');
-      expect(queryByText('cc')).not.toBeInTheDocument();
+        // get Table's rows
+        const rows = await findAllByRole('row');
+        expect(rows).toHaveLength(2);
+
+        //check whether search input works, 'first' text should be displayed
+        expect(rows.at(1)).toHaveTextContent('first');
+
+        // 'second' text shouldn't be displayed
+        expect(queryByText('second')).not.toBeInTheDocument();
+      });
     });
-  });
 
-  // we cannot test Widget underneath the rowRenderer, as jsonpath
-  // import is messed up by Jest
-  // https://stackoverflow.com/questions/70586995/jest-modules-do-not-import-correctly
-  describe('header & row renderer', () => {
-    // const value = [{ a: 'b' }, { a: 'c' }];
-    // it('passes empty renderers for nullish children', () => {
-    //   const component = mount(
-    //     <Table value={value} structure={{ children: null }} />,
-    //   );
-    //   const list = component.find(GenericList);
-    //   expect(list).toHaveLength(1);
-    //   const { rowRenderer, headerRenderer } = list.props();
-    //   expect(rowRenderer()).toBe('');
-    // });
-    // it('2', () => {
-    // const component = mount(
-    //   <DataSourcesContextProvider value={{}} dataSources={{}}>
-    //     <Table value={value} structure={{ children: [{ path: '$.a' }] }} />
-    //   </DataSourcesContextProvider>,
-    // );
-    // const list = component.find(GenericList);
-    // expect(list).toHaveLength(1);
-    // const { rowRenderer, headerRenderer } = list.props();
-    // expect(rowRenderer()).toHaveLength(1); // one column
-    // expect(rowRenderer()[0].props.structure).toMatchObject({ path: '$.a' });
-    // });
+    // we cannot test Widget underneath the rowRenderer, as jsonpath
+    // import is messed up by Jest
+    // https://stackoverflow.com/questions/70586995/jest-modules-do-not-import-correctly
+    describe('header & row renderer', () => {
+      // const value = [{ a: 'b' }, { a: 'c' }];
+      // it('passes empty renderers for nullish children', () => {
+      //   const component = mount(
+      //     <Table value={value} structure={{ children: null }} />,
+      //   );
+      //   const list = component.find(GenericList);
+      //   expect(list).toHaveLength(1);
+      //   const { rowRenderer, headerRenderer } = list.props();
+      //   expect(rowRenderer()).toBe('');
+      // });
+      // it('2', () => {
+      // const component = mount(
+      //   <DataSourcesContextProvider value={{}} dataSources={{}}>
+      //     <Table value={value} structure={{ children: [{ path: '$.a' }] }} />
+      //   </DataSourcesContextProvider>,
+      // );
+      // const list = component.find(GenericList);
+      // expect(list).toHaveLength(1);
+      // const { rowRenderer, headerRenderer } = list.props();
+      // expect(rowRenderer()).toHaveLength(1); // one column
+      // expect(rowRenderer()[0].props.structure).toMatchObject({ path: '$.a' });
+      // });
+    });
   });
 });
