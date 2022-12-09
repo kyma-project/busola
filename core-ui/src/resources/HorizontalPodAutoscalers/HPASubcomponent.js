f0b42a0bbedbef06af795391ff333e557d8dd74f
Agata Kucharska <agata.kucharska@sap.com>
2022-12-09 13:09:12 +0100

    Remove universalLinks

--- core-ui/src/resources/HorizontalPodAutoscalers/HPASubcomponent.js
+++ core-ui/src/resources/HorizontalPodAutoscalers/HPASubcomponent.js
@@ -3,12 +3,12 @@ import { useGet } from 'shared/hooks/BackendAPI/useGet';
 import { EMPTY_TEXT_PLACEHOLDER } from 'shared/constants';
 import { GenericList } from 'shared/components/GenericList/GenericList';
 import { useTranslation } from 'react-i18next';
-import { Link } from 'fundamental-react';
-import { navigateToResource } from 'shared/helpers/universalLinks';
+import { Link } from 'react-router-dom';
 import { Tokens } from 'shared/components/Tokens';
 import { MetricsBrief } from './helpers';
 import { useRecoilValue } from 'recoil';
 import { activeNamespaceIdState } from 'state/activeNamespaceIdAtom';
+import { useUrl } from 'hooks/useUrl';
 
 export const HPASubcomponent = props => {
   const { t } = useTranslation();
@@ -17,6 +17,7 @@ export const HPASubcomponent = props => {
     `/apis/autoscaling/v2beta2/namespaces/${namespaceId}/horizontalpodautoscalers`,
   );
   const [associatedHPA, setAssociatedHPA] = useState([]);
+  const { namespaceUrl } = useUrl();
 
   const {
     kind,
@@ -52,13 +53,14 @@ export const HPASubcomponent = props => {
       ]}
       rowRenderer={hpa => [
         <Link
-          onClick={() =>
-            navigateToResource({
+          className="fd-link"
+          to={resourceUrl({
+            kind: 'horizontalpodautoscaler',
+            metadata: {
               name: hpa.metadata.name,
-              kind: 'horizontalpodautoscaler',
               namespace: namespaceId,
-            })
-          }
+            },
+          })}
         >
           {hpa.metadata.name}
         </Link>,
