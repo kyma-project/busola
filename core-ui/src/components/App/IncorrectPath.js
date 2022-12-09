c831498cf243829fd3ad7bb681929d8177ec1b62
dariadomagala <daria.domagala@sap.com>
2022-12-09 13:11:56 +0100

    Tets

--- core-ui/src/components/App/IncorrectPath.js
+++ core-ui/src/components/App/IncorrectPath.js
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 
 export function IncorrectPath({
   to,
-  title = 'Incorrect path',
+  title = 'Incorrect path.',
   message = 'The provided path does not exist. You will get redirected.',
 }) {
   const { t } = useTranslation();
