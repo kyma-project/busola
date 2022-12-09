b059ac05ae3a3cc9a2627d3bf225f952c08b9dba
Lyczeq <lyczeq@gmail.com>
2022-12-09 11:27:49 +0100

    fix: adjust PageHeader unit tests

--- core-ui/src/shared/components/PageHeader/test/PageHeader.test.js
+++ core-ui/src/shared/components/PageHeader/test/PageHeader.test.js
@@ -1,23 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render } from 'testing/reactTestingUtils';
 
 import { PageHeader } from 'shared/components/PageHeader/PageHeader';
 
-const mockNavigate = jest.fn();
-
-jest.mock('@luigi-project/client', () => ({
-  linkManager: () => ({
-    fromClosestContext: () => ({
-      navigate: mockNavigate,
-    }),
-  }),
-}));
-
 describe('PageHeader', () => {
-  afterEach(() => {
-    mockNavigate.mockReset();
-  });
-
   it('Renders title', () => {
     const { getByText } = render(<PageHeader title="page title" />);
 
@@ -36,17 +22,15 @@ describe('PageHeader', () => {
   });
 
   it('Renders one breadcrumbItem with link', () => {
-    const breadcrumbItems = [{ name: 'item1', path: 'path1' }];
+    const breadcrumbItems = [{ name: 'item1', url: 'path1' }];
     const { getByText } = render(
       <PageHeader title="page title" breadcrumbItems={breadcrumbItems} />,
     );
 
-    const item = getByText('item1');
+    const anchorElement = getByText('item1');
+    expect(anchorElement).toBeInTheDocument();
 
-    expect(item).toBeInTheDocument();
-
-    item.click();
-
-    expect(mockNavigate).toHaveBeenCalledWith(breadcrumbItems[0].path);
+    const hrefAttribute = anchorElement.getAttribute('href');
+    expect(hrefAttribute).toBe(`/${breadcrumbItems[0].url}`);
   });
 });
