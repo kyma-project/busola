1924b7552c7a43ebd1bf68e02a6790c35d40eadd
Lyczeq <lyczeq@gmail.com>
2022-12-09 11:33:49 +0100

    fix: ReleaseDataPanel component unit tests

--- core-ui/src/components/HelmReleases/tests/ReleaseDataPanel.test.js
+++ core-ui/src/components/HelmReleases/tests/ReleaseDataPanel.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render } from 'testing/reactTestingUtils';
 import { ReleaseDataPanel } from '../ReleaseDataPanel';
 
 describe('ReleaseDataPanel', () => {
