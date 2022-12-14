c539ad041f3c99e937f3c4af60051459cb8b0e7c
Jakub Łukomski <dev@tenku.eu>
2022-12-14 12:47:25 +0100

    fix reset vars (#2146)
    * re-add resetting vars on yaml change, reset vars on preset change
    
    * bump image version
    
    * fix tests

--- core-ui/src/components/Extensibility/ExtensibilityCreate.js
+++ core-ui/src/components/Extensibility/ExtensibilityCreate.js
@@ -10,6 +10,7 @@ import { useNotification } from 'shared/contexts/NotificationContext';
 import { Spinner } from 'shared/components/Spinner/Spinner';
 import { useGetSchema } from 'hooks/useGetSchema';
 import { prettifyKind } from 'shared/utils/helpers';
+import { ModeSelector } from 'shared/ResourceForm/components/ModeSelector';
 
 import { ResourceSchema } from './ResourceSchema';
 import { usePreparePresets, createTemplate, getDefaultPreset } from './helpers';
@@ -145,6 +146,14 @@ export function ExtensibilityCreateCore({
           presetValue,
         );
         setStore(getUIStoreFromResourceObj(updatedResource));
+        resetVars();
+        readVars(updatedResource);
+      }}
+      onModeChange={(oldMode, newMode) => {
+        if (oldMode === ModeSelector.MODE_YAML) {
+          resetVars();
+          readVars(resource);
+        }
       }}
       formElementRef={formElementRef}
       createUrl={resourceUrl}
