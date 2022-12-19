52ba5d3717966323090606c78681995edafd747d
dariadomagala <daria.domagala@sap.com>
2022-12-19 11:18:04 +0100

    Wait for extensions to load to display a redirection error

--- core-ui/src/components/App/IncorrectPath.js
+++ core-ui/src/components/App/IncorrectPath.js
@@ -1,13 +1,18 @@
 import { Button, MessageBox } from 'fundamental-react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { extensionsState } from 'state/navigation/extensionsAtom';
 
 export function IncorrectPath({ to, title = '', message = '' }) {
   const { t } = useTranslation();
   const navigate = useNavigate();
+  const extensions = useRecoilValue(extensionsState);
 
   title = title || t('components.incorrect-path.title.default');
   message = message || t('components.incorrect-path.message.default');
+
+  if (!extensions) return null;
 
   return (
     <MessageBox
