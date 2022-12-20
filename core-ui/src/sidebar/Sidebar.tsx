9641f2a147bb3f5cf25b09ef033a49cc2381c9ce
Jakub Lyczko <lyczeq@gmail.com>
2022-12-20 07:46:31 +0100

    fix: refreshing app when changing namespace (#2196)
    * fix: refreshing whole app when changing ns
    
    * fix: style of Sidebar error, add sidebar error msg and border to it
    
    * fix: remove border from sidebar
    
    * bump deployment
    
    * fix: styling error message
    
    * fix codeowners

--- core-ui/src/sidebar/Sidebar.tsx
+++ core-ui/src/sidebar/Sidebar.tsx
@@ -1,19 +1,28 @@
+import { Suspense } from 'react';
+import { useTranslation } from 'react-i18next';
 import { ErrorBoundary } from 'shared/components/ErrorBoundary/ErrorBoundary';
+import { Spinner } from 'shared/components/Spinner/Spinner';
 import { SidebarNavigation } from 'sidebar/SidebarNavigation';
 import { Footer } from './Footer/Footer';
 
 import './Sidebar.scss';
 
 export const Sidebar = () => {
+  const { t } = useTranslation();
   const pathname = window.location.pathname;
   if (pathname === '/clusters' || pathname === '/no-permissions') return null;
 
   return (
     <aside className="sidebar">
       <section className="sidebar__content">
-        <ErrorBoundary customMessage="navigation error" displayButton={false}>
-          <SidebarNavigation />
-        </ErrorBoundary>
+        <Suspense fallback={<Spinner size="m" />}>
+          <ErrorBoundary
+            customMessage={t('navigation.errors.sidebar')}
+            displayButton={false}
+          >
+            <SidebarNavigation />
+          </ErrorBoundary>
+        </Suspense>
       </section>
       <Footer />
     </aside>
