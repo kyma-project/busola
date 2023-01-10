6d8e720315f0762347f8a792e4a0b73cd88ad0ef
Lyczeq <lyczeq@gmail.com>
2023-01-10 13:19:35 +0100

    feat: add creating a secret after creating new SA

--- core-ui/src/resources/ServiceAccounts/ServiceAccountCreate.js
+++ core-ui/src/resources/ServiceAccounts/ServiceAccountCreate.js
@@ -7,9 +7,27 @@ import * as Inputs from 'shared/ResourceForm/inputs';
 import { useGetList } from 'shared/hooks/BackendAPI/useGet';
 import { ResourceForm } from 'shared/ResourceForm';
 import { ComboboxArrayInput } from 'shared/ResourceForm/fields';
-
+import { useCreateResource } from 'shared/ResourceForm/useCreateResource';
 import { createServiceAccountTemplate } from './templates';
 import { validateServiceAccount } from './helpers';
+import { useNavigate } from 'react-router-dom';
+import { useUrl } from 'hooks/useUrl';
+
+const createDefaultSecret = serviceAccountName => {
+  return {
+    apiVersion: 'v1',
+    kind: 'Secret',
+    type: 'kubernetes.io/service-account-token',
+    metadata: {
+      name: `${serviceAccountName}`,
+      labels: {},
+      annotations: {
+        'kubernetes.io/service-account.name': serviceAccountName,
+      },
+    },
+    data: {},
+  };
+};
 
 export const ServiceAccountCreate = ({
   formElementRef,
@@ -17,14 +35,30 @@ export const ServiceAccountCreate = ({
   onChange,
   setCustomValid,
   resource: initialServiceAccount,
+  onError,
+  onCompleted,
   resourceUrl,
   ...props
 }) => {
   const { t } = useTranslation();
-
+  const navigate = useNavigate();
+  const { namespaceUrl } = useUrl();
   const [serviceAccount, setServiceAccount] = useState(
     cloneDeep(initialServiceAccount) || createServiceAccountTemplate(namespace),
   );
+
+  const [shouldCreateSecret, setShouldCreateSecret] = useState(false);
+
+  const { data } = useGetList()(`/api/v1/namespaces/${namespace}/secrets`);
+
+  const createSecretResource = useCreateResource({
+    singularName: 'Secret',
+    pluralKind: 'Secrets',
+    resource: createDefaultSecret(serviceAccount.metadata.name),
+    initialResource: null,
+    createUrl: `/api/v1/namespaces/${serviceAccount.metadata.namespace}/secrets`,
+    afterCreatedFn: () => {},
+  });
 
   React.useEffect(() => {
     setCustomValid(validateServiceAccount(serviceAccount));
@@ -41,7 +75,25 @@ export const ServiceAccountCreate = ({
     setServiceAccount({ ...serviceAccount });
   };
 
-  const { data } = useGetList()(`/api/v1/namespaces/${namespace}/secrets`);
+  async function afterServiceAccountCreate(defaultAfterCreateFn) {
+    if (initialServiceAccount || !shouldCreateSecret) {
+      defaultAfterCreateFn();
+      return;
+    }
+
+    navigate(namespaceUrl(`serviceaccounts/${serviceAccount.metadata.name}`));
+    const secretCreationResult = await createSecretResource();
+    defaultAfterCreateFn();
+    if (secretCreationResult === false) {
+      onError(
+        'Warning',
+        'Your ServiceAccount was created successfully, however Secret creation failed. You have to create it manually later.',
+        true,
+      );
+    } else {
+      onCompleted(`ServiceAccount ${serviceAccount.metadata.name} created`);
+    }
+  }
 
   return (
     <ResourceForm
@@ -54,28 +106,18 @@ export const ServiceAccountCreate = ({
       formElementRef={formElementRef}
       createUrl={resourceUrl}
       initialResource={initialServiceAccount}
+      afterCreatedFn={afterServiceAccountCreate}
     >
-      <ComboboxArrayInput
-        advanced
-        propertyPath="$.secrets"
-        title={t('service-accounts.headers.secrets')}
-        tooltipContent={t('service-accounts.create-modal.tooltips.secrets')}
-        setValue={secrets => {
-          const newSecrets = (secrets || []).map(secrets => {
-            return { name: secrets };
-          });
-          jp.value(serviceAccount, '$.secrets', newSecrets);
-          setServiceAccount({ ...serviceAccount });
-        }}
-        toInternal={values => (values || []).map(value => value?.name)}
-        options={(data || [])
-          .filter(
-            secret => secret.type === 'kubernetes.io/service-account-token',
-          )
-          .map(i => ({
-            key: i.metadata.name,
-            text: i.metadata.name,
-          }))}
+      <ResourceForm.FormField
+        label={'Automatically create connected Secret'}
+        tooltipContent={t(
+          'service-accounts.create-modal.tooltips.auto-mount-token',
+        )}
+        input={Inputs.Switch}
+        readOnly={!!initialServiceAccount}
+        onChange={() =>
+          setShouldCreateSecret(shouldCreateSecret => !shouldCreateSecret)
+        }
       />
       <ComboboxArrayInput
         advanced
