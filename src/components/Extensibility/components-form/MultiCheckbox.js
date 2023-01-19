f2d75f9bbec4369a3b058011a035281cc3aeb647
Agata Kucharska <agata.kucharska@sap.com>
2023-01-19 14:10:32 +0100

    Review changes

--- src/components/Extensibility/components-form/MultiCheckbox.js
+++ src/components/Extensibility/components-form/MultiCheckbox.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { getObjectValueWorkaround } from 'components/Extensibility/helpers';
 import { ResourceForm } from 'shared/ResourceForm';
 import * as Inputs from 'shared/ResourceForm/inputs';
 import {
@@ -25,70 +26,66 @@ export function MultiCheckbox({
   placeholder,
 }) {
   const { tFromStoreKeys, t: tExt, exists } = useGetTranslation();
+
+  if (!schema.get('options')) {
+    return null;
+  }
+
   const schemaPlaceholder = schema.get('placeholder');
   const readOnly = schema.get('readOnly') ?? false;
+
   const value = getValue(storeKeys, resource);
 
   const getCheckboxesOptions = () => {
-    if (schema.get('options')) {
-      const translationPath = storeKeys
-        .toArray()
-        .filter(el => typeof el === 'string')
-        .join('.');
+    const translationPath = storeKeys
+      .toArray()
+      .filter(el => typeof el === 'string')
+      .join('.');
 
-      let options = schema.toJS().options;
-      // if there's only 1 option, it will be not in an array
-      if (typeof options === 'string') {
-        options = [options];
-      }
-      if (!Array.isArray(options)) {
-        options = [];
-      }
-      console.log('!!!options', options);
-      const displayOptions = options.map(option => {
-        console.log('typeof option', typeof option, option);
-        if (typeof option === 'string') {
-          return {
-            key: option,
-            text: exists(translationPath + '.' + option)
-              ? tExt(translationPath + '.' + option)
-              : option,
-          };
-        }
-        let defaultText = exists(translationPath + '.' + option.key)
-          ? tExt(translationPath + '.' + option.key)
-          : option.key;
-
-        if (option.name) {
-          defaultText = exists(translationPath + '.' + option.name)
-            ? tExt(translationPath + '.' + option.name)
-            : option.name;
-        }
-        return {
-          key: option.key,
-          text: defaultText,
-          description: exists(translationPath + '.' + option.description)
-            ? tExt(translationPath + '.' + option.description)
-            : option.description,
-        };
-      });
-      console.log('!!!displayOptions', displayOptions);
-      return {
-        input: Inputs.Checkboxes,
-        options: displayOptions,
-      };
-    } else {
-      return {
-        input: Inputs.Switch,
-      };
+    let options = schema.toJS().options;
+    // if there's only 1 option, it will be not in an array
+    if (typeof options === 'string') {
+      options = [options];
     }
+    if (!Array.isArray(options)) {
+      options = [];
+    }
+    const displayOptions = options.map(option => {
+      if (typeof option === 'string') {
+        return {
+          key: option,
+          text: exists(translationPath + '.' + option)
+            ? tExt(translationPath + '.' + option)
+            : option,
+        };
+      }
+      let defaultText = exists(translationPath + '.' + option.key)
+        ? tExt(translationPath + '.' + option.key)
+        : option.key;
+
+      if (option.name) {
+        defaultText = exists(translationPath + '.' + option.name)
+          ? tExt(translationPath + '.' + option.name)
+          : option.name;
+      }
+      return {
+        key: option.key,
+        text: defaultText,
+        description: exists(translationPath + '.' + option.description)
+          ? tExt(translationPath + '.' + option.description)
+          : option.description,
+      };
+    });
+    return {
+      input: Inputs.Checkboxes,
+      options: displayOptions,
+    };
   };
 
   return (
     <ResourceForm.FormField
       value={value}
       setValue={value => {
-        console.log('value', value);
         onChange &&
           onChange({
             storeKeys,
