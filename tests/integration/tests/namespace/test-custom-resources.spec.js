dfb00df4f5220a8d0036f7ebc2fbb2d4781b4117
Lyczeq <lyczeq@gmail.com>
2023-01-24 15:30:41 +0100

    fix: custom-resources test

--- tests/integration/tests/namespace/test-custom-resources.spec.js
+++ tests/integration/tests/namespace/test-custom-resources.spec.js
@@ -15,27 +15,17 @@ context('Test Custom Resources', () => {
 
     cy.contains('h3', 'Custom Resources').should('be.visible');
 
-    cy.get('[role="search"] [aria-label="open-search"]').type('busola.example');
-
-    cy.get('table').should('have.length', 1);
+    cy.get('[role="search"] [aria-label="open-search"]').type(
+      'cert.gardener.cloud',
+    );
 
     cy.get('[role=row]')
-      .contains('Pizzas')
+      .contains('Certificates')
       .should('be.visible');
   });
 
   it('Check single CR list', () => {
-    cy.get('[role=row]')
-      .contains('Pizzas')
-      .click();
-
-    cy.get('[aria-label="title"]')
-      .contains('Pizzas')
-      .should('be.visible');
-
-    cy.contains(PIZZA_NAME).should('be.visible');
-
-    cy.contains('pizzas.busola.example.com').click();
+    cy.contains('cert.gardener.cloud').click();
 
     cy.url().should('match', /customresourcedefinitions/);
   });
