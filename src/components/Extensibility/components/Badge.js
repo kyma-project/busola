5eb999d4b667cfba89d5738a98c24e942f47e19e
Lyczeq <lyczeq@gmail.com>
2023-02-03 11:37:15 +0100

    fix: add console error when there was an error with matching highlight

--- src/components/Extensibility/components/Badge.js
+++ src/components/Extensibility/components/Badge.js
@@ -7,6 +7,7 @@ import { useGetPlaceholder } from 'components/Extensibility/helpers';
 import { Tooltip } from 'shared/components/Tooltip/Tooltip';
 
 import './Badge.scss';
+import { useTranslation } from 'react-i18next';
 
 export function Badge({
   value,
@@ -16,6 +17,7 @@ export function Badge({
   scope,
   arrayItems,
 }) {
+  const { t } = useTranslation();
   const { emptyLeafPlaceholder } = useGetPlaceholder(structure);
   const jsonata = useJsonata({
     resource: originalResource,
@@ -33,7 +35,15 @@ export function Badge({
         return rule.includes(value);
       } else {
         const [doesMatch, matchError] = jsonata(rule);
-        return matchError ? false : doesMatch;
+        if (matchError) {
+          console.error(
+            t('extensibility.configuration-error', {
+              error: matchError.message,
+            }),
+          );
+          return false;
+        }
+        return doesMatch;
       }
     });
     if (match) {
