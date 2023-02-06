644bbd6d0aa2806a37b5e3c231789bcb1696747b
mrCherry97 <mwisnia97@gmail.com>
2023-02-06 15:45:16 +0100

    fix links

--- src/components/App/IncorrectPath.js
+++ src/components/App/IncorrectPath.js
@@ -1,20 +1,67 @@
 import { Button, MessageBox } from 'fundamental-react';
 import { useTranslation } from 'react-i18next';
-import { useNavigate } from 'react-router-dom';
+import { useMatch, useNavigate } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import { extensionsState } from 'state/navigation/extensionsAtom';
 
 import './IncorrectPath.scss';
+import { useGetList } from 'shared/hooks/BackendAPI/useGet';
+import pluralize from 'pluralize';
+import { useUrl } from 'hooks/useUrl';
+import { Spinner } from 'shared/components/Spinner/Spinner';
+import { useNotification } from 'shared/contexts/NotificationContext';
 
 export function IncorrectPath({ to, title = '', message = '' }) {
   const { t } = useTranslation();
+  const { namespace, namespaceUrl, clusterUrl } = useUrl();
+  const notificationManager = useNotification();
   const navigate = useNavigate();
   const extensions = useRecoilValue(extensionsState);
 
   title = title || t('components.incorrect-path.title.default');
   message = message || t('components.incorrect-path.message.default');
 
+  const resourceUrl = `/apis/apiextensions.k8s.io/v1/customresourcedefinitions`;
+  const { namespaceResourceType = '', namespaceResourceName = '' } =
+    useMatch({
+      path:
+        '/cluster/:cluster/namespaces/:namespace/:namespaceResourceType/:namespaceResourceName',
+      end: false,
+    })?.params ?? {};
+
+  const { clusterResourceType = '', clusterResourceName = '' } =
+    useMatch({
+      path: '/cluster/:cluster/:clusterResourceType/:clusterResourceName',
+      end: false,
+    })?.params ?? {};
+
+  const resourceType = namespace ? namespaceResourceType : clusterResourceType;
+  const resourceName = namespace ? namespaceResourceName : clusterResourceName;
+
+  const { data } = useGetList(
+    crd => pluralize(crd.spec.names.kind.toLowerCase()) === resourceType,
+  )(resourceUrl, { skip: !extensions?.length });
+
   if (!extensions?.length) return null;
+
+  if (!data) {
+    return <Spinner />;
+  }
+
+  if (data.length !== 0) {
+    const crdGroup = data[0]?.spec?.group;
+
+    const path = `customresources/${resourceType}.${crdGroup}/${resourceName}`;
+
+    const link = namespace ? namespaceUrl(path) : clusterUrl(path);
+
+    if (link && crdGroup) {
+      notificationManager.notifySuccess({
+        content: 'This URL is not valid, you will be redirected',
+      });
+      navigate(link);
+    }
+  }
 
   return (
     <MessageBox
