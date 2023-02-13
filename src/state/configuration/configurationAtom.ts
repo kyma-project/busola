fce01f735037b8f2d9c0431ff34fdc93553760e8
Agata Kucharska <agata.kucharska@sap.com>
2023-02-13 13:58:41 +0100

    Debugging

--- src/state/configuration/configurationAtom.ts
+++ src/state/configuration/configurationAtom.ts
@@ -56,10 +56,10 @@ const getConfigs = async (fetchFn: FetchFn | undefined) => {
     const defaultParams = jsyaml.load(
       await defaultConfigResponse.text(),
     ) as Config;
-    const configParams = jsyaml.load(await configResponse.text()) as Config;
-    const mapParams = configMapResponse?.data?.config
-      ? (jsyaml.load(configMapResponse.data.config) as Config)
-      : {};
+    // const configParams = jsyaml.load(await configResponse.text()) as Config;
+    // const mapParams = configMapResponse?.data?.config
+    //   ? (jsyaml.load(configMapResponse.data.config) as Config)
+    //   : {};
 
     const mapParams1: Config = {
       config: {
