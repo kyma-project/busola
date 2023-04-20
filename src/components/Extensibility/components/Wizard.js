f93d1c7afc61c2743ff9b028078ddbb77e64a4ef
Mateusz Wisniewski <mwisnia97@gmail.com>
2023-04-20 11:13:56 +0200

    Allow edit for wizard (#2439)
    * fix empty default section
    
    * allow editing resource in wizard
    
    * fix editing wizard and parsing data
    
    * bump

--- src/components/Extensibility/components/Wizard.js
+++ src/components/Extensibility/components/Wizard.js
@@ -16,6 +16,7 @@ export function Wizard({
   originalResource,
   scope,
   arrayItems,
+  singleRootResource,
 }) {
   const { t: tExt } = useGetTranslation();
   const { t } = useTranslation();
@@ -50,6 +51,7 @@ export function Wizard({
           <ExtensibilityWizard
             onCancel={() => setShowWizard(false)}
             wizardName={wizardName}
+            singleRootResource={singleRootResource}
           />
         </ErrorBoundary>
       </Dialog>
