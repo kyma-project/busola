cc1dca3dbe965013ad5b938020673e8f1e18c6fe
chriskari <chriskari@gmx.de>
2023-04-21 17:58:18 +0200

    custom implementation of native checkValidity()-method

--- src/shared/hooks/useCustomFormValidator.js
+++ src/shared/hooks/useCustomFormValidator.js
@@ -6,9 +6,69 @@ export function useCustomFormValidator() {
   const [customValid, setCustomValid] = useState(true);
 
   const revalidate = (cv = customValid) => {
-    const formValid = formElementRef?.current?.checkValidity() ?? false;
-    setValid(cv && formValid);
+    setValid(
+      checkFormFields(formElementRef.current) &&
+        checkResourceForms(formElementRef.current),
+    );
+    //check not-required fields
   };
+
+  function checkFormFields(form) {
+    const formFields = form.querySelectorAll(
+      'div.fd-row.form-field:not(.multi-input)',
+    );
+    for (let i = 0; i < formFields.length; i++) {
+      const formField = formFields[i];
+      const isRequired =
+        formField.querySelector(
+          'div.form-field > div.form-field__label > label[aria-required="true"]',
+        ) !== null;
+      if (!isRequired) continue;
+      //Check if it has a parent and if so, if the parent is required as well
+      const hasNoParent = formField.parentNode.classList.contains(
+        'form-container',
+      );
+      const inputValid = formField.querySelector('[type]').checkValidity();
+      if (hasNoParent) {
+        if (!inputValid) {
+          return false;
+        }
+        continue;
+      }
+      const parentRequired = formField
+        .closest('div.resource-form__collapsible-section')
+        .classList.contains('required');
+      if (parentRequired) {
+        if (!inputValid) {
+          return false;
+        }
+        continue;
+      }
+    }
+    return true;
+  }
+
+  function checkResourceForms(form) {
+    const resourceForms = form.querySelectorAll(
+      'div.resource-form__collapsible-section.required',
+    );
+
+    for (let i = 0; i < resourceForms.length; i++) {
+      const content = resourceForms[i].querySelector('div.content');
+      // Checks if a GenericList has at least one child
+      if (content.children.length === 0) {
+        return false;
+      }
+      // Checks if a SimpleList or KeyValuePair has at least one child
+      let list = content.querySelector(
+        'div.content > .multi-input, div.content > .simple-list',
+      );
+      if (list?.querySelectorAll('ul > li').length < 2) {
+        return false;
+      }
+    }
+    return true;
+  }
 
   return {
     isValid,
