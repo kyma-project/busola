7cd5180246bd992d5ac958de528862d2b2af16c6
chriskari <chriskari@gmx.de>
2023-04-27 08:12:07 +0200

    validation now also checks non-required fields, if they are at least partially filled out

--- src/shared/hooks/useCustomFormValidator.js
+++ src/shared/hooks/useCustomFormValidator.js
@@ -10,33 +10,44 @@ export function useCustomFormValidator() {
     const formContainer =
       formElementRef.current.querySelector('div.form-container') ??
       formElementRef.current.firstChild;
-    setValid(cv && validateElement(formContainer));
+    setValid(cv && validateElement(formContainer, true).valid);
   };
 
-  // Recursively validates all of the element's children
-  function validateElement(element) {
-    let isValid = element.children.length > 0;
+  // Recursively validates all of the element's required children
+  // Only validates non-required children, if they are at least partially filled out
+  function validateElement(element, isRequired) {
+    let isValid =
+      !isRequired ||
+      element.children.length > 0 ||
+      (!isRequired && element.children.length === 0);
+    // tracks if at least one child has been filled out, important for the valiation of non-required parent-elements
+    let isFilled = false;
 
     for (let i = 0; i < element.children.length; i++) {
       const child = element.children[i];
-
       if (child.classList.contains('form-field')) {
         // Validates the KeyValuePair
         if (child.classList.contains('multi-input')) {
-          isValid = isValid && validateInputList(child);
+          const { valid, filled } = validateInputList(child, isRequired);
+          isValid = isValid && valid;
+          isFilled = isFilled || filled;
         }
         // Validates the FormField
         else {
-          isValid = isValid && validateFormField(child);
+          const { valid, filled } = validateFormField(child);
+          isValid = isValid && valid;
+          isFilled = isFilled || filled;
         }
       }
       // Validates the SimpleList
       else if (child.classList.contains('simple-list')) {
-        isValid = isValid && validateInputList(child);
+        const { valid, filled } = validateInputList(child, isRequired);
+        isValid = isValid && valid;
+        isFilled = isFilled || filled;
       }
-      // Validates the ResourceForm
+      //Validates the CollapsibleSection
       else if (child.classList.contains('resource-form__collapsible-section')) {
-        // Finds the children's container
+        // Finds the children's container and checks if it's required
         let contentParent = child.querySelector('div.content');
         if (
           contentParent.firstChild?.classList.contains(
@@ -45,33 +56,43 @@ export function useCustomFormValidator() {
         ) {
           contentParent = contentParent.firstChild;
         }
-        // Validates the resourceForm's children if it is required
-        if (child.classList.contains('required')) {
-          isValid = isValid && validateElement(contentParent);
+        const required = child.classList.contains('required');
+        // Validates the GenericList
+        if (child.querySelector('.actions').innerText === 'Add') {
+          const { valid, filled } = validateElement(contentParent, required);
+          isValid =
+            isValid && valid && (filled || contentParent.children.length === 0);
+          isFilled = isFilled || filled;
+        }
+        // Validates the ResourceForm
+        else {
+          const { valid, filled } = validateElement(contentParent, required);
+          isValid = isValid && valid;
+          isFilled = isFilled || filled;
         }
       }
     }
-
-    return isValid;
+    return { valid: isValid || (!isRequired && !isFilled), filled: isFilled };
   }
 
   // Validates the FormField's input
   function validateFormField(formField) {
     const input = formField.querySelector('input');
-    return input.checkValidity();
+    return { valid: input.checkValidity(), filled: input.value !== '' };
   }
 
-  function validateInputList(inputList) {
+  function validateInputList(inputList, isRequired) {
     // The list is invalid if it has no children
     const items = inputList.querySelectorAll('ul > li');
-    if (items.length < 2) return false;
+    if (isRequired && items.length < 2) return { valid: false, filled: false };
 
     // Validates the inputs of all the list's child elements
     const inputs = inputList.querySelectorAll('input');
     for (let i = 0; i < inputs.length; i++) {
-      if (!inputs[i].checkValidity()) return false;
+      if (!inputs[i].checkValidity())
+        return { valid: false, filled: items.length >= 2 };
     }
-    return true;
+    return { valid: true, filled: items.length >= 2 };
   }
 
   return {
