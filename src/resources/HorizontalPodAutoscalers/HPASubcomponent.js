c0e7cfff907648ceab61113a57139683939b920c
mrCherry97 <mwisnia97@gmail.com>
2023-05-22 14:18:18 +0200

    fix hpasubscomponent

--- src/resources/HorizontalPodAutoscalers/HPASubcomponent.js
+++ src/resources/HorizontalPodAutoscalers/HPASubcomponent.js
@@ -1,22 +1,28 @@
-import React from 'react';
-import { useRecoilValue } from 'recoil';
+import React, { Suspense } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
+import { Spinner } from 'shared/components/Spinner/Spinner';
+import { ResourcesList } from 'shared/components/ResourcesList/ResourcesList';
 
-import { useGetList } from 'shared/hooks/BackendAPI/useGet';
-import { useUrl } from 'hooks/useUrl';
+import { useRecoilValue } from 'recoil';
+import { extensionsState } from 'state/navigation/extensionsAtom';
 import { activeNamespaceIdState } from 'state/activeNamespaceIdAtom';
-import { EMPTY_TEXT_PLACEHOLDER } from 'shared/constants';
-import { GenericList } from 'shared/components/GenericList/GenericList';
-import { Tokens } from 'shared/components/Tokens';
-import { MetricsBrief } from './helpers';
+
+const ExtensibilityList = React.lazy(() =>
+  import('../../components/Extensibility/ExtensibilityList'),
+);
 
 export const HPASubcomponent = props => {
+  console.log(props);
   const { t } = useTranslation();
   const { kind, name } = props.metadata?.ownerReferences?.[0] ?? {};
-  const namespaceId = useRecoilValue(activeNamespaceIdState);
-  const { resourceUrl } = useUrl();
+  const namespace = useRecoilValue(activeNamespaceIdState);
+  const extensions = useRecoilValue(extensionsState);
 
+  const extensibilityHPAs = extensions?.find(
+    cR => cR.general?.resource?.kind === 'HorizontalPodAutoscaler',
+  );
+
+  const url = `/apis/autoscaling/v2/namespaces/${namespace}/horizontalpodautoscalers`;
   const hpaFilter = hpa => {
     if (!kind || !name) return true;
     return (
@@ -25,57 +31,36 @@ export const HPASubcomponent = props => {
     );
   };
 
-  const { data, error } = useGetList(hpaFilter)(
-    `/apis/autoscaling/v2/namespaces/${namespaceId}/horizontalpodautoscalers`,
-  );
-
-  const rowRenderer = hpa => [
-    <Link
-      className="fd-link"
-      to={resourceUrl({
-        kind: 'horizontalpodautoscaler',
-        metadata: {
-          name: hpa.metadata.name,
-          namespace: namespaceId,
-        },
-      })}
-    >
-      {hpa.metadata.name}
-    </Link>,
-    <MetricsBrief {...hpa} />,
-    hpa.spec.minReplicas,
-    hpa.status?.currentReplicas || EMPTY_TEXT_PLACEHOLDER,
-    hpa.spec.maxReplicas,
-    <Tokens
-      tokens={
-        hpa.status?.conditions?.reduce((result, condition) => {
-          if (condition.status === 'True') {
-            result.push(condition.type);
-          }
-          return result;
-        }, []) || []
-      }
-    />,
-  ];
+  if (extensibilityHPAs)
+    return (
+      <Suspense fallback={<Spinner />}>
+        <ExtensibilityList
+          filterFunction={hpaFilter}
+          overrideResMetadata={extensibilityHPAs}
+          isCompact
+          resourceUrl={url}
+          hasDetailsView
+          showTitle
+          disableCreate
+          title={t('horizontal-pod-autoscalers')}
+        />
+      </Suspense>
+    );
 
   return (
-    <GenericList
-      entries={data ?? []}
-      key="associated-hpa-list"
-      title={t('hpas.title')}
-      serverDataError={error}
-      headerRenderer={() => [
-        t('common.headers.name'),
-        t('hpas.headers.metrics'),
-        t('hpas.headers.min-pods'),
-        t('hpas.headers.replicas'),
-        t('hpas.headers.max-pods'),
-        t('common.headers.status'),
-      ]}
-      rowRenderer={rowRenderer}
-      searchSettings={{
-        textSearchProperties: ['metadata.name'],
-      }}
+    <ResourcesList
+      key="horizontal-pod-autoscalers"
+      hasDetailsView
+      resourceUrl={url}
+      title={t('horizontal-pod-autoscalers')}
+      resourceType={'horizontalpodautoscalers'}
+      namespace={namespace}
+      isCompact
+      showTitle
+      disableCreate
+      filter={hpaFilter}
     />
   );
 };
+
+export default HPASubcomponent;
