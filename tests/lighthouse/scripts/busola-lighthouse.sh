ec63825c6381d0b3d401245fca6ff673c7f7b738
Mateusz Wisniewski <mwisnia97@gmail.com>
2023-07-18 09:28:21 +0200

    Busola lighthouse (#2523)
    * add script for lunching lighthouse
    
    * fix running command:
    
    * typo
    
    * chmod
    
    * typo

---
+++ tests/lighthouse/scripts/busola-lighthouse.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+export CYPRESS_DOMAIN=http://localhost:3000
+export NO_COLOR=1
+export KUBECONFIG="$GARDENER_KYMA_PROW_KUBECONFIG"
+
+cat << EOF | kubectl create -f - --raw "/apis/core.gardener.cloud/v1beta1/namespaces/garden-kyma-prow/shoots/nkyma/adminkubeconfig" | jq -r ".status.kubeconfig" | base64 -d > "kubeconfig--kyma--nkyma.yaml"
+{
+    "apiVersion": "authentication.gardener.cloud/v1alpha1",
+    "kind": "AdminKubeconfigRequest",
+    "spec": {
+    "expirationSeconds": 10800
+    }
+}
+EOF
+
+cp kubeconfig--kyma--nkyma.yaml tests/lighthouse/fixtures/kubeconfig.yaml
+
+npm ci && npm run build
+npm i -g serve 
+serve -s build > $ARTIFACTS/busola.log &
+
+pushd backend
+npm start > $ARTIFACTS/backend.log &
+popd
+
+echo "waiting for server to be up..."
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' "$CYPRESS_DOMAIN")" != "200" ]]; do sleep 5; done
+sleep 10
+
+cd tests/lighthouse
+npm ci && npm run "test"
