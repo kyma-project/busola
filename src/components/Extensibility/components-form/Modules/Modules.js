a7fbb33ac50780a2fec170fe28277901439edb62
Agata Kucharska <akucharska@users.noreply.github.com>
2023-07-31 15:21:47 +0200

    Upgrade wizard with checkboxss (#2530)
    * Install web components
    
    * Create component for setting modules
    
    * something is working, something is not, I hate jsonata
    
    * :fix filling yaml
    
    * add scss and remove flexbox
    
    * get rid of ui5
    
    * completly get rid of ui5
    
    * fix for selected key
    
    * fix indexes one more time
    
    * add collapsible section
    
    * css adjustemts
    
    * some more css
    
    * remove extensions and wizards yaml
    
    * remove clg
    
    * one more fix
    
    * css rename
    
    * bump
    
    * remove unnecesary translation
    
    ---------
    
    Co-authored-by: mrCherry97 <mwisnia97@gmail.com>

---
+++ src/components/Extensibility/components-form/Modules/Modules.js
@@ -0,0 +1,236 @@
+import React from 'react';
+import { useUIStore } from '@ui-schema/ui-schema';
+
+import { useJsonata } from '../../hooks/useJsonata';
+import { useVariables } from '../../hooks/useVariables';
+import { fromJS } from 'immutable';
+import {
+  ComboboxInput,
+  MessageStrip,
+  Checkbox,
+  Link,
+  Icon,
+} from 'fundamental-react';
+
+import './Modules.scss';
+import { useGetTranslation } from 'components/Extensibility/helpers';
+import { ResourceForm } from 'shared/ResourceForm';
+import { useTranslation } from 'react-i18next';
+
+export function Modules({
+  storeKeys,
+  resource,
+  onChange,
+  schema,
+  required,
+  editMode,
+}) {
+  const { t: tExt } = useGetTranslation();
+  const { t } = useTranslation();
+  const sectionName = schema.get('name');
+
+  const setCheckbox = (fullValue, key, entryValue, checked, index) => {
+    if (checked) {
+      onChange({
+        storeKeys,
+        scopes: ['value', 'internal'],
+        type: 'list-item-add',
+        schema,
+        itemValue: fromJS({ [key]: entryValue }),
+        required,
+      });
+    } else {
+      onChange({
+        storeKeys,
+        scopes: ['value', 'internal'],
+        type: 'list-item-delete',
+        index,
+        schema,
+        required,
+      });
+    }
+  };
+  const { store } = useUIStore();
+  const { value } = store?.extractValues(storeKeys) || [];
+  const { itemVars } = useVariables();
+  const jsonata = useJsonata({
+    resource,
+    scope: value,
+    value,
+  });
+
+  const rule = schema.get('schemaRule');
+
+  const options = schema.get('options');
+  let parsedOptions = {};
+
+  function makeJsonata(propObject) {
+    if (typeof propObject === 'string') {
+      const [newEnum] = jsonata(
+        propObject,
+        itemVars(resource, rule.itemVars, storeKeys),
+        [],
+      );
+      return newEnum;
+    }
+    console.warn('Widget::Modules');
+    return null;
+  }
+
+  Object.keys(options).forEach(optionName => {
+    parsedOptions[optionName] = makeJsonata(options[optionName]);
+  });
+
+  const Items = parsedOptions?.name?.map((name, index) => {
+    const isChecked = !!(value ? value.toJS() : []).find(v => {
+      return v.name === name;
+    });
+
+    let channelModuleTemplate = [];
+
+    parsedOptions?.moduleTemplates?.map(moduleTemplate => {
+      if (
+        moduleTemplate?.metadata?.labels[
+          'operator.kyma-project.io/module-name'
+        ] === name
+      ) {
+        if (moduleTemplate?.spec?.descriptor?.component?.version)
+          return channelModuleTemplate.push({
+            text: moduleTemplate.spec.channel,
+            additionalText: `(version: ${moduleTemplate?.spec?.descriptor?.component?.version})`,
+          });
+
+        return channelModuleTemplate.push({
+          text: moduleTemplate.spec.channel,
+          additionalText: '',
+        });
+      } else {
+        return '';
+      }
+    });
+
+    const link = parsedOptions?.moduleTemplates?.find(moduleTemplate => {
+      const channel = resource?.spec?.modules
+        ? resource?.spec?.modules[index]?.channel ?? resource?.spec?.channel
+        : resource?.spec?.channel;
+
+      if (
+        moduleTemplate?.metadata?.labels[
+          'operator.kyma-project.io/module-name'
+        ] === name &&
+        moduleTemplate?.spec?.channel === channel
+      )
+        return moduleTemplate;
+      else return null;
+    })?.metadata?.annotations['operator.kyma-project.io/doc-url'];
+
+    const isBeta = parsedOptions?.moduleTemplates?.find(
+      moduleTemplate =>
+        moduleTemplate?.metadata?.labels[
+          'operator.kyma-project.io/module-name'
+        ] === name &&
+        moduleTemplate?.metadata?.labels['operator.kyma-project.io/beta'] ===
+          'true',
+    );
+
+    return (
+      <>
+        <div className="gridbox fd-margin-bottom--sm">
+          <div className="fd-align-vertical-center-ignor-checkbox-label">
+            {index === 0 ? `${sectionName}:` : ''}
+          </div>
+          <Checkbox
+            className="fd-align-vertical-center-ignor-checkbox-label"
+            key={name}
+            value={name}
+            checked={isChecked}
+            onChange={e => {
+              setCheckbox(
+                value,
+                'name',
+                name,
+                e.target.checked,
+                resource?.spec?.modules
+                  ? resource?.spec?.modules.findIndex(module => {
+                      return module.name === name;
+                    })
+                  : index,
+              );
+            }}
+          >
+            {name}
+          </Checkbox>
+          <ComboboxInput
+            label={t('extensibility.widgets.modules.module-channel-label')}
+            compact
+            disabled={!isChecked}
+            placeholder={t(
+              'extensibility.widgets.modules.module-channel-placeholder',
+            )}
+            options={channelModuleTemplate.map(option => {
+              return {
+                text: `${option.text} ${option.additionalText}`,
+                key: option.text,
+              };
+            })}
+            selectedKey={
+              resource?.spec?.modules
+                ? resource?.spec?.modules[
+                    resource?.spec?.modules.findIndex(module => {
+                      return module.name === name;
+                    })
+                  ]?.channel
+                : ''
+            }
+            onSelectionChange={(_, selected) => {
+              if (selected.key !== -1) {
+                onChange({
+                  storeKeys: storeKeys
+                    .push(
+                      resource?.spec?.modules
+                        ? resource?.spec?.modules.findIndex(module => {
+                            return module.name === name;
+                          })
+                        : index,
+                    )
+                    .push('channel'),
+                  scopes: ['value'],
+                  type: 'set',
+                  schema,
+                  data: { value: selected.key },
+                  required,
+                });
+              }
+            }}
+          />
+
+          {link ? (
+            <Link
+              href={link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="fd-align-vertical-center-ignor-checkbox-label"
+            >
+              {t('extensibility.widgets.modules.documentation')}
+              <Icon glyph="action" size="s" className="fd-margin-begin--tiny" />
+            </Link>
+          ) : null}
+        </div>
+        {parsedOptions?.betaAlert && isBeta && isChecked ? (
+          <MessageStrip
+            type="warning"
+            className="fd-margin-bottom--sm fd-margin-top--sm alert"
+          >
+            {tExt(parsedOptions?.betaAlert)}
+          </MessageStrip>
+        ) : null}
+      </>
+    );
+  });
+
+  return (
+    <ResourceForm.CollapsibleSection defaultOpen title={sectionName || ''}>
+      <div>{Items}</div>
+    </ResourceForm.CollapsibleSection>
+  );
+}
