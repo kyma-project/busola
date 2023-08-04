ac14822bb56cafaa780e6f22a65c61cec2c517fd
mrCherry97 <mwisnia97@gmail.com>
2023-08-04 14:32:06 +0200

    adjust script to k3d

--- tests/integration/scripts/busola-integration-k3s.sh
+++ tests/integration/scripts/busola-integration-k3s.sh
@@ -4,20 +4,22 @@ set -e
 export CYPRESS_DOMAIN=http://localhost:3000
 export NO_COLOR=1
 export KUBECONFIG="$GARDENER_KYMA_PROW_KUBECONFIG"
+OS="$(uname -s)"
+ARCH="$(uname -m)"
 
-cat << EOF | kubectl create -f - --raw "/apis/core.gardener.cloud/v1beta1/namespaces/garden-kyma-prow/shoots/nkyma/adminkubeconfig" | jq -r ".status.kubeconfig" | base64 -d > "kubeconfig--kyma--nkyma.yaml"
-{
-    "apiVersion": "authentication.gardener.cloud/v1alpha1",
-    "kind": "AdminKubeconfigRequest",
-    "spec": {
-    "expirationSeconds": 10800
-    }
-}
-EOF
+function deploy_k3d_kyma (){
+curl -sSLo kyma.tar.gz "https://github.com/kyma-project/cli/releases/latest/download/kyma_${OS}_${ARCH}.tar.gz"
+tar -zxvf kyma.tar.gz
+chmod +x ./kyma
 
-cp kubeconfig--kyma--nkyma.yaml tests/integration/fixtures/kubeconfig.yaml
+echo "Provisioning k3d cluster for Kyma"
+./kyma provision k3d --ci
+
+k3d kubeconfig get kyma > tests/integration/fixtures/kubeconfig.yaml
 k3d kubeconfig get k3d > tests/integration/fixtures/kubeconfig-k3s.yaml
+}
 
+function build_and_run_busola() {
 npm ci && npm run build
 npm i -g serve 
 serve -s build > $ARTIFACTS/busola.log &
@@ -29,6 +31,16 @@ popd
 echo "waiting for server to be up..."
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' "$CYPRESS_DOMAIN")" != "200" ]]; do sleep 5; done
 sleep 10
+}
+
+deploy_k3d_kyma  &> $ARTIFACTS/kyma-alpha-deploy.log &
+build_and_run_busola  &> $ARTIFACTS/busola-build.log &
+
+echo 'Waiting for deploy_k3d_kyma and build_and_run_busola'
+wait -n
+echo "First process finished"
+wait -n
+echo "Second process finished"
 
 cd tests/integration
 npm ci && npm run "test:$SCOPE"
