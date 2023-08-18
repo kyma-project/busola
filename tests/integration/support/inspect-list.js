056a95736ae50ef67c438a8caca942c9403f9bc1
chriskari <chriskari@gmx.de>
2023-08-18 15:33:01 +0200

    fix test-inspect-list

--- tests/integration/support/inspect-list.js
+++ tests/integration/support/inspect-list.js
@@ -1,5 +1,16 @@
 Cypress.Commands.add('inspectList', (resource, resourceName) => {
-  cy.contains('[aria-label="breadcrumb-item"]', resource).click();
+  const resourceUrl = resource.replace(/\s/g, '').toLowerCase();
+
+  cy.get('ui5-breadcrumbs')
+    .find(`ui5-link[href*=${resourceUrl}]`, {
+      includeShadowDom: true,
+    })
+    .should('contain.text', resource)
+    .find(`a[href*=${resourceUrl}]`, {
+      includeShadowDom: true,
+    })
+    .should('be.visible')
+    .click({ force: true });
 
   cy.get('[role="search"] [aria-label="open-search"]').type(resourceName);
 
