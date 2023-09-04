3e0e725bd4734a8f497d15deb372b3d09588d774
mrCherry97 <mwisnia97@gmail.com>
2023-09-04 14:12:51 +0200

    move pv and pvc panels

--- src/resources/PersistentVolumes/PersistentVolumeDetails.js
+++ src/resources/PersistentVolumes/PersistentVolumeDetails.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { LayoutPanel } from 'fundamental-react';
 import { Link } from 'react-router-dom';
 
 import { ResourceDetails } from 'shared/components/ResourceDetails/ResourceDetails';
@@ -14,6 +13,7 @@ import { useUrl } from 'hooks/useUrl';
 
 import { PersistentVolumeStatus } from './PersistentVolumeStatus';
 import { PersistentVolumeCreate } from './PersistentVolumeCreate';
+import { Panel, Title, Toolbar } from '@ui5/webcomponents-react';
 
 export function PersistentVolumeDetails(props) {
   const { t } = useTranslation();
@@ -36,96 +36,102 @@ export function PersistentVolumeDetails(props) {
 
   const PvDetails = ({ spec, metadata, status }) => (
     <div key="persistent-volumes-ref" data-testid="persistent-volumes-ref">
-      <LayoutPanel className="fd-margin--md">
-        <LayoutPanel.Header>
-          <LayoutPanel.Head title={t('pv.details')} />
-        </LayoutPanel.Header>
-        <LayoutPanel.Body>
-          <LayoutPanelRow
-            name={t('pv.headers.finalizers')}
-            value={<Tokens tokens={metadata.finalizers} />}
-          />
-          <LayoutPanelRow
-            name={t('pv.headers.capacity')}
-            value={spec.capacity?.storage}
-          />
-          <LayoutPanelRow
-            name={t('pv.headers.mount-options')}
-            value={<Tokens tokens={spec.mountOptions} />}
-          />
-          <LayoutPanelRow
-            name={t('pv.headers.access-modes')}
-            value={<Tokens tokens={spec.accessModes} />}
-          />
-          <LayoutPanelRow
-            name={t('pv.headers.reclaim-policy')}
-            value={spec.persistentVolumeReclaimPolicy}
-          />
-          <LayoutPanelRow
-            name={t('pv.headers.storage-class-name')}
-            value={
-              storageClasses?.find(
-                ({ metadata }) => metadata.name === spec?.storageClassName,
-              ) ? (
-                <Link
-                  className="fd-link"
-                  to={resourceUrl({
-                    kind: 'StorageClass',
+      <Panel
+        fixed
+        className="fd-margin--md"
+        header={
+          <Toolbar>
+            <Title level="H5">{t('pv.details')}</Title>
+          </Toolbar>
+        }
+      >
+        <LayoutPanelRow
+          name={t('pv.headers.finalizers')}
+          value={<Tokens tokens={metadata.finalizers} />}
+        />
+        <LayoutPanelRow
+          name={t('pv.headers.capacity')}
+          value={spec.capacity?.storage}
+        />
+        <LayoutPanelRow
+          name={t('pv.headers.mount-options')}
+          value={<Tokens tokens={spec.mountOptions} />}
+        />
+        <LayoutPanelRow
+          name={t('pv.headers.access-modes')}
+          value={<Tokens tokens={spec.accessModes} />}
+        />
+        <LayoutPanelRow
+          name={t('pv.headers.reclaim-policy')}
+          value={spec.persistentVolumeReclaimPolicy}
+        />
+        <LayoutPanelRow
+          name={t('pv.headers.storage-class-name')}
+          value={
+            storageClasses?.find(
+              ({ metadata }) => metadata.name === spec?.storageClassName,
+            ) ? (
+              <Link
+                className="fd-link"
+                to={resourceUrl({
+                  kind: 'StorageClass',
+                  metadata: {
+                    name: spec?.storageClassName,
+                  },
+                })}
+              >
+                {spec?.storageClassName}
+              </Link>
+            ) : (
+              <p>{spec?.storageClassName || EMPTY_TEXT_PLACEHOLDER}</p>
+            )
+          }
+        />
+        <LayoutPanelRow
+          name={t('pv.headers.claim-name')}
+          value={
+            persistentVolumeClaims?.find(
+              ({ metadata }) => metadata.name === spec?.claimRef?.name,
+            ) ? (
+              <Link
+                className="fd-link"
+                to={resourceUrl(
+                  {
+                    kind: 'PersistentVolumeClaim',
                     metadata: {
-                      name: spec?.storageClassName,
+                      name: spec?.claimRef?.name,
                     },
-                  })}
-                >
-                  {spec?.storageClassName}
-                </Link>
-              ) : (
-                <p>{spec?.storageClassName || EMPTY_TEXT_PLACEHOLDER}</p>
-              )
-            }
-          />
-          <LayoutPanelRow
-            name={t('pv.headers.claim-name')}
-            value={
-              persistentVolumeClaims?.find(
-                ({ metadata }) => metadata.name === spec?.claimRef?.name,
-              ) ? (
-                <Link
-                  className="fd-link"
-                  to={resourceUrl(
-                    {
-                      kind: 'PersistentVolumeClaim',
-                      metadata: {
-                        name: spec?.claimRef?.name,
-                      },
-                    },
-                    { namespace: spec?.claimRef?.namespace },
-                  )}
-                >
-                  {spec?.claimRef?.name}
-                </Link>
-              ) : (
-                <p>{spec?.claimRef?.name || EMPTY_TEXT_PLACEHOLDER}</p>
-              )
-            }
-          />
-        </LayoutPanel.Body>
-      </LayoutPanel>
+                  },
+                  { namespace: spec?.claimRef?.namespace },
+                )}
+              >
+                {spec?.claimRef?.name}
+              </Link>
+            ) : (
+              <p>{spec?.claimRef?.name || EMPTY_TEXT_PLACEHOLDER}</p>
+            )
+          }
+        />
+      </Panel>
 
-      <LayoutPanel className="fd-margin--md">
-        <LayoutPanel.Header>
-          <LayoutPanel.Head title={t('pv.nfs')} />
-        </LayoutPanel.Header>
-        <LayoutPanel.Body>
-          <LayoutPanelRow
-            name={t('pv.headers.server')}
-            value={spec.nfs?.server || EMPTY_TEXT_PLACEHOLDER}
-          />
-          <LayoutPanelRow
-            name={t('pv.headers.path')}
-            value={spec.nfs?.path || EMPTY_TEXT_PLACEHOLDER}
-          />
-        </LayoutPanel.Body>
-      </LayoutPanel>
+      <Panel
+        fixed
+        className="fd-margin--md"
+        header={
+          <Toolbar>
+            <Title level="H5">{t('pv.nfs')}</Title>
+          </Toolbar>
+        }
+      >
+        <LayoutPanelRow
+          name={t('pv.headers.server')}
+          value={spec.nfs?.server || EMPTY_TEXT_PLACEHOLDER}
+        />
+        <LayoutPanelRow
+          name={t('pv.headers.path')}
+          value={spec.nfs?.path || EMPTY_TEXT_PLACEHOLDER}
+        />
+      </Panel>
     </div>
   );
 
