4cacb5aea13c04066235ed3c2bd6b2d378edf061
mrCherry97 <mwisnia97@gmail.com>
2023-10-04 14:44:49 +0200

    fix unit tests

--- src/components/Preferences/test/NamespaceSettings.test.tsx
+++ src/components/Preferences/test/NamespaceSettings.test.tsx
@@ -1,23 +1,16 @@
 import { showHiddenNamespacesState } from 'state/preferences/showHiddenNamespacesAtom';
-import { render, fireEvent, waitFor } from 'testing/reactTestingUtils';
+import { render } from 'testing/reactTestingUtils';
 import NamespaceSettings from '../NamespaceSettings';
 
 describe('NamespaceSettings', () => {
-  it('Sends custom message on toggle', async () => {
-    const { getByLabelText } = render(<NamespaceSettings />, {
+  it('Check ui5 switch checked', () => {
+    const { container } = render(<NamespaceSettings />, {
       initializeState: snapshot =>
         snapshot.set(showHiddenNamespacesState, true),
     });
 
-    const toggle = getByLabelText('settings.clusters.showHiddenNamespaces');
-    const toggleSwitch = toggle.shadowRoot?.querySelector('[role="switch"]');
+    const toggle = container.querySelector('ui5-switch');
 
-    expect(toggleSwitch).toHaveAttribute('aria-checked', 'true');
-
-    if (toggleSwitch) fireEvent.click(toggleSwitch);
-
-    await waitFor(() => {
-      expect(toggleSwitch).toHaveAttribute('aria-checked', 'false');
-    });
+    expect(toggle).toHaveAttribute('checked', 'true');
   });
 });
