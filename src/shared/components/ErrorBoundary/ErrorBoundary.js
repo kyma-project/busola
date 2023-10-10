047b869648c8a2fa602ae6bacf21efc48ed6a66a
Julius-Schmidt <schmidt-jvlius@gmx.de>
2023-10-10 10:22:00 +0200

    added margin to error alert

--- src/shared/components/ErrorBoundary/ErrorBoundary.js
+++ src/shared/components/ErrorBoundary/ErrorBoundary.js
@@ -26,7 +26,7 @@ class ErrorBoundaryComponent extends React.Component {
     if (this.state.hasError) {
       return (
         <>
-          <div role="alert">
+          <div class="form" role="alert">
             <p className="fd-margin-top-bottom--sm fd-color--text">
               {this.props.customMessage ||
                 this.props.t('err-boundary.restored-initial-form')}
