525ad448087b77392cc02bdea498660efef9025b
Agata Kucharska <agata.kucharska@sap.com>
2023-11-09 12:23:46 +0100

    Remove unused stuff

--- src/state/preferences/isSidebarCondensedAtom.ts
+++ src/state/preferences/isSidebarCondensedAtom.ts
@@ -4,26 +4,9 @@ type IsSidebarCondensed = boolean;
 
 const DEFAULT_IS_SIDEBAR_CONDENSED = false;
 
-type AddLinkEffect = () => AtomEffect<IsSidebarCondensed>;
-
-export const changeSidebarWidthEffect: AddLinkEffect = () => ({ onSet }) => {
-  const sidebarCondensedWidth = '52px';
-  const sidebarOpenedWidth = '16rem';
-
-  onSet(isCondensed => {
-    const root: HTMLElement = document.querySelector(':root')!;
-    if (isCondensed) {
-      root.style.setProperty('--sidebar-width', sidebarCondensedWidth);
-    } else {
-      root.style.setProperty('--sidebar-width', sidebarOpenedWidth);
-    }
-  });
-};
-
 export const isSidebarCondensedState: RecoilState<IsSidebarCondensed> = atom<
   IsSidebarCondensed
 >({
   key: 'isSidebarCondensedState',
   default: DEFAULT_IS_SIDEBAR_CONDENSED,
-  effects: [changeSidebarWidthEffect()],
 });
