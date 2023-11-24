f2032561b68c91fa85c2c2c630d99b4348f16c40
chriskari <chriskari@gmx.de>
2023-11-25 00:21:08 +0100

    code clean-up

--- src/shared/hooks/useCustomFormValidator/validateFormElement.js
+++ src/shared/hooks/useCustomFormValidator/validateFormElement.js
@@ -12,31 +12,51 @@ export function validateFormElement(element, isRequired) {
     !isRequired ||
     element?.children.length > 0 ||
     (!isRequired && element?.children.length === 0);
-  let isPartiallyFilled = false; // tracks if at least one child has been filled out, important for the validation of non-required FormGroups
-  let isComplete = true; // tracks if all children have been filled out, important for the validation of non-required GenericLists
+  let isPartiallyFilled = false; // tracks if at least one child has been filled out (important for the validation of non-required FormGroups)
+  let isComplete = true; // tracks if all children have been filled out (important for the validation of non-required GenericLists)
 
   for (const child of element?.children) {
     if (isRequired && !isValid) break;
 
     let validationFunction, args;
     switch (true) {
-      case child.classList.contains('form-field'):
-        switch (true) {
-          // Validates the KeyValuePair
-          case child.classList.contains('multi-input'):
-            validationFunction = validateKeyValuePairs;
-            args = [child, isRequired];
-            break;
-          // Validates the MultiCheckboxes
-          case child.classList.contains('multi-checkbox'):
-            validationFunction = validateMultiCheckboxes;
-            args = [child, isRequired];
-            break;
-          // Validates the FormField
-          default:
-            validationFunction = validateFormField;
-            args = [child];
+      // Validates the CollapsibleSection (GenericList/ResourceForm) by recursively calling this function
+      case child.classList.contains('resource-form__collapsible-section'):
+        // Finds the children's container and checks if it is required
+        const required = child.classList.contains('required');
+        const content = child.querySelector('div.content');
+        const contentWrapper = child.querySelector(
+          'div.content > .collapsible-renderer__grid-wrapper',
+        );
+        const contentParent =
+          contentWrapper?.parentNode === content ? contentWrapper : content;
+
+        const { valid, filled, complete } = validateFormElement(
+          contentParent,
+          required,
+        );
+        // GenericList
+        if (child.querySelector('.actions').innerText === 'Add') {
+          isPartiallyFilled = isPartiallyFilled || filled;
+          isComplete = isComplete && complete;
+          isValid = isValid && valid && complete;
         }
+        // ResourceForm
+        else {
+          isPartiallyFilled = isPartiallyFilled || filled;
+          isComplete = isComplete && (complete || (valid && filled));
+          isValid = isValid && valid;
+        }
+        continue;
+      // Validates the KeyValuePair
+      case child.classList.contains('multi-input'):
+        validationFunction = validateKeyValuePairs;
+        args = [child, isRequired];
+        break;
+      // Validates the MultiCheckboxes
+      case child.classList.contains('multi-checkbox'):
+        validationFunction = validateMultiCheckboxes;
+        args = [child, isRequired];
         break;
       // Validates the SimpleList
       case child.classList.contains('simple-list'):
@@ -48,38 +68,11 @@ export function validateFormElement(element, isRequired) {
         validationFunction = validateRuntimeProfile;
         args = [child];
         break;
-      // Validates the CollapsibleSection
-      case child.classList.contains('resource-form__collapsible-section'):
-        // Finds the children's container and checks if it is required
-        const content = child.querySelector('div.content');
-        const gridWrapper = child.querySelector(
-          'div.content > .collapsible-renderer__grid-wrapper',
-        );
-        const contentParent =
-          gridWrapper?.parentNode === content ? gridWrapper : content;
-        const required = child.classList.contains('required');
-        // Validates the GenericList
-        if (child.querySelector('.actions').innerText === 'Add') {
-          const { valid, filled, complete } = validateFormElement(
-            contentParent,
-            required,
-          );
-          isPartiallyFilled = isPartiallyFilled || filled;
-          isComplete = isComplete && complete;
-          isValid = isValid && valid && complete;
-          continue;
-        }
-        // Validates the ResourceForm
-        else {
-          const { valid, filled, complete } = validateFormElement(
-            contentParent,
-            required,
-          );
-          isPartiallyFilled = isPartiallyFilled || filled;
-          isComplete = isComplete && (complete || (valid && filled));
-          isValid = isValid && valid;
-          continue;
-        }
+      // Validates the FormField
+      case child.classList.contains('form-field'):
+        validationFunction = validateFormField;
+        args = [child];
+        break;
       default:
         continue;
     }
@@ -88,6 +81,7 @@ export function validateFormElement(element, isRequired) {
     isPartiallyFilled = isPartiallyFilled || filled;
     isComplete = isComplete && filled;
   }
+
   return {
     valid: isValid || (!isRequired && !isPartiallyFilled),
     filled: isPartiallyFilled || isComplete,
