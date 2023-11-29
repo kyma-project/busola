242e91e42c09438891b97aa0232b17115df97995
Mateusz Wisniewski <mwisnia97@gmail.com>
2023-11-29 15:54:18 +0100

    change shellbar to combobox (#2656)
    * change shellbar to combobox
    
    * fix styles and move icon to begin
    
    * fix combobox
    
    * small screen fix
    
    * small screen fix
    
    * bump
    
    * lint fix
    
    * fix cm test
    
    * localy it is passing
    
    * rm extensions.yaml

---
+++ src/header/NamespaceChooser/NamespaceChooser.tsx
@@ -0,0 +1,53 @@
+import { useRecoilValue } from 'recoil';
+import { useTranslation } from 'react-i18next';
+import { useUrl } from 'hooks/useUrl';
+import { useMatch, useNavigate } from 'react-router-dom';
+import { namespacesState } from 'state/namespacesAtom';
+
+import { SideNavigationSubItem } from '@ui5/webcomponents-react';
+
+export function NamespaceChooser() {
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+  const { clusterUrl, namespaceUrl } = useUrl();
+  const allNamespaces = useRecoilValue(namespacesState);
+
+  const { resourceType = '' } =
+    useMatch({
+      path: '/cluster/:cluster/namespaces/:namespace/:resourceType',
+      end: false,
+    })?.params ?? {};
+
+  let namespaces = [
+    <SideNavigationSubItem
+      text={t('namespaces.namespaces-overview')}
+      data-key="overview"
+      onClick={() => navigate(clusterUrl(`namespaces`))}
+    />,
+    <SideNavigationSubItem
+      text={t('navigation.all-namespaces')}
+      data-key="all-namespaces"
+      onClick={() =>
+        navigate(namespaceUrl(resourceType, { namespace: '-all-' }))
+      }
+    />,
+  ];
+
+  allNamespaces.map(ns =>
+    namespaces.push(
+      <SideNavigationSubItem
+        text={ns}
+        data-key={ns}
+        onClick={e =>
+          navigate(
+            namespaceUrl(resourceType, {
+              namespace: e.target.dataset.key ?? undefined,
+            }),
+          )
+        }
+      />,
+    ),
+  );
+
+  return <>{namespaces}</>;
+}
