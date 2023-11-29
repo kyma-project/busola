b0b995c147945e7d94272439b366a184e330241b
mrCherry97 <mwisnia97@gmail.com>
2023-11-29 09:36:15 +0100

    lint fix

--- src/header/NamespaceChooser/NamespaceChooser.tsx
+++ src/header/NamespaceChooser/NamespaceChooser.tsx
@@ -1,30 +1,22 @@
 import { useRecoilValue } from 'recoil';
 import { useTranslation } from 'react-i18next';
-
+import { useUrl } from 'hooks/useUrl';
+import { useMatch, useNavigate } from 'react-router-dom';
 import { namespacesState } from 'state/namespacesAtom';
 
 import { SideNavigationSubItem } from '@ui5/webcomponents-react';
-import { activeNamespaceIdState } from 'state/activeNamespaceIdAtom';
-import { useMatch, useNavigate } from 'react-router-dom';
-import { useUrl } from 'hooks/useUrl';
 
 export function NamespaceChooser() {
   const { t } = useTranslation();
   const navigate = useNavigate();
   const { clusterUrl, namespaceUrl } = useUrl();
   const allNamespaces = useRecoilValue(namespacesState);
-  const namespace = useRecoilValue(activeNamespaceIdState);
 
   const { resourceType = '' } =
     useMatch({
       path: '/cluster/:cluster/namespaces/:namespace/:resourceType',
       end: false,
     })?.params ?? {};
-
-  const getNamespaceLabel = () => {
-    if (namespace === '-all-') return t('navigation.all-namespaces');
-    else return namespace || t('navigation.select-namespace');
-  };
 
   let namespaces = [
     <SideNavigationSubItem
