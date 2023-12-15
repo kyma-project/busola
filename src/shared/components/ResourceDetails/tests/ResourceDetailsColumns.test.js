62b996667a2ac38912dc66924df6c8131a8e1239
Agata Kucharska <agata.kucharska@sap.com>
2023-12-15 10:43:16 +0100

    Split test into separate files

---
+++ src/shared/components/ResourceDetails/tests/ResourceDetailsColumns.test.js
@@ -0,0 +1,54 @@
+import { Suspense } from 'react';
+import { render, waitFor } from 'testing/reactTestingUtils';
+import { ResourceDetails } from '../ResourceDetails';
+import { ThemeProvider } from '@ui5/webcomponents-react';
+
+jest.mock('shared/hooks/BackendAPI/useGet', () => ({
+  useGet: () => {
+    return {
+      loading: false,
+      error: null,
+      data: {
+        kind: 'TestKind',
+        metadata: {
+          name: 'test-resource-name',
+          namespace: 'test-resource-namespace',
+        },
+      },
+    };
+  },
+  useSingleGet: () => {},
+}));
+
+jest.mock('components/Extensibility/ExtensibilityInjections', () => () => (
+  <div />
+));
+
+describe('ResourceDetails Columns', () => {
+  it('Renders basic column', async () => {
+    const { queryByText } = render(
+      <ThemeProvider>
+        <Suspense fallback="loading">
+          <ResourceDetails
+            resourceUrl="test-resource-url"
+            resourceType="test-resource-type"
+            customColumns={[
+              {
+                header: 'some-header',
+                value: resource =>
+                  resource.metadata.name + ' | ' + resource.metadata.namespace,
+              },
+            ]}
+          />
+        </Suspense>
+      </ThemeProvider>,
+    );
+
+    await waitFor(() => {
+      expect(queryByText('some-header')).toBeInTheDocument();
+      expect(
+        queryByText('test-resource-name | test-resource-namespace'),
+      ).toBeInTheDocument();
+    });
+  }, 10000);
+});
