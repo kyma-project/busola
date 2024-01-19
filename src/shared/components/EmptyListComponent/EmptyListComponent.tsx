cd32bc08b7faf7c4d9ebe7eac237e5a2194085b5
Oliwia Gowor <oliwiagowor@gmail.com>
2024-01-19 08:40:34 +0100

    all illustrations

--- src/shared/components/EmptyListComponent/EmptyListComponent.tsx
+++ src/shared/components/EmptyListComponent/EmptyListComponent.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 import { useTranslation, Trans } from 'react-i18next';
 import { Button, IllustratedMessage } from '@ui5/webcomponents-react';
-import '@ui5/webcomponents-fiori/dist/illustrations/tnt/NoApplications.js';
+import '@ui5/webcomponents-fiori/dist/illustrations/AllIllustrations';
 import { Link } from 'shared/components/Link/Link';
 import './EmptyListComponent.scss';
 import { spacing } from '@ui5/webcomponents-react-base';
