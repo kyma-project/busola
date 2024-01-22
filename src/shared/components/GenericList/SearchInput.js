61c7496da1f2eb840b882c0a69c50e86c616f71a
Damian Badura <damian.badura@sap.com>
2024-01-22 13:47:25 +0100

    fix

--- src/shared/components/GenericList/SearchInput.js
+++ src/shared/components/GenericList/SearchInput.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Button, ComboBox, ComboBoxItem } from '@ui5/webcomponents-react';
 import { useTranslation } from 'react-i18next';
