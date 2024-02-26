f1673b629484d14d17a66d03736db8c1c7fc789f
mrCherry97 <mwisnia97@gmail.com>
2024-02-26 10:00:57 +0100

    remove unused import

--- src/components/HelmReleases/HelmReleasesList.js
+++ src/components/HelmReleases/HelmReleasesList.js
@@ -9,7 +9,6 @@ import { useGetList } from 'shared/hooks/BackendAPI/useGet';
 import { Labels } from 'shared/components/Labels/Labels';
 import { DynamicPageComponent } from 'shared/components/DynamicPageComponent/DynamicPageComponent';
 import { GenericList } from 'shared/components/GenericList/GenericList';
-import { Link as ExternalLink } from 'shared/components/Link/Link';
 
 import { decodeHelmRelease } from './decodeHelmRelease';
 import { findRecentRelease } from './findRecentRelease';
