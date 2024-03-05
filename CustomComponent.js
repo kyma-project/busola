50b22418e434744c679d91cd21db6b07f74b7a9e
mrCherry97 <mwisnia97@gmail.com>
2024-03-05 13:54:18 +0100

    transform to commonJS

--- CustomComponent.js
+++ CustomComponent.js
@@ -1,7 +1,24 @@
-import React from 'react';
+// MyComponentCompiled.js
+'use strict';
 
-const DynamicGitHubComponent = ({ githubUrl }) => {
-  return <p>TEST123</p>;
+Object.defineProperty(exports, '__esModule', {
+  value: true,
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
+
+var MyComponent = function MyComponent() {
+  return _react2.default.createElement(
+    'div',
+    null,
+    'Hello, I am a dynamic component!',
+  );
 };
 
-export default DynamicGitHubComponent;
+exports.default = MyComponent;
