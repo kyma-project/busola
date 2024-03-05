4d6ec6a09ee5e706a5105ac9fc0019f4064ccc67
mrCherry97 <mwisnia97@gmail.com>
2024-03-05 13:57:49 +0100

    transform to commonJS

--- CustomComponent.js
+++ CustomComponent.js
@@ -1,24 +1,8 @@
-// MyComponentCompiled.js
-'use strict';
+// Your pre-compiled CommonJS code
+const React = require('react');
 
-Object.defineProperty(exports, '__esModule', {
-  value: true,
-});
-
-var _react = require('react');
-
-var _react2 = _interopRequireDefault(_react);
-
-function _interopRequireDefault(obj) {
-  return obj && obj.__esModule ? obj : { default: obj };
-}
-
-var MyComponent = function MyComponent() {
-  return _react2.default.createElement(
-    'div',
-    null,
-    'Hello, I am a dynamic component!',
-  );
+const MyComponent = () => {
+  return React.createElement('p', null, 'TEST123');
 };
 
-exports.default = MyComponent;
+module.exports = MyComponent;
