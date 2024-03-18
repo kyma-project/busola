553c828d9c81b9806579b11d3522cbe2a50cdd19
mrCherry97 <mwisnia97@gmail.com>
2024-03-18 15:01:32 +0100

    fix: navigate to pod logs

--- src/resources/Pods/ContainersData.js
+++ src/resources/Pods/ContainersData.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
@@ -8,6 +7,8 @@ import { LayoutPanelRow } from 'shared/components/LayoutPanelRow/LayoutPanelRow'
 import { ContainerStatus } from './ContainerStatus';
 import { getPorts } from 'shared/components/GetContainersPorts';
 import { UI5Panel } from 'shared/components/UI5Panel/UI5Panel';
+import { useSetRecoilState } from 'recoil';
+import { columnLayoutState } from 'state/columnLayoutAtom';
 
 ContainersData.propTypes = {
   containers: PropTypes.arrayOf(PropTypes.object).isRequired,
@@ -16,43 +17,59 @@ ContainersData.propTypes = {
 export default function ContainersData({ type, containers, statuses }) {
   const { t } = useTranslation();
   const navigate = useNavigate();
+  const setLayout = useSetRecoilState(columnLayoutState);
 
-  const ContainerComponent = ({ container, status }) => (
-    <UI5Panel
-      disableMargin
-      title={container.name}
-      headerActions={
-        <Button
-          aria-label={'view-logs-for-' + container.name}
-          onClick={() => {
-            navigate(`containers/${container.name}`, { replace: true });
-          }}
-        >
-          {t('pods.buttons.view-logs')}
-        </Button>
-      }
-    >
-      <LayoutPanelRow
-        name={t('common.headers.status')}
-        value={<ContainerStatus status={status} />}
-      />
-      {container.image && (
-        <LayoutPanelRow name={t('pods.labels.image')} value={container.image} />
-      )}
-      {container.imagePullPolicy && (
+  const ContainerComponent = ({ container, status }) => {
+    return (
+      <UI5Panel
+        disableMargin
+        title={container.name}
+        headerActions={
+          <Button
+            aria-label={'view-logs-for-' + container.name}
+            onClick={() => {
+              setLayout({
+                midColumn: null,
+                endColumn: null,
+                layout: 'OneColumn',
+              });
+              navigate(
+                `${window.location.pathname.replace(
+                  window.location.search,
+                  '',
+                )}/containers/${container.name}`,
+              );
+            }}
+          >
+            {t('pods.buttons.view-logs')}
+          </Button>
+        }
+      >
         <LayoutPanelRow
-          name={t('pods.labels.image-pull-policy')}
-          value={container.imagePullPolicy}
+          name={t('common.headers.status')}
+          value={<ContainerStatus status={status} />}
         />
-      )}
-      {container.ports && (
-        <LayoutPanelRow
-          name={t('pods.labels.ports')}
-          value={getPorts(container.ports)}
-        />
-      )}
-    </UI5Panel>
-  );
+        {container.image && (
+          <LayoutPanelRow
+            name={t('pods.labels.image')}
+            value={container.image}
+          />
+        )}
+        {container.imagePullPolicy && (
+          <LayoutPanelRow
+            name={t('pods.labels.image-pull-policy')}
+            value={container.imagePullPolicy}
+          />
+        )}
+        {container.ports && (
+          <LayoutPanelRow
+            name={t('pods.labels.ports')}
+            value={getPorts(container.ports)}
+          />
+        )}
+      </UI5Panel>
+    );
+  };
 
   if (!containers) {
     return null;
