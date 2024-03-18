ea59ebfd812879c88746f1a182c1aec027af0968
mrCherry97 <mwisnia97@gmail.com>
2024-03-18 15:03:20 +0100

    fix: lint

--- src/resources/Pods/ContainersData.js
+++ src/resources/Pods/ContainersData.js
@@ -19,57 +19,52 @@ export default function ContainersData({ type, containers, statuses }) {
   const navigate = useNavigate();
   const setLayout = useSetRecoilState(columnLayoutState);
 
-  const ContainerComponent = ({ container, status }) => {
-    return (
-      <UI5Panel
-        disableMargin
-        title={container.name}
-        headerActions={
-          <Button
-            aria-label={'view-logs-for-' + container.name}
-            onClick={() => {
-              setLayout({
-                midColumn: null,
-                endColumn: null,
-                layout: 'OneColumn',
-              });
-              navigate(
-                `${window.location.pathname.replace(
-                  window.location.search,
-                  '',
-                )}/containers/${container.name}`,
-              );
-            }}
-          >
-            {t('pods.buttons.view-logs')}
-          </Button>
-        }
-      >
+  const ContainerComponent = ({ container, status }) => (
+    <UI5Panel
+      disableMargin
+      title={container.name}
+      headerActions={
+        <Button
+          aria-label={'view-logs-for-' + container.name}
+          onClick={() => {
+            setLayout({
+              midColumn: null,
+              endColumn: null,
+              layout: 'OneColumn',
+            });
+            navigate(
+              `${window.location.pathname.replace(
+                window.location.search,
+                '',
+              )}/containers/${container.name}`,
+            );
+          }}
+        >
+          {t('pods.buttons.view-logs')}
+        </Button>
+      }
+    >
+      <LayoutPanelRow
+        name={t('common.headers.status')}
+        value={<ContainerStatus status={status} />}
+      />
+      {container.image && (
+        <LayoutPanelRow name={t('pods.labels.image')} value={container.image} />
+      )}
+      {container.imagePullPolicy && (
         <LayoutPanelRow
-          name={t('common.headers.status')}
-          value={<ContainerStatus status={status} />}
+          name={t('pods.labels.image-pull-policy')}
+          value={container.imagePullPolicy}
         />
-        {container.image && (
-          <LayoutPanelRow
-            name={t('pods.labels.image')}
-            value={container.image}
-          />
-        )}
-        {container.imagePullPolicy && (
-          <LayoutPanelRow
-            name={t('pods.labels.image-pull-policy')}
-            value={container.imagePullPolicy}
-          />
-        )}
-        {container.ports && (
-          <LayoutPanelRow
-            name={t('pods.labels.ports')}
-            value={getPorts(container.ports)}
-          />
-        )}
-      </UI5Panel>
-    );
-  };
+      )}
+      {container.ports && (
+        <LayoutPanelRow
+          name={t('pods.labels.ports')}
+          value={getPorts(container.ports)}
+        />
+      )}
+    </UI5Panel>
+  );
 
   if (!containers) {
     return null;
