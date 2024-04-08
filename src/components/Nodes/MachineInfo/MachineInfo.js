36c201330d69227ded1d83f20c27422798524e4e
Oliwia Gowor <oliwiagowor@gmail.com>
2024-04-08 12:29:24 +0200

    change node details

--- src/components/Nodes/MachineInfo/MachineInfo.js
+++ src/components/Nodes/MachineInfo/MachineInfo.js
@@ -1,9 +1,8 @@
-import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { LayoutPanelRow } from 'shared/components/LayoutPanelRow/LayoutPanelRow';
-
 import './MachineInfo.scss';
 import { Card, CardHeader } from '@ui5/webcomponents-react';
+import { spacing } from '@ui5/webcomponents-react-base';
+import { DynamicPageComponent } from 'shared/components/DynamicPageComponent/DynamicPageComponent';
 
 export function MachineInfo({ nodeInfo, capacity }) {
   const formattedMemory =
@@ -11,38 +10,42 @@ export function MachineInfo({ nodeInfo, capacity }) {
   const { t } = useTranslation();
 
   return (
-    <Card
-      className="machine-info__card"
-      header={<CardHeader titleText={t('machine-info.title')} />}
-    >
-      <div className="machine-info__body">
-        <LayoutPanelRow
-          name={t('machine-info.operating-system')}
-          value={`${nodeInfo.operatingSystem} (${nodeInfo.osImage})`}
-        />
-        <LayoutPanelRow
-          name={t('machine-info.architecture-cpus')}
-          value={`${nodeInfo.architecture}, ${capacity.cpu} ${t(
-            'machine-info.cpus',
-          )}`}
-        />
-        <LayoutPanelRow
-          name={t('machine-info.pods-capacity')}
-          value={capacity.pods}
-        />
-        <LayoutPanelRow
-          name={t('machine-info.memory')}
-          value={`${formattedMemory} ${t('machine-info.gib')}`}
-        />
-        <LayoutPanelRow
-          name={t('machine-info.kube-proxy-version')}
-          value={nodeInfo.kubeProxyVersion}
-        />
-        <LayoutPanelRow
-          name={t('machine-info.kubelet-version')}
-          value={nodeInfo.kubeletVersion}
-        />
-      </div>
-    </Card>
+    <div style={spacing.sapUiSmallMarginBeginEnd}>
+      <Card
+        className="machine-info__card"
+        header={<CardHeader titleText={t('machine-info.title')} />}
+      >
+        <div style={spacing.sapUiSmallMargin} className="machine-info__body">
+          <DynamicPageComponent.Column
+            title={t('machine-info.operating-system')}
+          >
+            {`${nodeInfo.operatingSystem} (${nodeInfo.osImage})`}
+          </DynamicPageComponent.Column>
+          <DynamicPageComponent.Column
+            title={t('machine-info.architecture-cpus')}
+          >
+            {`${nodeInfo.architecture}, ${capacity.cpu} ${t(
+              'machine-info.cpus',
+            )}`}
+          </DynamicPageComponent.Column>
+          <DynamicPageComponent.Column title={t('machine-info.pods-capacity')}>
+            {capacity.pods}
+          </DynamicPageComponent.Column>
+          <DynamicPageComponent.Column title={t('machine-info.memory')}>
+            {`${formattedMemory} ${t('machine-info.gib')}`}
+          </DynamicPageComponent.Column>
+          <DynamicPageComponent.Column
+            title={t('machine-info.kube-proxy-version')}
+          >
+            {nodeInfo.kubeProxyVersion}
+          </DynamicPageComponent.Column>
+          <DynamicPageComponent.Column
+            title={t('machine-info.kubelet-version')}
+          >
+            {nodeInfo.kubeletVersion}
+          </DynamicPageComponent.Column>
+        </div>
+      </Card>
+    </div>
   );
 }
