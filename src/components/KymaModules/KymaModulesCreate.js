7c4d5b7410fedb6b7f5b65672556d92b51a37314
mrCherry97 <mwisnia97@gmail.com>
2024-04-19 12:26:02 +0200

    fix error from create, but still setting resource is not quite working

--- src/components/KymaModules/KymaModulesCreate.js
+++ src/components/KymaModules/KymaModulesCreate.js
@@ -1,16 +1,22 @@
+import { cloneDeep } from 'lodash';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { ResourceForm } from 'shared/ResourceForm';
 
 export default function KymaModulesCreate({ resource, ...props }) {
   const { t } = useTranslation();
+  const [kymaResource, setKymaResource] = useState(cloneDeep(resource));
+
   return (
     <ResourceForm
       {...props}
       pluralKind="kymas"
       singularName={t('kyma-modules.kyma')}
       resource={resource}
-      initialResource={resource}
+      initialResource={kymaResource}
+      setResource={setKymaResource}
+      createUrl={props.resourceUrl}
       onlyYaml
     />
   );
