adadf0c902fda0fd423ed1a4278fab47ae381c55
Oliwia Gowor <oliwiagowor@gmail.com>
2024-05-08 13:56:58 +0200

    change best-practices

--- tests/lighthouse/lighthouse.spec.js
+++ tests/lighthouse/lighthouse.spec.js
@@ -66,7 +66,7 @@ test('Busola Lighthouse audit', async () => {
     port: 9222,
     thresholds: {
       accessibility: 80,
-      'best-practices': 90,
+      'best-practices': 85,
     },
   });
 
