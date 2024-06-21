93c816ded05c1e4290a05582c71b09cc6f855740
Damian Badura <damian.badura@sap.com>
2024-06-21 15:40:23 +0200

    adjust radial chart

--- src/components/Extensibility/components/RadialChart.js
+++ src/components/Extensibility/components/RadialChart.js
@@ -2,7 +2,6 @@ import { Card, CardHeader } from '@ui5/webcomponents-react';
 import { useTranslation } from 'react-i18next';
 import { UI5RadialChart } from 'shared/components/UI5RadialChart/UI5RadialChart';
 import { useJsonata } from '../hooks/useJsonata';
-import { spacing } from '@ui5/webcomponents-react-base';
 
 export const RadialChart = ({ structure, value, originalResource }) => {
   const { t } = useTranslation();
@@ -33,22 +32,22 @@ export const RadialChart = ({ structure, value, originalResource }) => {
   }
 
   return (
-    <Card
-      className="radial-chart-card"
-      style={{
-        ...spacing.sapUiSmallMarginBegin,
-        ...spacing.sapUiSmallMarginBottom,
-      }}
-      header={<CardHeader titleText={t(structure?.name)} />}
-    >
-      <UI5RadialChart
-        color={structure.color}
-        value={value}
-        max={maxValue ? maxValue : structure?.maxValue}
-        additionalInfo={
-          additionalInfo ? additionalInfo : structure?.additionalInfo
-        }
-      />
-    </Card>
+    <div className={'item-wrapper tall'}>
+      <Card
+        className="radial-chart-card"
+        header={<CardHeader titleText={t(structure?.name)} />}
+      >
+        <UI5RadialChart
+          color={structure.color}
+          value={value}
+          max={maxValue ? maxValue : structure?.maxValue}
+          additionalInfo={
+            additionalInfo ? additionalInfo : structure?.additionalInfo
+          }
+        />
+      </Card>
+    </div>
   );
 };
+
+RadialChart.array = true;
