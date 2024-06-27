7f660eb6880e6fd754405d211d0dd9960898077c
Damian Badura <damian.badura@sap.com>
2024-06-27 16:53:10 +0200

    stabilize test

--- tests/integration/tests/extensibility/ext-test-extensibility.spec.js
+++ tests/integration/tests/extensibility/ext-test-extensibility.spec.js
@@ -2,23 +2,22 @@ import { chooseComboboxOption } from '../../support/helpers';
 import jsyaml from 'js-yaml';
 
 const EXTENSION_NAME = 'Potato Extension';
+const NAMESPACE_NAME = 'potato';
 const CR_NAME = 'first-potato';
 const FIRST_DESCRIPTION = 'My Description';
 const UPDATED_DESCRIPTION = 'Updated description';
 const SECOND_DETAIL = 'weight';
 
-context('Test Potatoes', () => {
+context('Test Extensibility Create/Update', () => {
   Cypress.skipAfterFail();
 
   before(() => {
     cy.handleExceptions();
 
     cy.loginAndSelectCluster();
-
-    // cy.createNamespace('potatoes');
   });
 
-  it('Creates the EXT potatoes config', () => {
+  it('Upload test resources', () => {
     cy.getLeftNav()
       .contains('Cluster Details')
       .click();
@@ -27,7 +26,7 @@ context('Test Potatoes', () => {
 
     cy.loadFiles(
       'examples/extensions/configuration/potatoes-crd.yaml',
-      'examples/extensions/samples/potato.yaml',
+      'examples/extensions/configuration/namespace.yaml',
     ).then(resources => {
       const input = resources.map(r => jsyaml.dump(r)).join('\n---\n');
       cy.pasteToMonaco(input);
@@ -42,6 +41,20 @@ context('Test Potatoes', () => {
       .find('.status-message-success')
       .should('have.length', 2);
 
+    cy.loadFiles('examples/extensions/samples/potato.yaml').then(resources => {
+      const input = resources.map(r => jsyaml.dump(r)).join('\n---\n');
+      cy.pasteToMonaco(input);
+    });
+
+    cy.get('ui5-dialog')
+      .contains('ui5-button', 'Upload')
+      .should('be.visible')
+      .click();
+
+    cy.get('ui5-dialog[header-text="Upload YAML"]')
+      .find('.status-message-success')
+      .should('have.length', 1);
+
     cy.get('ui5-dialog[header-text="Upload YAML"]').within(() => {
       cy.contains('ui5-button', 'Close')
         .should('be.visible')
@@ -49,7 +62,7 @@ context('Test Potatoes', () => {
     });
   });
 
-  it('Create Extensions', () => {
+  it('Create extension', () => {
     cy.navigateTo('Configuration', 'Extensions');
 
     cy.openCreate();
@@ -77,9 +90,10 @@ context('Test Potatoes', () => {
     cy.saveChanges('Create');
   });
 
-  it.skip('Check if Extensions is created', () => {
+  it('Check if Extensions is created', () => {
     cy.get('ui5-input[placeholder="Search"]:visible')
       .find('input')
+      .wait(1000)
       .clear()
       .type('potatoes')
       .get('ui5-li-suggestion-item:visible')
@@ -91,25 +105,22 @@ context('Test Potatoes', () => {
       .should('be.visible');
   });
 
-  it('Check extension view', () => {
+  it('Check if extension is displayed correctly', () => {
+    cy.reload(); //Reload is needed to load new extension
+
     cy.getLeftNav()
       .get('ui5-side-navigation-item[text="Namespaces"]')
       .click();
 
     cy.get('ui5-input[placeholder="Search"]:visible')
       .find('input')
+      .wait(1000)
       .clear()
-      .type('potatoes');
+      .type(NAMESPACE_NAME);
 
-    cy.clickGenericListLink('potatoes');
+    cy.clickGenericListLink(NAMESPACE_NAME);
 
-    cy.getLeftNav()
-      .get('ui5-side-navigation-item[text="Custom Resources"]')
-      .click();
-
-    cy.getLeftNav()
-      .get(`ui5-side-navigation-sub-item[text="${EXTENSION_NAME}"]`)
-      .click();
+    cy.navigateTo('Custom Resources', EXTENSION_NAME);
 
     cy.clickGenericListLink(CR_NAME);
 
@@ -122,23 +133,18 @@ context('Test Potatoes', () => {
   });
 
   it('Edit extension', () => {
-    cy.getLeftNav()
-      .get('ui5-side-navigation-item[text="Configuration"]')
-      .click();
-
-    cy.getLeftNav()
-      .get(`ui5-side-navigation-sub-item[text="Extensions"]`)
-      .click();
+    cy.navigateTo('Configuration', 'Extensions');
 
     cy.get('ui5-input[placeholder="Search"]:visible')
       .find('input')
+      .wait(1000)
       .clear()
       .type('potatoes')
       .get('ui5-li-suggestion-item:visible')
       .contains('potatoes')
       .click();
 
-    cy.clickGenericListLink('potatoes');
+    cy.clickGenericListLink(NAMESPACE_NAME);
 
     cy.getMidColumn().inspectTab('Edit');
 
@@ -160,17 +166,18 @@ context('Test Potatoes', () => {
     cy.saveChanges('Edit');
   });
 
-  it('Check extension view after edit', () => {
+  it('Check if extension is updated and displayed correctly', () => {
     cy.getLeftNav()
       .contains('Namespaces')
       .click();
 
     cy.get('ui5-input[placeholder="Search"]:visible')
       .find('input')
+      .wait(1000)
       .clear()
-      .type('potatoes');
+      .type(NAMESPACE_NAME);
 
-    cy.clickGenericListLink('potatoes');
+    cy.clickGenericListLink(NAMESPACE_NAME);
 
     cy.getLeftNav()
       .get('ui5-side-navigation-item[text="Custom Resources"]')
