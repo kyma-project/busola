462502a4f162d01bc68b7ed63889d018d7a5caaa
Damian Badura <damian.badura@sap.com>
2024-06-27 17:17:05 +0200

    export to consts

--- tests/integration/tests/extensibility/ext-test-extensibility.spec.js
+++ tests/integration/tests/extensibility/ext-test-extensibility.spec.js
@@ -1,12 +1,13 @@
 import { chooseComboboxOption } from '../../support/helpers';
 import jsyaml from 'js-yaml';
 
-const EXTENSION_NAME = 'Potato Extension';
+const EXTENSION_DISPLAY_NAME = 'Potato Extension';
 const NAMESPACE_NAME = 'potato';
 const CR_NAME = 'first-potato';
 const FIRST_DESCRIPTION = 'My Description';
 const UPDATED_DESCRIPTION = 'Updated description';
 const SECOND_DETAIL = 'weight';
+const EXTENSION_NAME = 'potatoes';
 
 context('Test Extensibility Create/Update', () => {
   Cypress.skipAfterFail();
@@ -69,15 +70,14 @@ context('Test Extensibility Create/Update', () => {
 
     chooseComboboxOption('[id="combobox-input"]', 'potato');
 
-    cy.get('ui5-input[value=Potatoes]')
+    cy.get(`ui5-input[value="Potatoes"]`)
       .find('input')
       .clear()
-      .type(EXTENSION_NAME);
+      .type(EXTENSION_DISPLAY_NAME);
 
     cy.get('[aria-label="expand Details Summary"]').click();
     cy.get('ui5-panel[data-testid="details-summary"]').within(() => {
       cy.get('ui5-input[value="description"]')
-        .debug()
         .find('input')
         .clear()
         .type(FIRST_DESCRIPTION);
@@ -95,13 +95,13 @@ context('Test Extensibility Create/Update', () => {
       .find('input')
       .wait(1000)
       .clear()
-      .type('potatoes')
+      .type(EXTENSION_NAME)
       .get('ui5-li-suggestion-item:visible')
-      .contains('potatoes')
+      .contains(EXTENSION_NAME)
       .click();
 
     cy.get('ui5-table-row')
-      .contains('potatoes')
+      .contains(EXTENSION_NAME)
       .should('be.visible');
   });
 
@@ -120,7 +120,7 @@ context('Test Extensibility Create/Update', () => {
 
     cy.clickGenericListLink(NAMESPACE_NAME);
 
-    cy.navigateTo('Custom Resources', EXTENSION_NAME);
+    cy.navigateTo('Custom Resources', EXTENSION_DISPLAY_NAME);
 
     cy.clickGenericListLink(CR_NAME);
 
@@ -139,9 +139,9 @@ context('Test Extensibility Create/Update', () => {
       .find('input')
       .wait(1000)
       .clear()
-      .type('potatoes')
+      .type(EXTENSION_NAME)
       .get('ui5-li-suggestion-item:visible')
-      .contains('potatoes')
+      .contains(EXTENSION_NAME)
       .click();
 
     cy.clickGenericListLink(NAMESPACE_NAME);
@@ -184,7 +184,7 @@ context('Test Extensibility Create/Update', () => {
       .click();
 
     cy.getLeftNav()
-      .get(`ui5-side-navigation-sub-item[text="${EXTENSION_NAME}"]`)
+      .get(`ui5-side-navigation-sub-item[text="${EXTENSION_DISPLAY_NAME}"]`)
       .click();
 
     cy.clickGenericListLink(CR_NAME);
