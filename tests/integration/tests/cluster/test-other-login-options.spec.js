86d8a71f1a922a628b8d02c39cb2d3d6712c0bc1
Oliwia Gowor <oliwiagowor@gmail.com>
2024-07-12 11:39:33 +0200

    remove token test

--- tests/integration/tests/cluster/test-other-login-options.spec.js
+++ tests/integration/tests/cluster/test-other-login-options.spec.js
@@ -11,50 +11,6 @@ context('Test other login options', () => {
     cy.handleExceptions();
   });
 
-  // Uncomment after resolving https://github.com/kyma-project/busola/issues/2511
-  it('Kubeconfig and token separately', () => {
-    cy.wrap(loadFile('kubeconfig.yaml')).then(kubeconfig => {
-      const token = kubeconfig.users[0].user.token;
-      kubeconfig.users[0].user.token = null;
-
-      cy.visit(`${config.clusterAddress}/clusters`);
-
-      cy.get('ui5-button:visible')
-        .contains('Connect')
-        .click();
-
-      cy.contains('Drop a .kubeconfig file or click to upload').attachFile(
-        {
-          fileContent: jsyaml.dump(kubeconfig),
-          filePath: 'kubeconfig.yaml',
-        },
-        {
-          subjectType: 'drag-n-drop',
-        },
-      );
-
-      cy.contains('Next').click();
-
-      cy.contains(
-        'ui5-message-strip',
-        "We couldn't find enough authentication information",
-      ).should('be.visible');
-
-      cy.contains('Token')
-        .parent()
-        .next()
-        .find('input')
-        .type(token);
-
-      cy.contains('ui5-button:visible', 'Next step').click();
-      cy.contains('ui5-button:visible', 'Next step').click();
-      cy.contains('ui5-button:visible', 'Connect cluster').click();
-
-      cy.url().should('match', /overview$/);
-      cy.contains('Cluster Details').should('be.visible');
-    });
-  });
-
   it('Reset endpoint', () => {
     cy.loginAndSelectCluster();
     cy.url().should('match', /overview$/);
