f795d87ac5d0ac404f4177338d5da0adeb21793d
Damian Badura <45110612+dbadura@users.noreply.github.com>
2024-07-16 14:00:39 +0200

    feat: Improve pods statistics (#3066)
    * first implementaion
    
    * remove console.log
    
    * fix import

--- src/resources/Namespaces/NamespaceWorkloads/NamespaceWorkloadsHelpers.js
+++ src/resources/Namespaces/NamespaceWorkloads/NamespaceWorkloadsHelpers.js
@@ -1,6 +1,45 @@
+import { calculatePodState } from 'resources/Pods/PodStatus';
+
 export function getHealthyReplicasCount(resource) {
   return resource?.filter(r => r.status.replicas === r.status.readyReplicas)
     ?.length;
+}
+
+export const PodStatusCounterKey = {
+  Pending: 'pending',
+  Healthy: 'healthy',
+  Failed: 'failed',
+};
+
+export function getStatusesPodCount(pods) {
+  if (!pods) {
+    return new Map();
+  }
+  const statusData = Map.groupBy(pods, pod => {
+    const podState = calculatePodState(pod);
+    return getPodState(podState.status);
+  });
+
+  statusData.forEach((value, key, map) => map.set(key, value.length));
+  return statusData;
+}
+
+function getPodState(status) {
+  switch (status) {
+    case 'Running':
+    case 'Succeeded':
+    case 'Completed':
+    case 'Terminated':
+      return 'healthy';
+    case 'Pending':
+    case 'Terminating':
+    case 'PodInitializing':
+    case 'ContainerCreating':
+    case 'Unknown':
+      return 'pending';
+    default:
+      return 'failed';
+  }
 }
 
 export function getHealthyStatusesCount(pods) {
