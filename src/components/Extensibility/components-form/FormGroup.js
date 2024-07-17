6a84c1858fdc9a995437e9af26d3ed9aaad5b279
Damian Badura <damian.badura@sap.com>
2024-07-17 09:36:19 +0200

    improve tooltip

--- src/components/Extensibility/components-form/FormGroup.js
+++ src/components/Extensibility/components-form/FormGroup.js
@@ -12,21 +12,22 @@ export function FormGroup({
 }) {
   const { WidgetRenderer } = widgets;
   const ownSchema = schema.delete('widget');
-  const { tFromStoreKeys } = useGetTranslation();
+  const { tFromStoreKeys, t: tExt } = useGetTranslation();
 
   const columns = schema.get('columns');
   const gridTemplateColumns = `repeat(${columns}, 1fr)`;
 
   const defaultOpen = schema.get('defaultExpanded') ?? false;
   const schemaRequired = schema.get('required') ?? required;
-  const description = schema.get('description');
+  const tooltipContent = schema.get('description');
+
   return (
     <ResourceForm.CollapsibleSection
       title={tFromStoreKeys(storeKeys, schema)}
       defaultOpen={defaultOpen}
       nestingLevel={nestingLevel}
       required={schemaRequired}
-      tooltipContent={description}
+      tooltipContent={tExt(tooltipContent)}
     >
       <div className="form-group__grid-wrapper" style={{ gridTemplateColumns }}>
         <WidgetRenderer
