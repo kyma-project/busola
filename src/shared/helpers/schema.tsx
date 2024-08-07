6e3de6dddaa90b19649af77c8788931b229842c1
Damian Badura <damian.badura@sap.com>
2024-08-07 14:07:56 +0200

    add test, simplify implementation

--- src/shared/helpers/schema.tsx
+++ src/shared/helpers/schema.tsx
@@ -1,39 +1,32 @@
+/**
+ * This method get description of k8s resource field. Similar in functionality to kubectl explain.
+ *
+ * @param schema json schema of k8s resource
+ * @param path path to desired field. kubectl explain format is accepted
+ */
 export function getDescription(schema: any, path: string): string {
   let contextSchema = schema;
-  console.log('get desc', path, path.split('.'), schema);
-
   path.split('.').forEach(pathItem => {
-    let el = null;
-    if (pathItem.endsWith('[]')) {
-      pathItem = pathItem.replace('[]', '');
-      el = contextSchema?.items?.properties?.[pathItem];
-    } else {
-      el = contextSchema?.properties?.[pathItem];
-    }
-    if (el) {
-      contextSchema = el;
-    }
+    //Check if it's array as it has fields under `items`
+    contextSchema = contextSchema?.items ? contextSchema.items : contextSchema;
+    contextSchema = contextSchema?.properties?.[pathItem];
   });
 
-  return contextSchema?.description
-    ? contextSchema?.description
-    : 'Description not found';
+  const objDesc = contextSchema?.description ? contextSchema.description : '';
+  const arrayDesc = contextSchema?.items?.description
+    ? contextSchema.items.description
+    : '';
+  if (!arrayDesc && !objDesc) {
+    return 'Description not found';
+  }
+  return objDesc + (arrayDesc ? ' ' + arrayDesc : '');
 }
 
 export function getPartialSchema(schema: any, path: string): string {
   let contextSchema = schema;
 
   path.split('.').forEach(pathItem => {
-    let el = null;
-    if (pathItem.endsWith('[]')) {
-      pathItem = pathItem.replace('[]', '');
-      el = contextSchema?.properties?.[pathItem]?.items;
-    } else {
-      el = contextSchema?.properties?.[pathItem];
-    }
-    if (el) {
-      contextSchema = el;
-    }
+    contextSchema = contextSchema?.properties?.[pathItem];
   });
 
   return contextSchema;
