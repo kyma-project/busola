c2393c5ea75111eca3b26abb7131fae6d77b5499
Damian Badura <45110612+dbadura@users.noreply.github.com>
2024-09-16 12:13:00 +0200

    feat: Add overlays with environments configuration (#3344)
    * adjust kustomize
    
    * remove unused file
    
    * fix build

--- resources/apply-resources-istio.sh
+++ resources/apply-resources-istio.sh
@@ -1,15 +1,18 @@
-if [ -z "$1" ] ; then  
+#!/bin/bash
+set -e
+
+if [ -z "$1" ] ; then
     echo "No domain passed as first argument, aborting." 
     exit 1
 fi
 
 export DOMAIN=$1
 export NAMESPACE=${2:-busola}
+export ENVIRONMENT=$3
+TMP_DIR="../temp/resources"
 
-mkdir -p ../temp/resources
-cp -rf . ../temp/resources
+./apply-resources.sh "$@"
 
-envsubst < ../temp/resources/istio/virtualservice-busola-template.yaml > ../temp/resources/istio/virtualservice-busola.yaml
+envsubst < "${TMP_DIR}"/istio/virtualservice-busola.tpl.yaml > "${TMP_DIR}"/istio/virtualservice-busola.yaml
 
-kubectl apply -k ../temp/resources/istio --namespace=$NAMESPACE
-
+kubectl apply -k "${TMP_DIR}"/istio --namespace=$NAMESPACE
