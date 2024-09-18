2430e9173adc185eaf450241dbb94185565a56c1
Oliwia Gowor <oliwiagowor@gmail.com>
2024-09-18 16:20:15 +0200

    test local address exception

--- src/state/utils/getBackendInfo.ts
+++ src/state/utils/getBackendInfo.ts
@@ -1,9 +1,15 @@
 const domain = window.location.hostname;
 
 const getBackendAddress = () => {
-  // dev busola
-  if (window.location.hostname.startsWith('localhost')) {
+  // local busola - needed for e2e tests to work locally
+  if (
+    window.location.hostname.startsWith('localhost') &&
+    window.location.port === '8080'
+  ) {
     return 'http://127.0.0.1:3001/backend';
+    // dev busola
+  } else if (window.location.hostname.startsWith('localhost')) {
+    return 'http://localhost:3001/backend';
     // on cluster
   } else {
     return '/backend';
