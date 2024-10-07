8b5ad54b70b297ac938fbc19459a4d8f33d60fbd
akucharska <agata.kucharska@sap.com>
2024-10-07 09:25:34 +0200

    Change ports of busole and extension config

--- tests/integration/config.js
+++ tests/integration/config.js
@@ -4,5 +4,5 @@ const domain = env.DOMAIN || 'https://local.kyma.dev';
 export default {
   domain: domain,
   localDev: env.LOCAL_DEV || false,
-  clusterAddress: env.LOCAL_DEV ? `http://localhost:8080` : `${domain}`,
+  clusterAddress: env.LOCAL_DEV ? `http://localhost:8000` : `${domain}`,
 };
