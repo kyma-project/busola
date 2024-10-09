396edd8017f7c7674428a6ef6d7a10026420d02a
mrCherry97 <mwisnia97@gmail.com>
2024-10-09 13:29:27 +0200

    tests: add namespace overview test

---
+++ tests/integration/tests/accessibility/test-namespace-overview.spec.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+context('Accessibility test Namespace overview', () => {
+  Cypress.skipAfterFail();
+
+  before(() => {
+    cy.handleExceptions();
+    cy.setUpContinuum('continuum/continuum.conf.js');
+  });
+
+  it('Acc test namespace overview', () => {
+    cy.loginAndSelectCluster();
+
+    cy.url().should('match', /overview$/);
+
+    cy.createNamespace('acc-test-namespace');
+
+    cy.runAllAccessibilityTests()
+      .printAccessibilityTestResults()
+      .submitAccessibilityConcernsToAMP();
+  });
+});
