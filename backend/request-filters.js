07a4f618ba8069881e03b21442c158731e2c82a9
chriskari <chriskari@gmx.de>
2024-10-11 15:24:14 +0200

    test: added unit tests for request filters

--- backend/request-filters.js
+++ backend/request-filters.js
@@ -1,4 +1,4 @@
-const localIpFilter = (_req, headersData) => {
+export const localIpFilter = (_req, headersData) => {
   //https://stackoverflow.com/a/62925185, but without 127.0.0.1 range
   const localIpRegex = /\b(0?10\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|172\.0?1[6-9]\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|172\.0?2[0-9]\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|172\.0?3[01]\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|192\.168\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|169\.254\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|::1|[fF][cCdD][0-9a-fA-F]{2}(?:[:][0-9a-fA-F]{0,4}){0,7}|[fF][eE][89aAbB][0-9a-fA-F](?:[:][0-9a-fA-F]{0,4}){0,7})(?:\/([789]|1?[0-9]{2}))?\b/;
 
@@ -9,7 +9,7 @@ const localIpFilter = (_req, headersData) => {
   }
 };
 
-const pathWhitelistFilter = req => {
+export const pathWhitelistFilter = req => {
   const path = req.originalUrl.replace(/^\/backend/, '');
 
   // list comes from "/" call to API server
@@ -32,24 +32,36 @@ const pathWhitelistFilter = req => {
   }
 };
 
-const pathInvalidCharacterFilter = req => {
+export const pathInvalidCharacterFilter = req => {
   const encodedPath = req.originalUrl;
-  let decodedPath;
+  let decodedPath = encodedPath;
+  let previousPath;
+  let decodeAttempts = 0;
+  const maxDecodeAttempts = 3;
 
-  try {
-    decodedPath = decodeURIComponent(encodedPath);
-  } catch (err) {
-    throw Error('Invalid URL encoding in path.');
+  // Iteratively decode the path until it stabilizes (no more changes) or reaches the max attempts
+  do {
+    previousPath = decodedPath;
+    try {
+      decodedPath = decodeURIComponent(decodedPath);
+      decodeAttempts++;
+    } catch (err) {
+      throw Error('Invalid URL encoding in path.');
+    }
+  } while (decodedPath !== previousPath && decodeAttempts < maxDecodeAttempts);
+
+  if (decodeAttempts === maxDecodeAttempts && decodedPath !== previousPath) {
+    throw Error('Path contains invalid encoding');
   }
 
   // Allow alphanumeric, dashes, underscores, dots, slashes, colons, tildes, question marks, equals, and ampersands
   const validPathRegex = /^[a-zA-Z0-9/_\-.:~?&=]+$/;
   if (decodedPath.includes('..') || !validPathRegex.test(decodedPath)) {
-    throw Error(`Path ${decodedPath} contains invalid characters.`);
+    throw Error(`Path contains invalid characters.`);
   }
 };
 
-const invalidHeaderFilter = req => {
+export const invalidHeaderFilter = req => {
   const headers = req.headers;
 
   if ('x-forwarded-for' in headers || 'forwarded' in headers) {
@@ -57,7 +69,7 @@ const invalidHeaderFilter = req => {
   }
 };
 
-const invalidRequestMethodFilter = req => {
+export const invalidRequestMethodFilter = req => {
   const path = req.originalUrl;
   const method = req.method;
 
---
+++ backend/request-filters.test.js
@@ -0,0 +1,297 @@
+import {
+  invalidHeaderFilter,
+  invalidRequestMethodFilter,
+  localIpFilter,
+  pathInvalidCharacterFilter,
+  pathWhitelistFilter,
+} from './request-filters';
+
+describe('invalidRequestMethodFilter', () => {
+  it('should throw an error for TRACE requests', () => {
+    const req = { originalUrl: '/some-path', method: 'TRACE' };
+
+    expect(() => invalidRequestMethodFilter(req)).toThrowError(
+      'Invalid request method',
+    );
+  });
+
+  it('should throw an error for OPTIONS requests not including "proxy" in path', () => {
+    const req = { originalUrl: '/some-path', method: 'OPTIONS' };
+
+    expect(() => invalidRequestMethodFilter(req)).toThrowError(
+      'Invalid request method',
+    );
+  });
+
+  it('should not throw an error for OPTIONS requests that include "proxy" in path', () => {
+    const req = { originalUrl: '/proxy/some-path', method: 'OPTIONS' };
+
+    expect(() => invalidRequestMethodFilter(req)).not.toThrow();
+  });
+
+  it('should throw an error for HEAD requests not including "proxy" in path', () => {
+    const req = { originalUrl: '/some-path', method: 'HEAD' };
+
+    expect(() => invalidRequestMethodFilter(req)).toThrowError(
+      'Invalid request method',
+    );
+  });
+
+  it('should not throw an error for HEAD requests that include "proxy" in path', () => {
+    const req = { originalUrl: '/proxy/some-path', method: 'HEAD' };
+
+    expect(() => invalidRequestMethodFilter(req)).not.toThrow();
+  });
+
+  it('should not throw an error for GET requests', () => {
+    const req = { originalUrl: '/some-path', method: 'GET' };
+
+    expect(() => invalidRequestMethodFilter(req)).not.toThrow();
+  });
+});
+
+describe('invalidHeaderFilter', () => {
+  it('should throw an error if x-forwarded-for header is present', () => {
+    const req = {
+      headers: {
+        'x-forwarded-for': '123.45.67.89',
+      },
+    };
+
+    expect(() => invalidHeaderFilter(req)).toThrowError(
+      'Request contains invalid headers.',
+    );
+  });
+
+  it('should throw an error if forwarded header is present', () => {
+    const req = {
+      headers: {
+        forwarded: 'for=123.45.67.89;by=proxy',
+      },
+    };
+
+    expect(() => invalidHeaderFilter(req)).toThrowError(
+      'Request contains invalid headers.',
+    );
+  });
+
+  it('should throw an error if both x-forwarded-for and forwarded headers are present', () => {
+    const req = {
+      headers: {
+        'x-forwarded-for': '123.45.67.89',
+        forwarded: 'for=123.45.67.89;by=proxy',
+      },
+    };
+
+    expect(() => invalidHeaderFilter(req)).toThrowError(
+      'Request contains invalid headers.',
+    );
+  });
+
+  it('should not throw an error if x-forwarded-for and forwarded headers are absent', () => {
+    const req = {
+      headers: {
+        host: 'localhost',
+        'user-agent': 'Mozilla/5.0',
+      },
+    };
+
+    expect(() => invalidHeaderFilter(req)).not.toThrow();
+  });
+});
+
+describe('pathInvalidCharacterFilter', () => {
+  it('should not throw an error for a valid path', () => {
+    const req = {
+      originalUrl: '/valid/path-123',
+    };
+
+    expect(() => pathInvalidCharacterFilter(req)).not.toThrow();
+  });
+
+  it('should throw an error for a path with invalid characters', () => {
+    const req = {
+      originalUrl: '/invalid/path<with>brackets',
+    };
+
+    expect(() => pathInvalidCharacterFilter(req)).toThrowError(
+      'Path contains invalid characters.',
+    );
+  });
+
+  it('should throw an error for a path containing ..', () => {
+    const req = {
+      originalUrl: '/valid/../invalid/path',
+    };
+
+    expect(() => pathInvalidCharacterFilter(req)).toThrowError(
+      'Path contains invalid characters.',
+    );
+  });
+
+  it('should throw an error for an improperly encoded path', () => {
+    const req = {
+      originalUrl: '/invalid%path',
+    };
+
+    expect(() => pathInvalidCharacterFilter(req)).toThrowError(
+      'Invalid URL encoding in path.',
+    );
+  });
+
+  it('should throw an error when maximum decode attempts are reached', () => {
+    const req = {
+      originalUrl: '/%2525252e%2525252e', // triple-encoded ".."
+    };
+
+    expect(() => pathInvalidCharacterFilter(req)).toThrowError(
+      'Path contains invalid encoding',
+    );
+  });
+
+  it('should handle multiple encoding attempts correctly', () => {
+    const req = {
+      originalUrl: '%252F', // double-encoded "/"
+    };
+
+    expect(() => pathInvalidCharacterFilter(req)).not.toThrow();
+  });
+
+  it('should throw en error for multi-encoded invalid caracters', () => {
+    const req = {
+      originalUrl: '/%252e%252e', // double-encoded ".."
+    };
+
+    expect(() => pathInvalidCharacterFilter(req)).toThrowError(
+      'Path contains invalid characters.',
+    );
+  });
+});
+
+describe('pathWhitelistFilter', () => {
+  it('should allow whitelisted paths', () => {
+    const req = {
+      originalUrl: '/backend/api/users',
+    };
+
+    expect(() => pathWhitelistFilter(req)).not.toThrow();
+  });
+
+  it('should allow whitelisted root path', () => {
+    const req = {
+      originalUrl: '/backend/.well-known',
+    };
+
+    expect(() => pathWhitelistFilter(req)).not.toThrow();
+  });
+
+  it('should throw error for non-whitelisted path', () => {
+    const req = {
+      originalUrl: '/backend/notallowed',
+    };
+
+    expect(() => pathWhitelistFilter(req)).toThrow(
+      'Path /notallowed is not whitelisted.',
+    );
+  });
+
+  it('should throw error for an empty path', () => {
+    const req = {
+      originalUrl: '/backend/',
+    };
+
+    expect(() => pathWhitelistFilter(req)).toThrow(
+      'Path / is not whitelisted.',
+    );
+  });
+
+  it('should throw error for undefined paths', () => {
+    const req = {
+      originalUrl: '',
+    };
+
+    expect(() => pathWhitelistFilter(req)).toThrow('Path  is not whitelisted.');
+  });
+});
+
+describe('localIpFilter', () => {
+  it('should allow a public IP address', () => {
+    const headersData = {
+      targetApiServer: {
+        host: '203.0.113.1',
+      },
+    };
+
+    expect(() => localIpFilter({}, headersData)).not.toThrow();
+  });
+
+  it('should throw an error for a local IP address in the 10.0.0.0/8 range', () => {
+    const headersData = {
+      targetApiServer: {
+        host: '10.0.0.1',
+      },
+    };
+
+    expect(() => localIpFilter({}, headersData)).toThrow(
+      'Local IP addresses are not allowed.',
+    );
+  });
+
+  it('should throw an error for a local IP address in the 172.16.0.0/12 range', () => {
+    const headersData = {
+      targetApiServer: {
+        host: '172.16.0.1',
+      },
+    };
+
+    expect(() => localIpFilter({}, headersData)).toThrow(
+      'Local IP addresses are not allowed.',
+    );
+  });
+
+  it('should throw an error for a local IP address in the 192.168.0.0/16 range', () => {
+    const headersData = {
+      targetApiServer: {
+        host: '192.168.1.1',
+      },
+    };
+
+    expect(() => localIpFilter({}, headersData)).toThrow(
+      'Local IP addresses are not allowed.',
+    );
+  });
+
+  it('should throw an error for a local IP address in the 169.254.0.0/16 range', () => {
+    const headersData = {
+      targetApiServer: {
+        host: '169.254.1.1',
+      },
+    };
+
+    expect(() => localIpFilter({}, headersData)).toThrow(
+      'Local IP addresses are not allowed.',
+    );
+  });
+
+  it('should throw an error for a local cluster address', () => {
+    const headersData = {
+      targetApiServer: {
+        host: 'myservice.cluster.local',
+      },
+    };
+
+    expect(() => localIpFilter({}, headersData)).toThrow(
+      'Local IP addresses are not allowed.',
+    );
+  });
+
+  it('should allow an external cluster address', () => {
+    const headersData = {
+      targetApiServer: {
+        host: 'myservice.cluster.external',
+      },
+    };
+
+    expect(() => localIpFilter({}, headersData)).not.toThrow();
+  });
+});
