a4adad4bb260e6915a574e68de00f0c67d614438
mrCherry97 <mwisnia97@gmail.com>
2024-10-14 14:38:35 +0200

    tests: add accessibility test for performance panel

---
+++ tests/integration/tests/accessibility/test-performance.spec.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+import config from '../../config';
+
+context('Accessibility test Cluster list and overview', () => {
+  Cypress.skipAfterFail();
+
+  before(() => {
+    cy.handleExceptions();
+    cy.setUpContinuum('continuum/continuum.conf.js');
+    cy.loginAndSelectCluster();
+  });
+
+  it('Acc test with performance panel open', () => {
+    cy.get('[title="Profile"]').click();
+
+    cy.get('ui5-menu-li:visible')
+      .contains('Preferences')
+      .click({ force: true });
+
+    cy.runAllAccessibilityTests()
+      .printAccessibilityTestResults()
+      .submitAccessibilityConcernsToAMP(Cypress.env('AMP_REPORT_NAME'));
+  });
+});
