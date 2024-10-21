cb2c944c6e6e70505af8af67ec5760dcf453407a
Oliwia Gowor <oliwiagowor@gmail.com>
2024-10-21 10:35:03 +0200

    cleanup

--- src/components/KymaModules/components/ModuleStatus.tsx
+++ src/components/KymaModules/components/ModuleStatus.tsx
@@ -2,28 +2,24 @@ import { StatusBadge } from 'shared/components/StatusBadge/StatusBadge';
 import { useModuleStatus } from '../support';
 
 export const ModuleStatus = ({ resource }: any) => {
-  const { data: moduleStatus } = useModuleStatus(resource);
+  const { data: status } = useModuleStatus(resource);
 
-  const moduleState = moduleStatus?.state || 'Unknown';
-  const moduleMessage = moduleStatus?.description;
+  const moduleState = status?.state || 'Unknown';
+  const moduleMessage = status?.description;
 
   return (
-    <div style={{ display: 'flex', gap: '0.5rem' }}>
-      {moduleState && (
-        <StatusBadge
-          resourceKind="kymas"
-          type={
-            moduleState === 'Ready'
-              ? 'Success'
-              : moduleState === 'Processing' || moduleState === 'Deleting'
-              ? 'None'
-              : moduleState || 'None'
-          }
-          tooltipContent={moduleMessage}
-        >
-          {moduleState}
-        </StatusBadge>
-      )}
-    </div>
+    <StatusBadge
+      resourceKind="kymas"
+      type={
+        moduleState === 'Ready'
+          ? 'Success'
+          : moduleState === 'Processing' || moduleState === 'Deleting'
+          ? 'None'
+          : moduleState || 'None'
+      }
+      tooltipContent={moduleMessage}
+    >
+      {moduleState}
+    </StatusBadge>
   );
 };
