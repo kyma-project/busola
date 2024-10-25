dcb50a5e20a36b0a27efa35755c89a751ee3d319
Oliwia Gowor <oliwiagowor@gmail.com>
2024-10-25 16:20:00 +0200

    fix: adjust dynamicPage

--- src/shared/components/DynamicPageComponent/DynamicPageComponent.js
+++ src/shared/components/DynamicPageComponent/DynamicPageComponent.js
@@ -2,17 +2,19 @@ import PropTypes from 'prop-types';
 import {
   Button,
   DynamicPage,
-  DynamicPageHeader,
-  DynamicPageTitle,
   FlexBox,
   ObjectPage,
+  ObjectPageHeader,
   ObjectPageSection,
+  ObjectPageTitle,
+  DynamicPageHeader,
+  DynamicPageTitle,
   Title,
 } from '@ui5/webcomponents-react';
 
 import './DynamicPageComponent.scss';
-import { spacing } from '@ui5/webcomponents-react-base';
-import { useEffect, useState } from 'react';
+import { spacing } from 'shared/helpers/spacing';
+import { useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useRecoilState } from 'recoil';
 import { columnLayoutState } from 'state/columnLayoutAtom';
@@ -20,6 +22,40 @@ import { HintButton } from '../DescriptionHint/DescriptionHint';
 import { isResourceEditedState } from 'state/resourceEditedAtom';
 import { isFormOpenState } from 'state/formOpenAtom';
 import { handleActionIfFormOpen } from '../UnsavedMessageBox/helpers';
+
+const useGetHeaderHeight = dynamicPageRef => {
+  const [headerHeight, setHeaderHeight] = useState(undefined);
+  useEffect(() => {
+    const headerObserver = new ResizeObserver(([header]) => {
+      setHeaderHeight(header.contentRect.height);
+    });
+    if (dynamicPageRef.current) {
+      // wait for the custom element to be defined (adjust the tag-name if you're using the scoping feature)
+      void customElements.whenDefined('ui5-dynamic-page').then(() => {
+        const { shadowRoot } = dynamicPageRef.current;
+
+        // wait for the shadowRoot to be populated
+        const shadowRootObserver = new MutationObserver(() => {
+          const header = shadowRoot.querySelector('header');
+          if (header) {
+            shadowRootObserver.disconnect();
+            headerObserver.observe(header);
+          }
+        });
+
+        if (shadowRoot && shadowRoot.childElementCount) {
+          headerObserver.observe(shadowRoot.querySelector('header'));
+        } else {
+          shadowRootObserver.observe(shadowRoot, { childList: true });
+        }
+      });
+    }
+    return () => {
+      headerObserver.disconnect();
+    };
+  }, []);
+  return headerHeight;
+};
 
 const Column = ({ title, children, columnSpan, image, style = {} }) => {
   const styleComputed = { gridColumn: columnSpan, ...style };
@@ -60,6 +96,9 @@ export const DynamicPageComponent = ({
   const [isFormOpen, setIsFormOpen] = useRecoilState(isFormOpenState);
   const [selectedSectionIdState, setSelectedSectionIdState] = useState('view');
 
+  const dynamicPageRef = useRef(null);
+  const headerHeight = useGetHeaderHeight(dynamicPageRef);
+
   const handleColumnClose = () => {
     window.history.pushState(
       window.history.state,
@@ -91,12 +130,12 @@ export const DynamicPageComponent = ({
         });
   };
 
-  const headerTitle = (
-    <DynamicPageTitle
+  const headerTitle = inlineEditForm ? (
+    <ObjectPageTitle
       style={title === 'Clusters Overview' ? { display: 'none' } : null}
       header={
         <FlexBox alignItems="Center">
-          <Title level="H3" className="bold-title">
+          <Title level="H3" size="H3" className="bold-title">
             {title}
           </Title>
           {protectedResource && (
@@ -115,7 +154,134 @@ export const DynamicPageComponent = ({
           )}
         </FlexBox>
       }
-      actions={
+      actionsBar={
+        <>
+          {actions && (
+            <div className="page-header__actions">
+              {actions}
+              {(window.location.search.includes('layout') ||
+                (!window.location.search.includes('layout') &&
+                  layoutColumn?.showCreate?.resourceType)) &&
+              layoutNumber !== 'StartColumn' ? (
+                <span className="separator" />
+              ) : null}
+            </div>
+          )}
+          {window.location.search.includes('layout') ||
+          (!window.location.search.includes('layout') &&
+            layoutColumn?.showCreate?.resourceType) ? (
+            layoutColumn.layout !== 'OneColumn' ? (
+              layoutNumber !== 'StartColumn' ? (
+                <>
+                  {layoutColumn.layout === 'TwoColumnsMidExpanded' ||
+                  ((layoutColumn.layout === 'ThreeColumnsMidExpanded' ||
+                    layoutColumn.layout === 'ThreeColumnsEndExpanded') &&
+                    layoutNumber !== 'MidColumn') ? (
+                    <Button
+                      accessibleName="enter-full-screen"
+                      design="Transparent"
+                      icon="full-screen"
+                      onClick={() => {
+                        const newLayout =
+                          layoutNumber === 'MidColumn'
+                            ? 'MidColumnFullScreen'
+                            : 'EndColumnFullScreen';
+                        setLayoutColumn({
+                          ...layoutColumn,
+                          layout: newLayout,
+                        });
+                        window.history.pushState(
+                          window.history.state,
+                          '',
+                          `${window.location.pathname}${
+                            layoutColumn?.showCreate?.resourceType
+                              ? ''
+                              : '?layout=' + newLayout
+                          }`,
+                        );
+                      }}
+                    />
+                  ) : null}
+                  {layoutColumn.layout === 'MidColumnFullScreen' ||
+                  layoutColumn.layout === 'EndColumnFullScreen' ? (
+                    <Button
+                      accessibleName="close-full-screen"
+                      design="Transparent"
+                      icon="exit-full-screen"
+                      onClick={() => {
+                        const newLayout =
+                          layoutNumber === 'MidColumn'
+                            ? layoutColumn.endColumn === null
+                              ? 'TwoColumnsMidExpanded'
+                              : 'ThreeColumnsMidExpanded'
+                            : 'ThreeColumnsEndExpanded';
+                        setLayoutColumn({
+                          ...layoutColumn,
+                          layout: newLayout,
+                        });
+                        window.history.pushState(
+                          window.history.state,
+                          '',
+                          `${window.location.pathname}${
+                            layoutColumn?.showCreate?.resourceType
+                              ? ''
+                              : '?layout=' + newLayout
+                          }`,
+                        );
+                      }}
+                    />
+                  ) : null}
+                  <Button
+                    accessibleName="close-column"
+                    design="Transparent"
+                    icon="decline"
+                    onClick={() => {
+                      handleActionIfFormOpen(
+                        isResourceEdited,
+                        setIsResourceEdited,
+                        isFormOpen,
+                        setIsFormOpen,
+                        () => handleColumnClose(),
+                      );
+                    }}
+                  />
+                </>
+              ) : null
+            ) : null
+          ) : null}
+        </>
+      }
+    />
+  ) : (
+    <DynamicPageTitle
+      style={title === 'Clusters Overview' ? { display: 'none' } : null}
+      heading={
+        <FlexBox alignItems="Center" justifyContent="Center">
+          <Title
+            style={{ fontSize: 'var(--sapObjectHeader_Title_FontSize)' }}
+            level="H3"
+            size="H3"
+            className="bold-title"
+          >
+            {title}
+          </Title>
+          {protectedResource && (
+            <span style={spacing.sapUiTinyMarginBegin}>
+              {protectedResourceWarning}
+            </span>
+          )}
+          {description && (
+            <HintButton
+              style={spacing.sapUiTinyMargin}
+              setShowTitleDescription={setShowTitleDescription}
+              showTitleDescription={showTitleDescription}
+              description={description}
+              ariaTitle={title}
+            />
+          )}
+        </FlexBox>
+      }
+      actionsBar={
         <>
           {actions && (
             <div className="page-header__actions">
@@ -215,14 +381,27 @@ export const DynamicPageComponent = ({
     />
   );
 
-  const headerContent =
-    title !== 'Clusters Overview' && children ? (
-      <DynamicPageHeader className="header-wrapper">
-        <section className={`column-wrapper ${columnWrapperClassName || ''}`}>
-          {children}
-        </section>
-      </DynamicPageHeader>
-    ) : null;
+  const headerContent = inlineEditForm ? (
+    <>
+      {title !== 'Clusters Overview' && children ? (
+        <ObjectPageHeader className="header-wrapper">
+          <section className={`column-wrapper ${columnWrapperClassName || ''}`}>
+            {children}
+          </section>
+        </ObjectPageHeader>
+      ) : null}
+    </>
+  ) : (
+    <>
+      {title !== 'Clusters Overview' && children ? (
+        <DynamicPageHeader className="header-wrapper">
+          <section className={`column-wrapper ${columnWrapperClassName || ''}`}>
+            {children}
+          </section>
+        </DynamicPageHeader>
+      ) : null}
+    </>
+  );
 
   const [stickyHeaderHeight, setStickyHeaderHeight] = useState(0);
 
@@ -243,11 +422,11 @@ export const DynamicPageComponent = ({
       <ObjectPage
         mode="IconTabBar"
         className={`page-header ${className}`}
+        headerPinned
         alwaysShowContentHeader
-        showHideHeaderButton={false}
-        headerContentPinnable={false}
-        headerTitle={headerTitle}
-        headerContent={customHeaderContent ?? headerContent}
+        hidePinButton={true}
+        titleArea={headerTitle}
+        headerArea={customHeaderContent ?? headerContent}
         selectedSectionId={selectedSectionIdState}
         onBeforeNavigate={e => {
           if (isFormOpen.formOpen) {
@@ -298,19 +477,13 @@ export const DynamicPageComponent = ({
   return (
     <DynamicPage
       className="page-header"
-      alwaysShowContentHeader
-      showHideHeaderButton={false}
-      headerContentPinnable={false}
-      backgroundDesign="Solid"
-      headerTitle={headerTitle}
-      headerContent={headerContent}
-      footer={footer}
+      hidePinButton
+      titleArea={headerTitle}
+      headerArea={headerContent}
+      footerArea={footer}
+      ref={dynamicPageRef}
     >
-      {({ stickyHeaderHeight }) => {
-        return typeof content === 'function'
-          ? content(stickyHeaderHeight)
-          : content;
-      }}
+      {typeof content === 'function' ? content(headerHeight) : content}
     </DynamicPage>
   );
 };
