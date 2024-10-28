b8a1c3896b5b1a2150d964cc9f8f64083a6ec18e
Oliwia Gowor <oliwiagowor@gmail.com>
2024-10-28 09:39:01 +0100

    fix: not selectable tokens

--- src/shared/components/Tokens.js
+++ src/shared/components/Tokens.js
@@ -1,6 +1,4 @@
-import React from 'react';
-
-import { Token } from '@ui5/webcomponents-react';
+import { Tag } from '@ui5/webcomponents-react';
 import { EMPTY_TEXT_PLACEHOLDER } from 'shared/constants';
 
 import { spacing } from 'shared/helpers/spacing';
@@ -9,12 +7,15 @@ const Tokens = ({ tokens }) => (
   <>
     {tokens?.length
       ? tokens.map(scope => (
-          <Token
+          <Tag
             key={scope}
             style={spacing.sapUiTinyMarginEnd}
-            readOnly
-            text={scope}
-          />
+            design="Set2"
+            colorScheme="9"
+            hideStateIcon
+          >
+            {scope}
+          </Tag>
         ))
       : EMPTY_TEXT_PLACEHOLDER}
   </>
