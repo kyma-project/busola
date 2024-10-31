487264bf77ed0e77e37528beed8636efc3e1757b
akucharska <agata.kucharska@sap.com>
2024-10-31 07:27:50 +0100

    Clear externals

--- src/components/Externals/Externals.js
+++ src/components/Externals/Externals.js
@@ -1,13 +1,11 @@
 export default function Externals() {
   return (
-    <>
-      <iframe
-        className="lolo"
-        id="iframe"
-        width="100%"
-        height="50%"
-        src="http://localhost:3001/backend/api/v1/namespaces/kyma-system/services/btp-manager-metrics-service:8080/proxy/"
-      ></iframe>
-    </>
+    <iframe
+      id="custom-extension"
+      title="Custom extesnion"
+      width="100%"
+      height="50%"
+      src="http://localhost:3001/backend/api/v1/namespaces/kyma-system/services/btp-manager-metrics-service:8080/proxy/"
+    />
   );
 }
