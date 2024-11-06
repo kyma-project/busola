f02dc83329b89971e3a48449ae002a31f8ab21d7
chriskari <chriskari@gmx.de>
2024-11-06 15:14:33 +0100

    feat: remove decoding and translationpaths from NumberRenderer, adjust ComboboxInput to handle numeric values

--- src/components/Extensibility/components-form/NumberRenderer.js
+++ src/components/Extensibility/components-form/NumberRenderer.js
@@ -1,8 +1,3 @@
-import React from 'react';
-import { useState } from 'react';
-import { Button } from '@ui5/webcomponents-react';
-import { useTranslation } from 'react-i18next';
-
 import { ResourceForm } from 'shared/ResourceForm';
 import * as Inputs from 'shared/ResourceForm/inputs';
 import {
@@ -12,22 +7,16 @@ import {
 
 export function NumberRenderer({
   onChange,
-  onKeyDown,
   value,
   schema,
   storeKeys,
   required,
-  compact,
   placeholder,
   editMode,
-  ...props
 }) {
-  const { t } = useTranslation();
-  const { tFromStoreKeys, t: tExt, exists } = useGetTranslation();
+  const { tFromStoreKeys, t: tExt } = useGetTranslation();
 
   const schemaPlaceholder = schema.get('placeholder');
-  const decodable = schema.get('decodable');
-  const [decoded, setDecoded] = useState(true);
 
   const numberProps = Object.fromEntries(
     ['min', 'max'].map(prop => [prop, schema.get(prop)]),
@@ -37,11 +26,6 @@ export function NumberRenderer({
 
   const getTypeSpecificProps = () => {
     if (schema.get('enum')) {
-      const translationPath = storeKeys
-        .toArray()
-        .filter(el => typeof el === 'number')
-        .join('.');
-
       let enumOptions = schema.toJS().enum;
       // if there's only 1 option, it will be not in an array
       if (typeof enumOptions === 'number') {
@@ -55,52 +39,20 @@ export function NumberRenderer({
         }
       }
 
-      const displayOptions = enumOptions.map(option => {
-        if (typeof option === 'number') {
-          return {
-            key: option,
-            text: exists(option)
-              ? tExt(option)
-              : exists(`${translationPath}.${option}`)
-              ? tExt(`${translationPath}.${option}`)
-              : option,
-          };
-        }
+      const displayOptions = enumOptions
+        .filter(option => typeof option === 'number')
+        .map(option => ({
+          key: option,
+          text: option,
+        }));
 
-        return {
-          key: option.key,
-          text: option.name
-            ? tExt(option.name)
-            : exists(`${translationPath}.${option.key}`)
-            ? tExt(`${translationPath}.${option.key}`)
-            : option.key,
-        };
-      });
-
-      return { input: Inputs.ComboboxInput, options: displayOptions };
-    } else if (!decodable) {
-      return { input: Inputs.Text };
-    } else {
       return {
-        input: params => (
-          <>
-            <Inputs.Text {...params} />
-            {decodable && (
-              <div className="bsl-col-md--1">
-                <Button
-                  design="Transparent"
-                  icon={decoded ? 'hide' : 'show'}
-                  onClick={() => setDecoded(!decoded)}
-                >
-                  {decoded
-                    ? t('secrets.buttons.encode')
-                    : t('secrets.buttons.decode')}
-                </Button>
-              </div>
-            )}
-          </>
-        ),
+        input: Inputs.ComboboxInput,
+        options: displayOptions,
+        isNumeric: true,
       };
+    } else {
+      return { input: Inputs.Number };
     }
   };
 
@@ -120,8 +72,6 @@ export function NumberRenderer({
       label={tFromStoreKeys(storeKeys, schema)}
       placeholder={tExt(schemaPlaceholder) || tExt(placeholder)}
       data-testid={storeKeys.join('.') || tFromStoreKeys(storeKeys, schema)}
-      input={Inputs.Number}
-      compact={compact}
       disabled={disableOnEdit && editMode}
       {...numberProps}
       {...getTypeSpecificProps()}
