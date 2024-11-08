a550b7a3bb79edc115589d81439d1f59424d3eba
Oliwia Gowor <oliwiagowor@gmail.com>
2024-11-08 11:25:40 +0100

    fix: adjust spacing

--- src/shared/components/Tokens.js
+++ src/shared/components/Tokens.js
@@ -1,15 +1,13 @@
 import { Tag } from '@ui5/webcomponents-react';
 import { EMPTY_TEXT_PLACEHOLDER } from 'shared/constants';
 
-import { spacing } from 'shared/helpers/spacing';
-
 const Tokens = ({ tokens }) => (
   <>
     {tokens?.length
       ? tokens.map(scope => (
           <Tag
             key={scope}
-            style={spacing.sapUiTinyMarginEnd}
+            className="sap-margin-end-tiny"
             design="Set2"
             colorScheme="9"
             hideStateIcon
