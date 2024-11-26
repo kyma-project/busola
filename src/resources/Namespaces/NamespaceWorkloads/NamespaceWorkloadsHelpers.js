5c02d34d52c8e73be9eaa48da75eb70d6282eac7
akucharska <agata.kucharska@sap.com>
2024-11-26 12:31:38 +0100

    Fix deployments card

--- src/resources/Namespaces/NamespaceWorkloads/NamespaceWorkloadsHelpers.js
+++ src/resources/Namespaces/NamespaceWorkloads/NamespaceWorkloadsHelpers.js
@@ -1,12 +1,12 @@
 import { calculatePodState } from 'resources/Pods/PodStatus';
 
 export function getHealthyReplicasCount(resource) {
-  return resource?.filter(
-    r =>
-      r.status.replicas &&
-      r.status.readyReplicas &&
-      r.status.replicas === r.status.readyReplicas,
-  )?.length;
+  return resource?.filter(r => {
+    const running = r.status.readyReplicas || 0;
+    const expected = r.status.replicas || r.spec.replicas || 0;
+
+    return running === expected;
+  })?.length;
 }
 
 export const PodStatusCounterKey = {
