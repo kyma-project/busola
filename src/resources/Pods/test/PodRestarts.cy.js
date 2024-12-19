0dc19a3cfe583291414569655e43cd190fdbfdb1
chriskari <chriskari@gmx.de>
2024-12-19 23:19:39 +0100

    migrated podrestarts

---
+++ src/resources/Pods/test/PodRestarts.cy.js
@@ -0,0 +1,21 @@
+/* global cy */
+import PodRestarts from '../PodRestarts';
+
+describe('PodRestarts', () => {
+  it('Shows 0 for no statuses', () => {
+    cy.mount(<PodRestarts statuses={[]} />);
+
+    cy.get('[role="status"]').should('contain.text', '0');
+  });
+
+  it('Sums up restart statuses', () => {
+    const statuses = [
+      { name: 'container-1', restartCount: 10 },
+      { name: 'container-2', restartCount: 3 },
+    ];
+
+    cy.mount(<PodRestarts statuses={statuses} />);
+
+    cy.get('[role="status"]').should('contain.text', '13');
+  });
+});
