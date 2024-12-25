1996bf8fbf01e4cb643a3b5bd60b0265d289220d
chriskari <chriskari@gmx.de>
2024-12-25 12:24:37 +0100

    migrated SideDrawer

---
+++ src/shared/components/SideDrawer/test/SideDrawer.cy.js
@@ -0,0 +1,24 @@
+/* global cy */
+import { SideDrawer } from '../SideDrawer';
+
+describe('SideDrawer', () => {
+  const testText1 = 'hi there';
+  const testContent1 = <p>{testText1}</p>;
+
+  const testText2 = 'oh, hello';
+  const testContent2 = <h3>{testText2}</h3>;
+
+  it('Renders content', () => {
+    cy.mount(<SideDrawer isOpenInitially={true}>{testContent1}</SideDrawer>);
+
+    cy.contains(testText1).should('exist');
+  });
+
+  it('Renders bottom content', () => {
+    cy.mount(
+      <SideDrawer bottomContent={testContent2}>{testContent1}</SideDrawer>,
+    );
+
+    cy.contains(testText2).should('exist');
+  });
+});
