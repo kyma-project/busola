1df8085f28db0a1b0dd91135f9359a523ad1e312
Damian Badura <45110612+dbadura@users.noreply.github.com>
2025-01-22 09:07:07 +0100

    fix: fix displaying namespace resource (#3617)
    * fix displaying namespace resource
    
    * remove console.log

--- src/resources/Namespaces/ResourcesUsage.js
+++ src/resources/Namespaces/ResourcesUsage.js
@@ -139,7 +139,7 @@ export const ResourcesUsage = ({ namespace }) => {
       pollingInterval: 3300,
     },
   );
-  if (resourceQuotas?.length < 1) return null;
+  if (!resourceQuotas || resourceQuotas?.length < 1) return null;
   return (
     <>
       <div className="item-wrapper tall">
