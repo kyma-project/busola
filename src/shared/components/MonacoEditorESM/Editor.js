cfd9d989c8256fc1588c17be63cacbd4c2f2a3bc
Oliwia Gowor <oliwiagowor@gmail.com>
2025-02-10 10:50:07 +0100

    fix: conditions in useEffect

--- src/shared/components/MonacoEditorESM/Editor.js
+++ src/shared/components/MonacoEditorESM/Editor.js
@@ -67,18 +67,13 @@ export function Editor({
 
   // update editor when was error
   useEffect(() => {
-    if (prevValueRef.current !== value && editorInstance && error) {
-      editorInstance.setValue(value);
-      prevValueRef.current = value;
+    if (prevValueRef.current !== value && editorInstance) {
+      if (error || readOnly) {
+        editorInstance.setValue(value);
+        prevValueRef.current = value;
+      }
     }
-  }, [value, editorInstance, error]);
-
-  useEffect(() => {
-    if (prevValueRef.current !== value && readOnly) {
-      editorInstance.setValue(value);
-      prevValueRef.current = value;
-    }
-  }, [value, editorInstance, readOnly]);
+  }, [value, editorInstance, error, readOnly]);
 
   useUpdateValueOnParentChange({
     updateValueOnParentChange,
