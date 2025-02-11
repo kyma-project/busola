8702a5d6c8124ebf4951c608b54a7dc883e543ab
mrCherry97 <mwisnia97@gmail.com>
2025-02-11 14:27:29 +0100

    test

--- .github/scripts/create_changelog.sh
+++ .github/scripts/create_changelog.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash	
+
+PREVIOUS_RELEASE=$2 # for testability	
+
+# standard bash error handling
 set -o nounset  # treat unset variables as an error and exit immediately.
 set -o errexit  # exit immediately when a command fails.
 set -E          # needs to be set if we want the ERR trap
