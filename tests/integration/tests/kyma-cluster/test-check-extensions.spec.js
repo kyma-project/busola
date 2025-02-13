3dc556145044de490f3554e3bda4cdabac964432
mrCherry97 <mwisnia97@gmail.com>
2025-02-13 11:28:41 +0100

    me and the devil

--- tests/integration/tests/kyma-cluster/test-check-extensions.spec.js
+++ tests/integration/tests/kyma-cluster/test-check-extensions.spec.js
@@ -4,9 +4,7 @@ import { useCategory } from '../../support/helpers';
 context('Test Cluster Extensions views', () => {
   before(() => {
     cy.loginAndSelectCluster();
-    cy.wait(500);
-    cy.reload();
-    cy.wait(1000);
+    cy.wait(10000);
   });
 
   // Telemetry
