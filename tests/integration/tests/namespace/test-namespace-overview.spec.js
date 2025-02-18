2bfc31b28c054f772f674bd17248ac83753dfee0
Pietocha <konrad.pietocha@sap.com>
2025-02-18 08:34:58 +0100

    Test correction

--- tests/integration/tests/namespace/test-namespace-overview.spec.js
+++ tests/integration/tests/namespace/test-namespace-overview.spec.js
@@ -39,17 +39,17 @@ context(
     });
 
     it('checks the visibility of charts', () => {
-      cy.get('body').then($body => {
+      cy.get('ui5-dynamic-page').then($parent => {
         // Check if the loading spinner is visible.
-        if ($body.find('[aria-label="Loading"]').length) {
+        if ($parent.find('[aria-label="Loading"]').length) {
           cy.get('ui5-busy-indicator').should('be.visible');
           // Check if the error message is visible when error occurs.
-        } else if ($body.find('.pods-metrics-error').length) {
+        } else if ($parent.find('.pods-metrics-error').length) {
           cy.get(
             'ui5-card-header[title-text="Error while loading memory consumption data"]',
           ).should('be.visible');
           // Check if the proper charts are visible.
-        } else if ($body.find('.radial-chart-card').length) {
+        } else if ($parent.find('.radial-chart-card').length) {
           cy.contains('CPU Usage').should('be.visible');
           cy.contains('Memory Usage').should('be.visible');
           // If there is no any pods usage on the namespace.
