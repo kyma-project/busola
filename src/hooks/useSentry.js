9a95939fb6250c532f5f856dfea2ccb8b7305572
mrCherry97 <mwisnia97@gmail.com>
2025-02-20 09:55:59 +0100

    additional improvments, thanks chriskari

--- src/hooks/useSentry.js
+++ src/hooks/useSentry.js
@@ -1,36 +1,37 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import * as Sentry from '@sentry/react';
 import { useFeature } from './useFeature';
 
+const initSentry = dsn => {
+  Sentry.init({
+    dsn,
+    release: 'busola',
+    integrations: [
+      new Sentry.browserTracingIntegration(),
+      Sentry.replayIntegration(),
+    ],
+    tracesSampleRate: 1.0,
+    // Session Replay
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+  });
+};
+
 export function useSentry() {
-  const [dsn, setDsn] = useState();
+  const [dsn, setDsn] = useState(null);
+  const feature = useFeature('SENTRY');
 
-  const initSentry = dsn => {
-    Sentry.init({
-      dsn,
-      release: 'busola',
-      integrations: [
-        new Sentry.browserTracingIntegration(),
-        Sentry.replayIntegration(),
-      ],
-      tracesSampleRate: 1.0,
-      // Session Replay
-      replaysSessionSampleRate: 0.1,
-      replaysOnErrorSampleRate: 1.0,
-    });
-  };
-
-  try {
-    const feature = useFeature('SENTRY') || {};
-    if (feature.isEnabled && feature.config?.dsn) {
-      const nextDsn = feature.config.dsn;
-
-      if (nextDsn !== dsn) {
-        setDsn(nextDsn);
-        initSentry(nextDsn);
+  useEffect(() => {
+    try {
+      if (feature?.isEnabled && feature?.config?.dsn) {
+        const nextDsn = feature.config.dsn;
+        if (nextDsn !== dsn) {
+          setDsn(nextDsn);
+          initSentry(nextDsn);
+        }
       }
+    } catch (e) {
+      console.warn('Sentry not enabled due to error', e);
     }
-  } catch (e) {
-    console.warn('Sentry not enabled due to error', e);
-  }
+  }, [feature, dsn]);
 }
