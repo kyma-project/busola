ea4d5b6689ae8ff2020be22313cc575f8a066ddf
Oliwia Gowor <oliwiagowor@gmail.com>
2025-03-14 09:36:10 +0100

    fix: adjust loader and label

--- src/components/KymaCompanion/components/Chat/messages/TasksList.tsx
+++ src/components/KymaCompanion/components/Chat/messages/TasksList.tsx
@@ -1,11 +1,10 @@
 import {
   BusyIndicator,
   FlexBox,
+  Label,
   ObjectStatus,
-  Text,
 } from '@ui5/webcomponents-react';
 import { useTranslation } from 'react-i18next';
-import './TasksList.scss';
 
 export default function TasksList({
   className,
@@ -37,9 +36,14 @@ export default function TasksList({
                       large={true}
                     />
                   ) : (
-                    <span className="ai-steps-loader" />
+                    <BusyIndicator
+                      active
+                      size="S"
+                      delay={0}
+                      className="ai-busy-indicator"
+                    />
                   )}
-                  <Text className="text">{task?.task_name}</Text>
+                  <Label className="text">{task?.task_name}</Label>
                   <div className="loading-status"></div>
                 </FlexBox>
               );
@@ -51,10 +55,15 @@ export default function TasksList({
               alignItems="Center"
               className="loading-item"
             >
-              <span className="ai-steps-loader" />
-              <Text className="text">
+              <BusyIndicator
+                active
+                size="S"
+                delay={0}
+                className="ai-busy-indicator"
+              />
+              <Label className="text">
                 {t('kyma-companion.opener.preparing-final-answer')}
-              </Text>
+              </Label>
               <div className="loading-status"></div>
             </FlexBox>
           )}
