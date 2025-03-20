9229f3eb436cd89de74f7b03839312f282923f1e
chriskari <chriskari@gmx.de>
2025-03-20 23:46:04 +0100

    fix: use flex-wrap for layout of cards in resource details

--- src/shared/components/ResourceStatusCard/ResourceStatusCard.js
+++ src/shared/components/ResourceStatusCard/ResourceStatusCard.js
@@ -13,36 +13,34 @@ export function ResourceStatusCard({
   const { t } = useTranslation();
 
   return (
-    <div className="sap-margin-x-small">
-      <Card
-        header={
-          <div className="resource-status-card__header">
-            <CardHeader titleText={t('common.headers.status')} />
-            <div className="header__status-badge">{statusBadge}</div>
+    <Card
+      header={
+        <div className="resource-status-card__header">
+          <CardHeader titleText={t('common.headers.status')} />
+          <div className="header__status-badge">{statusBadge}</div>
+        </div>
+      }
+      className="resource-status-card"
+    >
+      {customColumns && (
+        <div className="resource-status-card__details-grid sap-margin-small sap-margin-top-tiny">
+          {customColumns}
+        </div>
+      )}
+      {customColumnsLong && (
+        <div className="sap-margin-small sap-margin-top-tiny">
+          {customColumnsLong}
+        </div>
+      )}
+      {conditions && (
+        <>
+          <div className="title bsl-has-color-status-4 sap-margin-x-small">
+            {`${t('common.headers.conditions')}:`}
           </div>
-        }
-        className="resource-status-card"
-      >
-        {customColumns && (
-          <div className="resource-status-card__details-grid sap-margin-small sap-margin-top-tiny">
-            {customColumns}
-          </div>
-        )}
-        {customColumnsLong && (
-          <div className="sap-margin-small sap-margin-top-tiny">
-            {customColumnsLong}
-          </div>
-        )}
-        {conditions && (
-          <>
-            <div className="title bsl-has-color-status-4 sap-margin-x-small">
-              {`${t('common.headers.conditions')}:`}
-            </div>
-            <ConditionList conditions={conditions} />
-          </>
-        )}
-        {customConditionsComponent}
-      </Card>
-    </div>
+          <ConditionList conditions={conditions} />
+        </>
+      )}
+      {customConditionsComponent}
+    </Card>
   );
 }
