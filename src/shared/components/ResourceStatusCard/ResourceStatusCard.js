4cc7d2b05c5bdadcb6640ae1ae3a1c3fafbc51cc
chriskari <chriskari@gmx.de>
2025-03-20 23:53:47 +0100

    fix: keep div wrapper of status card

--- src/shared/components/ResourceStatusCard/ResourceStatusCard.js
+++ src/shared/components/ResourceStatusCard/ResourceStatusCard.js
@@ -13,34 +13,36 @@ export function ResourceStatusCard({
   const { t } = useTranslation();
 
   return (
-    <Card
-      header={
-        <div className="resource-status-card__header">
-          <CardHeader titleText={t('common.headers.status')} />
-          <div className="header__status-badge">{statusBadge}</div>
-        </div>
-      }
-      className="resource-status-card"
-    >
-      {customColumns && (
-        <div className="resource-status-card__details-grid sap-margin-small sap-margin-top-tiny">
-          {customColumns}
-        </div>
-      )}
-      {customColumnsLong && (
-        <div className="sap-margin-small sap-margin-top-tiny">
-          {customColumnsLong}
-        </div>
-      )}
-      {conditions && (
-        <>
-          <div className="title bsl-has-color-status-4 sap-margin-x-small">
-            {`${t('common.headers.conditions')}:`}
+    <div>
+      <Card
+        header={
+          <div className="resource-status-card__header">
+            <CardHeader titleText={t('common.headers.status')} />
+            <div className="header__status-badge">{statusBadge}</div>
           </div>
-          <ConditionList conditions={conditions} />
-        </>
-      )}
-      {customConditionsComponent}
-    </Card>
+        }
+        className="resource-status-card"
+      >
+        {customColumns && (
+          <div className="resource-status-card__details-grid sap-margin-small sap-margin-top-tiny">
+            {customColumns}
+          </div>
+        )}
+        {customColumnsLong && (
+          <div className="sap-margin-small sap-margin-top-tiny">
+            {customColumnsLong}
+          </div>
+        )}
+        {conditions && (
+          <>
+            <div className="title bsl-has-color-status-4 sap-margin-x-small">
+              {`${t('common.headers.conditions')}:`}
+            </div>
+            <ConditionList conditions={conditions} />
+          </>
+        )}
+        {customConditionsComponent}
+      </Card>
+    </div>
   );
 }
