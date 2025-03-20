b903857b8e63bf5eeec718e813a4b1af04931f0f
mrCherry97 <mwisnia97@gmail.com>
2025-03-20 10:51:18 +0100

    fix undefined for resource and urlPath

--- src/components/Extensibility/helpers/getExtensibilityPath.ts
+++ src/components/Extensibility/helpers/getExtensibilityPath.ts
@@ -1,9 +1,7 @@
 import pluralize from 'pluralize';
 
 export const getExtensibilityPath = ({
-  urlPath,
   resource: { kind },
 }: {
-  urlPath?: string;
   resource: { kind: string };
-}) => urlPath ?? pluralize((kind ?? '').toLowerCase());
+}) => pluralize((kind ?? '').toLowerCase());
