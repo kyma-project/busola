ece33bd8fa41452b747b783994b45fb0e2d1db8a
Christian Karidas <105549337+chriskari@users.noreply.github.com>
2025-04-02 08:54:18 +0200

    feat: Error handling for companion (#3792)
    * feat: handle error when final chunk is received
    
    * feat: adjust errorMessage ui
    
    * feat: add function to retry previous prompt
    
    * remove handleError from followups, display error notices next to users messages
    
    * feat: error handling for followups
    
    * fix: typo

--- src/components/KymaCompanion/components/Chat/messages/Message.tsx
+++ src/components/KymaCompanion/components/Chat/messages/Message.tsx
@@ -1,14 +1,8 @@
-import { Text } from '@ui5/webcomponents-react';
+import { Icon, Text } from '@ui5/webcomponents-react';
+import { useTranslation } from 'react-i18next';
 import { formatMessage } from 'components/KymaCompanion/utils/formatMarkdown';
 import TasksList from './TasksList';
 import './Message.scss';
-
-interface MessageProps {
-  className: string;
-  messageChunks: MessageChunk[];
-  isLoading: boolean;
-  disableFormatting?: boolean;
-}
 
 export interface MessageChunk {
   event?: string;
@@ -24,30 +18,58 @@ export interface MessageChunk {
       }[];
       next: string;
     };
+    error?: string | null;
   };
 }
 
+interface MessageProps {
+  author: 'user' | 'ai';
+  messageChunks: MessageChunk[];
+  isLoading: boolean;
+  hasError: boolean;
+  isLatestMessage: boolean;
+  disableFormatting?: boolean;
+}
+
 export default function Message({
-  className,
+  author,
   messageChunks,
   isLoading,
+  hasError,
+  isLatestMessage,
   disableFormatting = false,
 }: MessageProps): JSX.Element {
+  const { t } = useTranslation();
   if (isLoading) {
     return <TasksList messageChunks={messageChunks} />;
   }
-  const text = messageChunks.slice(-1)[0]?.data?.answer?.content;
 
-  let segmentedText = null;
-  if (disableFormatting) {
-    segmentedText = text;
-  } else {
-    segmentedText = formatMessage(text);
-  }
+  const displayError =
+    hasError &&
+    ((author === 'user' && !isLatestMessage) ||
+      (author === 'ai' && isLatestMessage));
+
+  const finalChunk = messageChunks.at(-1);
+  const text = finalChunk?.data?.answer?.content ?? '';
+  const segmentedText = disableFormatting ? text : formatMessage(text);
+
+  const className = author === 'user' ? 'right-aligned' : 'left-aligned';
 
   return (
-    <div className={'message ' + className}>
-      <Text className="text">{segmentedText}</Text>
+    <div className={'message-container ' + className}>
+      <div className={`message ${className}${displayError ? ' error' : ''}`}>
+        <Text className="text">{segmentedText}</Text>
+      </div>
+      {displayError && (
+        <div className={'message-error ' + className}>
+          <Text className="error-text">
+            {author === 'user'
+              ? t('kyma-companion.error.chat-error')
+              : t('kyma-companion.error.suggestions-error')}
+          </Text>
+          <Icon name="error" design="Negative" className="error-icon" />
+        </div>
+      )}
     </div>
   );
 }
