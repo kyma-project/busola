b4f4eb64c9acca75b284ba188a5190730f674dfd
mrCherry97 <mwisnia97@gmail.com>
2025-04-09 11:01:03 +0200

    idk if this will fix, but locally works

--- tests/integration/support/navigate-to.js
+++ tests/integration/support/navigate-to.js
@@ -1,6 +1,6 @@
 Cypress.Commands.add('navigateTo', (leftNav, resource) => {
-  cy.wait(1000);
-  cy.getLeftNav()
+  cy.wait(500)
+    .getLeftNav()
     .get(`ui5-side-navigation-item[text="${leftNav}"]`)
     .as('btn-1')
     .should('be.visible');
