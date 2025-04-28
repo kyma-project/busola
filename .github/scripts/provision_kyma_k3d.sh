5b87bce676340514a288eed197e8bb93dddb73a3
Damian Badura <damian.badura@sap.com>
2025-04-28 11:02:41 +0200

    fix

--- .github/scripts/provision_kyma_k3d.sh
+++ .github/scripts/provision_kyma_k3d.sh
@@ -14,7 +14,7 @@ if [ ! -f "./bin/kyma" ]; then
 fi
 
 echo "Provisioning k3d cluster for Kyma"
-k3d registry create kyma-registry --port 5000
+k3d registry create kyma-registry.localhost --port 5000
 
 # kyma alpha deploy command expects a cluster with an internal k3d registry, so we provide one
-k3d cluster create kyma --kubeconfig-switch-context -p 80:80@loadbalancer -p 443:443@loadbalancer --registry-use kyma-registry
+k3d cluster create kyma --kubeconfig-switch-context -p 80:80@loadbalancer -p 443:443@loadbalancer --registry-use kyma-registry.localhost:5000
