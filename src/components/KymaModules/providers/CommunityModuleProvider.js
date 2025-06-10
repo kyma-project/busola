89fcbfd12049337e62c782266c70b857597bafc6
Pietocha <konrad.pietocha@sap.com>
2025-06-10 14:10:54 +0200

    Add initial changes for delete

--- src/components/KymaModules/providers/CommunityModuleProvider.js
+++ src/components/KymaModules/providers/CommunityModuleProvider.js
@@ -1,6 +1,14 @@
 import { createContext, useContext, useEffect, useState } from 'react';
+import { t } from 'i18next';
 import { useGetInstalledModules } from '../hooks';
 import { ModuleTemplatesContext } from './ModuleTemplatesProvider';
+import { createPortal } from 'react-dom';
+import { ModulesDeleteBox } from '../components/ModulesDeleteBox';
+import { checkSelectedModule } from '../support';
+import { Button } from '@ui5/webcomponents-react';
+import { cloneDeep } from 'lodash';
+import { useNotification } from 'shared/contexts/NotificationContext';
+import { useCreateResource } from 'shared/ResourceForm/useCreateResource';
 
 export const CommunityModuleContext = createContext({
   setOpenedModuleIndex: () => {},
@@ -15,16 +23,16 @@ export function CommunityModuleContextProvider({
   children,
   layoutState,
   showDeleteDialog,
+  DeleteMessageBox,
+  handleResourceDelete,
+  setLayoutColumn,
 }) {
-  const [, setOpenedModuleIndex] = useState();
-  const [, setDetailsOpen] = useState(false);
+  const [openedModuleIndex, setOpenedModuleIndex] = useState();
+  const [detailsOpen, setDetailsOpen] = useState(false);
+  const [initialUnchangedResource, setInitialUnchangedResource] = useState();
+  const [kymaResourceState, setKymaResourceState] = useState();
 
-  useEffect(() => {
-    if (layoutState?.layout) {
-      setDetailsOpen(layoutState?.layout !== 'OneColumn');
-    }
-  }, [layoutState]);
-
+  const notification = useNotification();
   const { moduleTemplatesLoading, communityModuleTemplates } = useContext(
     ModuleTemplatesContext,
   );
@@ -33,6 +41,46 @@ export function CommunityModuleContextProvider({
     installed: installedCommunityModules,
     loading: communityModulesLoading,
   } = useGetInstalledModules(communityModuleTemplates, moduleTemplatesLoading);
+  /////////////////////////// TODO: Is it necessary? KymaResource? resourceUrl?
+  const handleModuleUninstall = useCreateResource({
+    singularName: 'Kyma',
+    pluralKind: 'Kymas',
+    resource: kymaResourceState,
+    initialResource: initialUnchangedResource,
+    updateInitialResource: setInitialUnchangedResource,
+    createUrl:
+      ' /apis/operator.kyma-project.io/v1beta2/namespaces/kyma-system/kymas/default',
+    afterCreatedFn: () =>
+      notification.notifySuccess({
+        content: t('kyma-modules.module-uninstall'),
+      }),
+  });
+
+  useEffect(() => {
+    if (layoutState?.layout) {
+      setDetailsOpen(layoutState?.layout !== 'OneColumn');
+    }
+  }, [layoutState]);
+
+  useEffect(() => {
+    /////////////////////////// TODO: Is it proper for KymaResource and KymaResourceState?
+    if (installedCommunityModules?.[openedModuleIndex]?.resource) {
+      setKymaResourceState(
+        installedCommunityModules[openedModuleIndex].resource,
+      );
+      setInitialUnchangedResource(
+        cloneDeep(installedCommunityModules[openedModuleIndex].resource),
+      );
+    }
+  }, [installedCommunityModules, openedModuleIndex]);
+
+  const deleteModuleButton = (
+    <div>
+      <Button onClick={() => handleResourceDelete({})} design="Transparent">
+        {t('common.buttons.delete-module')}
+      </Button>
+    </div>
+  );
 
   return (
     <CommunityModuleContext.Provider
@@ -41,8 +89,38 @@ export function CommunityModuleContextProvider({
         showDeleteDialog: showDeleteDialog,
         installedCommunityModules: installedCommunityModules,
         communityModulesLoading: communityModulesLoading,
+        DeleteMessageBox: DeleteMessageBox,
+        deleteModuleButton: deleteModuleButton,
+        handleResourceDelete: handleResourceDelete,
       }}
     >
+      {createPortal(
+        !communityModulesLoading &&
+          !moduleTemplatesLoading &&
+          showDeleteDialog && (
+            <ModulesDeleteBox
+              DeleteMessageBox={DeleteMessageBox}
+              selectedModules={installedCommunityModules}
+              chosenModuleIndex={
+                openedModuleIndex ??
+                // Find index of the selected module after a refresh or other case after which we have undefined.
+                installedCommunityModules.findIndex(module =>
+                  checkSelectedModule(module, layoutState),
+                )
+              }
+              kymaResource={kymaResourceState}
+              kymaResourceState={kymaResourceState}
+              moduleTemplates={communityModuleTemplates}
+              detailsOpen={detailsOpen}
+              setKymaResourceState={setKymaResourceState}
+              setInitialUnchangedResource={setInitialUnchangedResource}
+              setChosenModuleIndex={setOpenedModuleIndex}
+              handleModuleUninstall={handleModuleUninstall}
+              setLayoutColumn={setLayoutColumn}
+            />
+          ),
+        document.body,
+      )}
       {children}
     </CommunityModuleContext.Provider>
   );
