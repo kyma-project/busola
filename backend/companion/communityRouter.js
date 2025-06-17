93e1aa37b889d8b1a99464d1dfbce6b788e2ebcf
Pietocha <konrad.pietocha@sap.com>
2025-06-17 12:05:48 +0200

    Check link

--- backend/companion/communityRouter.js
+++ backend/companion/communityRouter.js
@@ -7,6 +7,13 @@ router.use(cors());
 
 async function handleGetCommunityResource(req, res) {
   const { link } = JSON.parse(req.body.toString());
+
+  // Only allow URLs that start with https.
+  const allowedDomain = /^https:\/\//;
+  if (!allowedDomain.test(link)) {
+    return res.status(400).json('Invalid or untrusted link provided.');
+  }
+
   try {
     const response = await fetch(link);
     const data = await response.text();
