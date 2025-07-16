6569b0dd3c121b57c892de0bcd0950cc584c9b92
mrCherry97 <mwisnia97@gmail.com>
2025-07-16 10:33:07 +0200

    chore: Adjust extensibility FeatureCard to use generic FC component

--- src/components/Extensibility/components/FeaturedCard/FeaturedCard.js
+++ src/components/Extensibility/components/FeaturedCard/FeaturedCard.js
@@ -1,130 +1,29 @@
-import { Button, Card, Text, Title } from '@ui5/webcomponents-react';
-import { useEffect, useState } from 'react';
-import modulesIllustration from './assets/Modules/modulesIllustration.svg';
-import modulesIllustrationHC from './assets/Modules/modulesIllustrationHC.svg';
-import AiIllustrationLight from './assets/AI/AiIllustrationLight.svg';
-import AiIllustrationDark from './assets/AI/AiIllustrationDark.svg';
-import AiIllustrationHClight from './assets/AI/AiIllustrationHClight.svg';
-import AiIllustrationHCdark from './assets/AI/AiIllustrationHCdark.svg';
 import { Widget, InlineWidget } from '../Widget';
-import { useRecoilValue } from 'recoil';
-import { isSystemThemeDark, themeState } from 'state/preferences/themeAtom';
+import { FeatureCardBanner } from 'shared/components/FeatureCard/FeatureCard';
+
 import './FeaturedCard.scss';
 
-const getIllustration = (illustration, theme) => {
-  switch (illustration) {
-    case 'None':
-      return null;
-    case 'AI':
-      switch (theme) {
-        case 'sap_horizon_hcw':
-          return AiIllustrationHClight;
-        case 'sap_horizon_hcb':
-          return AiIllustrationHCdark;
-        case 'sap_horizon':
-          return AiIllustrationLight;
-        case 'sap_horizon_dark':
-          return AiIllustrationDark;
-        case 'light_dark':
-        default:
-          return isSystemThemeDark() ? AiIllustrationDark : AiIllustrationLight;
-      }
-    case 'Modules':
-    default:
-      switch (theme) {
-        case 'sap_horizon_hcw':
-        case 'sap_horizon_hcb':
-          return modulesIllustrationHC;
-        case 'sap_horizon':
-        case 'sap_horizon_dark':
-        case 'light_dark':
-        default:
-          return modulesIllustration;
-      }
-  }
-};
-
-const getBackgroundStyle = design => {
-  switch (design) {
-    case 'information-1':
-      return {
-        background:
-          'linear-gradient(to right, var(--sapLegendBackgroundColor6), var(--sapLegendBackgroundColor5))',
-      };
-    case 'information-2':
-    default:
-      return {
-        background:
-          'linear-gradient(to right, var(--sapLegendBackgroundColor20), var(--sapLegendBackgroundColor5))',
-      };
-  }
-};
-
 export function FeaturedCard({ value, structure, schema, ...props }) {
-  const theme = useRecoilValue(themeState);
-  const [hideBanner, setHideBanner] = useState(false);
-  const hideBannerKey = `hideBanner${structure?.id}`;
+  const { id, title, description, illustration, design } = structure;
 
-  useEffect(() => {
-    if (!structure?.id) return;
-    const storedHideValue = localStorage.getItem(hideBannerKey);
-    if (storedHideValue !== null) setHideBanner(storedHideValue === 'true');
-  }, [hideBannerKey, structure]);
-
-  const handleToggle = () => {
-    const updatedBoolean = true;
-    setHideBanner(updatedBoolean);
-    localStorage.setItem(hideBannerKey, updatedBoolean);
-  };
-
-  if (!structure?.id || hideBanner) {
-    return <></>;
-  }
-
-  const illustration = getIllustration(structure?.illustration, theme);
   return (
-    <div className="sap-margin-small">
-      <Card className="feature-card-container">
-        <div
-          className="feature-card"
-          style={getBackgroundStyle(structure?.design)}
-        >
-          <Button
-            design="Transparent"
-            icon="decline"
-            className="decline-button"
-            onClick={handleToggle}
-          />
-          <div className="outer-container" style={{ margin: '2rem 2.5rem' }}>
-            <div className="inner-container">
-              <Title level="H1" size="H1" wrappingType="Normal">
-                {structure?.title}
-              </Title>
-              <Text>{structure?.description}</Text>
-              <div className="button-container sap-margin-top-small">
-                {structure.children?.slice(0, 2).map((def, idx) => (
-                  <Widget
-                    key={idx}
-                    value={value}
-                    structure={def}
-                    schema={schema}
-                    inlineRenderer={InlineWidget}
-                    inlineContext={true}
-                    {...props}
-                  />
-                ))}
-              </div>
-            </div>
-            {illustration && (
-              <img
-                src={illustration}
-                alt="FeaturedCard Illustration"
-                className="illustration"
-              />
-            )}
-          </div>
-        </div>
-      </Card>
-    </div>
+    <FeatureCardBanner
+      id={id}
+      title={title}
+      description={description}
+      image={illustration}
+      design={design}
+      buttons={structure.children?.slice(0, 2).map((def, idx) => (
+        <Widget
+          key={idx}
+          value={value}
+          structure={def}
+          schema={schema}
+          inlineRenderer={InlineWidget}
+          inlineContext={true}
+          {...props}
+        />
+      ))}
+    />
   );
 }
