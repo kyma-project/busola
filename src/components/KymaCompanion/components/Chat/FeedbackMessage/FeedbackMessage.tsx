78c1d77e32692b97596509dac7204cf0e3342941
Mateusz Wisniewski <mwisnia97@gmail.com>
2025-07-22 10:08:23 +0200

    feat: Add FeedbackMessage on feedback backend detection (#4020)
    * feat: Add FeedbackMessage on feedback backend detection
    
    * remove clg
    
    * fix design of the illustrated message
    
    * fix margins
    
    * fix margins
    
    * fix margins
    
    * fix margins
    
    * add test for feedback message
    
    * Update public/i18n/en.yaml
    
    Co-authored-by: Małgorzata Świeca <malgorzata.swieca@sap.com>
    
    ---------
    
    Co-authored-by: Małgorzata Świeca <malgorzata.swieca@sap.com>

---
+++ src/components/KymaCompanion/components/Chat/FeedbackMessage/FeedbackMessage.tsx
@@ -0,0 +1,42 @@
+import {
+  Button,
+  Card,
+  IllustratedMessage,
+  Text,
+} from '@ui5/webcomponents-react';
+import { useFeature } from 'hooks/useFeature';
+import { useTranslation } from 'react-i18next';
+import { configFeaturesNames } from 'state/types';
+
+export default function FeedbackMessage(): JSX.Element {
+  const { t } = useTranslation();
+  const { config: companionConfig } = useFeature(
+    configFeaturesNames.KYMA_COMPANION,
+  );
+
+  return (
+    <div className="sap-margin-x-tiny">
+      <Card>
+        <IllustratedMessage
+          name="Survey"
+          design="Small"
+          key="feedback-message-chat"
+          style={{ padding: '1.5rem' }}
+          titleText={t('kyma-companion.feedback.title')}
+          subtitle={<Text>{t('kyma-companion.feedback.subtitle')}</Text>}
+        >
+          <Button
+            key="ai-feedback-chat"
+            endIcon="inspect"
+            design="Emphasized"
+            onClick={() => {
+              window.open(companionConfig?.feedbackLink, '_blank');
+            }}
+          >
+            {t('kyma-companion.banner.buttons.feedback')}
+          </Button>
+        </IllustratedMessage>
+      </Card>
+    </div>
+  );
+}
