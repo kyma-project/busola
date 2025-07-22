b84742397ad76697c7c0056c25c2937a48babae8
akucharska <agata.kucharska@sap.com>
2025-07-22 08:44:25 +0200

    Clean up

--- src/components/KymaCompanion/components/Chat/TimestampLabel/TimestampLabel.tsx
+++ src/components/KymaCompanion/components/Chat/TimestampLabel/TimestampLabel.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { ReadableCreationTimestamp } from 'shared/components/ReadableCreationTimestamp/ReadableCreationTimestamp';
 
 import { Text } from '@ui5/webcomponents-react';
 import './TimestampLabel.scss';
