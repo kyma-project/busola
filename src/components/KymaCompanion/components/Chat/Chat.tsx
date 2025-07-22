b25b19ef53babd2af8b63619b4d2f3880310092b
Agata Kucharska <akucharska@users.noreply.github.com>
2025-07-22 12:01:03 +0200

    feat: Add timestamp (#4024)
    * Add timestamp
    
    * Don't hardcode today
    
    * Clean up
    
    * Clean up
    
    * Fix tests

--- src/components/KymaCompanion/components/Chat/Chat.tsx
+++ src/components/KymaCompanion/components/Chat/Chat.tsx
@@ -13,6 +13,7 @@ import getChatResponse from 'components/KymaCompanion/api/getChatResponse';
 import { usePromptSuggestions } from 'components/KymaCompanion/hooks/usePromptSuggestions';
 import { AIError } from '../KymaCompanion';
 import ContextLabel from './ContextLabel/ContextLabel';
+import TimestampLabel from './TimestampLabel/TimestampLabel';
 import QueryInput from './Input/QueryInput';
 import {
   Author,
@@ -36,6 +37,7 @@ type ChatProps = {
   error: AIError;
   setError: React.Dispatch<React.SetStateAction<AIError>>;
   hide: boolean;
+  time: Date | null;
 };
 
 export const Chat = ({
@@ -48,6 +50,7 @@ export const Chat = ({
   isReset,
   setIsReset,
   hide = false,
+  time,
 }: ChatProps) => {
   const { t } = useTranslation();
   const chatRef = useRef<HTMLDivElement | null>(null);
@@ -344,6 +347,7 @@ export const Chat = ({
         className="chat-list sap-margin-x-tiny sap-margin-top-tiny"
         ref={chatRef}
       >
+        {time && <TimestampLabel time={time} />}
         {chatHistory.map((group, groupIndex) => {
           const isLastGroup = groupIndex === chatHistory.length - 1;
 
