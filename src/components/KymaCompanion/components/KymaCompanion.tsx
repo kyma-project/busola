4bc36aa7c9dc06b88c4d6cec98b06530918cae04
akucharska <agata.kucharska@sap.com>
2025-07-22 08:44:01 +0200

    Clean up

--- src/components/KymaCompanion/components/KymaCompanion.tsx
+++ src/components/KymaCompanion/components/KymaCompanion.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Button, Card, Title } from '@ui5/webcomponents-react';
 import { useRecoilState } from 'recoil';
