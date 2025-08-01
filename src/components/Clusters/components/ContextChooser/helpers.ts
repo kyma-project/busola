d93107cbfba6e41f225be2b33416f73a385c5664
mrCherry97 <mwisnia97@gmail.com>
2025-08-01 13:19:24 +0200

    fix no args

--- src/components/Clusters/components/ContextChooser/helpers.ts
+++ src/components/Clusters/components/ContextChooser/helpers.ts
@@ -3,11 +3,11 @@ import { EMPTY_TEXT_PLACEHOLDER } from 'shared/constants';
 export function getUserDetail(
   contextName: string,
   parameter: string,
-  users?: Array<{ name: string; user: { exec: { args: string[] } } }>,
+  users?: Array<{ name: string; user: { exec: { args?: string[] } } }>,
 ) {
   const user = (users || []).find(user => user.name === contextName);
   if (user && user.user) {
-    const clientIDArg = user.user.exec.args.find(arg =>
+    const clientIDArg = (user.user.exec.args || []).find(arg =>
       arg.startsWith(parameter),
     );
     return clientIDArg
