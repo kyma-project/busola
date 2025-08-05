80683b3b4b30149514173a2004690d5c22a1ddf3
Damian Badura <damian.badura@sap.com>
2025-08-05 12:28:07 +0200

    add first implementaion

---
+++ tests/integration/tests/extensibility/ext-test-custom-component.spec.js
@@ -0,0 +1,24 @@
+const TEST_VALUE = 'Test Data for Monaco';
+
+context('Test Custom Components', () => {
+  Cypress.skipAfterFail();
+
+  before(() => {
+    cy.setBusolaFeature('EXTENSIBILITY', true);
+    cy.loginAndSelectCluster();
+  });
+
+  // it('Upload custom components')
+
+  // Custom component should be deployer earlier by kustomize, so we can check if it's rendered
+  it('Check if custom component is rendering', () => {
+    cy.navigateTo('Kyma', 'Busola Web Components example');
+
+    cy.get('div[id="custom-html"]').should('be.visible');
+    cy.get('div[id="extension-web-component"]').should('be.visible');
+    cy.get('my-custom-page').should('be.visible');
+
+    cy.pasteToMonaco(TEST_VALUE);
+    cy.get('my-custom-page').contains(TEST_VALUE);
+  });
+});
