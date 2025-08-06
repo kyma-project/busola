d7ae82659162ea3e7f1d2dca81338e60b251170c
Damian Badura <damian.badura@sap.com>
2025-08-06 21:23:51 +0200

    remove unnesary comment

--- tests/integration/tests/extensibility/ext-test-custom-component.spec.js
+++ tests/integration/tests/extensibility/ext-test-custom-component.spec.js
@@ -8,7 +8,6 @@ context('Test Custom Components', () => {
     cy.loginAndSelectCluster();
   });
 
-  // Custom component should be deployer earlier by kustomize, so we can check if it's rendered
   it('Check if custom component is rendering', () => {
     cy.navigateTo('Kyma', 'Busola Web Components example');
 
