8165a29805e14640e96f609f62bc0ec36a1bffd3
mrCherry97 <mwisnia97@gmail.com>
2025-08-19 16:08:28 +0200

    eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,16 +1,58 @@
 import js from '@eslint/js';
 import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
 import pluginReact from 'eslint-plugin-react';
-import { defineConfig } from 'eslint/config';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import pluginTypeScript from '@typescript-eslint/eslint-plugin';
+import parserTypeScript from '@typescript-eslint/parser';
 
-export default defineConfig([
+export default [
+  js.configs.recommended,
+  globalIgnores(['tests/**']),
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    plugins: { js },
-    extends: ['js/recommended'],
-    languageOptions: { globals: globals.browser },
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: parserTypeScript,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+        project: './tsconfig.json',
+        tsconfigRootDir: import.meta.dirname,
+      },
+      globals: { ...globals.browser, ...globals.node },
+    },
+    plugins: {
+      react: pluginReact,
+      'react-hooks': pluginReactHooks,
+      '@typescript-eslint': pluginTypeScript,
+    },
+    settings: { react: { version: 'detect' } },
+    rules: {
+      // React
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+      'react/jsx-uses-vars': 'off',
+      'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.jsx'] }],
+
+      // React Hooks
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+
+      // TypeScript
+      'no-unused-vars': ['off'],
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/ban-ts-comment': 'warn',
+      '@typescript-eslint/no-explicit-any': 'off',
+
+      // General
+      'no-console': 'warn',
+      'no-debugger': 'warn',
+    },
   },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-]);
+];
