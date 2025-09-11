31d8e35741b4e7c3a94c377b0ba4aafb416710a0
Anna Woloszyn <anna.woloszyn@sap.com>
2025-09-11 11:53:54 +0200

    chore: remove PropTypes from ExtensibilityRoutes

--- src/components/App/ExtensibilityRoutes.jsx
+++ src/components/App/ExtensibilityRoutes.jsx
@@ -5,7 +5,6 @@ import { Route, useParams } from 'react-router';
 import { useAtomValue } from 'jotai';
 import { FlexibleColumnLayout } from '@ui5/webcomponents-react';
 import { useTranslation } from 'react-i18next';
-import PropTypes from 'prop-types';
 
 import { columnLayoutAtom } from 'state/columnLayoutAtom';
 import { useUrl } from 'hooks/useUrl';
@@ -160,9 +159,4 @@ export const createExtensibilityRoutes = (extension, language, ...props) => {
       />
     </React.Fragment>
   );
-};
-
-createExtensibilityRoutes.propTypes = {
-  extension: PropTypes.object,
-  language: PropTypes.string,
 };
