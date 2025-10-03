2538a2cd6ab6615d762b195d55ab008e4483eb89
Anna Wołoszyn <anna.woloszyn@sap.com>
2025-10-03 08:06:51 +0200

    feat: Improve TimeFromNow widget (#4228)
    * feat: add prop specifying rounding of the result in ReadableElapsedTimeFromNow
    
    * refactor: decide unit automatically if roundingResultType is not specified
    
    * chore: remove console log
    
    * refactor: remove resultRoundingType prop from TimeFromNow widget

--- src/shared/components/ReadableElapsedTimeFromNow/ReadableElapsedTimeFromNow.tsx
+++ src/shared/components/ReadableElapsedTimeFromNow/ReadableElapsedTimeFromNow.tsx
@@ -1,3 +1,5 @@
+import { useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
 import { EMPTY_TEXT_PLACEHOLDER } from 'shared/constants';
 
 const toSeconds = 1000;
@@ -5,30 +7,39 @@ const toMinutes = 60;
 const toHours = 60;
 const toDays = 24;
 
-export const getElapsedTime = (
-  timestamp: string,
-  valueUnit: string,
-): string => {
-  if (!timestamp) {
-    return EMPTY_TEXT_PLACEHOLDER;
-  }
-
-  const startDate = new Date(timestamp);
-  const now = new Date();
-  const timeDiff = now.valueOf() - startDate.valueOf();
-
-  const timeDiffDays = Math.round(
-    timeDiff / toSeconds / toMinutes / toHours / toDays,
-  );
-  return timeDiffDays.toString() + ' ' + valueUnit;
+const formatResult = (value: number, valueUnit: string) => {
+  return <>{Math.round(value).toString() + ' ' + valueUnit}</>;
 };
 
 export const ReadableElapsedTimeFromNow = ({
   timestamp,
-  valueUnit,
 }: {
   timestamp: string;
-  valueUnit: string;
 }): JSX.Element => {
-  return <>{getElapsedTime(timestamp, valueUnit)}</>;
+  const { t } = useTranslation();
+
+  const result = useMemo(() => {
+    if (!timestamp) {
+      return <>{EMPTY_TEXT_PLACEHOLDER}</>;
+    }
+
+    const startDate = new Date(timestamp);
+    const now = new Date();
+    const timeDiff = now.valueOf() - startDate.valueOf();
+
+    const seconds = timeDiff / toSeconds;
+    const minutes = seconds / toMinutes;
+    const hours = minutes / toHours;
+    const days = hours / toDays;
+
+    if (seconds < 60)
+      return formatResult(seconds, t('common.value-units.seconds-ago'));
+    else if (minutes < 60)
+      return formatResult(minutes, t('common.value-units.minutes-ago'));
+    else if (hours < 24)
+      return formatResult(hours, t('common.value-units.hours-ago'));
+    else return formatResult(days, t('common.value-units.days-ago'));
+  }, [timestamp, t]);
+
+  return result;
 };
