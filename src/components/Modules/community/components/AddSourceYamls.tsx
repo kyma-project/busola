31ea46f5f213357f12f4fd94704429d567683394
Oliwia Gowor <72342415+OliwiaGowor@users.noreply.github.com>
2025-10-13 12:32:17 +0200

    feat: Add namespace chooser for the Community Modules Module Templates (#4231)
    * feat: Add namespace dropdown
    
    * feat: add check for duplicates
    
    * fix: cleanup & reset states on close
    
    * test: fix lighthouse test
    
    * feat: add spinner
    
    * Apply suggestions from code review
    
    Co-authored-by: Grzegorz Karaluch <grzegorz.karaluch@sap.com>
    
    ---------
    
    Co-authored-by: Grzegorz Karaluch <grzegorz.karaluch@sap.com>

--- src/components/Modules/community/components/AddSourceYamls.tsx
+++ src/components/Modules/community/components/AddSourceYamls.tsx
@@ -1,24 +1,38 @@
 import {
   Button,
+  ComboBox,
+  ComboBoxItem,
   FlexBox,
   Input,
   Label,
+  List,
+  ListItemCustom,
+  ListItemStandard,
   MessageBox,
+  MessageStrip,
+  Text,
 } from '@ui5/webcomponents-react';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 import { createPortal } from 'react-dom';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useMemo, useState } from 'react';
 import { usePost } from 'shared/hooks/BackendAPI/usePost';
 import { useNotification } from 'shared/contexts/NotificationContext';
 
-import { postForCommunityResources } from 'components/Modules/community/communityModulesHelpers';
 import { useUploadResources } from 'resources/Namespaces/YamlUpload/useUploadResources';
 
-import 'components/Modules/community/components/AddSourceYamls.scss';
-import { HttpError } from 'shared/hooks/BackendAPI/config';
 import { FlexBoxDirection } from '@ui5/webcomponents-react/dist/enums/FlexBoxDirection';
+import { namespacesAtom } from 'state/namespacesAtom';
+import { useAtomValue } from 'jotai';
+import { HintButton } from 'shared/components/HintButton/HintButton';
+import { ModuleTemplatesContext } from 'components/Modules/providers/ModuleTemplatesProvider';
 
 import { DEFAULT_K8S_NAMESPACE } from 'components/Modules/support';
+import { useUrl } from 'hooks/useUrl';
+import { useNavigate } from 'react-router';
+import { useGetYAMLModuleTemplates } from 'components/Modules/hooks';
+import 'components/Modules/community/components/AddSourceYamls.scss';
+import { Spinner } from 'shared/components/Spinner/Spinner';
+
 const DEFAULT_SOURCE_URL =
   'https://kyma-project.github.io/community-modules/all-modules.yaml';
 
@@ -26,13 +40,35 @@ export const AddSourceYamls = () => {
   const { t } = useTranslation();
   const notification = useNotification();
   const post = usePost();
+  const { clusterUrl, namespaceUrl } = useUrl();
+  const navigate = useNavigate();
 
   const [showAddSource, setShowAddSource] = useState(false);
-  const [error, setError] = useState<string | null>(null);
   const [sourceURL, setSourceURL] = useState(DEFAULT_SOURCE_URL);
   const [resourcesToApply, setResourcesToApply] = useState<{ value: any }[]>(
     [],
   );
+  const [showDescription, setShowDescription] = useState(false);
+
+  const {
+    resources: fetchedResources,
+    error,
+    loading,
+  } = useGetYAMLModuleTemplates(sourceURL, post);
+  const allNamespaces = useAtomValue(namespacesAtom);
+  const { communityModuleTemplates } = useContext(ModuleTemplatesContext);
+
+  const existingModuleTemplates = useMemo(() => {
+    const communityItems = communityModuleTemplates?.items || [];
+
+    return fetchedResources.flatMap((resource: any) =>
+      communityItems.filter(
+        (mt: any) =>
+          mt.metadata?.name === resource.value?.metadata?.name &&
+          mt.spec?.version === resource.value?.spec?.version,
+      ),
+    );
+  }, [fetchedResources, communityModuleTemplates]);
 
   const uploadResources = useUploadResources(
     resourcesToApply,
@@ -41,41 +77,36 @@ export const AddSourceYamls = () => {
     DEFAULT_K8S_NAMESPACE,
   );
 
-  useEffect(() => {
-    if (sourceURL.endsWith('.yaml')) {
-      (async function () {
-        try {
-          const allResources = await postForCommunityResources(post, sourceURL);
-          const allowedToApply = filterResources(allResources);
-          const formatted = allowedToApply?.map((r: any) => {
-            return { value: r };
-          });
-          setError(null);
-          setResourcesToApply(formatted);
-        } catch (e) {
-          if (e instanceof HttpError) {
-            setError(
-              t('modules.community.messages.source-yaml-fetch-failed', {
-                error: e.message,
-              }),
-            );
-          }
-        }
-      })();
-    } else {
-      setError(t('modules.community.messages.source-yaml-invalid-url'));
-    }
-  }, [sourceURL]); // eslint-disable-line react-hooks/exhaustive-deps
+  const applyNamespace = (namespace: string) => {
+    const namespacedResources = resourcesToApply.map((resource) => ({
+      ...resource,
+      value: {
+        ...resource.value,
+        metadata: { ...resource.value.metadata, namespace },
+      },
+    }));
 
-  const filterResources = (resources: any) => {
-    return (resources || []).filter(
-      (resource: any) =>
-        resource?.kind === 'ModuleTemplate' ||
-        (resource?.kind === 'CustomResourceDefinition' &&
-          resource?.metadata?.name ===
-            'moduletemplates.operator.kyma-project.io'),
-    );
+    setResourcesToApply(namespacedResources);
   };
+
+  useEffect(() => {
+    if (existingModuleTemplates.length > 0) {
+      setResourcesToApply(
+        fetchedResources.filter(
+          (resource: any) =>
+            !existingModuleTemplates.some(
+              (mt: any) =>
+                mt.metadata.name === resource.value.metadata.name &&
+                mt.spec.version === resource.value.spec.version,
+            ),
+        ),
+      );
+
+      return;
+    }
+
+    setResourcesToApply(fetchedResources);
+  }, [fetchedResources, existingModuleTemplates]); // eslint-disable-line react-hooks/exhaustive-deps
 
   const handleApplySourceYAMLs = async () => {
     if (error) {
@@ -97,6 +128,23 @@ export const AddSourceYamls = () => {
       });
     }
   };
+
+  const handleItemClick = async (name: string, namespace?: string) => {
+    const path = `customresources/moduletemplates.operator.kyma-project.io/${name}`;
+    const link = namespace
+      ? namespaceUrl(path, { namespace: namespace || '-all-' })
+      : clusterUrl(path);
+
+    navigate(link);
+  };
+
+  const handleClose = () => {
+    setSourceURL(DEFAULT_SOURCE_URL);
+    setResourcesToApply([]);
+    setShowDescription(false);
+    setShowAddSource(false);
+  };
+
   return (
     <>
       <Button
@@ -110,56 +158,170 @@ export const AddSourceYamls = () => {
         {t('modules.community.source-yaml.add-source-yaml')}
       </Button>
 
-      {createPortal(
-        <MessageBox
-          open={showAddSource}
-          className="sourceurl-messagebox"
-          titleText={t('modules.community.source-yaml.add-source-yaml')}
-          actions={[
-            <Button
-              accessibleName="add-yamls"
-              design="Emphasized"
-              key="add-yamls"
-              onClick={async () => {
-                await handleApplySourceYAMLs();
-              }}
+      {showAddSource &&
+        createPortal(
+          <MessageBox
+            open={showAddSource}
+            className="sourceurl-messagebox"
+            titleText={t('modules.community.source-yaml.add-source-yaml')}
+            onClose={handleClose}
+            actions={[
+              <Button
+                accessibleName="add-yamls"
+                design="Emphasized"
+                key="add-yamls"
+                disabled={!!error || resourcesToApply.length === 0}
+                onClick={async () => {
+                  await handleApplySourceYAMLs();
+                }}
+              >
+                {t('common.buttons.add')}
+              </Button>,
+              <Button
+                accessibleName="cancel-add-yamls"
+                design="Transparent"
+                key="cancel-add-yamls"
+                onClick={() => {
+                  setShowAddSource(false);
+                }}
+              >
+                {t('common.buttons.cancel')}
+              </Button>,
+            ]}
+          >
+            <FlexBox direction={FlexBoxDirection.Column} gap={'0.5rem'}>
+              <Label for="source-url">
+                {t('modules.community.source-yaml.source-yaml-url') + ':'}
+              </Label>
+              <Input
+                type="Text"
+                id="source-url"
+                value={sourceURL}
+                onInput={(e: any) => {
+                  setSourceURL((e.target as HTMLInputElement).value);
+                }}
+                accessibleName="Source YAML URL"
+                showClearIcon
+                className="full-width"
+              />
+              <Label wrappingType="Normal">
+                {t('modules.community.source-yaml.example-format')}
+              </Label>
+            </FlexBox>
+            <FlexBox
+              direction={FlexBoxDirection.Column}
+              gap={'0.5rem'}
+              className="sap-margin-top-small"
             >
-              {t('common.buttons.add')}
-            </Button>,
-            <Button
-              accessibleName="cancel-add-yamls"
-              design="Transparent"
-              key="cancel-add-yamls"
-              onClick={() => {
-                setShowAddSource(false);
-              }}
-            >
-              {t('common.buttons.cancel')}
-            </Button>,
-          ]}
-        >
-          <FlexBox direction={FlexBoxDirection.Column} gap={'0.5rem'}>
-            <Label for="source-url">
-              {t('modules.community.source-yaml.source-yaml-url') + ':'}
-            </Label>
-            <Input
-              type="Text"
-              id="source-url"
-              value={sourceURL}
-              onInput={(e: any) => {
-                setSourceURL((e.target as HTMLInputElement).value);
-              }}
-              accessibleName="Source YAML URL"
-              showClearIcon
-              className="full-width"
-            />
-            <Label wrappingType="Normal" style={{ marginTop: '5px' }}>
-              {t('modules.community.source-yaml.example-format')}
-            </Label>
-          </FlexBox>
-        </MessageBox>,
-        document.body,
-      )}
+              <FlexBox direction={FlexBoxDirection.Row} alignItems="Center">
+                <Label for="source-url">
+                  {t('common.headers.namespace') + ':'}
+                </Label>
+                <HintButton
+                  setShowTitleDescription={setShowDescription}
+                  showTitleDescription={showDescription}
+                  description={
+                    t(
+                      'modules.community.source-yaml.namespace-description',
+                    ) as string
+                  }
+                ></HintButton>
+              </FlexBox>
+              <ComboBox
+                id="yaml-namespace-combobox"
+                onChange={(e) => applyNamespace(e.target.value)}
+              >
+                {allNamespaces?.map((ns) => (
+                  <ComboBoxItem text={ns} key={ns} />
+                ))}
+              </ComboBox>
+            </FlexBox>
+            {loading ? (
+              <Spinner />
+            ) : (
+              <>
+                {existingModuleTemplates.length > 0 && (
+                  <FlexBox
+                    direction={FlexBoxDirection.Column}
+                    gap={'0.5rem'}
+                    className="sap-margin-top-small"
+                  >
+                    <MessageStrip design="Critical" hideCloseButton>
+                      {t('modules.community.source-yaml.modules-wont-be-added')}
+                    </MessageStrip>
+                    <List>
+                      {existingModuleTemplates?.map((mt: any) => {
+                        return (
+                          <ListItemCustom
+                            key={mt?.metadata.uid}
+                            onClick={() => {
+                              handleItemClick(
+                                mt.metadata.name,
+                                mt?.metadata.namespace,
+                              );
+                            }}
+                          >
+                            <Text>
+                              <Trans
+                                i18nKey={
+                                  'modules.community.source-yaml.module-already-exists'
+                                }
+                                values={{
+                                  moduleTemplate: `${mt.metadata.name} ${mt.spec.version ? ` (v${mt.spec.version})` : ''}`,
+                                  namespace: mt?.metadata.namespace,
+                                }}
+                              >
+                                <span style={{ fontWeight: 'bold' }}></span>
+                              </Trans>
+                            </Text>
+                          </ListItemCustom>
+                        );
+                      })}
+                    </List>
+                  </FlexBox>
+                )}
+                <FlexBox
+                  direction={FlexBoxDirection.Column}
+                  gap={'0.5rem'}
+                  className="sap-margin-top-small"
+                >
+                  <List
+                    header={
+                      <Text className="to-add-list-header">
+                        {t(
+                          'modules.community.source-yaml.module-templates-to-add',
+                        )}
+                      </Text>
+                    }
+                  >
+                    {resourcesToApply.length === 0 && (
+                      <ListItemStandard>
+                        <Text>
+                          {t(
+                            'modules.community.source-yaml.no-module-templates',
+                          )}
+                        </Text>
+                      </ListItemStandard>
+                    )}
+                    {resourcesToApply?.map((mt) => {
+                      return (
+                        <ListItemCustom key={mt.value.metadata.uid}>
+                          <Text>
+                            {mt.value.metadata.name}
+                            {mt.value.spec.version
+                              ? ` (v${mt.value.spec.version})`
+                              : ''}
+                          </Text>
+                        </ListItemCustom>
+                      );
+                    })}
+                  </List>
+                </FlexBox>
+              </>
+            )}
+          </MessageBox>,
+          document.body,
+        )}
     </>
   );
 };
