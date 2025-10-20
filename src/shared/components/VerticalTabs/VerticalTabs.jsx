bd1034f769eaa4dcfcece2f2e730811e6df8b271
Anna Woloszyn <anna.woloszyn@sap.com>
2025-10-20 16:37:58 +0200

    fix: arrow navigation in preferences dialog

--- src/shared/components/VerticalTabs/VerticalTabs.jsx
+++ src/shared/components/VerticalTabs/VerticalTabs.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import { useRef, useState } from 'react';
 
 import PropTypes from 'prop-types';
 import { TileButton } from 'shared/components/TileButton/TileButton';
 
 import './VerticalTabs.scss';
+import { useEventListener } from 'hooks/useEventListener';
 
 const childrenPropType = PropTypes.oneOfType([
   PropTypes.arrayOf(PropTypes.node),
@@ -28,11 +29,27 @@ VerticalTabs.propTypes = {
 };
 
 export function VerticalTabs({ tabs, children }) {
-  const [tabId, setTabId] = React.useState(children[0]?.props.id || 0);
+  const [tabId, setTabId] = useState(children[0]?.props.id || 0);
+  const listRef = useRef(null);
+
+  useEventListener(
+    'keydown',
+    (e) => {
+      const { key } = e;
+      if (key === 'ArrowDown' && tabId <= tabs?.length - 1) {
+        setTabId(tabId + 1);
+        listRef.current.children[tabId].children[0].focus();
+      } else if (key === 'ArrowUp' && tabId > 1) {
+        setTabId(tabId - 1);
+        listRef.current.children[tabId - 2].children[0].focus();
+      }
+    },
+    [tabId, tabs],
+  );
 
   return (
     <section className="vertical-tabs-wrapper">
-      <ul>
+      <ul ref={listRef}>
         {tabs.map(({ id, ...props }) => (
           <TileButton
             key={id}
