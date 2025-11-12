36987cc65b12141a2056b9deaac29535b7287f00
Mateusz Wisniewski <mwisnia97@gmail.com>
2025-11-12 14:51:20 +0100

    fix: Cluster overview resize problems (#4349)
    * fix: Cluster overview resize problems
    
    * little cleanup
    
    * little cleanup

--- src/components/Extensibility/components/Widget.jsx
+++ src/components/Extensibility/components/Widget.jsx
@@ -148,33 +148,47 @@ export function Widget({
   const [visible, setVisible] = useState(true);
   const [visibilityError, setVisibilityError] = useState(null);
 
+  const stableStructure = useMemo(
+    () => structure,
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [structure?.source, structure?.visibility],
+  );
+  const stableIndex = useMemo(() => index, [index]);
+  const stableValue = useMemo(() => value, [value]);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const stableArrayItems = useMemo(() => arrayItems, [arrayItems?.length]);
+
   useEffect(() => {
+    let canceled = false;
+
     const setStatesFromJsonata = async () => {
-      const [evaluatedChildValue] = await jsonata(structure.source, {
-        index: index,
+      const [evaluatedChildValue] = await jsonata(stableStructure.source, {
+        index: stableIndex,
       });
       const [result, error] = await jsonata(
-        structure.visibility?.toString(),
-        {
-          value: evaluatedChildValue,
-        },
+        stableStructure.visibility?.toString(),
+        { value: evaluatedChildValue },
         true,
       );
+      if (canceled) return;
       setChildValue(evaluatedChildValue);
       setVisible(result);
       setVisibilityError(error);
     };
+
     setStatesFromJsonata();
+    return () => {
+      canceled = true;
+    };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [
-    structure.source,
-    index,
-    structure.visibility,
+    stableStructure,
+    stableIndex,
+    stableValue,
+    stableArrayItems,
     originalResource,
     singleRootResource,
     embedResource,
-    value,
-    arrayItems,
   ]);
 
   if (visibilityError) {
