719dc4d44a75a8031700be51837bb2b8da2cc2b4
Oliwia Gowor <oliwia.gowor@sap.com>
2025-11-18 11:59:06 +0100

    fix: displaying community modules in list

--- src/components/Modules/community/communityModulesHelpers.ts
+++ src/components/Modules/community/communityModulesHelpers.ts
@@ -107,7 +107,8 @@ export function getInstalledModules(
   managers: any,
 ): ModuleTemplateListType {
   const installedModuleTemplates = moduleTemplates.items?.filter((module) => {
-    const foundManager = managers[module.metadata.name];
+    const foundManager =
+      managers[`${module.metadata.name}:${module.spec.manager.namespace}`];
     if (!foundManager) {
       return false;
     }
@@ -131,7 +132,12 @@ export function getNotInstalledModules(
 ): ModuleTemplateListType {
   const notInstalledModuleTemplates = moduleTemplates.items?.filter(
     (module) => {
-      const foundManager = managers[module.metadata.name];
+      const foundManager = managers?.items?.find((manager: any) => {
+        return (
+          manager.metadata.name === module.metadata.name &&
+          manager.metadata.namespace === module.metadata.namespace
+        );
+      });
 
       return !foundManager;
     },
