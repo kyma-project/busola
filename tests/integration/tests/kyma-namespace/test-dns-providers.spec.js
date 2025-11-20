ed60f1707c0f23cc5146fb9d1c68c89a88de184f
Oliwia Gowor <oliwia.gowor@sap.com>
2025-11-20 09:33:57 +0100

    test: fix test

--- tests/integration/tests/kyma-namespace/test-dns-providers.spec.js
+++ tests/integration/tests/kyma-namespace/test-dns-providers.spec.js
@@ -9,12 +9,34 @@ const PROVIDER_INCLUDED_DOMAIN = 'test.kyma.local';
 const PROVIDER_INCLUDED_DOMAIN_2 = 'test2.kyma.local';
 const PROVIDER_EXCLUDED_DOMAIN = 'sth.kyma.local';
 
+const FILE_NAME = 'test-secret.yaml';
+
+async function loadSecret(namespace, fileName) {
+  const resource = await loadFile(fileName);
+  const newResource = { ...resource };
+
+  newResource.metadata.namespace = namespace;
+
+  return newResource;
+}
+
 context('Test DNS Providers', () => {
   Cypress.skipAfterFail();
 
   before(() => {
     cy.loginAndSelectCluster();
     cy.goToNamespaceDetails();
+
+    cy.navigateTo('Configuration', 'Secrets');
+    cy.openCreate();
+    cy.wrap(loadSecret(Cypress.env('NAMESPACE_NAME'), FILE_NAME)).then(
+      (SECRET_CONFIG) => {
+        const secret = JSON.stringify(SECRET_CONFIG);
+        cy.pasteToMonaco(secret);
+      },
+    );
+    cy.saveChanges('Create');
+    cy.contains('ui5-title', 'secret-test').should('be.visible');
   });
 
   it('Create DNS Provider', () => {
