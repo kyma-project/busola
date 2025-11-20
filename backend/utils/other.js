543b7d12fdafc03a5e175748226c3b45f3cd3e30
Damian Badura <damian.badura@sap.com>
2025-11-20 20:23:42 +0100

    add implementation for env configuration

--- backend/utils/other.js
+++ backend/utils/other.js
@@ -1,7 +1,24 @@
+import * as fs from 'node:fs';
+
 export function requestLogger(httpModule) {
   var original = httpModule.request;
   httpModule.request = function (options, callback) {
     console.log('Outgoing HTTP request with options', options);
     return original(options, callback);
   };
+}
+
+export function fillActiveEnvForFrontend(env) {
+  try {
+    const activeEnv = `ENVIRONMENT=${env}\n`;
+    fs.writeFile('core-ui/active.env', activeEnv, (err) => {
+      if (err) {
+        console.warn(`Couldn't write env:${env} to active.env file`, err);
+      }
+    });
+  } catch (e) {
+    console.warn(`Couldn't write env:${env} to active.env file`, e);
+    return;
+  }
+  console.log('Set active.env file for frontend');
 }
