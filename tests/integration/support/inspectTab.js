b81884d2894f90c3a67ef0c6ebe73c56f90badca
chriskari <chriskari@gmx.de>
2025-11-28 20:21:41 +0100

    test: stabilize inspectTab helper

--- tests/integration/support/inspectTab.js
+++ tests/integration/support/inspectTab.js
@@ -2,16 +2,15 @@ Cypress.Commands.add(
   'inspectTab',
   { prevSubject: ['optional', 'element'] },
   (subject, tabName) => {
-    (subject
-      ? cy.wrap(subject).find('ui5-tabcontainer')
-      : cy.get('ui5-tabcontainer')
+    return (
+      subject
+        ? cy.wrap(subject).find('ui5-tabcontainer')
+        : cy.get('ui5-tabcontainer')
     )
       .find('[role="tablist"]')
       .find('[role="tab"]')
       .contains(tabName)
       .should('be.visible')
-      .as('tabButton');
-
-    return cy.get('@tabButton').click();
+      .click();
   },
 );
