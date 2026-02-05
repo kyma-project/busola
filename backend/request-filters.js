032586ca7628a54937ddbae686a513ecb713f90b
chriskari <chriskari@gmx.de>
2026-02-05 16:00:08 +0100

    add LRU DNS cache for proxy request validation

--- backend/request-filters.js
+++ backend/request-filters.js
@@ -1,12 +1,17 @@
-export const localIpFilter = (_req, headersData) => {
-  //https://stackoverflow.com/a/62925185, but without 127.0.0.1 range
-  const localIpRegex =
-    /\b(0?10\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|172\.0?1[6-9]\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|172\.0?2[0-9]\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|172\.0?3[01]\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|192\.168\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|169\.254\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|::1|[fF][cCdD][0-9a-fA-F]{2}(?:[:][0-9a-fA-F]{0,4}){0,7}|[fF][eE][89aAbB][0-9a-fA-F](?:[:][0-9a-fA-F]{0,4}){0,7})(?:\/([789]|1?[0-9]{2}))?\b/;
+import net from 'net';
+import { isPrivateIp } from './utils/network-utils';
 
+export const localIpFilter = (_req, headersData) => {
   const host = headersData.targetApiServer.host || '';
 
-  if (localIpRegex.test(host) || host.endsWith('.cluster.local')) {
+  if (host.endsWith('.cluster.local')) {
     throw Error('Local IP addresses are not allowed.');
+  }
+
+  if (net.isIP(host)) {
+    if (isPrivateIp(host)) {
+      throw Error('Local IP addresses are not allowed.');
+    }
   }
 };
 
