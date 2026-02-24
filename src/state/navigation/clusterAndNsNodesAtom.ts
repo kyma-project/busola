f6557170c2fb4058029848aa825a7244441d7d09
Anna Woloszyn <anna.woloszyn@sap.com>
2026-02-24 08:25:09 +0100

    fix: do not show community modules card and nav item when flag is disabled

--- src/state/navigation/clusterAndNsNodesAtom.ts
+++ src/state/navigation/clusterAndNsNodesAtom.ts
@@ -8,7 +8,7 @@ import { permissionSetsAtom } from '../permissionSetsAtom';
 import { NavNode, Scope } from '../types';
 import { shouldNodeBeVisible } from './filters/shouldNodeBeVisible';
 import { addAdditionalNodes } from './addAdditionalNodes';
-import { moduleTemplatesCountAtom } from 'state/moduleTemplatesCountAtom';
+import { kymaResourcesAtom } from 'state/kymaResourcesAtom';
 
 export const clusterAndNsNodesAtom = atom<Promise<NavNode[]>>(async (get) => {
   const resourceList: NavNode[] = get(resourceListAtom);
@@ -16,7 +16,7 @@ export const clusterAndNsNodesAtom = atom<Promise<NavNode[]>>(async (get) => {
   const openapiPathIdList = get(openapiPathIdListAtom);
   const permissionSet = await get(permissionSetsAtom);
   const configuration = get(configurationAtom);
-  const moduleTemplatesCount = await get(moduleTemplatesCountAtom);
+  const kymaResources = await get(kymaResourcesAtom);
 
   const features = configuration?.features || {};
 
@@ -50,7 +50,7 @@ export const clusterAndNsNodesAtom = atom<Promise<NavNode[]>>(async (get) => {
     navNodes,
     scope,
     features!,
-    moduleTemplatesCount,
+    !!kymaResources,
   );
 
   return navNodesWithAddons;
