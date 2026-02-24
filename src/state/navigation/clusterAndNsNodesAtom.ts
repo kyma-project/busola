1499669c27b6ee43e3b0efaa1717453874c7a092
Agata Kucharska <akucharska@users.noreply.github.com>
2026-02-24 12:28:31 +0100

    chore: Remove unused code (#4654)

--- src/state/navigation/clusterAndNsNodesAtom.ts
+++ src/state/navigation/clusterAndNsNodesAtom.ts
@@ -8,7 +8,6 @@ import { permissionSetsAtom } from '../permissionSetsAtom';
 import { NavNode, Scope } from '../types';
 import { shouldNodeBeVisible } from './filters/shouldNodeBeVisible';
 import { addAdditionalNodes } from './addAdditionalNodes';
-import { moduleTemplatesCountAtom } from 'state/moduleTemplatesCountAtom';
 
 export const clusterAndNsNodesAtom = atom<Promise<NavNode[]>>(async (get) => {
   const resourceList: NavNode[] = get(resourceListAtom);
@@ -16,7 +15,6 @@ export const clusterAndNsNodesAtom = atom<Promise<NavNode[]>>(async (get) => {
   const openapiPathIdList = get(openapiPathIdListAtom);
   const permissionSet = await get(permissionSetsAtom);
   const configuration = get(configurationAtom);
-  const moduleTemplatesCount = await get(moduleTemplatesCountAtom);
 
   const features = configuration?.features || {};
 
@@ -46,12 +44,7 @@ export const clusterAndNsNodesAtom = atom<Promise<NavNode[]>>(async (get) => {
   );
 
   const scope: Scope = activeNamespaceId ? 'namespace' : 'cluster';
-  const navNodesWithAddons = addAdditionalNodes(
-    navNodes,
-    scope,
-    features!,
-    moduleTemplatesCount,
-  );
+  const navNodesWithAddons = addAdditionalNodes(navNodes, scope, features!);
 
   return navNodesWithAddons;
 });
