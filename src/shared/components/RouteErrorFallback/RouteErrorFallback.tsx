1d795d53f5fc97df2ef203760a99ede7996e636a
chriskari <chriskari@gmx.de>
2026-03-10 09:29:00 +0100

    feat: Add global error page for router errors

---
+++ src/shared/components/RouteErrorFallback/RouteErrorFallback.tsx
@@ -0,0 +1,28 @@
+import { Button, IllustratedMessage } from '@ui5/webcomponents-react';
+import '@ui5/webcomponents-fiori/dist/illustrations/UnableToLoad.js';
+import { useTranslation } from 'react-i18next';
+
+export function RouteErrorFallback() {
+  const { t } = useTranslation();
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      <IllustratedMessage
+        name="UnableToLoad"
+        titleText={t('err-boundary.sth-went-wrong')}
+        subtitleText={t('err-boundary.reload-page')}
+      >
+        <Button onClick={() => window.location.reload()}>
+          {t('err-boundary.reload')}
+        </Button>
+      </IllustratedMessage>
+    </div>
+  );
+}
