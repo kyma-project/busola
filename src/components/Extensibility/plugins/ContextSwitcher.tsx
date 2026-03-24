1c627b5ee7325c8f4ce57da86e2a47053a00d0ee
Pietocha <konrad.pietocha@sap.com>
2026-03-24 20:04:13 +0100

    Refactor ContextSwitcher

--- src/components/Extensibility/plugins/ContextSwitcher.tsx
+++ src/components/Extensibility/plugins/ContextSwitcher.tsx
@@ -1,6 +1,10 @@
+import { WidgetProps } from '@ui-schema/ui-schema';
 import { getNextPlugin } from '@ui-schema/ui-schema/PluginStack';
 
-export function ContextSwitcher({ currentPluginIndex, ...props }) {
+export function ContextSwitcher({
+  currentPluginIndex,
+  ...props
+}: { currentPluginIndex: number } & WidgetProps) {
   const nextPluginIndex = currentPluginIndex + 1;
   const Plugin = getNextPlugin(nextPluginIndex, props.widgets);
   // eslint-disable-next-line react-hooks/static-components
