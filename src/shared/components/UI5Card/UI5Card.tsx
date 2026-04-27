fb28febed221bd7eda35355a9ca84d29a6bbc9f1
Christian Karidas <105549337+chriskari@users.noreply.github.com>
2026-04-27 21:50:55 +0200

    feat: Add UI5Card component as replacement for UI5Panel (#4890)

---
+++ src/shared/components/UI5Card/UI5Card.tsx
@@ -0,0 +1,83 @@
+import { createContext, ReactNode, useContext } from 'react';
+import { Card, CardHeader, Title } from '@ui5/webcomponents-react';
+import { Toolbar } from '@ui5/webcomponents-react-compat/dist/components/Toolbar/index.js';
+import { ToolbarSpacer } from '@ui5/webcomponents-react-compat/dist/components/ToolbarSpacer/index.js';
+import './UI5Card.scss';
+
+const NestedCardContext = createContext(false);
+
+type UI5CardProps = {
+  title: string | ReactNode;
+  headerActions?: ReactNode;
+  modeActions?: ReactNode;
+  keyComponent?: string;
+  className?: string;
+  children?: ReactNode;
+  testid?: string;
+  accessibleName?: string;
+  role?: string;
+};
+
+export const UI5Card = ({
+  title,
+  headerActions,
+  modeActions = null,
+  keyComponent = 'UI5Card',
+  className = '',
+  children,
+  testid,
+  accessibleName,
+  role,
+}: UI5CardProps) => {
+  const isNested = useContext(NestedCardContext);
+  const shouldHaveMargin = isNested;
+
+  const useNativeHeader =
+    !modeActions && !headerActions && typeof title === 'string';
+
+  const nativeHeader = (
+    <CardHeader titleText={typeof title === 'string' ? title : undefined} />
+  );
+
+  const toolbarHeader = (
+    <Toolbar
+      className={`bsl-card-toolbar${
+        modeActions ? ' bsl-card-toolbar--with-mode-actions' : ''
+      }`}
+    >
+      {typeof title === 'string' ? <Title level="H4">{title}</Title> : title}
+      {headerActions && modeActions && (
+        <>
+          <div className="header-actions invisible" aria-hidden="true">
+            {headerActions}
+          </div>
+          <ToolbarSpacer />
+          {modeActions}
+          <ToolbarSpacer className="toolbar-spacer" />
+          <div className="header-actions">{headerActions}</div>
+        </>
+      )}
+      {headerActions && !modeActions && (
+        <>
+          <ToolbarSpacer />
+          {headerActions}
+        </>
+      )}
+    </Toolbar>
+  );
+
+  return (
+    <NestedCardContext.Provider value={true}>
+      <Card
+        role={role}
+        data-testid={testid}
+        key={keyComponent}
+        className={`${className} ${shouldHaveMargin ? 'sap-margin-small' : ''}`}
+        accessibleName={accessibleName}
+        header={useNativeHeader ? nativeHeader : toolbarHeader}
+      >
+        {children}
+      </Card>
+    </NestedCardContext.Provider>
+  );
+};
