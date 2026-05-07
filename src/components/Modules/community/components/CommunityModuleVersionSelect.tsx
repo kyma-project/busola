fb65a1ae823bcd3cbb72faed9adf734717298533
Anna Woloszyn <anna.woloszyn@sap.com>
2026-05-07 18:39:52 +0200

    fix: handle undefined namespace value in edition select

--- src/components/Modules/community/components/CommunityModuleVersionSelect.tsx
+++ src/components/Modules/community/components/CommunityModuleVersionSelect.tsx
@@ -32,7 +32,7 @@ export default function CommunityModuleVersionSelect({
             key={`${idx}-${installedVersion.moduleTemplate.name}|${installedVersion.moduleTemplate.namespace}`}
             value={`${version.moduleTemplate.name}|${version.moduleTemplate.namespace}`}
           >
-            {`${version.textToDisplay} (namespace: ${version.moduleTemplate.namespace})`}
+            {`${version.textToDisplay}${version.moduleTemplate.namespace ? ` (namespace: ${version.moduleTemplate.namespace})` : ''}`}
           </Option>
         ))}
       </Select>
