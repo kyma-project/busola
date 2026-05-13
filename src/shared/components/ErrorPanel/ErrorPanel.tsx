9d4d0d3a36a201e58eacd6885cbfe3cbf7d4bd4d
Oliwia Gowor <oliwia.gowor@sap.com>
2026-05-13 21:47:32 +0200

    feat: Migrate ErrorPanel to UI5Card

--- src/shared/components/ErrorPanel/ErrorPanel.tsx
+++ src/shared/components/ErrorPanel/ErrorPanel.tsx
@@ -1,6 +1,6 @@
 import { getErrorMessage } from 'shared/utils/helpers';
 import { useTranslation } from 'react-i18next';
-import { UI5Panel } from '../UI5Panel/UI5Panel';
+import { UI5Card } from '../UI5Card/UI5Card';
 
 type ErrorPanelProps = {
   error: any;
@@ -10,17 +10,18 @@ export const ErrorPanel = ({ error, title }: ErrorPanelProps) => {
   const { t } = useTranslation();
 
   return (
-    <UI5Panel
+    <UI5Card
       title={title || 'Error'}
       accessibleName={t('components.accessible-name.error')}
     >
       <div
+        className="sap-margin-x-small sap-margin-bottom-tiny"
         style={{
           fontSize: '18px',
         }}
       >
         {getErrorMessage(error, t('components.error-panel.error'))}
       </div>
-    </UI5Panel>
+    </UI5Card>
   );
 };
