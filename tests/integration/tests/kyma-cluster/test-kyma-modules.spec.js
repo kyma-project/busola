f8bf732943344247d27be592f0f34a3a18ee1393
akucharska <agata.kucharska@sap.com>
2026-06-01 11:04:13 +0200

    Fix tests

--- tests/integration/tests/kyma-cluster/test-kyma-modules.spec.js
+++ tests/integration/tests/kyma-cluster/test-kyma-modules.spec.js
@@ -57,6 +57,7 @@ context('Test Kyma Modules views', () => {
 
     cy.get('[data-testid="create-form-footer-bar"]')
       .contains('ui5-button:visible', 'Add')
+      .should('not.be.disabled')
       .click();
 
     // Check if already installed module is not visible
@@ -75,6 +76,7 @@ context('Test Kyma Modules views', () => {
 
     cy.get('[data-testid="create-form-footer-bar"]')
       .contains('ui5-button:visible', 'Add')
+      .should('not.be.disabled')
       .click();
 
     cy.wait(7000);
@@ -82,6 +84,15 @@ context('Test Kyma Modules views', () => {
     cy.get('ui5-table-row').contains('eventing').should('be.visible');
 
     cy.get('ui5-table-row').contains('api-gateway').should('be.visible');
+
+    // Check if "You have added all the modules" is  visible
+    cy.get('ui5-panel[data-testid="kyma-modules-list"]')
+      .contains('ui5-button', 'Add')
+      .click();
+
+    cy.find('ui5-illustrated-message')
+      .find('ui5-title', 'You have added all the modules')
+      .should('be.visible');
   });
 
   it('Test number of Modules in Modules Overview card', () => {
