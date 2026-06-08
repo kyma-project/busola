187c472e747483d3f88e788e2a6e4fd7e219931c
Damian Badura <damian.badura@sap.com>
2026-06-08 12:08:52 +0200

    improve script

--- .github/scripts/fetch_failed_jobs.sh
+++ .github/scripts/fetch_failed_jobs.sh
@@ -3,6 +3,8 @@ set -e
 set -u
 set -o pipefail
 
+echoerr() { echo "$@" 1>&2; }
+
 PR_NUMBER=${PR_NUMBER:?PR Number not provided}
 PROJECT_NAME=${PROJECT_NAME:-kyma-project/busola}
 
@@ -12,7 +14,7 @@ FAILED_JOBS=$(echo "${PR_STATUS}" | jq '[.[] | select(.state == "FAILURE")]')
 FAILED_JOBS_COUNT=$(echo "${FAILED_JOBS}" | jq 'length')
 
 if [[ "${FAILED_JOBS_COUNT}" -eq 0 ]]; then
-  echo "No Jobs failed"
+  echoerr "No Jobs failed"
   exit 0
 fi
 
@@ -25,10 +27,10 @@ echo "${FAILED_JOBS}" | jq --compact-output '.[]' | while read -r job; do
   RUN_ID="${RUN_ID%%/*}"
   JOB_ID="${LINK#*/job/}"
 
-  echo "Fetching logs and artifacts for: Name: ${NAME} RUN: ${RUN_ID}, JOB: ${JOB_ID} to ${TMP_DIR}"
+  echoerr "Fetching logs and artifacts for: Name: ${NAME} RUN: ${RUN_ID}, JOB: ${JOB_ID} to ${TMP_DIR}"
   mkdir -p "${TMP_DIR}"/"${NAME}/artifacts"
   gh run view --repo "${PROJECT_NAME}" "${RUN_ID}" --log-failed > "${TMP_DIR}"/"${NAME}"/"${RUN_ID}"-"${JOB_ID}".log
 #  turn off error check
   gh run download --repo "${PROJECT_NAME}" "${RUN_ID}" --dir "${TMP_DIR}"/"${NAME}"/artifacts/ || true
 done
-echo $TMP_DIR
+echo "$TMP_DIR"
