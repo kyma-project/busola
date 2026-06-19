79c3783ab1d5f4c20e8ea94d6d5019d4f3369510
Pietocha <konrad.pietocha@sap.com>
2026-06-19 10:17:43 +0200

    Kyma CLI banner has been added

---
+++ src/components/KymaCLIBanner/KymaCLIBanner.tsx
@@ -0,0 +1,38 @@
+import { Button } from '@ui5/webcomponents-react';
+import { useTranslation } from 'react-i18next';
+import { FeatureCardBanner } from 'shared/components/FeatureCard/FeatureCard';
+
+export function KymaCLIBanner() {
+  const { t } = useTranslation();
+  const documentationUrl =
+    'https://kyma-project.io/external-content/cli/docs/user/README.html#kyma-cli';
+
+  return (
+    <FeatureCardBanner
+      id="kyma-cli-banner"
+      className="kyma-cli-banner-card"
+      title={t('kyma-cli.banner.title')}
+      description={t('kyma-cli.banner.description')}
+      design={'information-2'}
+      // image="" // TODO: Add Kyma CLI image???
+      buttons={
+        <Button
+          accessibleRole="Link"
+          accessibleName={t('kyma-cli.banner.buttons.documentation')}
+          accessibleDescription="Open in new tab link"
+          endIcon="inspect"
+          onClick={() => {
+            const newWindow = window.open(
+              documentationUrl,
+              '_blank',
+              'noopener, noreferrer',
+            );
+            if (newWindow) newWindow.opener = null;
+          }}
+        >
+          {t('kyma-cli.banner.buttons.documentation')}
+        </Button>
+      }
+    />
+  );
+}
