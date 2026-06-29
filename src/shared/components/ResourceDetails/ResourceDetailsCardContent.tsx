5d1da4cacf6e6cdb5406ae5a832bd52bbce2b48f
Oliwia Gowor <oliwia.gowor@sap.com>
2026-06-29 21:49:02 +0200

    fix: Columns in cards

--- src/shared/components/ResourceDetails/ResourceDetailsCardContent.tsx
+++ src/shared/components/ResourceDetails/ResourceDetailsCardContent.tsx
@@ -7,7 +7,7 @@ import { K8sResource } from 'types';
 import { Resource } from 'components/Extensibility/contexts/DataSources';
 import { Dispatch, JSX, ReactNode, SetStateAction } from 'react';
 import { CustomColumn } from './ResourceCustomStatusColumns';
-import { FormGroup, FormItem, Label } from '@ui5/webcomponents-react';
+import { FormItem, Label } from '@ui5/webcomponents-react';
 
 type ResourceDetailsCardContentProps = {
   resource: K8sResource & Resource;
@@ -41,83 +41,81 @@ export const ResourceDetailsCardContent = ({
       titleText={t('cluster-overview.headers.metadata')}
       content={
         <>
-          <FormGroup>
+          {/* <FormGroup> */}
+          <FormItem
+            key="Resource Type"
+            labelContent={
+              <Label showColon>{t('common.headers.resource-type')}</Label>
+            }
+          >
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              {resource.kind}
+              {description && (
+                <HintButton
+                  className="sap-margin-begin-tiny"
+                  setShowTitleDescription={setShowTitleDescription}
+                  showTitleDescription={showTitleDescription}
+                  description={description}
+                  ariaTitle={resource?.kind}
+                />
+              )}
+            </div>
+          </FormItem>
+          <FormItem
+            key="Age"
+            labelContent={<Label showColon>{t('common.headers.age')}</Label>}
+          >
+            <div>
+              <ReadableElapsedTimeFromNow
+                timestamp={resource.metadata.creationTimestamp}
+              />
+            </div>
+          </FormItem>
+          {!hideLastUpdate && (
             <FormItem
-              key="Resource Type"
+              key="Last Update"
               labelContent={
-                <Label showColon>{t('common.headers.resource-type')}</Label>
+                <Label showColon>{t('common.headers.last-update')}</Label>
               }
             >
-              <div style={{ display: 'flex', alignItems: 'center' }}>
-                {resource.kind}
-                {description && (
-                  <HintButton
-                    className="sap-margin-begin-tiny"
-                    setShowTitleDescription={setShowTitleDescription}
-                    showTitleDescription={showTitleDescription}
-                    description={description}
-                    ariaTitle={resource?.kind}
-                  />
-                )}
-              </div>
+              <div>{renderUpdateDate(lastUpdate)}</div>
             </FormItem>
+          )}
+          {filteredDetailsCardColumns.map((col) => (
             <FormItem
-              key="Age"
-              labelContent={<Label showColon>{t('common.headers.age')}</Label>}
+              key={col.header}
+              labelContent={<Label showColon>{col.header ?? ''}</Label>}
             >
-              <div>
-                <ReadableElapsedTimeFromNow
-                  timestamp={resource.metadata.creationTimestamp}
-                />
-              </div>
+              {col.value(resource)}
             </FormItem>
-            {!hideLabels && (
-              <FormItem
-                key="Labels"
-                labelContent={
-                  <Label showColon>{t('common.headers.labels')}</Label>
-                }
-              >
-                <Labels
-                  labels={resource.metadata.labels || {}}
-                  shortenLongLabels
-                />
-              </FormItem>
-            )}
-            {!hideLastUpdate && (
-              <FormItem
-                key="Last Update"
-                labelContent={
-                  <Label showColon>{t('common.headers.last-update')}</Label>
-                }
-              >
-                <div>{renderUpdateDate(lastUpdate)}</div>
-              </FormItem>
-            )}
-
-            {filteredDetailsCardColumns.map((col) => (
-              <FormItem
-                key={col.header}
-                labelContent={<Label showColon>{col.header ?? ''}</Label>}
-              >
-                {col.value(resource)}
-              </FormItem>
-            ))}
-
-            {!hideAnnotations && (
-              <FormItem
-                key="Annotations"
-                labelContent={
-                  <Label showColon>{t('common.headers.annotations')}</Label>
-                }
-              >
-                <Labels
-                  labels={resource.metadata.annotations || {}}
-                  shortenLongLabels
-                />
-              </FormItem>
-            )}
-          </FormGroup>
+          ))}
+          {!hideLabels && (
+            <FormItem
+              className="card-labels"
+              key="Labels"
+              labelContent={
+                <Label showColon>{t('common.headers.labels')}</Label>
+              }
+            >
+              <Labels
+                labels={resource.metadata.labels || {}}
+                shortenLongLabels
+              />
+            </FormItem>
+          )}
+          {!hideAnnotations && (
+            <FormItem
+              key="Annotations"
+              labelContent={
+                <Label showColon>{t('common.headers.annotations')}</Label>
+              }
+            >
+              <Labels
+                labels={resource.metadata.annotations || {}}
+                shortenLongLabels
+              />
+            </FormItem>
+          )}
         </>
       }
     />
