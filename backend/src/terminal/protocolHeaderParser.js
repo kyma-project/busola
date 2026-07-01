3e485fea3cc60a3cadd58f6bbd0eb76709067c5c
Damian Badura <damian.badura@sap.com>
2026-07-01 15:50:36 +0200

    improve parsing code with unit tests

--- backend/src/terminal/protocolHeaderParser.js
+++ backend/src/terminal/protocolHeaderParser.js
@@ -1,15 +1,24 @@
 /* global Buffer */
-// TODO: implement stricter validation
+import { InvalidInputError } from '../errors/errors';
+
 export default function parseProtocolHeaders(secHeader) {
-  const headers = secHeader.split(',');
-  if (headers.length < 1) {
-    throw new Error('WebSocket Auth headers are empty');
+  const headers = secHeader.trim().split(',');
+  if (headers.length < 2) {
+    throw new InvalidInputError('WebSocket Auth headers are empty');
   }
   const protocol = headers[0];
   headers.shift();
   const extractedHeaders = new Map(
     headers.map((header) => {
+      if (header === '') {
+        throw new InvalidInputError('Malformed headers');
+      }
       const [rawKey, rawValue] = header.split('.value.');
+      if (rawKey === '' || rawValue === '') {
+        throw new InvalidInputError(
+          'Malformed header format for key: ' + rawKey,
+        );
+      }
       const key = rawKey.replace('base64url.header.', '').trim();
       const value = Buffer.from(rawValue ?? '', 'base64url').toString();
       return [key, value];
@@ -21,6 +30,13 @@ export default function parseProtocolHeaders(secHeader) {
   const ca = extractedHeaders.get('x-cluster-certificate-authority-data');
   const clusterURL = extractedHeaders.get('x-cluster-url');
   const token = extractedHeaders.get('x-k8s-authorization');
+
+  if (!ca) {
+    throw new InvalidInputError('CA cert header is missing');
+  }
+  if (!clusterURL) {
+    throw new InvalidInputError('Cluster URL header is missing');
+  }
 
   return {
     protocol,
--- backend/src/terminal/protocolHeaderParser.test.js
+++ backend/src/terminal/protocolHeaderParser.test.js
@@ -20,20 +20,88 @@ const clusterURLEncoded = encodeBase64URL(clusterURL);
 const clientKey = 'client-key';
 const clientKeyEncoded = encodeBase64URL(encodeBase64(clientKey));
 
+const token = 'Bearer XYZ.ABC.LMN';
+const tokenEncoded = encodeBase64URL(token);
+
 describe('Parse protocl headers with auth data', () => {
-  it('All data is presed and mTLS is used', () => {
+  it('All data is present and mTLS is used', () => {
     //GIVEN
     const protocolHeader = `v4.channel.k8s.io, base64url.header.x-client-certificate-data.value.${clientCertEncoded}, base64url.header.x-cluster-url.value.${clusterURLEncoded}, base64url.header.x-cluster-certificate-authority-data.value.${caEncoded}, base64url.header.x-client-key-data.value.${clientKeyEncoded}`;
 
     //WHEN
     const output = parseProtocolHeaders(protocolHeader);
 
-    console.log(output);
     //THEN
     expect(output.protocol).toEqual('v4.channel.k8s.io');
     expect(output.clientKey).toEqual(clientKey);
     expect(output.clientCert).toEqual(clientCert);
     expect(output.ca).toEqual(ca);
     expect(output.clusterURL).toEqual(clusterURL);
+  });
+
+  it('All data is present and token is used', () => {
+    //GIVEN
+    const protocolHeader = `v4.channel.k8s.io, base64url.header.x-k8s-authorization.value.${tokenEncoded}, base64url.header.x-cluster-url.value.${clusterURLEncoded}, base64url.header.x-cluster-certificate-authority-data.value.${caEncoded}`;
+
+    //WHEN
+    const output = parseProtocolHeaders(protocolHeader);
+
+    //THEN
+    expect(output.protocol).toEqual('v4.channel.k8s.io');
+    expect(output.token).toEqual(token);
+    expect(output.ca).toEqual(ca);
+    expect(output.clusterURL).toEqual(clusterURL);
+  });
+});
+
+describe('Throws error when required data is missing', () => {
+  it('Headers doesn`t have CA header', () => {
+    //GIVEN
+    const protocolHeader = `v4.channel.k8s.io, base64url.header.x-cluster-url.value.${clusterURLEncoded}`;
+
+    //WHEN
+    expect(() => parseProtocolHeaders(protocolHeader)).toThrow(
+      'CA cert header is missing',
+    );
+  });
+
+  it('Headers doesn`t have X-Cluster-URL header', () => {
+    //GIVEN
+    const protocolHeader = `v4.channel.k8s.io, base64url.header.x-cluster-certificate-authority-data.value.${caEncoded}`;
+
+    //WHEN
+    expect(() => parseProtocolHeaders(protocolHeader)).toThrow(
+      'Cluster URL header is missing',
+    );
+  });
+});
+
+describe('Throws error when parsing protocol invalid headers', () => {
+  it('Only protocol is send', () => {
+    //GIVEN
+    const protocolHeader = `v4.channel.k8s.io,`;
+
+    //WHEN
+    expect(() => parseProtocolHeaders(protocolHeader)).toThrow(
+      'Malformed headers',
+    );
+  });
+  it('Only protocol is send', () => {
+    //GIVEN
+    const protocolHeader = `v4.channel.k8s.io`;
+
+    //WHEN
+    expect(() => parseProtocolHeaders(protocolHeader)).toThrow(
+      'WebSocket Auth headers are empty',
+    );
+  });
+  it('Auth header is malformed', () => {
+    //GIVEN
+    const protocolHeader = `v4.channel.k8s.io, base64url.header.x-client-certificate-data.value.`;
+
+    //WHEN
+    expect(() => parseProtocolHeaders(protocolHeader)).toThrow(
+      'Malformed header format for key:  base64url.header.x-client-certificate-data',
+    );
   });
 });
