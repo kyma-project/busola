30e1f14944358e12fd5fece734d2ba1874ca05f9
Anna Woloszyn <anna.woloszyn@sap.com>
2026-08-05 10:25:09 +0200

    feat: collapse labels and annotations in resource details card

--- src/shared/components/ResourceDetails/ResourceDetailsCardContent.tsx
+++ src/shared/components/ResourceDetails/ResourceDetailsCardContent.tsx
@@ -7,7 +7,14 @@ import { K8sResource } from 'types';
 import { Resource } from 'components/Extensibility/contexts/DataSources';
 import { Dispatch, JSX, ReactNode, SetStateAction } from 'react';
 import { CustomColumn } from './ResourceCustomStatusColumns';
-import { FormItem, Label, Text } from '@ui5/webcomponents-react';
+import {
+  FormItem,
+  Label,
+  Text,
+  Panel,
+  FormGroup,
+  Form,
+} from '@ui5/webcomponents-react';
 
 type ResourceDetailsCardContentProps = {
   resource: K8sResource & Resource;
@@ -44,91 +51,118 @@ export const ResourceDetailsCardContent = ({
     (!hideAnnotations ? 1 : 0);
   const needsPadding = !hideLabels && !hideAnnotations && totalItems % 2 !== 0;
 
+  const labelsAnnotationsHeader = () => {
+    if (!hideLabels && !hideAnnotations)
+      return t('common.headers.labels-annotations');
+    else if (!hideLabels) return t('common.headers.labels');
+    else if (!hideAnnotations) return t('common.headers.annotations');
+    else return undefined;
+  };
+
+  const labels = hideLabels ? null : (
+    <FormGroup>
+      <FormItem
+        className="card-labels"
+        key="Labels"
+        labelContent={<Label showColon>{t('common.headers.labels')}</Label>}
+      >
+        <Labels labels={resource.metadata.labels || {}} shortenLongLabels />
+      </FormItem>
+    </FormGroup>
+  );
+
+  const annotations = hideAnnotations ? null : (
+    <FormGroup>
+      <FormItem
+        key="Annotations"
+        labelContent={
+          <Label showColon>{t('common.headers.annotations')}</Label>
+        }
+      >
+        <Labels
+          labels={resource.metadata.annotations || {}}
+          shortenLongLabels
+        />
+      </FormItem>
+    </FormGroup>
+  );
+
   return (
     <ResourceDetailsCard
       titleText={t('cluster-overview.headers.metadata')}
       content={
-        <>
-          <FormItem
-            key="Resource Type"
-            labelContent={
-              <Label showColon>{t('common.headers.resource-type')}</Label>
-            }
-          >
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-              {resource.kind}
-              {description && (
-                <HintButton
-                  className="sap-margin-begin-tiny"
-                  setShowTitleDescription={setShowTitleDescription}
-                  showTitleDescription={showTitleDescription}
-                  description={description}
-                  ariaTitle={resource?.kind}
+        <div>
+          <div>
+            <FormItem
+              key="Resource Type"
+              labelContent={
+                <Label showColon>{t('common.headers.resource-type')}</Label>
+              }
+            >
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                {resource.kind}
+                {description && (
+                  <HintButton
+                    className="sap-margin-begin-tiny"
+                    setShowTitleDescription={setShowTitleDescription}
+                    showTitleDescription={showTitleDescription}
+                    description={description}
+                    ariaTitle={resource?.kind}
+                  />
+                )}
+              </div>
+            </FormItem>
+            <FormItem
+              key="Age"
+              labelContent={<Label showColon>{t('common.headers.age')}</Label>}
+            >
+              <Text>
+                <ReadableElapsedTimeFromNow
+                  timestamp={resource.metadata.creationTimestamp}
                 />
-              )}
-            </div>
-          </FormItem>
-          <FormItem
-            key="Age"
-            labelContent={<Label showColon>{t('common.headers.age')}</Label>}
+              </Text>
+            </FormItem>
+            {!hideLastUpdate && (
+              <FormItem
+                key="Last Update"
+                labelContent={
+                  <Label showColon>{t('common.headers.last-update')}</Label>
+                }
+              >
+                <Text>{renderUpdateDate(lastUpdate)}</Text>
+              </FormItem>
+            )}
+            {filteredDetailsCardColumns.map((col) => (
+              <FormItem
+                key={col.header}
+                labelContent={<Label showColon>{col.header ?? ''}</Label>}
+              >
+                <div>{col.value(resource)}</div>
+              </FormItem>
+            ))}
+            {needsPadding && (
+              <FormItem key="padding" labelContent={<Label />}>
+                <span />
+              </FormItem>
+            )}
+          </div>
+        </div>
+      }
+      bottomContent={
+        <Panel
+          headerText={labelsAnnotationsHeader()}
+          className="labels-annotations-panel"
+          collapsed
+        >
+          <Form
+            className="labels-annotations-panel__content"
+            labelSpan="S12 M12 L12 XL12"
+            layout="S2 M2 L2 XL2"
           >
-            <Text>
-              <ReadableElapsedTimeFromNow
-                timestamp={resource.metadata.creationTimestamp}
-              />
-            </Text>
-          </FormItem>
-          {!hideLastUpdate && (
-            <FormItem
-              key="Last Update"
-              labelContent={
-                <Label showColon>{t('common.headers.last-update')}</Label>
-              }
-            >
-              <Text>{renderUpdateDate(lastUpdate)}</Text>
-            </FormItem>
-          )}
-          {filteredDetailsCardColumns.map((col) => (
-            <FormItem
-              key={col.header}
-              labelContent={<Label showColon>{col.header ?? ''}</Label>}
-            >
-              <div>{col.value(resource)}</div>
-            </FormItem>
-          ))}
-          {needsPadding && (
-            <FormItem key="padding" labelContent={<Label />}>
-              <span />
-            </FormItem>
-          )}
-          {!hideLabels && (
-            <FormItem
-              className="card-labels"
-              key="Labels"
-              labelContent={
-                <Label showColon>{t('common.headers.labels')}</Label>
-              }
-            >
-              <Labels
-                labels={resource.metadata.labels || {}}
-                shortenLongLabels
-              />
-            </FormItem>
-          )}
-          {!hideAnnotations && (
-            <FormItem
-              key="Annotations"
-              labelContent={
-                <Label showColon>{t('common.headers.annotations')}</Label>
-              }
-            >
-              <Labels
-                labels={resource.metadata.annotations || {}}
-                shortenLongLabels
-              />
-            </FormItem>
-          )}
-        </>
+            {!hideLabels && labels}
+            {!hideAnnotations && annotations}
+          </Form>
+        </Panel>
       }
     />
   );
