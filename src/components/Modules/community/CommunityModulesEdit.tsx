1bbe34aea432926ddd6b3bcf817bb4d10ee2c650
Oliwia Gowor <oliwia.gowor@sap.com>
2026-08-17 23:07:34 +0200

    fix: revert modules change

--- src/components/Modules/community/CommunityModulesEdit.tsx
+++ src/components/Modules/community/CommunityModulesEdit.tsx
@@ -58,11 +58,11 @@ function onSave(
       });
 
       notification.notifySuccess({
-        content: t('kyma-modules.messages.modules-updated'),
+        content: t('modules.community.messages.modules-updated'),
       });
     } catch (e) {
       notification.notifyError({
-        content: t('kyma-modules.messages.modules-update-failed'),
+        content: t('modules.community.messages.modules-update-failed'),
       });
       console.error(e);
     }
