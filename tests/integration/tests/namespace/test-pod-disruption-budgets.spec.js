527f46c67b685b6c782cb0833af36587b0cb1c4f
Anna Woloszyn <anna.woloszyn@sap.com>
2026-08-20 13:37:28 +0200

    fix: pdb test wrong navigateTo params

--- tests/integration/tests/namespace/test-pod-disruption-budgets.spec.js
+++ tests/integration/tests/namespace/test-pod-disruption-budgets.spec.js
@@ -22,7 +22,7 @@ context('Test Pod Disruption Budgets', () => {
   });
 
   it('Creates a Pod Disruption Budget', () => {
-    cy.navigateTo('Pod Disruption Budgets');
+    cy.navigateTo('Configuration', 'Pod Disruption Budgets');
 
     cy.openCreate();
 
