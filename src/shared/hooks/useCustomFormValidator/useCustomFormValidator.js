b251709b4a8f8fcab0acdc442a3504bb4fc8081b
Oliwia Gowor <oliwia.gowor@sap.com>
2026-09-11 15:17:21 +0200

    fix: no children case

--- src/shared/hooks/useCustomFormValidator/useCustomFormValidator.js
+++ src/shared/hooks/useCustomFormValidator/useCustomFormValidator.js
@@ -10,7 +10,7 @@ export function useCustomFormValidator() {
     // Has to adjusted after every Resource Form structure change
     const formContainer = formElementRef.current?.querySelector(
       '.resource-form ui5-form-item',
-    ).children[0];
+    )?.children[0];
     if (formContainer) {
       setValid(cv && validateFormElement(formContainer, true).valid);
     }
