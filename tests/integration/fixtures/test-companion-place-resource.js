b490acb86fe102b05fe090e057af2dc7e06f7309
Oliwia Gowor <oliwia.gowor@sap.com>
2026-09-14 11:42:23 +0200

    test: add fields to edit

--- tests/integration/fixtures/test-companion-place-resource.js
+++ tests/integration/fixtures/test-companion-place-resource.js
@@ -58,6 +58,13 @@ spec:
       containers:
       - name: test
         image: test:latest
+        resources:
+          requests:
+            memory: 64Mi
+            cpu: 50m
+          limits:
+            memory: 128Mi
+            cpu: 100m
 \`\`\`
             </div>
             <div class="link" link-type="Update">
