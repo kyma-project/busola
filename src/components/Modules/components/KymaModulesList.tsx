c7882bc3de0cf19e7081b95d22d8df59cf32d34a
chriskari <chriskari@gmx.de>
2026-09-14 23:19:20 +0200

    feat: Display available since date in modules lists

--- src/components/Modules/components/KymaModulesList.tsx
+++ src/components/Modules/components/KymaModulesList.tsx
@@ -67,6 +67,7 @@ export const KymaModulesList = ({
     t('kyma-modules.namespaces'),
     t('kyma-modules.channel'),
     t('kyma-modules.version'),
+    t('kyma-modules.available-since'),
     t('kyma-modules.module-state'),
     t('kyma-modules.installation-state'),
     t('kyma-modules.documentation'),
