d2a6aca20b8ba469d452825da7f8fe6840fa140e
chriskari <chriskari@gmx.de>
2026-09-14 11:51:35 +0200

    dropdown input hardening

--- tests/integration/support/helpers.js
+++ tests/integration/support/helpers.js
@@ -1,8 +1,7 @@
 export function chooseComboboxOption(selector, optionText, force = false) {
   const combobox = () => cy.get(`ui5-combobox${selector}`);
 
-  // wait for options to exist before typing
-  combobox().find('ui5-cb-item').should('have.length.greaterThan', 0);
+  combobox().find('ui5-cb-item').should('contain', optionText);
 
   combobox()
     .find('input')
