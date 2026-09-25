9d2d42fa128645bff44eb4cd6eabf73678c653b9
Pietocha <konrad.pietocha@sap.com>
2026-09-25 10:20:51 +0200

    Columnslayout for FormGroup corrected

--- src/components/Extensibility/components/Columns.tsx
+++ src/components/Extensibility/components/Columns.tsx
@@ -7,8 +7,13 @@ interface ColumnsProps {
 }
 
 export function Columns({ structure, ...props }: ColumnsProps) {
+  const allFormGroups = (structure.children || []).every(
+    (child: any) => child.widget === 'FormGroup',
+  );
+  const layout = allFormGroups ? 'S1 M2 L2 XL2' : 'S1 M1 L2 XL2';
+
   return (
-    <Form layout="S1 M1 L2 XL2" data-testid="extensibility-columns">
+    <Form layout={layout} data-testid="extensibility-columns">
       {(structure.children || []).map((child: any) => (
         <FormGroup key={`form-group-${child.path || child.name}`}>
           <Widget structure={child} {...props} />
